c295e2ebc5b9b8b846076c63190d1af698e9cc58
Zdenek Hynek <zdenek.hynek@gmail.com>
2015-07-31 12:18:48 +0200

    replace country icon

--- public/js/app/views/chart/App.Views.Chart.Legend.js
+++ public/js/app/views/chart/App.Views.Chart.Legend.js
@@ -54,8 +54,11 @@
 				if( addEntityBtn.empty() ) {
 					addEntityBtn = wrap.append('g').attr('class', 'nv-add-btn');
 					addEntityBtn.append('rect').attr( { 'class': 'add-btn-bg', 'width': '100', 'height': '25', 'transform': 'translate(0,-5)' } );
-					addEntityBtn.append('path').attr( { 'd': 'M16,0 L16,14', 'class': 'nv-box' } );
-					addEntityBtn.append('path').attr( { 'd': 'M9,7 L22,7', 'class': 'nv-box' } );
+					var addEntityBtnG = addEntityBtn.append('g').attr( { 'class': 'add-btn-path' } )
+					addEntityBtnG.append('path').attr( { 'd': 'M15,0 L15,14', 'class': 'nv-box' } )
+					addEntityBtnG.append('path').attr( { 'd': 'M8,7 L22,7', 'class': 'nv-box' } );
+					//http://android-ui-utils.googlecode.com/hg-history/ac955e6376470d9599ead07b4599ef937824f919/asset-studio/dist/res/clipart/icons/refresh.svg?r=ac955e6376470d9599ead07b4599ef937824f919
+					addEntityBtn.append('path').attr( { 'd': 'M160.469,242.194c0-44.414,36.023-80.438,80.438-80.438c19.188,0,36.711,6.844,50.5,18.078L259.78,209.93l99.945,11.367    l0.805-107.242l-30.766,29.289c-23.546-21.203-54.624-34.164-88.804-34.164c-73.469,0-133.023,59.562-133.023,133.016    c0,2.742,0.242-2.266,0.414,0.445l53.68,7.555C161.03,245.108,160.469,247.562,160.469,242.194z M371.647,237.375l-53.681-7.555    c1.017,5.086,1.556,2.617,1.556,7.992c0,44.414-36.008,80.431-80.43,80.431c-19.133,0-36.602-6.798-50.383-17.97l31.595-30.078    l-99.93-11.366l-0.812,107.25l30.789-29.312c23.531,21.141,54.57,34.055,88.688,34.055c73.468,0,133.023-59.555,133.023-133.008    C372.062,235.078,371.812,240.085,371.647,237.375z', 'class': 'nv-box change-btn-path', 'transform': 'scale(.04) translate(150,-50)' } );
 					addEntityBtn.append('text').attr( {'x':28,'y':11} ).text('Add country');
 					addEntityBtn.on( 'click', function( d, i ) {
 						//group by variables
@@ -78,8 +81,12 @@
 					//addEntityBtn.attr( "display", "none" );
 					addEntityBtn.select( "text" ).text( "Add country" );
 					addEntityBtn.select( "rect" ).attr( "width", "100" );
+					addEntityBtn.select( ".add-btn-path" ).attr( "display", "block" );
+					addEntityBtn.select( ".change-btn-path" ).attr( "display", "none" );
 					addEntityBtn.attr( "display", "block" );
 				} else if( addCountryMode === "change-country" ) {
+					addEntityBtn.select( ".add-btn-path" ).attr( "display", "none" );
+					addEntityBtn.select( ".change-btn-path" ).attr( "display", "block" );
 					addEntityBtn.select( "text" ).text( "Change country" );
 					addEntityBtn.select( "rect" ).attr( "width", "120" );
 					addEntityBtn.attr( "display", "block" );
