21e4a1f83b826d8c39f8f26acc03e30a257f8eb4
Zdenek Hynek <zdenek.hynek@gmail.com>
2015-09-10 09:06:28 +0100

    cached colors

--- public/js/app/App.Utils.js
+++ public/js/app/App.Utils.js
@@ -450,6 +450,15 @@
 		})
 	};
 
+	App.Utils.getRandomColor = function () {
+		var letters = '0123456789ABCDEF'.split('');
+		var color = '#';
+		for (var i = 0; i < 6; i++ ) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	};
+
 
 
 })();
