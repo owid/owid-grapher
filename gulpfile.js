88fa31c1516801f2dc2174051d994c4eaa6437e4
Zdenek Hynek <zdenek.hynek@gmail.com>
2015-09-21 18:50:58 +0100

    switch from css to scss

--- gulpfile.js
+++ gulpfile.js
@@ -13,40 +13,54 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
 
-	mix.styles( [
-		'libs/bootstrap.min.css',
-		'libs/font-awesome/css/font-awesome.min.css',
-		'libs/chosen.css',
-		'libs/nv.d3.css'
-	], 'public/css/libs/front.css' );
+    mix.styles( [
+        'libs/bootstrap.min.css',
+        'libs/font-awesome/css/font-awesome.min.css',
+        'libs/chosen.css',
+        'libs/nv.d3.css'
+    ], 'public/css/libs/front.css' );
 
-	mix.styles( [
-		'main.css',
-		'chart.css'
-	], 'public/css/front.css' );
+    /*mix.styles( [
+        'main.css',
+        'chart.css'
+    ], 'public/css/front.css' );*/
+    
+    mix.sass( [
+        'main.scss',
+        'chart.scss'
+    ], 'public/css/front.css' );
 
-	mix.styles( [
-		'libs/bootstrap.min.css',
-		'libs/font-awesome/css/font-awesome.min.css',
-		'libs/AdminLTE.min.css',
-		'libs/datepicker3.css',
-		'libs/_all-skins.min.css',
-		'libs/ion.rangeSlider.css',
-		'libs/ion.rangeSlider.skinFlat.css',
-		'libs/bootstrap3-wysihtml5.min.css',
-		'libs/chosen.css',
-		'libs/nv.d3.css',
-	], 'public/css/libs/admin.css' );
+    mix.styles( [
+        'libs/bootstrap.min.css',
+        'libs/font-awesome/css/font-awesome.min.css',
+        'libs/AdminLTE.min.css',
+        'libs/datepicker3.css',
+        'libs/_all-skins.min.css',
+        'libs/ion.rangeSlider.css',
+        'libs/ion.rangeSlider.skinFlat.css',
+        'libs/bootstrap3-wysihtml5.min.css',
+        'libs/chosen.css',
+        'libs/nv.d3.css',
+    ], 'public/css/libs/admin.css' );
 
-	mix.styles( [
-		'admin/admin.css',
-		'admin/data.css',
-		'admin/import.css',
-		'admin/charts.css',
-		'chart.css',
-		'main.css'
-	], 'public/css/admin.css' );
+    /*mix.styles( [
+        'admin/admin.css',
+        'admin/data.css',
+        'admin/import.css',
+        'admin/charts.css',
+        'chart.css',
+        'main.css'
+    ], 'public/css/admin.css' );*/
 
-		
+    mix.sass([
+        'admin/admin.scss',
+        'admin/data.scss',
+        'admin/import.scss',
+        'admin/charts.scss',
+        'chart.scss',
+        'main.scss'
+    ], 'public/css/admin.css' );
+
+        
 
 });
