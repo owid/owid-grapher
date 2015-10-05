8d223e51e98c963322279e589dbc825c2e0bf3fb
Zdenek Hynek <zdenek.hynek@gmail.com>
2015-10-05 18:25:26 +0100

    finish browserfying project

--- public/js/libs-admin.js
+++ public/js/libs-admin.js
@@ -32106,4 +32106,66 @@ var colorbrewer = {YlGn: {
 11: ["#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5"],
 12: ["#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f"]
 }};
+;( function() {
+	
+	"use strict";
+
+	$( ".datepicker" ).datepicker();
+
+	$( ".datasource-editor" ).wysihtml5( 'deepExtend', {
+				parserRules: {
+					tags: {
+						table: {},
+						tr: {},
+						th: {},
+						td: {}
+					}
+				},
+				"html": true
+			});
+
+	//validate 
+	var $validateForms = $( ".validate-form" );
+	if( $validateForms.length ) {
+
+		$validateForms.on( "submit", function( evt ) {
+			var $form = $( evt.currentTarget ),
+				valid = Utils.FormHelper.validate( $form );
+			if( !valid ) {
+				evt.preventDefault();
+				evt.stopImmediatePropagation();
+			}
+		} );
+	}
+
+	//delete buttons
+	$( ".delete-btn, .btn-danger" ).on( "click", function( evt ) {
+
+		var confirm = window.confirm( "Are you sure?" );
+		if( !confirm ) {
+			evt.preventDefault();
+		}
+
+	});
+
+	//index variable module
+	var $indexVariableModule = $( ".index-variable-module" );
+	if( $indexVariableModule.length ) {
+		
+		//get all ids of the current table and stuff it into input
+		var $table = $indexVariableModule.find( "table" ),
+			$tds = $table.find( "td:first-child" ),
+			$valueIdsInput = $indexVariableModule.find( "[name=value_ids]" ),
+			ids = $.map( $tds, function( v, i ) {
+				return $( v ).text();
+			} );
+
+		$valueIdsInput.val( JSON.stringify(ids) );
+
+	}
+
+	//chosen select
+	$( ".chosen-select" ).chosen();
+
+})();
 //# sourceMappingURL=libs-admin.js.map
