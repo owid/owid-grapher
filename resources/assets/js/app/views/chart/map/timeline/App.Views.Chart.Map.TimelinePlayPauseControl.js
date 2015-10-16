14fac5fd71779201b2dc6a2f48c3c9c5f9ba23cc
Zdenek Hynek <zdenek.hynek@gmail.com>
2015-10-16 13:15:44 +0100

    basic layout for new map timeline control

---
+++ resources/assets/js/app/views/chart/map/timeline/App.Views.Chart.Map.TimelinePlayPauseControl.js
@@ -0,0 +1,98 @@
+;( function() {
+	
+	"use strict";
+
+	var App = require( "./../../../../namespaces.js" );
+
+	App.Views.Map.TimelinePlayPauseControl = Backbone.View.extend({
+
+		el: "#map-chart-tab .map-controls-header",
+		events: {
+			"input .target-year-control input": "onTargetYearInput",
+			"change .target-year-control input": "onTargetYearChange",
+			"click .region-control li": "onRegionClick",
+			"click .settings-control input": "onSettingsInput",
+		},
+
+		initialize: function( options ) {
+
+			this.dispatcher = options.dispatcher;
+
+			var mapConfig = App.ChartModel.get( "map-config" );
+			
+			//year slider
+			this.$targetYearControl = this.$el.find( ".target-year-control" );
+			this.$targetYearLabel = this.$targetYearControl.find( ".target-year-label" );
+			this.$targetYearInput = this.$targetYearControl.find( "input" );
+			
+			//region selector
+			this.$regionControl = this.$el.find( ".region-control" );
+			this.$regionControlLabel = this.$regionControl.find( ".region-label" );
+			this.$regionControlLis = this.$regionControl.find( "li" );
+
+			//settings-control selector
+			this.$settingsControl = this.$el.find( ".settings-control" );
+
+			App.ChartModel.on( "change", this.onChartModelChange, this );
+			App.ChartModel.on( "change-map", this.onChartModelChange, this );
+
+			return this.render();
+		},
+
+		render: function() {
+			
+			var mapConfig = App.ChartModel.get( "map-config" );
+			
+			this.$targetYearLabel.text( mapConfig.targetYear );
+			this.$regionControlLabel.text( mapConfig.projection );
+
+			this.$targetYearInput.attr( "min", mapConfig.minYear );
+			this.$targetYearInput.attr( "max", mapConfig.maxYear );
+			this.$targetYearInput.attr( "step", mapConfig.timeInterval );
+			this.$targetYearInput.val( parseInt( mapConfig.targetYear, 10 ) );
+
+			this.$regionControlLis.removeClass( "highlight" );
+			this.$regionControlLis.filter( "." + mapConfig.projection + "-projection" ).addClass( "highlight" );
+
+			//is interval mode display
+			if( isNaN( mapConfig.minYear ) || isNaN( mapConfig.maxYear ) ) {
+				this.$targetYearInput.attr( "disabled", true );
+			}
+
+		},
+
+		onChartModelChange: function( evt ) {
+			this.render();
+		},
+		
+		onTargetYearInput: function( evt ) {
+			var $this = $( evt.target ),
+				targetYear = parseInt( $this.val(), 10 );
+			this.$targetYearLabel.text( targetYear, false, "change-map" );
+		},
+
+		onTargetYearChange: function( evt ) {
+			var $this = $( evt.target ),
+				targetYear = parseInt( $this.val(), 10 );
+			App.ChartModel.updateMapConfig( "targetYear", targetYear, false, "change-map" );
+			this.render();
+		},
+
+		onRegionClick: function( evt ) {
+			var $this = $( evt.target );
+			App.ChartModel.updateMapConfig( "projection", $this.text(), false, "change-map" );
+			this.render();
+		},
+
+		onSettingsInput: function( evt ) {
+			var $this = $( evt.target ),
+				mode = ( $this.is( ":checked" ) )? "specific": "no-interpolation";
+			App.ChartModel.updateMapConfig( "mode", mode, false, "change-map" );
+			this.render();
+		}
+
+	});
+
+	module.exports = App.Views.Map.TimelinePlayPauseControl;
+
+})();
