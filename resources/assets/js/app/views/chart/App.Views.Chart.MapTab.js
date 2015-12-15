29d37b81efe9474548a82082f7450857a21d6c9b
Zdenek Hynek <zdenek.hynek@gmail.com>
2015-12-15 15:25:30 +0000

    Fixed extent.

--- resources/assets/js/app/views/chart/App.Views.Chart.MapTab.js
+++ resources/assets/js/app/views/chart/App.Views.Chart.MapTab.js
@@ -262,7 +262,7 @@
 		},
 
 		getProjection: function( projectionName ) {
-			var projections = this.projections,
+			var projections = App.Views.Chart.MapTab.projections,
 				newProjection = ( projections[ projectionName ] )? projections[ projectionName ]: projections.World;
 			return newProjection;
 			
@@ -283,94 +283,94 @@
 
 		onChartModelResize: function() {
 			this.onResize();
-		},
-	
-		projections: { 
-			"World": function(element) {
-				//empiric
-				var k = 6;
-				var projection = d3.geo.eckert3()
-					.scale(element.offsetWidth/k)
-					.translate([element.offsetWidth / 2, element.offsetHeight / 2])
-					.precision(.1);
-				var path = d3.geo.path().projection(projection);
-				return {path: path, projection: projection};
-			},
-			"Africa": function(element) {
-				//empiric
-				var k = 3.2;
-				var projection = d3.geo.conicConformal()
-					.rotate([-25, 0])
-					.center([0, 0])
-					.parallels([30, -20])
-					.scale(element.offsetWidth/k)
-					.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-				var path = d3.geo.path().projection(projection);
-				return {path: path, projection: projection};
-			},
-			"N.America": function(element) {
-				//empiric
-				var k = 3.2;
-				var projection = d3.geo.conicConformal()
-					.rotate([98, 0])
-					.center([0, 38])
-					.parallels([29.5, 45.5])
-					.scale(element.offsetWidth/k)
-					.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-				var path = d3.geo.path().projection(projection);
-				return {path: path, projection: projection};
-			},
-			"S.America": function(element) {
-				//empiric
-				var k = 3.6;
-				var projection = d3.geo.conicConformal()
-					.rotate([68, 0])
-					.center([0, -14])
-					.parallels([10, -30])
-					.scale(element.offsetWidth/k)
-					.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-				var path = d3.geo.path().projection(projection);
-				return {path: path, projection: projection};
-			},
-			"Asia": function(element) {
-				//empiric
-				var k = 3.2;
-				var projection = d3.geo.conicConformal()
-					.rotate([-105, 0])
-					.center([0, 37])
-					.parallels([10, 60])
-					.scale(element.offsetWidth/k)
-					.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-				var path = d3.geo.path().projection(projection);
-				return {path: path, projection: projection};
-			},
-			"Europe": function(element) {
-				//empiric
-				var k = 1.7;
-				var projection = d3.geo.conicConformal()
-					.rotate([-15, 0])
-					.center([0, 55])
-					.parallels([60, 40])
-					.scale(element.offsetWidth/k)
-					.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-				var path = d3.geo.path().projection(projection);
-				return {path: path, projection: projection};
-			},
-			"Australia": function(element) {
-				//empiric
-				var k = 3.2;
-				var projection = d3.geo.conicConformal()
-					.rotate([-135, 0])
-					.center([0, -20])
-					.parallels([-10, -30])
-					.scale(element.offsetWidth/k)
-					.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-				var path = d3.geo.path().projection(projection);
-				return {path: path, projection: projection};
-			}
 		}
-
+	
 	});
+
+	App.Views.Chart.MapTab.projections = {
+		"World": function(element) {
+			//empiric
+			var k = 6;
+			var projection = d3.geo.eckert3()
+				.scale(element.offsetWidth/k)
+				.translate([element.offsetWidth / 2, element.offsetHeight / 2])
+				.precision(.1);
+			var path = d3.geo.path().projection(projection);
+			return {path: path, projection: projection};
+		},
+		"Africa": function(element) {
+			//empiric
+			var k = 3.2;
+			var projection = d3.geo.conicConformal()
+				.rotate([-25, 0])
+				.center([0, 0])
+				.parallels([30, -20])
+				.scale(element.offsetWidth/k)
+				.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+			var path = d3.geo.path().projection(projection);
+			return {path: path, projection: projection};
+		},
+		"N.America": function(element) {
+			//empiric
+			var k = 3.2;
+			var projection = d3.geo.conicConformal()
+				.rotate([98, 0])
+				.center([0, 38])
+				.parallels([29.5, 45.5])
+				.scale(element.offsetWidth/k)
+				.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+			var path = d3.geo.path().projection(projection);
+			return {path: path, projection: projection};
+		},
+		"S.America": function(element) {
+			//empiric
+			var k = 3.6;
+			var projection = d3.geo.conicConformal()
+				.rotate([68, 0])
+				.center([0, -14])
+				.parallels([10, -30])
+				.scale(element.offsetWidth/k)
+				.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+			var path = d3.geo.path().projection(projection);
+			return {path: path, projection: projection};
+		},
+		"Asia": function(element) {
+			//empiric
+			var k = 3.2;
+			var projection = d3.geo.conicConformal()
+				.rotate([-105, 0])
+				.center([0, 37])
+				.parallels([10, 60])
+				.scale(element.offsetWidth/k)
+				.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+			var path = d3.geo.path().projection(projection);
+			return {path: path, projection: projection};
+		},
+		"Europe": function(element) {
+			//empiric
+			var k = 1.7;
+			var projection = d3.geo.conicConformal()
+				.rotate([-15, 0])
+				.center([0, 55])
+				.parallels([60, 40])
+				.scale(element.offsetWidth/k)
+				.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+			var path = d3.geo.path().projection(projection);
+			return {path: path, projection: projection};
+		},
+		"Australia": function(element) {
+			//empiric
+			var k = 3.2;
+			var projection = d3.geo.conicConformal()
+				.rotate([-135, 0])
+				.center([0, -20])
+				.parallels([-10, -30])
+				.scale(element.offsetWidth/k)
+				.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+			var path = d3.geo.path().projection(projection);
+			return {path: path, projection: projection};
+		}
+	};
 
 	module.exports = App.Views.Chart.MapTab;
 
