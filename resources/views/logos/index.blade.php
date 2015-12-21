2f444ab0d2734a8b925b42c389c648ffbe7d7935
Zdenek Hynek <zdenek.hynek@gmail.com>
2015-12-21 09:59:18 +0100

    Style cleanup.

--- resources/views/logos/index.blade.php
+++ resources/views/logos/index.blade.php
@@ -4,7 +4,7 @@
 	<h2>Logos</h2>
 	<a href="{{ route('logos.create') }}" class="btn btn-success btn-new-logo"><i class="fa fa-plus"></i> Create new logo</a>
 	@if ( !$logos->count() )
-		There are no logos.
+		<p>There are no logos.</p>
 	@else
 		<ul>
 			@foreach( $logos as $logo )
