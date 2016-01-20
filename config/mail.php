3bf851c9f686dd50b68edff7b97d60c217369804
Jaiden Mispy <jaiden@mispy.me>
2016-01-20 14:59:45 -0800

    Add a default sender address to config/mail.php
    This lets password resets work with mailtrap and other smtp hosts where
    the username is not an email address.

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'no-reply@ourworldindata.org', 'name' => null],
 
 	/*
 	|--------------------------------------------------------------------------
