5f66ea0ab8a2fdac82ede4f16fa2ffc82a6dd57b
Jaiden Mispy <jaiden@mispy.me>
2016-02-19 21:56:47 +1100

    A more sophisticated deploy.sh
    It now controls what exactly is synced very carefully, using
    rsync --delete --delete-excluded. A temporary directory is used
    for syncing to which is then copied and used to replace the original
    target folder using (nearly atomic) moves.
    
    Data which must persist on the server, such as .env and uploads, are kept
    in a separate directory and symlinked in following a deployment update.
    
    Deploying to a staging test dir is now supported too.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-RSYNC="rsync -havz --no-perms --progress --exclude config/database.php --exclude .git --exclude node_modules --exclude storage/logs --exclude .env"
-$RSYNC $DIR/ owid:/home/explain3/public_html/grapher
+RSYNC="rsync -havz --no-perms --progress --delete --delete-excluded --exclude .git --exclude node_modules --exclude \"storage/logs/*\" --exclude \"storage/debugbar/*\" --exclude .env"
+HOST="explain3@ourworldindata.org"
+ROOT="/home/explain3"
+
+if [ "$1" == "test" ]; then
+  NAME="test-grapher"
+elif [ "$1" == "live" ]; then
+  NAME="grapher"
+else
+  echo "Please select either live or test."
+  exit 1
+fi
+
+OLD_REPO="$ROOT/tmp/$NAME-old"
+SYNC_TARGET="$ROOT/tmp/$NAME"
+TMP_NEW="$ROOT/tmp/$NAME-new"
+LIVE_TARGET="$ROOT/public_html/$NAME"
+LIVE_DATA="$ROOT/$NAME-data"
+
+ssh $HOST "rm -r $OLD_REPO"
+$RSYNC $DIR/ $HOST:$SYNC_TARGET
+ssh $HOST 'bash -l -e -s' <<EOF
+  cp -r $SYNC_TARGET $TMP_NEW
+  mv $LIVE_TARGET $OLD_REPO
+  mv $TMP_NEW $LIVE_TARGET
+  ln -s $LIVE_DATA/env $LIVE_TARGET/.env
+  ln -s $LIVE_DATA/uploads $LIVE_TARGET/public/uploads
+EOF
