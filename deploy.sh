44053c18348f58f26c8ff2e2ca98ba0871141552
Jaiden Mispy <jaiden@mispy.me>
2016-02-20 02:37:58 +1100

    Update deploy script to account for symlinks

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ fi
 OLD_REPO="$ROOT/tmp/$NAME-old"
 SYNC_TARGET="$ROOT/tmp/$NAME"
 TMP_NEW="$ROOT/tmp/$NAME-new"
-LIVE_TARGET="$ROOT/public_html/$NAME"
+LIVE_TARGET="$ROOT/$NAME-code"
 LIVE_DATA="$ROOT/$NAME-data"
 
 ssh $HOST "rm -r $OLD_REPO"
@@ -25,6 +25,8 @@ ssh $HOST 'bash -l -e -s' <<EOF
   cp -r $SYNC_TARGET $TMP_NEW
   mv $LIVE_TARGET $OLD_REPO
   mv $TMP_NEW $LIVE_TARGET
-  ln -s $LIVE_DATA/env $LIVE_TARGET/.env
-  ln -s $LIVE_DATA/uploads $LIVE_TARGET/public/uploads
+  ln -sf $LIVE_DATA/.htaccess $LIVE_TARGET/public/.htaccess
+  ln -sf $LIVE_DATA/env $LIVE_TARGET/.env
+  ln -sf $LIVE_DATA/uploads $LIVE_TARGET/public/uploads
+  ln -sf $LIVE_TARGET/public $ROOT/public_html/$NAME
 EOF
