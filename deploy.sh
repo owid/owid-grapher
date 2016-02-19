606bff340b980bb2a0429321a292838c4cae16f0
Jaiden Mispy <jaiden@mispy.me>
2016-02-19 19:27:26 +1100

    Better rsync deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-RSYNC="rsync -havz --no-perms --progress"
-$RSYNC $DIR/public owid:/home/explain3/public_html/grapher
-$RSYNC $DIR/resources owid:/home/explain3/public_html/grapher/
-$RSYNC $DIR/app owid:/home/explain3/public_html/grapher/
-$RSYNC $DIR/bootstrap owid:/home/explain3/public_html/grapher/
+RSYNC="rsync -havz --no-perms --progress --exclude config/database.php --exclude .git --exclude node_modules --exclude storage/logs --exclude .env"
+$RSYNC $DIR/ owid:/home/explain3/public_html/grapher
