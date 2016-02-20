e8e5bc0acbb2a0ee0b50f797960bac1ad80c1bc2
Jaiden Mispy <jaiden@mispy.me>
2016-02-20 21:05:30 +1100

    Don't show full screen link on charts elsewhere

--- resources/views/charts/partials/_chart-footer.blade.php
+++ resources/views/charts/partials/_chart-footer.blade.php
@@ -3,7 +3,9 @@
 		<a href="#" class="chart-export-btn" target="_blank">Save chart</a>
 		<a href="#" class="chart-save-btn" style="display:none;"></a>
 	</div>
-	<a class="fancybox-iframe" href="{{ Request::url() }}">Full screen view</a>
+	@if (!empty(Request::header('referer')) && parse_url(Request::header('referer'))['host'] == parse_url(Config::get("app.url"))['host'])
+		<a class="fancybox-iframe" href="{{ Request::url() }}">Full screen view</a>
+	@endif
 	<p class="chart-sources"></p>
 	<p class="chart-description"></p>
 </div>
