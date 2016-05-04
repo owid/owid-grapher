8a08464100ed52ddd481c7c695405171a6a348cb
Jaiden Mispy <jaiden@mispy.me>
2016-05-04 16:37:19 +1000

    Testing database config

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
 	|
 	*/
 
-	'default' => 'mysql',
+	'default' => env('DB_CONNECTION', 'mysql'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -58,18 +58,6 @@ return [
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-			'strict'    => false,
-		],
-
-		'testing' => [
-			'driver'    => 'mysql',
-			'host'      => env('DB_TEST_HOST', 'localhost'),
-			'database'  => env('DB_TEST_DATABASE', 'forge'),
-			'username'  => env('DB_TEST_USERNAME', 'forge'),
-			'password'  => env('DB_TEST_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
