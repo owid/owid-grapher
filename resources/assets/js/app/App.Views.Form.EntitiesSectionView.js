a430fc667da2b73f52aafaa751eabfd77bdd796c
Jaiden Mispy <jaiden@mispy.me>
2016-05-21 06:08:37 +1000

    Fix the editor adding countries too

--- resources/assets/js/app/App.Views.Form.EntitiesSectionView.js
+++ resources/assets/js/app/App.Views.Form.EntitiesSectionView.js
@@ -2,6 +2,9 @@
 	"use strict";
 	owid.namespace("App.Views.Form.EntitiesSectionView");
 
+	/**
+	 * Responsible for the "Pick your countries" section in the editor
+	 **/
 	App.Views.Form.EntitiesSectionView = Backbone.View.extend({
 
 		el: "#form-view #data-tab .entities-section",
@@ -10,59 +13,37 @@
 			"change [name='add-country-mode']": "onAddCountryModeChange"
 		},
 
-		initialize: function( options ) {
-			
+		initialize: function(options) {
 			this.dispatcher = options.dispatcher;
-			//App.AvailableEntitiesCollection.on( "change add remove reset", this.render, this );
-			//available entities are changing just on fetch so listen just for that
-			App.AvailableEntitiesCollection.on( "reset fetched", this.render, this );
-			
-			this.$entitiesSelect = this.$el.find( ".countries-select" );
-			this.$addCountryControlInput = this.$el.find( "[name='add-country-control']" );
-
-			this.render();
-
+			this.$entitiesSelect = this.$el.find(".countries-select");
+			this.$addCountryControlInput = this.$el.find("[name='add-country-control']");
+			App.DataModel.on("change:availableEntities", this.render.bind(this));
 		},
 
 		render: function() {
+			// Fill the entity selector with default option and all the available entities
+			this.$entitiesSelect.empty();
+			this.$entitiesSelect.append("<option selected disabled>Select entity</option>");
+			var availableEntities = App.DataModel.get("availableEntities");
+			_.each(availableEntities, function(entity) {
+				this.$entitiesSelect.append($("<option value='" + entity.id + "'>" + entity.name + "</option>"));
+			}.bind(this));
 
-			var $entitiesSelect = this.$entitiesSelect;
-			$entitiesSelect.empty();
-			
-			//append default 
-			$entitiesSelect.append( $( "<option selected disabled>Select entity</option>" ) );
-
-			App.AvailableEntitiesCollection.each( function( model ) {
-				$entitiesSelect.append( $( "<option value='" + model.get( "id" ) + "'>" + model.get( "name" ) + "</option>" ) );
-			});
-
-			var addCountryControl = App.ChartModel.get( "add-country-control" );
-			this.$addCountryControlInput.prop( "checked", addCountryControl );
-
-			//based on stored add-country-mode
-			var addCountryMode = App.ChartModel.get( "add-country-mode" );
-			this.$el.find( "[name='add-country-mode']" ).filter( "[value='" + addCountryMode + "']" ).prop( "checked", true );
-
+			// Allow choosing whether or not the viewer can add their own countries
+			var addCountryMode = App.ChartModel.get("add-country-mode");
+			this.$el.find("[name='add-country-mode']").filter("[value='" + addCountryMode + "']").prop("checked", true);
 		},
 
-		onCountriesSelect: function( evt ) {
+		onCountriesSelect: function(evt) {
+			var selectedId = this.$entitiesSelect.val(),
+				entity = App.DataModel.get("variableData").entityKey[selectedId];
 
-			var $select = $( evt.target ),
-				val = $select.val(),
-				$option = $select.find( "option[value=" + val + "]" ),
-				text = $option.text();
-
-			App.ChartModel.addSelectedCountry( { id: val, name: text } );
-
+			App.ChartModel.addSelectedCountry({ id: +selectedId, name: entity.name });
 		},
 
-		onAddCountryModeChange: function( evt ) {
-
-			var $input = $( "[name='add-country-mode']:checked" );
-			App.ChartModel.set( "add-country-mode", $input.val() );
-
+		onAddCountryModeChange: function(evt) {
+			var $input = $("[name='add-country-mode']:checked");
+			App.ChartModel.set("add-country-mode", $input.val());
 		}
-
-
 	});
 })();
