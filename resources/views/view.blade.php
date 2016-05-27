0baff32cca680cd1625bf6ba3d500003e0e11811
Jaiden Mispy <jaiden@mispy.me>
2016-05-27 23:24:24 +1000

    Use chartMeta->description in actual description meta tag

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<title>{{ $chartMeta->title }} - Our World In Data</title>
-		<meta name="description" content="">
+		<meta name="description" content="{{ $chartMeta->description }}">
 		<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'>
 		<link rel="apple-touch-icon" href="apple-touch-icon.png">
 		<!-- Place favicon.ico in the root directory -->
