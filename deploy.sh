ee938029712fab113bdc75b6ee58810dd571c364
Jaiden Mispy <jaiden@mispy.me>
2016-08-25 13:29:02 +1000

    deployment public_html => ourworldindata.org

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ ssh -t $HOST 'bash -e -s' <<EOF
   ln -sf $LIVE_DATA/env $LIVE_TARGET/.env
   ln -sf $LIVE_DATA/uploads $LIVE_TARGET/public/uploads
   ln -sf $LIVE_DATA/exports $LIVE_TARGET/public/exports
-  ln -sf $LIVE_TARGET/public $ROOT/public_html/$NAME
+  ln -sf $LIVE_TARGET/public $ROOT/ourworldindata.org/$NAME
   cd $LIVE_TARGET && php artisan migrate --force
 EOF
 
