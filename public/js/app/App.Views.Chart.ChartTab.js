29ba33df3b2c4f6ba28a9955d5164ccfafac3b6c
Jaiden Mispy <jaiden@mispy.me>
2016-10-05 12:04:19 +1100

    Fix x axis log toggle

--- public/js/app/App.Views.Chart.ChartTab.js
+++ public/js/app/App.Views.Chart.ChartTab.js
@@ -421,8 +421,8 @@
 					// MISPY: Custom calculation of axis ticks, since nvd3 doesn't
 					// account for log scale when doing its own calc and that can result in
 					// overlapping axis labels.
-					var minPower10 = Math.ceil(Math.log(yDomain[0]) / Math.log(10));
-					var maxPower10 = Math.floor(Math.log(yDomain[1]) / Math.log(10));
+					var minPower10 = Math.ceil(Math.log(xDomain[0]) / Math.log(10));
+					var maxPower10 = Math.floor(Math.log(xDomain[1]) / Math.log(10));
 
 					var tickValues = [];
 					for (var i = minPower10; i <= maxPower10; i++) {
