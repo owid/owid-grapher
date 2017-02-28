fd2006dadc21c0ec9a12c01af06ac9e0ee2abe17
Jaiden Mispy <jaiden@mispy.me>
2017-02-28 14:27:16 +0800

    Fix tests

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -9,15 +9,25 @@ class Helper
 {
     public static function js($entry)
     {
-        $manifest = json_decode(File::get('build/manifest.json'));
-        $path = env('APP_ENV', 'production') == 'production' ? '/build/'.$manifest->{$entry.'.js'} : '/build/'.$entry.'.bundle.js';
+        $path = "";
+        if (env('APP_ENV', 'production') == 'production') {
+            $manifest = json_decode(File::get('build/manifest.json'));
+            $path = '/build/'.$manifest->{$entry.'.js'};
+        } else {
+            $path = '/build/'.$entry.'.bundle.js';
+        }
         return '<script src="' . URL::to($path) . '"></script>';
     }
 
     public static function css($entry)
     {
-        $manifest = json_decode(File::get('build/manifest.json'));
-        $path = env('APP_ENV', 'production') == 'production' ? '/build/'.$manifest->{$entry.'.css'} : '/build/'.$entry.'.bundle.css';
+        $path = "";
+        if (env('APP_ENV', 'production') == 'production') {
+            $manifest = json_decode(File::get('build/manifest.json'));
+            $path = '/build/'.$manifest->{$entry.'.css'};
+        } else {
+            $path = '/build/'.$entry.'.bundle.css';
+        }
         return '<link href="' . URL::to($path) . '" rel="stylesheet" type="text/css">';
     }
 }
