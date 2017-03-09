cbe839851dad730eddddc0192f08aec052d03706
Jaiden Mispy <jaiden@mispy.me>
2017-03-09 23:44:13 +0800

    Slope chart style hover detection

--- js/charts/PointsWithLabels.jsx
+++ js/charts/PointsWithLabels.jsx
@@ -19,6 +19,8 @@ import {observer} from 'mobx-react'
 import Bounds from './Bounds'
 import NoData from './NoData'
 import AxisScale from './AxisScale'
+import styles from './ScatterPlot.css'
+import {getRelativeMouse} from './Util'
 
 export type ScatterSeries = {
     color: string,
@@ -61,59 +63,84 @@ export default class PointsWithLabels extends Component {
             ]);
     }
 
-    @computed get hovered() {
-        return "none"
-    }
-
     // Used if no color is specified for a series
     @computed get defaultColorScale() {
         return d3.scaleOrdinal().range(d3.schemeCategory20)        
     }
 
     @computed get pointData() {
-        const {data, xScale, yScale, defaultColorScale, sizeScale, hovered} = this
-
+        const {data, xScale, yScale, defaultColorScale, sizeScale, focusKey} = this
 
         return _.map(data, d => {
+            const values = _.map(d.values, v => {
+                return {
+                    x: Math.floor(xScale.place(v.x)),
+                    y: Math.floor(yScale.place(v.y)),                       
+                    size: sizeScale(v.size||1) * (focusKey == d ? 1.5 : 1),
+                }
+            })
+
             return {
+                key: d.key,
                 color: d.color || defaultColorScale(d.key),
-                values : _.map(d.values, v => {
-                    return {
-                        x: xScale.place(v.x),
-                        y: yScale.place(v.y),                       
-                        size: sizeScale(v.size||1) * (hovered == d ? 1.5 : 1),
-                    }
-                })
+                values: values
             }
         })
     }
 
+    @computed get allColors() : string[] {
+        return _.uniq(_.map(this.pointData, 'color'))
+    }
+
+    @observable focusKey = null
+
+    @action.bound onMouseMove() {
+        const mouse = d3.mouse(this.base)
+
+        if (!this.bounds.containsPoint(...mouse))
+            this.focusKey = null
+        else {
+            const closestSeries = _.sortBy(this.pointData, (series) => {
+                if (series.values.length == 1) {
+                    return Math.pow(series.values[0].x-mouse[0], 2)+Math.pow(series.values[0].y-mouse[1], 2)
+                } else {
+                    return _.min(_.map(series.values, (v, i) => {
+                        return Math.pow(v.x-mouse[0], 2)+Math.pow(v.y-mouse[1], 2)
+                    }))
+                }
+            })[0]
+            this.focusKey = closestSeries.key
+        }       
+    }
+
+    componentDidMount() {
+        d3.select("html").on("mousemove", this.onMouseMove)
+    }    
+
     render() {
-        const {bounds, pointData, xScale, yScale, sizeScale, hovered} = this
+        const {bounds, pointData, xScale, yScale, sizeScale, focusKey, allColors} = this
 
         if (_.isEmpty(pointData))
             return <NoData bounds={bounds}/>
 
-        return <g>
-            {_.map(pointData, d => 
-                <g class="entity">
-                    {_.map(d.values, (v, i) =>
-                        [
-                            <circle
-                                cx={v.x} cy={v.y}
-                                fill={d.color} stroke="#000" stroke-width={0.3}
-                                r={v.size}
-                                fill-opacity={0.7}
-                            />,
-                            (i < d.values.length - 1 && <line
-                                x1={v.x} x2={d.values[i+1].x} y1={v.y} y2={d.values[i+1].y}
-                                stroke={d.color}
-                                stroke-opacity={0.7}
-                            />)
-                        ]
+        return <g class={styles.ScatterPlot}>
+            <g class="entities" fillOpacity={0.6}>
+                <defs>
+                    {_.map(allColors, color =>
+                        <marker key={color} id={"arrow"+color.slice(1)} fill={color} viewBox="0 -5 10 10" refx={5} refY={0} markerWidth={4} markerHeight={4} orient="auto">
+                            <path d="M0,-5L10,0L0,5"/>
+                        </marker>
                     )}
-                </g>
-            )}
+                </defs>
+                {_.map(pointData, d => 
+                    <polyline 
+                        stroke={d.color}
+                        points={_.map(d.values, v => `${v.x},${v.y}`).join(' ')} 
+                        fill="none" 
+                        strokeOpacity={focusKey == d.key ? 1 : 0.1}
+                        marker-mid={false && `url(#arrow${d.color.slice(1)})`}/> 
+                )}
+            </g>
         </g>
     }
 }
