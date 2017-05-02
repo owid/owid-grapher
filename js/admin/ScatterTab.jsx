6d43ea0c8a70ef5a127cb2cbad9a608d3844582b
Jaiden Mispy <jaiden@mispy.me>
2017-05-02 22:41:58 +0800

    Fix scatter timeline config, filter non-numeric data from scatters

--- js/admin/ScatterTab.jsx
+++ js/admin/ScatterTab.jsx
@@ -2,7 +2,7 @@ import * as d3 from 'd3'
 import _ from 'lodash'
 import dataflow from '../charts/owid.dataflow'
 import React, {Component} from 'react'
-import {observable, computed, action} from 'mobx'
+import {observable, computed, action, toJS} from 'mobx'
 import {observer} from 'mobx-react'
 
 @observer
@@ -12,7 +12,7 @@ export default class ScatterTab extends Component {
     }
 
     @observable timeline = {
-        defaultYear: null
+        defaultYear: 'latest'
     }
 
     @computed get isEnabled() {
@@ -35,147 +35,44 @@ export default class ScatterTab extends Component {
             this.props.chart.timeline = null
     }
 
+    @action.bound onTolerance(e) {
+        const tolerance = parseInt(e.target.value)
+        if (_.isFinite(tolerance)) {
+            this.timeline.tolerance = tolerance
+            this.save()
+        }
+    }
+
+    @action.bound onDefaultYear(e) {
+        this.timeline.defaultYear = e.target.value
+        this.save()
+    }
+
+    save() {
+        if (this.isEnabled)
+            this.props.chart.timeline = toJS(this.timeline)
+    }
+
     render() {
         const {isEnabled, timeline} = this
 
         return <div id="scatter-tab" class="tab-pane">
             <section>
                 <h2>Timeline</h2>
-                <p class="form-section-desc">   Note that the timeline settings will override any variable settings for target year and tolerance.</p>
+                <p class="form-section-desc">Note that the timeline settings will override any variable settings for target year and tolerance.</p>
                 <label class="clickable"><input type="checkbox" value={!!isEnabled} onChange={this.onToggleTimeline}/> Enable timeline</label>
                 {isEnabled && <div class="timeline-settings">
-                    <label><i class="fa fa-info-circle" data-toggle="tooltip" title="" data-original-title="Specify a range of years from which to pull data. For example, if the map shows 1990 and tolerance is set to 1, then data from 1989 or 1991 will be shown if no data is available for 1990."></i> Tolerance of data:
-                        <input name="tolerance" class="form-control" placeholder="Tolerance of data"/>
-                    </label> 
-                    <label>Default year to show: 
-                        <select name="defaultYear">
+                    <label><i class="fa fa-info-circle" data-toggle="tooltip" title="Specify a range of years from which to pull data. For example, if the chart shows 1990 and tolerance is set to 1, then data from 1989 or 1991 will be shown if no data is available for 1990."></i> Tolerance of data:
+                        <input name="tolerance" class="form-control" placeholder="Tolerance of data" onChange={this.onTolerance}/>
+                    </label>
+                    <label>Default year to show:
+                        <select name="defaultYear" onChange={this.onDefaultYear} value={timeline.defaultYear}>
                             <option value="latest">Latest year</option>
                             <option value="earliest">Earliest year</option>
                         </select>
                     </label>
                 </div>}
-            </section>        
+            </section>
         </div>
     }
 }
-
-/*owid.config.scatter = function(chart) {
-    var config = dataflow();
-
-    config.inputs({
-        formNode: undefined,
-        timeRanges: null,
-        defaultYear: 'latest',
-        tolerance: null,
-        isEnabled: false
-    });
-
-    config.flow('tab : formNode', function(formNode) {
-        var form = d3.select(formNode);
-
-        if (form.select('.scatter-item').empty()) {
-            form.select('.nav-tabs').append('li')
-                .attr('class', 'nav-item scatter-item')
-                .append('a')
-                  .attr('class', 'nav-link')
-                  .attr('data-toggle', 'tab')
-                  .attr('href', '#scatter-tab')
-                  .text('Scatter');
-        }
-
-        if (form.select('#scatter-tab').empty()) {
-            form.select('.tab-content').append('div')
-                .attr('id', 'scatter-tab')
-                .attr('class', 'tab-pane');
-        }
-
-        var tab = form.select('#scatter-tab');
-        tab.selectAll('*').remove();
-        return tab;
-    });
-
-    config.flow('isEnabledInput, timeRangesInput, toleranceInput, defaultYearInput : tab', function(tab) {
-        tab.html(
-
-        );
-
-        // Bind events
-        var isEnabledInput = tab.select('[name=isEnabled]');
-        isEnabledInput.on('change', function() {
-            config.update({ isEnabled: isEnabledInput.property('checked') });
-        });
-
-        var timeRangesInput = tab.select('[name=timeRanges]');
-        timeRangesInput.on('change', function() {
-            try {
-                var timeRanges = owid.timeRangesFromString(timeRangesInput.property('value'));
-                config.update({ timeRanges: timeRanges });
-
-                timeRangesInput.classed('has-error', false);
-            } catch (e) {
-                if (e instanceof RangeError)    
-                    timeRangesInput.classed('has-error', true);
-                else
-                    throw e;
-            }
-        });
-
-        var toleranceInput = tab.select('[name=tolerance]');
-        toleranceInput.on('change', function() {
-            config.update({ tolerance: toleranceInput.property('value') });
-        });
-
-        var defaultYearInput = tab.select('[name=defaultYear]');
-        defaultYearInput.on('change', function() {
-            config.update({ defaultYear: defaultYearInput.property('value') });
-        });
-
-        return [isEnabledInput, timeRangesInput, toleranceInput, defaultYearInput];
-    });
-
-    config.flow('tab, isEnabled', function(tab, isEnabled) {
-        tab.select('.timeline-settings').classed('hidden', !isEnabled);
-    });
-
-    config.flow('isEnabled, isEnabledInput', function(isEnabled, isEnabledInput) {
-        isEnabledInput.property('checked', !!isEnabled);
-    });
-
-    config.flow('timeRanges, timeRangesInput', function(timeRanges, timeRangesInput) {
-        var timeRangeStr = owid.timeRangesToString(timeRanges);
-        if (timeRangeStr == timeRangesInput.attr('placeholder'))
-            timeRangesInput.property('value', '');
-        else
-            timeRangesInput.property('value', timeRangeStr);
-    });
-
-    config.flow('defaultYear, defaultYearInput', function(defaultYear, defaultYearInput) {
-        defaultYearInput.property('value', defaultYear);
-    });
-
-    config.flow('tolerance, toleranceInput', function(tolerance, toleranceInput) {
-        toleranceInput.property('value', tolerance);
-    });
-
-    config.flow('isEnabled, timeRanges, defaultYear, tolerance', function(isEnabled, timeRanges, defaultYear, tolerance) {
-        if (!isEnabled)
-            chart.model.unset('timeline');
-        else {
-            var timeline = {};
-            if (!_.isEmpty(timeRanges)) timeline.timeRanges = timeRanges;
-            if (_.isFinite(tolerance)) timeline.tolerance = parseInt(tolerance);
-            timeline.defaultYear = defaultYear;
-            chart.model.set('timeline', timeline);
-        }
-    });
-
-    function update() {
-        var timelineConfig = chart.model.get('timeline');
-        config.update(_.extend({}, timelineConfig, { isEnabled: !!timelineConfig }));
-    }
-
-    chart.model.on('change', update);
-    update();
-    
-    return config;
-};*/
