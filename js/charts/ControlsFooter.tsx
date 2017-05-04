2c5fdc734e95293c882873f93ea38cdfecdea4db
Jaiden Mispy <jaiden@mispy.me>
2017-05-04 18:45:10 +0800

    Fix ControlsFooter spacing on mobile for new buttons

--- js/charts/ControlsFooter.tsx
+++ js/charts/ControlsFooter.tsx
@@ -141,11 +141,11 @@ export default class ControlsFooter extends React.Component<ControlsFooterProps,
                     {_.map(tabNames, (tabName) => {
                         return <li className={"tab clickable" + (tabName == props.activeTabName ? ' active' : '')} onClick={() => this.onTabChange(tabName)}><a>{tabName}</a></li>
                     })}
-                    <li className={"tab clickable" + (props.activeTabName == 'download' ? ' active' : '')} onClick={() => this.onTabChange('download')} title="Download as .png or .svg">
+                    <li className={"tab clickable icon" + (props.activeTabName == 'download' ? ' active' : '')} onClick={() => this.onTabChange('download')} title="Download as .png or .svg">
                         <a><i className="fa fa-download"/></a>
                     </li>
-                    {props.chartView.isEmbed && <li className="clickable"><a title="Open chart in new tab" href={this.linkUrl} target="_blank"><i className="fa fa-expand"/></a></li>}
-                    <li className="clickable"><a title="Share" onClick={this.onShareMenu}><i className="fa fa-share-alt"/></a></li>
+                    {props.chartView.isEmbed && <li className="clickable icon"><a title="Open chart in new tab" href={this.linkUrl} target="_blank"><i className="fa fa-expand"/></a></li>}
+                    <li className="clickable icon"><a title="Share" onClick={this.onShareMenu}><i className="fa fa-share-alt"/></a></li>
                 </ul>
             </nav>
             {isShareMenuActive && <ShareMenu chartView={this.props.chartView} config={this.props.config} onDismiss={() => this.isShareMenuActive = false}/>}
