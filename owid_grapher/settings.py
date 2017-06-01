74f385d5986bbcbb730d122a5e3ad1719c827047
Jaiden Mispy <jaiden@mispy.me>
2017-06-01 20:34:34 +0800

    default_storage_engine=INNODB

--- owid_grapher/settings.py
+++ owid_grapher/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
             'sql_mode': 'traditional',
             'charset': 'utf8',
             'init_command': 'SET '
-                'storage_engine=INNODB,'
+                'default_storage_engine=INNODB,'
                 'character_set_connection=utf8,'
                 'collation_connection=utf8_bin'
         },
