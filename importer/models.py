b5dc87af75634532d57f2baf60a0a623e3b00367
aaldaber <aaldaber@gmail.com>
2017-06-12 00:04:44 +0600

    Implementing the WDI fetcher that performs an initial import of data from an Excel file
    wdi_importer is able to import all WDI data values and info about
    variables, sources and categories from a provided World Bank's link
    pointing to a zip file.
    Adding a logger for wdi_importer.
    Adding import history model.
    Adding entity_info column in the entities table.
    Minor changes to the country_name_tool.

--- importer/models.py
+++ importer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class ImportHistory(models.Model):
+    import_type = models.CharField(max_length=255)
+    import_time = models.DateTimeField()
+    import_notes = models.TextField()
+    import_state = models.TextField()
