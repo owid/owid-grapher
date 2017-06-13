f5eac83a6eea05ffb513203404ddeeee5d403732
Jaiden Mispy <jaiden@mispy.me>
2017-06-13 11:18:42 +0800

    Shouldn't need webpack for travis tests, actually

--- owid_grapher/templatetags/webpack.py
+++ owid_grapher/templatetags/webpack.py
@@ -10,10 +10,10 @@ register = template.Library()
 
 @register.simple_tag
 def webpack(asset_name: str) -> str:
-    if settings.DEBUG:
-        return "http://localhost:8090/%s" % asset_name
-    else:
+    if settings.ENV == 'production':
         # Read version-stamped urls from manifest.json once and cache for process lifetime
         if not hasattr(webpack, 'manifest'):
             webpack.manifest = json.loads(open(os.path.join(settings.BASE_DIR, "public/build/manifest.json")).read())
         return static("/build/" + webpack.manifest[asset_name])
+    else:
+        return "http://localhost:8090/%s" % asset_name
