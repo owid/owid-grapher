22fa1b95fa89855b8d1e27d56fdcdb5c70cafbed
Aibek Aldabergenov <aaldaber@gmail.com>
2017-10-10 19:02:42 +0600

    Fixing Cloudflare URL purge script

--- owid_grapher/various_scripts/purge_cloudflare_cache_queue.py
+++ owid_grapher/various_scripts/purge_cloudflare_cache_queue.py
@@ -8,14 +8,41 @@ import CloudFlare
 
 
 def purge_cloudflare_cache_queue():
-    all_urls = CloudflarePurgeQueue.objects.all().values('url')
-    urls_to_purge = [item['url'] for item in all_urls]
-    try:
+    while CloudflarePurgeQueue.objects.first():
         cf = CloudFlare.CloudFlare(email=settings.CLOUDFLARE_EMAIL, token=settings.CLOUDFLARE_KEY)
-        cf.zones.purge_cache.delete(settings.CLOUDFLARE_ZONE_ID, data={"files": urls_to_purge})
-        CloudflarePurgeQueue.objects.filter(url__in=urls_to_purge).delete()
-    except CloudFlare.exceptions.CloudFlareAPIError:
-        pass
+        counter = 0
+        urls_to_purge = []
+        url_objects_in_db = []
+        all_urls = CloudflarePurgeQueue.objects.all()
+        for oneurl in all_urls:
+            urls_to_purge.append(oneurl.url)
+            url_objects_in_db.append(oneurl)
+            counter += 1
+            if counter == 30:
+                try:
+                    cf.zones.purge_cache.delete(settings.CLOUDFLARE_ZONE_ID, data={"files": urls_to_purge})
+                    CloudflarePurgeQueue.objects.filter(id__in=[item.pk for item in url_objects_in_db]).delete()
+                except CloudFlare.exceptions.CloudFlareAPIError:  # one of the links caused an exception
+                    for onelink in urls_to_purge:
+                        try:
+                            cf.zones.purge_cache.delete(settings.CLOUDFLARE_ZONE_ID, data={"files": [onelink]})
+                            CloudflarePurgeQueue.objects.filter(url=onelink).delete()
+                        except CloudFlare.exceptions.CloudFlareAPIError:
+                            CloudflarePurgeQueue.objects.filter(url=onelink).delete()
+                urls_to_purge = []
+                url_objects_in_db = []
+                break
+        if urls_to_purge:
+            try:
+                cf.zones.purge_cache.delete(settings.CLOUDFLARE_ZONE_ID, data={"files": urls_to_purge})
+                CloudflarePurgeQueue.objects.filter(id__in=[item.pk for item in url_objects_in_db]).delete()
+            except CloudFlare.exceptions.CloudFlareAPIError:  # one of the links caused an exception
+                for onelink in urls_to_purge:
+                    try:
+                        cf.zones.purge_cache.delete(settings.CLOUDFLARE_ZONE_ID, data={"files": [onelink]})
+                        CloudflarePurgeQueue.objects.filter(url=onelink).delete()
+                    except CloudFlare.exceptions.CloudFlareAPIError:
+                        CloudflarePurgeQueue.objects.filter(url=onelink).delete()
 
 
 if __name__ == "__main__":
