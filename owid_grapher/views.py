adfc699f7c134b8968ed39671c27131c86cf07fe
Jaiden Mispy <jaiden@mispy.me>
2017-10-19 22:56:26 +0800

    Add polyfill for iframeless embeds

--- owid_grapher/views.py
+++ owid_grapher/views.py
@@ -38,10 +38,26 @@ def embed_snippet(request):
         link.href = '""" + chartsCss + """';
         document.head.appendChild(link);
 
+        var hasPolyfill = false;
+        var hasGrapher = false;
+
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.onload = function() {
-            window.Grapher.embedAll();
+            hasPolyfill = true;
+            if (hasGrapher)
+                window.Grapher.embedAll();
+        }
+        script.src = "https://cdn.polyfill.io/v2/polyfill.min.js?features=es6,fetch"
+        document.head.appendChild(script);
+
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = function() {
+            hasGrapher = true;
+            if (hasPolyfill)
+                window.Grapher.embedAll();
         }
         script.src = '""" + chartsJs + """';
         document.head.appendChild(script);
