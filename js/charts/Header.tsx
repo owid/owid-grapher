b6805090af7319a4065b806dc6450a974feeda16
Jaiden Mispy <jaiden@mispy.me>
2017-12-09 20:36:32 +0800

    Don't assume browser context in header

--- js/charts/Header.tsx
+++ js/charts/Header.tsx
@@ -3,6 +3,7 @@ import TextWrap from './TextWrap'
 import { computed } from 'mobx'
 import { observer } from 'mobx-react'
 import ChartConfig from './ChartConfig'
+const isNode: boolean = require('detect-node')
 
 interface LogoProps {
     svg: string
@@ -18,21 +19,15 @@ class Logo {
         return 40
     }
 
-    @computed get bbox() {
-        const div = document.createElement('div')
-        div.innerHTML = this.props.svg
-        document.body.appendChild(div)
-        const bbox = (div.childNodes[0] as SVGSVGElement).getBBox()
-        document.body.removeChild(div)
-        return bbox
-    }
+    @computed get origWidth() { return 211 }
+    @computed get origHeight() { return 130 }
 
     @computed get scale(): number {
-        return this.bbox.height === 0 ? 1 : this.targetHeight / this.bbox.height
+        return this.origHeight === 0 ? 1 : this.targetHeight / this.origHeight
     }
 
-    @computed get width() { return this.bbox.width * this.scale }
-    @computed get height() { return this.bbox.height * this.scale }
+    @computed get width() { return this.origWidth * this.scale }
+    @computed get height() { return this.origHeight * this.scale }
 
     render(targetX: number, targetY: number) {
         const { props, scale } = this
@@ -61,15 +56,19 @@ export default class Header {
         return this.props.chart.subtitle
     }
 
-    @computed get logo() {
-        return new Logo({ svg: this.props.chart.logosSVG[0] })
+    @computed get logo(): Logo|undefined {
+        const {chart} = this.props
+        return chart.logosSVG ? new Logo({ svg: this.props.chart.logosSVG[0] }) : undefined
     }
 
+    @computed get logoWidth(): number { return this.logo ? this.logo.width : 0 }
+    @computed get logoHeight(): number { return this.logo ? this.logo.height : 0 }
+
     @computed get title() {
-        const { props, logo } = this
+        const { props, logoWidth } = this
         let { titleText } = this
 
-        const maxWidth = props.maxWidth - logo.width - 15
+        const maxWidth = props.maxWidth - logoWidth - 15
         // HACK (Mispy): Stop the title jumping around during timeline transitions
         if (props.chart.data.minYear === props.chart.data.maxYear && props.chart.data.isShowingTimeline) {
             titleText = titleText + " in 2000"
@@ -90,7 +89,7 @@ export default class Header {
 
     @computed get subtitleWidth() {
         // If the subtitle is entirely below the logo, we can go underneath it
-        return this.title.height > this.logo.height ? this.props.maxWidth : this.props.maxWidth - this.logo.width - 10
+        return this.title.height > this.logoHeight ? this.props.maxWidth : this.props.maxWidth - this.logoWidth - 10
     }
 
     @computed get subtitle() {
@@ -103,7 +102,7 @@ export default class Header {
     }
 
     @computed get height() {
-        return Math.max(this.title.height + this.subtitle.height + 2, this.logo.height)
+        return Math.max(this.title.height + this.subtitle.height + 2, this.logoHeight)
     }
 
     render(x: number, y: number) {
@@ -118,11 +117,11 @@ class HeaderView extends React.Component<{ x: number, y: number, header: Header 
         const { title, titleText, logo, subtitle } = props.header
         const { chart, maxWidth } = props.header.props
 
-        if (!chart.isEmbed)
+        if (!isNode && !chart.isEmbed)
             document.title = titleText
 
         return <g className="HeaderView">
-            {logo.height > 0 && logo.render(props.x + maxWidth - logo.width, props.y)}
+            {logo && logo.height > 0 && logo.render(props.x + maxWidth - logo.width, props.y)}
             <a href={chart.url.canonicalUrl} target="_blank">
                 {title.render(props.x, props.y, { fill: "#555" })}
             </a>
