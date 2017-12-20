ad3132ee40353b3ea613ed25f6b810a5a90a0dd4
Jaiden Mispy <jaiden@mispy.me>
2017-12-21 05:08:55 +0800

    outDir

--- src/wpStatic.ts
+++ src/wpStatic.ts
@@ -3,23 +3,26 @@ import * as request from 'request-promise'
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
-async function scrapePage(slug: string, baseUrl: string) {
+async function scrapePage(slug: string, baseUrl: string, outDir: string) {
     try {
-        const html = await request(`${baseUrl}/${slug}`)
+        let html = await request(`${baseUrl}/${slug}`)
 
-        const outDir = `tmp/`
-        const outPath = path.join(outDir, `${slug}/index.html`)
+        if (slug === "/") slug = "index"
+        const outPath = path.join(outDir, `${slug}.html`)
         await fs.mkdirp(path.dirname(outPath))
-        await fs.writeFile(outPath, html.replace(new RegExp(baseUrl, 'g'), ""))
+
+        html = html.replace(new RegExp(baseUrl, 'g'), "").replace("/grapher/embedCharts.js", "https://ourworldindata.org/grapher/embedCharts.js")
+
+        await fs.writeFile(outPath, html)
     } catch (err) {
         console.error(slug, err.message)
     }
 }
 
-async function writeRedirects(db: DatabaseConnection) {
+async function writeRedirects(db: DatabaseConnection, outDir: string) {
     const rows = await db.query(`SELECT url, action_data, action_code FROM wp_redirection_items`)
     const redirects = rows.map(row => `${row.url} ${row.action_data} ${row.action_code}`)
-    await fs.writeFile(`tmp/_redirects`, redirects.join("\n"))
+    await fs.writeFile(`${outDir}/_redirects`, redirects.join("\n"))
 }
 
 async function getPermalinks(db: DatabaseConnection) {
@@ -31,22 +34,22 @@ async function getPermalinks(db: DatabaseConnection) {
     return permalinks
 }
 
-async function main(baseUrl: string) {
+async function main(baseUrl: string, outDir: string) {
     const db = createConnection({ database: "owid_wordpress" })
 
-    const redirects = writeRedirects(db)
+    const redirects = writeRedirects(db, outDir)
     const postsQuery = db.query(`SELECT ID, post_name FROM wp_posts WHERE (post_type='page' OR post_type='post') AND post_status='publish'`)
 
     const permalinks = await getPermalinks(db)
     const rows = await postsQuery
 
-    await scrapePage("/", baseUrl)
+    await scrapePage("/", baseUrl, outDir)
     // Scrape in little batches to avoid overwhelming the server
     for (let i = 0; i < rows.length; i += 10) {
         console.log(i)
         await Promise.all(rows.slice(i, i+10).map(row => {
             const slug = (permalinks[row.ID] || row.post_name).replace(/\/$/, "")
-            return scrapePage(slug, baseUrl)
+            return scrapePage(slug, baseUrl, outDir)
         }))
     }
     await redirects
@@ -54,4 +57,4 @@ async function main(baseUrl: string) {
     db.end()
 }
 
-main("http://l:8080")
+main("http://l:8080", "/Users/mispy/static-owid/wp/")
