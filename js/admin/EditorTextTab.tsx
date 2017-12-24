3acd4ed5fcddec9f66bf94dd46afd951aab19eb4
Jaiden Mispy <jaiden@mispy.me>
2017-12-24 15:13:02 +0800

    Add some more info to editor

--- js/admin/EditorTextTab.tsx
+++ js/admin/EditorTextTab.tsx
@@ -19,10 +19,10 @@ export default class EditorTextTab extends React.Component<{ editor: ChartEditor
                 <BindAutoString field="title" store={chart.props} auto={chart.data.title} softCharacterLimit={100}/>
                 <Toggle label="Hide automatic time/entity" value={!!chart.props.hideTitleAnnotation} onValue={action((value: boolean) => chart.props.hideTitleAnnotation = value||undefined)}/>
                 <AutoTextField label="/grapher" value={chart.data.slug} onValue={this.onSlug} isAuto={chart.props.slug === undefined} onToggleAuto={_ => chart.props.slug = chart.props.slug === undefined ? chart.data.slug : undefined} helpText="Human-friendly URL for this chart"/>
-                <BindString field="subtitle" store={chart.props} placeholder="Briefly describe the context of the data" textarea softCharacterLimit={280}/>
+                <BindString field="subtitle" store={chart.props} placeholder="Briefly describe the context of the data. It's best to avoid duplicating any information which can be easily inferred from other visual elements of the chart." textarea softCharacterLimit={280}/>
             </Section>
             <Section name="Footer">
-                <BindAutoString label="Source" field="sourceDesc" store={chart.props} auto={chart.data.sourcesLine} softCharacterLimit={60}/>
+                <BindAutoString label="Source" field="sourceDesc" store={chart.props} auto={chart.data.sourcesLine} helpText="Short comma-separated list of source names" softCharacterLimit={60}/>
                 <BindString label="Origin url" field="originUrl" store={chart.props} placeholder={chart.data.originUrl} helpText="The page containing this chart where more context can be found"/>
                 <BindString label="Footer note" field="note" store={chart.props} helpText="Any important clarification needed to avoid miscommunication" softCharacterLimit={140}/>
             </Section>
