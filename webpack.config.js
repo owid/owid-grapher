d48f88e9726c991c990577d6c435e756fd5fc80c
Jaiden Mispy <jaiden@mispy.me>
2018-03-20 11:08:08 +0800

    Use new webpack 'mode' feature

--- webpack.config.js
+++ webpack.config.js
@@ -4,94 +4,77 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
 const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
-const isProduction = process.argv.indexOf('-p') !== -1
 
-module.exports = {
-    context: __dirname,
-    entry: {
-        charts: "./js/charts.entry.ts",
-        admin: "./js/admin.entry.ts",
-        oldadmin: "./js/oldadmin.entry.ts"
-    },
-    output: {
-        path: path.join(__dirname, "grapher_admin/static/build"),
-        filename: (isProduction ? "[name].bundle.[chunkhash].js" : "[name].js")
-    },
-  	resolve: {
-        extensions: [".ts", ".tsx", ".js", ".css"],
-        alias: {
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat',
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production'
+    return {
+        context: __dirname,
+        entry: {
+            charts: "./js/charts.entry.ts",
+            admin: "./js/admin.entry.ts",
+            oldadmin: "./js/oldadmin.entry.ts"
         },
-        modules: [
-  	        path.join(__dirname, "js/libs"),
-            path.join(__dirname, "css/libs"),
-            path.join(__dirname, "node_modules"),
-        ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true,
-                    configFile: path.join(__dirname, "tsconfig.client.json")
+        output: {
+            path: path.join(__dirname, "grapher_admin/static/build"),
+            filename: (isProduction ? "[name].bundle.[chunkhash].js" : "[name].js")
+        },
+          resolve: {
+            extensions: [".ts", ".tsx", ".js", ".css"],
+            alias: {
+                'react': 'preact-compat',
+                'react-dom': 'preact-compat',
+            },
+            modules: [
+                path.join(__dirname, "css/libs"),
+                path.join(__dirname, "node_modules"),
+            ],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true,
+                        configFile: path.join(__dirname, "tsconfig.client.json")
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'postcss-loader'] })
+                },
+                {
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'sass-loader'] })
+                },
+                {
+                    test: /\.(jpe?g|gif|png|eot|woff|ttf|svg|woff2)$/,
+                    loader: 'url-loader?limit=10000'
                 }
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'postcss-loader'] })
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'sass-loader'] })
-            },
-            {
-                test: /\.(jpe?g|gif|png|eot|woff|ttf|svg|woff2)$/,
-                loader: 'url-loader?limit=10000'
+            ],
+        },
+    
+        plugins: (isProduction ? [
+            // This plugin extracts css files required in the entry points
+            // into a separate CSS bundle for download
+            new ExtractTextPlugin('[name].bundle.[chunkhash].css'),
+            // Output manifest so server can figure out the hashed filenames
+            new ManifestPlugin(),
+        ] : [
+            new ExtractTextPlugin('[name].css'),    
+            // Output manifest so server can figure out the hashed filenames
+            new ManifestPlugin(),
+        ]),
+        devServer: {
+            host: 'localhost',
+            port: 8090,
+            contentBase: 'public',
+            disableHostCheck: true,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+                "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
             }
-        ],
-    },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: (isProduction ? false : "cheap-module-eval-source-map"),
-
-    plugins: (isProduction ? [
-        // This plugin extracts css files required in the entry points
-        // into a separate CSS bundle for download
-        new ExtractTextPlugin('[name].bundle.[chunkhash].css'),
-
-        // CSS optimization
-        /*new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.bundle.*\.css$/,
-            cssProcessorOptions: { discardComments: { removeAll: true } }
-        }),*/
-
-        // JS optimization
-        new ParallelUglifyPlugin({
-            cachePath: path.join(__dirname, 'tmp')
-        }),
-
-        // Output manifest so server can figure out the hashed
-        // filenames
-        new ManifestPlugin(),
-    ] : [
-        new ExtractTextPlugin('[name].css'),
-
-        // Output manifest so server can figure out the hashed
-        // filenames
-        new ManifestPlugin(),
-    ]),
-    devServer: {
-        host: 'localhost',
-        port: 8090,
-        contentBase: 'public',
-        disableHostCheck: true,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-        }
-    },
+        },    
+    }
 }
