a9e6cd9728e15e4dc689a3b84a566d2b0b932619
Jaiden Mispy <jaiden@mispy.me>
2018-07-06 06:19:05 +0800

    Add data and metadata exporters

---
+++ src/scripts/exportDataValues.ts
@@ -0,0 +1,35 @@
+// Script to export the data_values for all variables attached to charts
+
+import * as db from '../db'
+import * as shell from 'shelljs'
+import * as _ from 'lodash'
+import * as settings from '../settings'
+import * as path from 'path'
+import * as fs from 'fs-extra'
+
+async function dataExport() {
+    db.connect()
+
+    const tmpFile = "/tmp/owid_datavalues.sql"
+    const outputPath = path.join(settings.BASE_DIR, `fixtures/owid_datavalues.sql`)
+
+    const variableIds = (await db.query("SELECT DISTINCT variableId FROM chart_dimensions")).map((row: any) => row.variableId)
+
+    console.log(`Exporting data for ${variableIds.length} variables to ${outputPath}`)
+
+    shell.exec(`rm -f ${tmpFile}`)
+
+    let count = 0
+    for (const chunk of _.chunk(variableIds, 100)) {
+        shell.exec(`mysqldump --no-create-info ${settings.DB_NAME} data_values --where="variableId IN (${chunk.join(",")})" >> ${tmpFile}`)
+
+        count += chunk.length
+        console.log(count)
+    }
+
+    await fs.move(tmpFile, outputPath, { overwrite: true })
+
+    db.end()
+}
+
+dataExport()
---
+++ src/scripts/exportMetadata.ts
@@ -0,0 +1,30 @@
+// Script to export everything in the database except sensitive info and data_values (which is big)
+
+import * as db from '../db'
+import * as shell from 'shelljs'
+import * as settings from '../settings'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+
+async function dataExport() {
+    db.connect()
+
+    const outputPath = path.join(settings.BASE_DIR, `fixtures/owid_metadata.sql`)
+
+    console.log(`Exporting database structure and metadata to ${outputPath}...`)
+
+    // Dump all tables except data_values
+    shell.exec(`mysqldump ${settings.DB_NAME} --ignore-table=${settings.DB_NAME}.data_values -r /tmp/owid_metadata.sql`)
+
+    // Strip passwords
+    shell.exec(`sed -i -e "s/bcrypt[^']*//g" /tmp/owid_metadata.sql`)
+
+    // Add default admin user
+    await fs.appendFile("/tmp/owid_metadata.sql", "INSERT INTO users (`password`, `is_superuser`, `email`, `name`, `created_at`, `updated_at`, `is_active`) VALUES ('bcrypt$$2b$12$EXfM7cWsjlNchpinv.j6KuOwK92hihg5r3fNssty8tLCUpOubST9u', 1, 'admin@example.com', 'admin', '2016-01-01 00:00:00', '2016-01-01 00:00:00', 1);\n")
+
+    await fs.move("/tmp/owid_metadata.sql", outputPath, { overwrite: true })
+
+    db.end()
+}
+
+dataExport()
