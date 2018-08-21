32ac31e9fa707b9bb6163368323273ae5eac14e5
Jaiden Mispy <jaiden@mispy.me>
2018-08-22 02:06:39 +0800

    Autoload typeorm models in development console

--- src/console.ts
+++ src/console.ts
@@ -1,8 +1,17 @@
 import * as db from './db'
 
-
+// Return value of prepareConsole is used as global namespace for `yarn c`
 export async function prepareConsole(): Promise<any> {
-    await db.connect()
+    const connection = await db.connect()
 
-    return { db: db }
+    const exposeToConsole: {[key: string]: any} = {
+        db: db
+    }
+
+    // Expose all typeorm models
+    for (const meta of connection.entityMetadatas) {
+        exposeToConsole[meta.targetName] = meta.target
+    }
+
+    return exposeToConsole
 }
