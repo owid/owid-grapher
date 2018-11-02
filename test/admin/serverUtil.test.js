540beea7e3ff0853603058eed4514161d5fdef1f
Daniel Gavrilov <dgavrilov95@gmail.com>
2018-11-02 08:41:59 +0000

    Implement exec() and tryExec() with tests

---
+++ test/admin/serverUtil.test.js
@@ -0,0 +1,38 @@
+const { exec, tryExec } = require('../../dist/src/admin/serverUtil')
+
+describe('serverUtil', () => {
+
+    describe('exec()', () => {
+        it('should resolve when there is a zero exit code', () => {
+            expect(exec(`echo "it works"; exit 0`)).resolves.toEqual({
+                stdout: 'it works\n',
+                stderr: ''
+            })
+        })
+
+        it('should reject when there is a non-zero exit code', () => {
+            expect(exec(`echo "does not work"; exit 1`)).rejects.toMatchObject({
+                stdout: 'does not work\n'
+            })
+        })
+    })
+
+    describe('tryExec()', () => {
+        it('should resolve when there is a zero exit code', () => {
+            expect(tryExec(`echo "it works"; exit 0`)).resolves.toEqual({
+                stdout: 'it works\n',
+                stderr: ''
+            })
+        })
+
+        it('should resolve and inject error when there is a non-zero exit code', () => {
+            const command = tryExec(`echo "does not work"; exit 1`)
+            expect(command).resolves.toMatchObject({
+                stdout: 'does not work\n',
+                stderr: ''
+            })
+            expect(command).resolves.toHaveProperty('error')
+        })
+    })
+
+})
