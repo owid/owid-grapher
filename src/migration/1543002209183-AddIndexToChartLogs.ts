e0478516a238c0b8cba7ef10d673f23125f0d5bc
Sherin Kurian <sherin@instacart.com>
2018-11-23 11:47:16 -0800

    add migrations for tracking chart config history

---
+++ src/migration/1543002209183-AddIndexToChartLogs.ts
@@ -0,0 +1,12 @@
+import {MigrationInterface, QueryRunner} from "typeorm"
+
+export class AddIndexToChartLogs1543002209183 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("CREATE INDEX `chartId` ON chart_logs(chartId)")
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
