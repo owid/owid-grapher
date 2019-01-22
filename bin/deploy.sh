9d5a8114c852bc1d94c7e06f39aca6d609ea5b3a
Jaiden Mispy <jaiden@mispy.me>
2019-01-21 17:01:45 -0800

    yarn pre-deploy => yarn check

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -34,7 +34,7 @@ then
   FINAL_DATA="$ROOT/$NAME-data"
 
   # Run pre-deploy builds and checks
-  yarn pre-deploy
+  yarn check
 
   # Rsync the local repository to a temporary location on the server
   $RSYNC $DIR/ $HOST:$SYNC_TARGET
