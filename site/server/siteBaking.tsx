fdc54a92cf62174ad013e3a1fd81d2bd4397b191
Jaiden Mispy <jaiden@mispy.me>
2019-02-01 14:45:59 -0800

    Fix sitemap...

--- site/server/siteBaking.tsx
+++ site/server/siteBaking.tsx
@@ -161,7 +161,7 @@ export async function makeSitemap() {
     const sitemap = `<?xml version="1.0" encoding="utf-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.map(url => `    <url>
-        <loc>${url.loc}</url>
+        <loc>${url.loc}</loc>
         <lastmod>${url.lastmod}</lastmod>
     </url>`).join("\n")}
 </urlset>`
