fd824597d11a1bdfdd41385c88b82b5a5cee18f1
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-02-04 12:45:34 -0800

    Set up Stripe client

--- settings.ts
+++ settings.ts
@@ -22,3 +22,5 @@ export const BLOG_POSTS_PER_PAGE: number = 21
 
 export const ALGOLIA_ID: string = process.env.ALGOLIA_ID||""
 export const ALGOLIA_SEARCH_KEY: string = process.env.ALGOLIA_SEARCH_KEY||""
+
+export const STRIPE_PUBLIC_KEY: string = "pk_test_nIHvmH37zsoltpw3xMssPIYq"
