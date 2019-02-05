6c415c3d8aaa11b2e5d06b89cc37930bc4169698
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-02-04 16:00:49 -0800

    Move stripe script to Head

--- site/server/views/DonatePage.tsx
+++ site/server/views/DonatePage.tsx
@@ -8,7 +8,9 @@ import { SiteFooter } from './SiteFooter'
 
 export const DonatePage = () => {
     return <html>
-        <Head canonicalUrl={`${settings.BAKED_BASE_URL}/donate`} pageTitle="Donate"/>
+        <Head canonicalUrl={`${settings.BAKED_BASE_URL}/donate`} pageTitle="Donate">
+            <script src="https://js.stripe.com/v3/" />
+        </Head>
         <body>
             <SiteHeader/>
             <main>
@@ -47,7 +49,6 @@ export const DonatePage = () => {
 
             <SiteFooter/>
 
-            <script src="https://js.stripe.com/v3/" />
             <script dangerouslySetInnerHTML={{__html: `
                 runDonateForm()
             `}}/>
