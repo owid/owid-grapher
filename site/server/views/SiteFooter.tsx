f823e6f4c7f64664c03dda2ba6c9fb31281fb69b
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-02-21 16:59:45 -0800

    Add yellow donate section before footer on every page

--- site/server/views/SiteFooter.tsx
+++ site/server/views/SiteFooter.tsx
@@ -1,65 +1,85 @@
 import * as React from 'react'
 import { webpack } from 'utils/server/staticGen'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 export const SiteFooter = () => {
-    return <footer className="site-footer">
-        <div className="wrapper">
-            <div className="owid-row">
-                <div className="owid-col owid-col--lg-1">
-                    <ul>
-                        <li><a href="/about" data-track-click data-track-note="footer-navigation">About</a></li>
-                        <li><a href="/about#contact" data-track-click data-track-note="footer-navigation">Contact</a></li>
-                        <li><a href="/jobs" data-track-click data-track-note="footer-navigation">Jobs</a></li>
-                        <li><a href="/about#supporters" data-track-click data-track-note="footer-navigation">Supporters</a></li>
-                        <li><a href="/about/how-to-use-our-world-in-data" data-track-click data-track-note="footer-navigation">How to use</a></li>
-                        <li><a href="/donate" data-track-click data-track-note="footer-navigation">Donate</a></li>
-                    </ul>
-                </div>
-                <div className="owid-col owid-col--lg-1">
-                    <ul>
-                        <li><a href="/blog" data-track-click data-track-note="footer-navigation">Blog</a></li>
-                        <li><a href="/charts" data-track-click data-track-note="footer-navigation">All charts</a></li>
-                    </ul>
-                    <ul>
-                        <li><a href="https://twitter.com/OurWorldInData" data-track-click data-track-note="footer-navigation">Twitter</a></li>
-                        <li><a href="https://www.facebook.com/OurWorldinData" data-track-click data-track-note="footer-navigation">Facebook</a></li>
-                        <li><a href="https://github.com/owid" data-track-click data-track-note="footer-navigation">GitHub</a></li>
-                        <li><a href="/feed" data-track-click data-track-note="footer-navigation">RSS Feed</a></li>
-                    </ul>
-                </div>
-                <div className="owid-col owid-col--lg-1">
-                    <div className="logos">
-                        <a href="https://www.oxfordmartin.ox.ac.uk/research/programmes/global-development" className="partner-logo" data-track-click data-track-note="footer-navigation">
-                            <img src="/oxford-logo-rect.png" alt="University of Oxford logo"/>
-                        </a>
-                        <a href="https://global-change-data-lab.org/" className="partner-logo" data-track-click data-track-note="footer-navigation">
-                            <img src="/gcdl-logo.png" alt="Global Change Data Lab logo"/>
-                        </a>
-                        <a href="/owid-at-ycombinator" className="partner-logo" data-track-click data-track-note="footer-navigation">
-                            <img src="/yc-logo.png" alt="Y Combinator logo"/>
-                        </a>
+    return <React.Fragment>
+        <section className="donate-footer">
+            <div className="wrapper">
+                <div className="owid-row flex-align-center">
+                    <div className="owid-col owid-col--lg-3 owid-padding-bottom--sm-3">
+                        <p>Our World in Data is free and accessible for everyone.</p>
+                        <p>Help us do this work by making a donation.</p>
                     </div>
-                </div>
-                <div className="owid-col flex-2">
-                    <div className="legal">
-                        <p>
-                            License: All of Our World in Data is completely open access and all work is licensed under the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons BY license</a>. You have the permission to use, distribute, and reproduce in any medium, provided the source and authors are credited.
-                        </p>
-                        <p>
-                            Please consult our full <a href="/about#legal">legal disclaimer</a>.
-                        </p>
-                        {/* <a href="/" className="owid-logo">Our World in Data</a> */}
+                    <div className="owid-col owid-col--lg-1">
+                        <a href="/donate" className="owid-button donate-button" data-track-click data-track-note="donate-footer">
+                            <span className="label">Donate now</span>
+                            <span className="icon"><FontAwesomeIcon icon={faAngleRight} /></span>
+                        </a>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="feedbackPromptContainer"></div>
-        <script src={webpack('commons.js', 'site')}/>
-        <script src={webpack('owid.js', 'site')}/>
-        <script dangerouslySetInnerHTML={{__html: `
-            runHeaderMenus();
-            runFeedback();
-            Grapher.embedAll();
-        `}}/>
-    </footer>
+        </section>
+        <footer className="site-footer">
+            <div className="wrapper">
+                <div className="owid-row">
+                    <div className="owid-col owid-col--lg-1">
+                        <ul>
+                            <li><a href="/about" data-track-click data-track-note="footer-navigation">About</a></li>
+                            <li><a href="/about#contact" data-track-click data-track-note="footer-navigation">Contact</a></li>
+                            <li><a href="/jobs" data-track-click data-track-note="footer-navigation">Jobs</a></li>
+                            <li><a href="/about#supporters" data-track-click data-track-note="footer-navigation">Supporters</a></li>
+                            <li><a href="/about/how-to-use-our-world-in-data" data-track-click data-track-note="footer-navigation">How to use</a></li>
+                            <li><a href="/donate" data-track-click data-track-note="footer-navigation">Donate</a></li>
+                        </ul>
+                    </div>
+                    <div className="owid-col owid-col--lg-1">
+                        <ul>
+                            <li><a href="/blog" data-track-click data-track-note="footer-navigation">Blog</a></li>
+                            <li><a href="/charts" data-track-click data-track-note="footer-navigation">All charts</a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="https://twitter.com/OurWorldInData" data-track-click data-track-note="footer-navigation">Twitter</a></li>
+                            <li><a href="https://www.facebook.com/OurWorldinData" data-track-click data-track-note="footer-navigation">Facebook</a></li>
+                            <li><a href="https://github.com/owid" data-track-click data-track-note="footer-navigation">GitHub</a></li>
+                            <li><a href="/feed" data-track-click data-track-note="footer-navigation">RSS Feed</a></li>
+                        </ul>
+                    </div>
+                    <div className="owid-col owid-col--lg-1">
+                        <div className="logos">
+                            <a href="https://www.oxfordmartin.ox.ac.uk/research/programmes/global-development" className="partner-logo" data-track-click data-track-note="footer-navigation">
+                                <img src="/oxford-logo-rect.png" alt="University of Oxford logo"/>
+                            </a>
+                            <a href="https://global-change-data-lab.org/" className="partner-logo" data-track-click data-track-note="footer-navigation">
+                                <img src="/gcdl-logo.png" alt="Global Change Data Lab logo"/>
+                            </a>
+                            <a href="/owid-at-ycombinator" className="partner-logo" data-track-click data-track-note="footer-navigation">
+                                <img src="/yc-logo.png" alt="Y Combinator logo"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="owid-col flex-2">
+                        <div className="legal">
+                            <p>
+                                License: All of Our World in Data is completely open access and all work is licensed under the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons BY license</a>. You have the permission to use, distribute, and reproduce in any medium, provided the source and authors are credited.
+                            </p>
+                            <p>
+                                Please consult our full <a href="/about#legal">legal disclaimer</a>.
+                            </p>
+                            {/* <a href="/" className="owid-logo">Our World in Data</a> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="feedbackPromptContainer"></div>
+            <script src={webpack('commons.js', 'site')}/>
+            <script src={webpack('owid.js', 'site')}/>
+            <script dangerouslySetInnerHTML={{__html: `
+                runHeaderMenus();
+                runFeedback();
+                Grapher.embedAll();
+            `}}/>
+        </footer>
+    </React.Fragment>
 }
