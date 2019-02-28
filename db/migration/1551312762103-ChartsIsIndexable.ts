5a4abcf993fac75b7add47aa4ad3e901cf00752e
Jaiden Mispy <jaiden@mispy.me>
2019-02-27 16:25:21 -0800

    Set is_indexable as part of migration

--- db/migration/1551312762103-ChartsIsIndexable.ts
+++ db/migration/1551312762103-ChartsIsIndexable.ts
@@ -1,9 +1,18 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
+import _ = require('lodash')
+import { PUBLIC_TAG_PARENT_IDS } from "settings";
 
 export class ChartsIsIndexable1551312762103 implements MigrationInterface {
-
     public async up(queryRunner: QueryRunner): Promise<any> {
-        queryRunner.query("alter table charts ADD is_indexable BOOLEAN NOT NULL DEFAULT FALSE")
+        await queryRunner.query("alter table charts ADD is_indexable BOOLEAN NOT NULL DEFAULT FALSE")
+
+        const chartTags = await queryRunner.query("select ct.chartId, t.parentId from chart_tags ct join tags t on ct.tagId = t.id") as { chartId: number, parentId: number }[]
+
+        for (const ct of chartTags) {
+            if (PUBLIC_TAG_PARENT_IDS.includes(ct.parentId)) {
+                await queryRunner.query("update charts set is_indexable = ? where id = ?", [true, ct.chartId])
+            }
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
