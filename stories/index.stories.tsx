68a06f51fe2875b20643f0780697ef0a58a2c213
Jaiden Mispy <jaiden@mispy.me>
2019-03-12 14:32:09 -0700

    Add Storybook

---
+++ stories/index.stories.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { FeedbackForm } from 'site/client/Feedback'
+import 'site/client/owid.scss'
+
+storiesOf('Feedback', module)
+    .add('normal', () => <FeedbackForm onDismiss={action('dismissed')}/>)
+
+// storiesOf('Button', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emoji', () => (
+//     <Button onClick={action('clicked')}>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍 💯
+//       </span>
+//     </Button>
+//   ));
