584f4c4f1d3eb3485b4ba66bc4571f3e12eb9775
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-03-29 14:13:17 +0000

    Bring back the old donation amounts

--- site/client/DonateForm.tsx
+++ site/client/DonateForm.tsx
@@ -11,19 +11,19 @@ import stripe from './stripe'
 
 type Interval = "once" | "monthly"
 
-const ONETIME_DONATION_AMOUNTS = [50,100,500,1000,10000]
-const MONTHLY_DONATION_AMOUNTS = [20,50,100,200,500]
+const ONETIME_DONATION_AMOUNTS = [10,50,100,500,1000]
+const MONTHLY_DONATION_AMOUNTS = [5,10,20,50,100]
 
-const ONETIME_DEFAULT_INDEX = 2
-const MONTHLY_DEFAULT_INDEX = 2
+const ONETIME_DEFAULT_INDEX = 1
+const MONTHLY_DEFAULT_INDEX = 1
 
 const MIN_DONATION = 1
 const MAX_DONATION = 100000
 
 @observer
 export class DonateForm extends React.Component {
-    @observable interval: Interval = "once"
-    @observable presetAmount?: number = ONETIME_DONATION_AMOUNTS[ONETIME_DEFAULT_INDEX]
+    @observable interval: Interval = "monthly"
+    @observable presetAmount?: number = MONTHLY_DONATION_AMOUNTS[MONTHLY_DEFAULT_INDEX]
     @observable customAmount: string = ""
     @observable isCustom: boolean = false
     @observable name: string = ""
