aa728ffa24eef885bb2dd68a3d5f605e8bf8a1e3
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-04-26 18:29:39 +0100

    Add transitions to entry excerpt links

--- site/client/runEntryExcerptLinks.ts
+++ site/client/runEntryExcerptLinks.ts
@@ -2,11 +2,32 @@ import { select, selectAll } from 'd3-selection'
 
 export function runEntryExcerptLinks() {
     const container = selectAll("#entries")
+    const entryItems = container.selectAll(".entry-item")
+
+    let collapsedHeight: number
+    let zIndex: number = 100
+
     container.selectAll(".entry-item").each(function() {
-        const element = this as Element
+        const element = this as HTMLElement
         if (element.parentNode) {
-            const elementCopy = element.parentNode.insertBefore(element.cloneNode(true), element)
-            select(elementCopy as Element).classed("floating", true)
+            const elementCopy = element.parentNode.appendChild(element.cloneNode(true))
+            collapsedHeight = element.offsetHeight
+            select(elementCopy as HTMLElement)
+                .classed("floating", true)
+                .style("height", `${element.offsetHeight}px`)
+                .style("max-height", "none")
         }
+    })
+
+    container.selectAll(".entry-item-container").on("mouseenter", function() {
+        const floating = select(this).select(".floating")
+        const targetHeight = (floating.node() as HTMLElement).scrollHeight
+        floating
+            .style("height", `${targetHeight}px`)
+            .style("z-index", zIndex++)
+    })
+
+    container.selectAll(".entry-item-container").on("mouseleave", function() {
+        select(this).select(".floating").style("height", `${collapsedHeight}px`)
     })
 }
