e25104c256d55b2a5eb9a0f4038e538759e3fdec
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-06-03 23:30:28 +0100

    Upgrade Stripe dependencies

--- site/client/stripe.ts
+++ site/client/stripe.ts
@@ -1,6 +1,6 @@
 const Stripe = (window as any).Stripe
 import { STRIPE_PUBLIC_KEY } from 'settings'
 
-const stripe = Stripe ? Stripe(STRIPE_PUBLIC_KEY, { betas: ['checkout_beta_4'] }) : undefined
+const stripe = Stripe ? Stripe(STRIPE_PUBLIC_KEY) : undefined
 
 export default stripe
