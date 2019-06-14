c7fd75bacaa5c86a18555d9a9e47daf76a37f5fc
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-06-14 11:20:50 +0100

    Re-enable categories in Wordpress

--- theme/functions.php
+++ theme/functions.php
@@ -35,10 +35,12 @@ add_theme_support('post-thumbnails');
 add_post_type_support( 'page', 'excerpt' );
 
 function owid_init() {
-    unregister_taxonomy_for_object_type('post_tag', 'post');
-    unregister_taxonomy_for_object_type('post_tag', 'page');
-    unregister_taxonomy_for_object_type('category', 'post');
-    unregister_taxonomy_for_object_type('category', 'page');
+    // Re-enabling this. At the time it was removed, we decided to manage
+    // categories through the OWID Admin, but that doesn't work yet.
+    // unregister_taxonomy_for_object_type('post_tag', 'post');
+    // unregister_taxonomy_for_object_type('post_tag', 'page');
+    // unregister_taxonomy_for_object_type('category', 'post');
+    // unregister_taxonomy_for_object_type('category', 'page');
 }
 
 add_action('init', 'owid_init');
