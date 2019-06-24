23a6dec42b63b87b0a39b21dd4e52c4084a55697
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-06-24 18:13:21 +0100

    Remove separators from suggested query on 404

--- site/client/NotFoundPageMain.tsx
+++ site/client/NotFoundPageMain.tsx
@@ -3,6 +3,6 @@ import {Analytics} from './Analytics'
 export function runNotFoundPage() {
     const query = window.location.pathname.split("/")
     const searchInput = document.getElementById("search_q") as HTMLInputElement
-    if (searchInput && query.length) searchInput.value = decodeURIComponent(query[query.length-1])
+    if (searchInput && query.length) searchInput.value = decodeURIComponent(query[query.length-1]).replace(/[\-_\+|]/g, ' ')
     Analytics.logEvent("NOT_FOUND", { href: window.location.href })
 }
