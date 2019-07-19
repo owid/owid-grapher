572f389fe9badeac5a8b5cfe972aac1f40cd35a3
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-07-19 17:05:54 +0100

    Show big add country button when no country is selected

--- charts/NoData.tsx
+++ charts/NoData.tsx
@@ -1,8 +1,38 @@
 import * as React from 'react'
+import { action } from 'mobx'
+import { observer } from 'mobx-react'
+
 import { Bounds } from './Bounds'
 import { TextWrap } from './TextWrap'
-import {observer} from 'mobx-react'
 import { ChartViewContext, ChartViewContextType } from './ChartViewContext'
+import { ControlsOverlay } from './Controls'
+import { ChartConfig } from './ChartConfig'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
+import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons/faExchangeAlt'
+import { ChartView } from './ChartView'
+
+@observer
+export class Message extends React.Component<{ chart: ChartConfig, chartView: ChartView, bounds: Bounds, message?: string }> {
+    @action.bound onDataSelect() {
+        this.props.chartView.isSelectingData = true
+    }
+
+    render() {
+        const { chart, bounds, message } = this.props
+        return <div className='NoData' style={{ position: 'absolute', top: bounds.top, left: bounds.left, width: bounds.width, height: bounds.height }}>
+            <p className='message'>{message || "No available data"}</p>
+            <div className='actions'>
+                {chart.data.canAddData && <button className='action' onClick={this.onDataSelect}>
+                    <FontAwesomeIcon icon={faPlus} /> Add {chart.entityType}
+                </button>}
+                {chart.data.canChangeEntity && <button className='action' onClick={this.onDataSelect}>
+                    <FontAwesomeIcon icon={faExchangeAlt} /> Change {chart.entityType}
+                </button>}
+            </div>
+        </div>
+    }
+}
 
 @observer
 export class NoData extends React.Component<{ bounds: Bounds, message?: string }> {
@@ -11,9 +41,9 @@ export class NoData extends React.Component<{ bounds: Bounds, message?: string }
 
     render() {
         const { bounds, message } = this.props
-        const msgWrap = new TextWrap({ fontSize: this.context.chart.baseFontSize, maxWidth: bounds.width, text: message || "No available data" })
-        return <g className="NoData">
-            {msgWrap.render(bounds.x + bounds.width / 2 - msgWrap.width / 2, bounds.y + bounds.height / 2 - msgWrap.height / 2, { fill: "#444" })}
-        </g>
+        const { chart, chartView } = this.context
+        return <ControlsOverlay id='no-data'>
+            <Message chart={chart} chartView={chartView} bounds={bounds} message={message} />
+        </ControlsOverlay>
     }
 }
