f5aba37b06a78b5cc8b6bdda3776816ea6fae915
Matthieu Bergel <hello@matthieubergel.org>
2019-08-07 19:30:34 +0200

    refactor: cleanup action hooks

--- owid.php
+++ owid.php
@@ -17,30 +17,29 @@ function owid_plugin_register() {
 			plugins_url( 'build/index.js', __FILE__ ),
 			array( 'wp-plugins', 'wp-edit-post', 'wp-element', 'wp-components', 'wp-data', 'wp-compose' )
 	);
+
 	wp_register_style(
 		'owid-plugin-css',
 		plugins_url( 'src/style.css', __FILE__ )
 );
+
+	// Register custom post meta field. The content of the field will be saved
+	// separately from the serialized HTML
+	register_post_meta( 'post', 'owid_plugin_deep_link_meta_field', array(
+		'show_in_rest' => true,
+		'single' => true,
+		'type' => number,
+		) 
+	);
 }
+
+function owid_plugin_assets_enqueue() {
+	$screen = get_current_screen();
+	if( $screen->post_type === 'post' ) {
+		wp_enqueue_script( 'owid-plugin-script' );
+		wp_enqueue_style( 'owid-plugin-css' );
+	}
+}
+
 add_action( 'init', 'owid_plugin_register' );
-
-function owid_plugin_script_enqueue() {
-	wp_enqueue_script( 'owid-plugin-script' );
-}
-add_action( 'enqueue_block_editor_assets', 'owid_plugin_script_enqueue' );
-
-function owid_plugin_style_enqueue() {
-	wp_enqueue_style( 'owid-plugin-css' );
-}
-add_action( 'enqueue_block_editor_assets', 'owid_plugin_style_enqueue' );
-
-// Register custom post meta field. The content of the field will be saved
-// separately from the serialized HTML
-function owid_register_post_meta() {
-  register_post_meta( 'post', 'owid_plugin_deep_link_meta_field', array(
-      'show_in_rest' => true,
-      'single' => true,
-      'type' => 'string',
-  ) );
-}
-add_action( 'init', 'owid_register_post_meta' );
+add_action( 'enqueue_block_editor_assets', 'owid_plugin_assets_enqueue' );
