7a6f646988a83086414d22014299111b74847c8a
Matthieu Bergel <hello@matthieubergel.org>
2019-08-08 14:56:29 +0200

    feat: add full deep link to WP REST API response

--- owid.php
+++ owid.php
@@ -11,35 +11,81 @@ Plugin Name: Our World In Data
  *  https://developer.wordpress.org/block-editor/tutorials/metabox/meta-block-1-intro/
  */
 
-function owid_plugin_register() {
+function owid_plugin_register()
+{
 	wp_register_script(
-			'owid-plugin-script',
-			plugins_url( 'build/index.js', __FILE__ ),
-			array( 'wp-plugins', 'wp-edit-post', 'wp-element', 'wp-components', 'wp-data', 'wp-compose' )
+		'owid-plugin-script',
+		plugins_url('build/index.js', __FILE__),
+		array('wp-plugins', 'wp-edit-post', 'wp-element', 'wp-components', 'wp-data', 'wp-compose')
 	);
 
 	wp_register_style(
 		'owid-plugin-css',
-		plugins_url( 'src/style.css', __FILE__ )
-);
+		plugins_url('src/style.css', __FILE__)
+	);
 
 	// Register custom post meta field. The content of the field will be saved
 	// separately from the serialized HTML
-	register_post_meta( 'post', 'owid_plugin_deep_link_meta_field', array(
-		'show_in_rest' => true,
-		'single' => true,
-		'type' => 'integer',
-		) 
+	register_post_meta(
+		'post',
+		'owid_plugin_deep_link_meta_field',
+		array(
+			'show_in_rest' => true,
+			'single' => true,
+			'type' => 'integer',
+		)
 	);
 }
 
-function owid_plugin_assets_enqueue() {
+function owid_plugin_assets_enqueue()
+{
 	$screen = get_current_screen();
-	if( $screen->post_type === 'post' ) {
-		wp_enqueue_script( 'owid-plugin-script' );
-		wp_enqueue_style( 'owid-plugin-css' );
+	if ($screen->post_type === 'post') {
+		wp_enqueue_script('owid-plugin-script');
+		wp_enqueue_style('owid-plugin-css');
 	}
 }
 
-add_action( 'init', 'owid_plugin_register' );
-add_action( 'enqueue_block_editor_assets', 'owid_plugin_assets_enqueue' );
+
+
+add_action('init', 'owid_plugin_register');
+add_action('enqueue_block_editor_assets', 'owid_plugin_assets_enqueue');
+
+add_action(
+	'rest_api_init',
+	function () {
+		register_rest_field(
+			'post',
+			'deep_link',
+			[
+				'get_callback' => function (array $outerPost) {
+					$deepLink = $outerPost['link'];
+
+					// Compute the deep link if the blog post reading context is an
+					// entry (as opposed to its own page)
+
+					if (isset($outerPost['meta']['owid_plugin_deep_link_meta_field']) && $outerPost['meta']['owid_plugin_deep_link_meta_field'] !== 0) {
+
+						// Get the entry link from the sidebar plugin
+						$entryLink = get_permalink($outerPost['meta']['owid_plugin_deep_link_meta_field']);
+
+						// Checking the first block of the post (outerPost) for a reusable
+						// block (innerPost). Then get the first heading.
+						$outerPostBlocks = parse_blocks($outerPost['content']['raw']);
+						if ($outerPostBlocks[0]['blockName'] === "core/block") {
+							$innerPost = get_post($outerPostBlocks[0]['attrs']['ref']);
+							$innerPostBlocks = parse_blocks($innerPost->post_content);
+							if ($innerPostBlocks[0]['blockName'] === 'core/heading') {
+								// Manually get the id (not exposed in block attributes)
+								$heading = $innerPostBlocks[0];
+								preg_match('/id="([^"]+)/', $heading['innerHTML'], $matches);
+								$deepLink = isset($matches[1]) ? $entryLink . "#" . $matches[1] : $deepLink;
+							}
+						}
+					}
+					return $deepLink;
+				},
+			]
+		);
+	}
+);
