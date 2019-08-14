ea9031b3f1e4a995ad929c8358e6d02ca77c1433
Matthieu Bergel <hello@matthieubergel.org>
2019-08-14 14:47:41 +0200

    feat: add authors, featured media and title (raw) to API

--- owid.php
+++ owid.php
@@ -48,10 +48,66 @@ function owid_plugin_assets_enqueue()
 	}
 }
 
-
-
 add_action('init', 'owid_plugin_register');
 add_action('enqueue_block_editor_assets', 'owid_plugin_assets_enqueue');
+
+/*
+ * API fields
+ */
+
+function getDeepLink(array $outerPost)
+{
+	$deepLink = $outerPost['link'];
+
+	// Compute the deep link if the blog post reading context is an
+	// entry (as opposed to its own page)
+
+	if (isset($outerPost['meta'][READING_CONTEXT_META_FIELD]) && $outerPost['meta'][READING_CONTEXT_META_FIELD] !== 0) {
+
+		// Get the entry link from the sidebar plugin
+		$entryLink = get_permalink($outerPost['meta'][READING_CONTEXT_META_FIELD]);
+
+		// Checking the first block of the post (outerPost) for a reusable
+		// block (innerPost). Then get the first heading.
+		$outerPostBlocks = parse_blocks($outerPost['content']['raw']);
+		if ($outerPostBlocks[0]['blockName'] === "core/block") {
+			$innerPost = get_post($outerPostBlocks[0]['attrs']['ref']);
+			$innerPostBlocks = parse_blocks($innerPost->post_content);
+			if ($innerPostBlocks[0]['blockName'] === 'core/heading') {
+				// Manually get the id (not exposed in block attributes)
+				$heading = $innerPostBlocks[0];
+				preg_match('/id="([^"]+)/', $heading['innerHTML'], $matches);
+				$deepLink = isset($matches[1]) ? $entryLink . "#" . $matches[1] : $deepLink;
+			}
+		}
+	}
+	return wp_make_link_relative($deepLink);
+}
+
+function getAuthorsName(array $post)
+{
+	$authorNames = [];
+	$authors = get_coauthors($post['id']);
+	foreach ($authors as $author) {
+		$authorNames[] = $author->data->display_name;
+	}
+	return $authorNames;
+}
+
+function getFeaturedMediaPath(array $post)
+{
+	$featured_media_url = wp_get_attachment_image_url($post['featured_media'], 'medium_large');
+	if ($featured_media_url) {
+		return wp_make_link_relative($featured_media_url);
+	} else {
+		return null;
+	}
+}
+
+function getTitleRaw(array $post)
+{
+	return $post['title']['raw'];
+}
 
 add_action(
 	'rest_api_init',
@@ -59,35 +115,22 @@ add_action(
 		register_rest_field(
 			'post',
 			'deep_link',
-			[
-				'get_callback' => function (array $outerPost) {
-					$deepLink = $outerPost['link'];
-
-					// Compute the deep link if the blog post reading context is an
-					// entry (as opposed to its own page)
-
-					if (isset($outerPost['meta'][READING_CONTEXT_META_FIELD]) && $outerPost['meta'][READING_CONTEXT_META_FIELD] !== 0) {
-
-						// Get the entry link from the sidebar plugin
-						$entryLink = get_permalink($outerPost['meta'][READING_CONTEXT_META_FIELD]);
-
-						// Checking the first block of the post (outerPost) for a reusable
-						// block (innerPost). Then get the first heading.
-						$outerPostBlocks = parse_blocks($outerPost['content']['raw']);
-						if ($outerPostBlocks[0]['blockName'] === "core/block") {
-							$innerPost = get_post($outerPostBlocks[0]['attrs']['ref']);
-							$innerPostBlocks = parse_blocks($innerPost->post_content);
-							if ($innerPostBlocks[0]['blockName'] === 'core/heading') {
-								// Manually get the id (not exposed in block attributes)
-								$heading = $innerPostBlocks[0];
-								preg_match('/id="([^"]+)/', $heading['innerHTML'], $matches);
-								$deepLink = isset($matches[1]) ? $entryLink . "#" . $matches[1] : $deepLink;
-							}
-						}
-					}
-					return $deepLink;
-				},
-			]
+			['get_callback' => 'getDeepLink']
+		);
+		register_rest_field(
+			'post',
+			'authors_name',
+			['get_callback' => 'getAuthorsName']
+		);
+		register_rest_field(
+			'post',
+			'featured_media_path',
+			['get_callback' => 'getFeaturedMediaPath']
+		);
+		register_rest_field(
+			'post',
+			'title_raw',
+			['get_callback' => 'getTitleRaw']
 		);
 	}
 );
