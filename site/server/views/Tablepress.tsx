dc222523cf172bb18b6276d5400f8581f51119d4
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-08-22 12:29:56 +0100

    Handle #rowspan# in Tablepress

--- site/server/views/Tablepress.tsx
+++ site/server/views/Tablepress.tsx
@@ -1,17 +1,67 @@
 import * as React from 'react'
 
+interface Cell {
+    data: string
+    colspan: number
+    rowspan: number
+}
+
+function cell(data?: string) {
+    return {
+        data: data || "",
+        colspan: 1,
+        rowspan: 1
+    }
+}
+
+const ROWSPAN_TOKEN = '#rowspan#'
+
+function parseTable(table: string[][]): Cell[][] {
+    const resultTable: Cell[][] = []
+    table.forEach((row, r) => {
+        const resultRow: Cell[] = []
+        row.forEach((data, c) => {
+            if (data === ROWSPAN_TOKEN) {
+                let i = r - 1
+                while (i >= 0 && table[i][c] === ROWSPAN_TOKEN) {
+                    i--
+                }
+                if (i >= 0) {
+                    resultTable[i][c].rowspan++
+                } else {
+                    resultRow.push(cell())
+                }
+            } else {
+                resultRow.push(cell(data))
+            }
+        })
+        resultTable.push(resultRow)
+    })
+    return resultTable
+}
+
 export default function Tablepress(props: { data: string[][] }) {
     const {data} = props
+    const table = parseTable(data)
+    const [headerRow, ...body] = table
     return <table className="tablepress">
         <thead>
             <tr>
-                {data[0].map((title, i) => <th key={i} dangerouslySetInnerHTML={{__html: title}}/>)}
+                {headerRow.map((cell, i) => <th
+                    key={i}
+                    dangerouslySetInnerHTML={{__html: cell.data}}
+                />)}
             </tr>
         </thead>
         <tbody className="row-hover">
-            {data.slice(1).map((row, i) =>
+            {body.map((row, i) =>
                 <tr key={i}>
-                    {row.map((value, j) => <td key={j} dangerouslySetInnerHTML={{__html: value}}/>)}
+                    {row.map((cell, j) => <td
+                        key={j}
+                        colSpan={cell.colspan}
+                        rowSpan={cell.rowspan}
+                        dangerouslySetInnerHTML={{__html: cell.data}}
+                    />)}
                 </tr>
             )}
         </tbody>
