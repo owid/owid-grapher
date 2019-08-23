6d250829478de80b33c8691c2f8c0add25be295c
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-08-23 12:53:16 +0100

    Pass database credentials to mysqldump

--- scripts/exportMetadata.ts
+++ scripts/exportMetadata.ts
@@ -3,7 +3,7 @@
 import * as db from 'db/db'
 import * as fs from 'fs-extra'
 
-import { DB_NAME } from 'serverSettings'
+import { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT } from 'serverSettings'
 import { exec } from 'utils/server/serverUtil'
 
 async function dataExport() {
@@ -11,9 +11,13 @@ async function dataExport() {
 
     console.log(`Exporting database structure and metadata to /tmp/owid_metadata.sql...`)
 
+    // Expose password to mysqldump
+    // Safer than passing as an argument because it's not shown in 'ps aux'
+    process.env.MYSQL_PWD = DB_PASS
+
     // Dump all tables including schema but exclude the rows of data_values
-    await exec(`mysqldump ${DB_NAME} --ignore-table=${DB_NAME}.sessions --ignore-table=${DB_NAME}.user_invitations --ignore-table=${DB_NAME}.data_values -r /tmp/owid_metadata.sql`)
-    await exec(`mysqldump --no-data ${DB_NAME} sessions user_invitations data_values >> /tmp/owid_metadata.sql`)
+    await exec(`mysqldump -u '${DB_USER}' -h '${DB_HOST}' -P ${DB_PORT} ${DB_NAME} --ignore-table=${DB_NAME}.sessions --ignore-table=${DB_NAME}.user_invitations --ignore-table=${DB_NAME}.data_values -r /tmp/owid_metadata.sql`)
+    await exec(`mysqldump -u '${DB_USER}' -h '${DB_HOST}' -P ${DB_PORT} --no-data ${DB_NAME} sessions user_invitations data_values >> /tmp/owid_metadata.sql`)
 
     // Strip passwords
     await exec(`sed -i -e "s/bcrypt[^']*//g" /tmp/owid_metadata.sql`)
