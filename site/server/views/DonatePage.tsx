ee3ab9628af1084be11c71c8c8507cbf7aa3e46f
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-09-07 08:20:11 +0100

    Update DonatePage.tsx

--- site/server/views/DonatePage.tsx
+++ site/server/views/DonatePage.tsx
@@ -24,11 +24,11 @@ export const DonatePage = () => {
                     <div className="wrapper">
                         <div className="columns">
                             <div className="column">
-                                <p>To bring about a better future, we all need a long-run perspective on how the world is changing. That’s why we make all our work free and accessible for everyone.</p>
+                                <p>To bring about a better future, we need data and research to understand the big problems the world is facing and how to make progress against them. That’s why we make all our work free and accessible for everyone.</p>
                                 <p>We are a nonprofit. This means we rely on donations and grants to keep us going. Reader donations are essential to our work, providing us with the stability and independence we need, so we can focus on showing the data and evidence we think everyone needs to know.</p>
                                 <p>Donating is also one way to show us that you find our work helpful and valuable. Knowing this is a huge source of inspiration for our team.</p>
                                 <p>If you want to help us do more, please donate today – it will make a real difference.</p>
-                                <p>Thank you, <br />Max, Esteban, Hannah, Joe, Jaiden and Daniel</p>
+                                <p>Thank you, <br />Max, Esteban, Hannah, Joe and Daniel</p>
                                 <hr/>
                                 <p><a href="/about">About us</a> &ndash; <a href="https://ourworldindata.org/uploads/2019/02/Donations-Policy-Global-Change-Data-Lab.pdf">Donations Policy</a> &ndash; <a href="/donate/faq">Donations FAQ</a></p>
                             </div>
