5d009202abcc10207b94cd3778d6a9e0bc8739ec
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-09-11 13:17:51 +0100

    Change to new host in owid_wp_getlive.sh

--- bin/owid_wp_getlive.sh
+++ bin/owid_wp_getlive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 RSYNC="rsync -havz --progress"
-HOST="owid@terra"
+HOST="owid@owid-live"
 
 ssh $HOST "mysqldump --default-character-set=utf8mb4 owid_wordpress -r /home/owid/owid_wordpress.sql"
 $RSYNC $HOST:/home/owid/owid_wordpress.sql ~/backup/
@@ -10,5 +10,5 @@ $RSYNC --delete $HOST:/home/owid/owid.cloud/wp-content/uploads ~/owid.cloud/wp-c
 mysql -e "DROP DATABASE dev_wordpress;"
 mysql --default-character-set="utf8mb4" -e "CREATE DATABASE dev_wordpress;"
 mysql --default-character-set="utf8mb4" -D dev_wordpress -e "source ~/backup/owid_wordpress.sql;"
-echo "UPDATE wp_options SET option_value='http://localhost:8080' WHERE option_name='siteurl';" | mysql -D dev_wordpress 
-echo "UPDATE wp_options SET option_value='http://localhost:8080' WHERE option_name='home';" | mysql -D dev_wordpress 
+echo "UPDATE wp_options SET option_value='http://localhost:8080' WHERE option_name='siteurl';" | mysql -D dev_wordpress
+echo "UPDATE wp_options SET option_value='http://localhost:8080' WHERE option_name='home';" | mysql -D dev_wordpress
