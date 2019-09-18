c886bde366d7fed2644a255570d088fc0794d8ea
Matthieu Bergel <hello@matthieubergel.org>
2019-09-18 23:33:19 +0200

    refactor: merge tpl into singular.php

---
+++ web/app/themes/owid-theme/singular.php
@@ -0,0 +1,12 @@
+<?php 
+  // Redirect from admin site for live urls
+	// if (!is_preview()) {
+	// 	$url = "https://ourworldindata.org" . $_SERVER['REQUEST_URI'];
+	// 	wp_redirect($url, 302);
+	// 	exit;
+	// }
+
+	$ID = get_the_ID();
+	wp_redirect("/admin/posts/preview/$ID", 302);
+	exit;
+?>
