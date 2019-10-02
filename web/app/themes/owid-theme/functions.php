11e2532451446bb572dd071ddc91decece1708bc
Matthieu Bergel <hello@matthieubergel.org>
2019-10-02 18:51:26 +0200

    fix: remove colliding env variables in postUpdateHook

--- web/app/themes/owid-theme/functions.php
+++ web/app/themes/owid-theme/functions.php
@@ -10,31 +10,38 @@ remove_filter('template_redirect', 'redirect_canonical');
 add_filter('show_admin_bar', '__return_false');
 
 // Allow uploading SVGs
-function cc_mime_types($mimes) {
+function cc_mime_types($mimes)
+{
     $mimes['svg'] = 'image/svg+xml';
     return $mimes;
 }
 
 add_filter('upload_mimes', 'cc_mime_types');
 
-function build_static($post_ID, $post_after, $post_before) {
-	if ($post_after->post_status == "publish" || $post_before->post_status == "publish") {
-		global $wpdb;
-		$current_user = wp_get_current_user();
+function build_static($post_ID, $post_after, $post_before)
+{
+    if ($post_after->post_status == "publish" || $post_before->post_status == "publish") {
+        $current_user = wp_get_current_user();
         putenv('PATH=' . getenv('PATH') . ':/bin:/usr/local/bin:/usr/bin');
-		$cmd = "cd " . dirname(__FILE__) . "/codelink && yarn tsn scripts/postUpdatedHook.ts " . escapeshellarg($current_user->user_email) . " " . escapeshellarg($current_user->display_name) . " " . escapeshellarg($post_after->ID) . " " . escapeshellarg($post_after->post_name) . " > /tmp/wp-static.log 2>&1 &";
-		error_log($cmd);
-		exec($cmd);
-	}
+        // Unsets colliding .env variables between PHP and node apps
+        // The DB password does not collide and hence is not listed here (DB_PASS (node) vs DB_PASSWORD (php))
+        putenv('DB_HOST');
+        putenv('DB_USER');
+        putenv('DB_NAME');
+        putenv('DB_PORT');
+        $cmd = "cd " . dirname(__FILE__) . "/codelink && yarn tsn scripts/postUpdatedHook.ts " . escapeshellarg($current_user->user_email) . " " . escapeshellarg($current_user->display_name) . " " . escapeshellarg($post_after->ID) . " " . escapeshellarg($post_after->post_name) . " > /tmp/wp-static.log 2>&1 &";
+        exec($cmd);
+    }
 }
 
 add_action('post_updated', 'build_static', 10, 3);
 
 add_theme_support('post-thumbnails');
 
-add_post_type_support( 'page', 'excerpt' );
+add_post_type_support('page', 'excerpt');
 
-function owid_init() {
+function owid_init()
+{
     // Re-enabling this. At the time it was removed, we decided to manage
     // categories through the OWID Admin, but that doesn't work yet.
     // unregister_taxonomy_for_object_type('post_tag', 'post');
@@ -44,5 +51,3 @@ function owid_init() {
 }
 
 add_action('init', 'owid_init');
-
-?>
