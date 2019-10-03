d9fa03ce6a279c8c9f712c3f3b5091d0d59a449d
Matthieu Bergel <hello@matthieubergel.org>
2019-10-03 11:01:41 +0200

    refactor: reverting autosave interval to default

--- config/application.php
+++ config/application.php
@@ -94,7 +94,6 @@ Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 Config::define('DISALLOW_FILE_EDIT', true);
 // Disable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', true);
-Config::define('AUTOSAVE_INTERVAL', 600);
 
 /**
  * Debugging Settings
