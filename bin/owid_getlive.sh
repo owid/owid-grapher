b7d87bb5bc85f205bcb9a4570e6f07e858ae3764
Matthieu Bergel <hello@matthieubergel.org>
2019-10-07 16:23:13 +0200

    chore: remove getlive scripts
    Use refresh scripts from owid-wordpress repository instead

--- bin/owid_getlive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e 
-curl -Lo /tmp/owid_metadata.sql.gz https://files.ourworldindata.org/owid_metadata.sql.gz
-gunzip < /tmp/owid_metadata.sql.gz | mysql -D owid
-curl -Lo /tmp/owid_chartdata.sql.gz https://files.ourworldindata.org/owid_chartdata.sql.gz
-gunzip < /tmp/owid_chartdata.sql.gz | mysql -D owid
