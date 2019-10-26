b9ac32e606636c23bd9e12bec4ff770f2ec3c7aa
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-10-26 16:09:13 +0100

    Use deploy script in scripts/bakeSite.ts

--- scripts/bakeSite.ts
+++ scripts/bakeSite.ts
@@ -1,19 +1,5 @@
-import { SiteBaker } from 'site/server/SiteBaker'
 import * as parseArgs from 'minimist'
-import { log } from 'utils/server/log'
+import { deploy } from 'deploy/deploy'
 const argv = parseArgs(process.argv.slice(2))
 
-async function main(email: string, name: string, message: string) {
-    const baker = new SiteBaker({})
-
-    try {
-        await baker.bakeAll()
-        await baker.deploy(message || "Automated update", email, name)
-    } catch (err) {
-        log.error(err)
-    } finally {
-        baker.end()
-    }
-}
-
-main(argv._[0], argv._[1], argv._[2])
+deploy(argv._[2], argv._[0], argv._[1])
