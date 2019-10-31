4bf70a10c5b9b9632075fffaea31c8b8b77e5297
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-10-31 15:25:53 +0000

    Allow supplying a file path cli param to exportChartData.ts for consistency

--- scripts/exportChartData.ts
+++ scripts/exportChartData.ts
@@ -2,15 +2,16 @@
 
 import * as db from 'db/db'
 import * as _ from 'lodash'
-import * as settings from 'settings'
+import * as parseArgs from 'minimist'
 
 import { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT } from 'serverSettings'
 import { exec } from 'utils/server/serverUtil'
 
+const argv = parseArgs(process.argv.slice(2))
+const filePath = argv._[0] || "/tmp/owid_chartdata.sql"
+
 async function dataExport() {
     await db.connect()
-
-    const tmpFile = "/tmp/owid_chartdata.sql"
 
     const variablesToExportQuery = `
         SELECT DISTINCT cd.variableId FROM chart_dimensions cd
@@ -19,9 +20,9 @@ async function dataExport() {
 
     const variableIds = (await db.query(variablesToExportQuery)).map((row: any) => row.variableId)
 
-    console.log(`Exporting data for ${variableIds.length} variables to ${tmpFile}`)
+    console.log(`Exporting data for ${variableIds.length} variables to ${filePath}`)
 
-    await exec(`rm -f ${tmpFile}`)
+    await exec(`rm -f ${filePath}`)
 
     // Expose password to mysqldump
     // Safer than passing as an argument because it's not shown in 'ps aux'
@@ -29,7 +30,7 @@ async function dataExport() {
 
     let count = 0
     for (const chunk of _.chunk(variableIds, 100)) {
-        await exec(`mysqldump --default-character-set=utf8mb4 --no-create-info -u '${DB_USER}' -h '${DB_HOST}' -P ${DB_PORT} ${DB_NAME} data_values --where="variableId IN (${chunk.join(",")})" >> ${tmpFile}`)
+        await exec(`mysqldump --default-character-set=utf8mb4 --no-create-info -u '${DB_USER}' -h '${DB_HOST}' -P ${DB_PORT} ${DB_NAME} data_values --where="variableId IN (${chunk.join(",")})" >> ${filePath}`)
 
         count += chunk.length
         console.log(count)
