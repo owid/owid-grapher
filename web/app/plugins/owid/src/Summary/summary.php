ced81ca63e56b9ba9ea55f7bc3726ae39b5fc3b5
Matthieu Bergel <hello@matthieubergel.org>
2019-11-25 17:21:03 +0100

    refactor: remove hydration PoC

--- web/app/plugins/owid/src/Summary/summary.php
+++ web/app/plugins/owid/src/Summary/summary.php
@@ -4,14 +4,12 @@ namespace OWID\blocks\summary;
 
 function render($attributes, $content)
 {
-  $block = <<<EOD
-	<block type="owid-summary">
-		<attributes>
-			<title>Summary</title>
-		</attributes>
-		<content>$content</content>
-	</block>
+	$block = <<<EOD
+	<div class="wp-block-owid-summary">
+		<h2>Summary</h2>
+		$content
+	</div>
 EOD;
 
-  return $block;
+	return $block;
 }
