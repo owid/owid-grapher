43be1afd03c2fef25e2f17d236d2111522af10b8
Matthieu Bergel <hello@matthieubergel.org>
2019-11-27 18:46:14 +0100

    refactor(additional info): replace custom block by InnerBlocks template

--- web/app/plugins/owid/src/AdditionalInformation/AdditionalInformation.js
+++ web/app/plugins/owid/src/AdditionalInformation/AdditionalInformation.js
@@ -1,11 +1,25 @@
-import { InnerBlocks, RichText } from "@wordpress/block-editor";
-import { createBlock } from "@wordpress/blocks";
-import MediaContainer from "../MediaContainer/MediaContainer";
+import { InnerBlocks } from "@wordpress/block-editor";
 
 const blockStyle = {
   border: "1px dashed lightgrey",
   padding: "0 1rem"
 };
+
+const TEMPLATE = [
+  ["core/heading", { level: 3 }],
+  [
+    "core/columns",
+    {},
+    [
+      ["core/column", { width: 25 }, [["core/image"]]],
+      [
+        "core/column",
+        { width: 75 },
+        [["core/paragraph", { placeholder: "Enter side content..." }]]
+      ]
+    ]
+  ]
+];
 
 const AdditionalInformation = {
   title: "Additional information",
@@ -14,69 +28,10 @@ const AdditionalInformation = {
   supports: {
     html: false
   },
-  attributes: {
-    title: {
-      type: "string"
-    },
-    mediaId: {
-      type: "integer"
-    },
-    mediaUrl: {
-      type: "string"
-    },
-    mediaAlt: {
-      type: "string"
-    }
-  },
-  transforms: {
-    from: [
-      {
-        type: "block",
-        blocks: ["core/paragraph"],
-        transform: ({ content }) => {
-          return createBlock("owid/additional-information", {}, [
-            createBlock("core/paragraph", { content })
-          ]);
-        }
-      }
-    ]
-  },
-  edit: ({
-    attributes: { title, mediaId, mediaUrl, mediaAlt },
-    setAttributes
-  }) => {
+  edit: () => {
     return (
       <div style={blockStyle}>
-        <RichText
-          tagName="h3"
-          value={title}
-          onChange={newTitle => {
-            setAttributes({ title: newTitle });
-          }}
-          placeholder="Write heading..."
-        />
-        <div style={{ display: "flex" }}>
-          <div style={{ flex: "1 0 40%", marginRight: "1rem" }}>
-            <MediaContainer
-              onSelectMedia={media => {
-                // Try the "large" size URL, falling back to the "full" size URL below.
-                // const src = get( media, [ 'sizes', 'large', 'url' ] ) || get( media, [ 'media_details', 'sizes', 'large', 'source_url' ] );
-                setAttributes({
-                  mediaId: media.id,
-                  // mediaUrl: src || media.url,
-                  mediaUrl: media.url,
-                  mediaAlt: media.alt
-                });
-              }}
-              mediaId={mediaId}
-              mediaUrl={mediaUrl}
-              mediaAlt={mediaAlt}
-            />
-          </div>
-          <div style={{ flex: "1 0 60%" }}>
-            <InnerBlocks />
-          </div>
-        </div>
+        <InnerBlocks template={TEMPLATE} />
       </div>
     );
   },
--- web/app/plugins/owid/src/AdditionalInformation/additional-information.php
+++ web/app/plugins/owid/src/AdditionalInformation/additional-information.php
@@ -4,19 +4,8 @@ namespace OWID\blocks\additional_information;
 
 function render($attributes, $content)
 {
-	$title = $attributes['title'] ? $attributes['title'] : 'Additional information';
-
-	$figure = null;
-	if (!empty($attributes['mediaUrl'])) {
-		$figure = '<figure>' . wp_get_attachment_image($attributes['mediaId'], 'medium_large') . '</figure>';
-	}
-
 	$block = <<<EOD
 	<block type="additional-information">
-		<attributes>
-			<title>$title</title>
-			$figure
-		</attributes>
 		<content>$content</content>
 	</block>
 EOD;
