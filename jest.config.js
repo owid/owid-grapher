48c98a1b55ec342a1e6cb50ff1febbc89f60dda0
Jason Crawford <jason@jasoncrawford.org>
2019-11-27 13:28:38 -0800

    Add Enzyme and a simple test for ExplorePage
    This is a stub test for a stub component, but it gives us a pattern for
    testing React components going forward

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     preset: "ts-jest",
-    roots: ["test"],
     testEnvironment: "node",
     moduleNameMapper: {
         "^(admin|site|charts|utils|db|settings)/(.*)$": "<rootDir>/$1/$2",
         "^settings$": "<rootDir>/settings",
         "^serverSettings$": "<rootDir>/serverSettings"
-    }
+    },
+    setupFilesAfterEnv: ['<rootDir>/test/enzymeSetup.ts']
 }
