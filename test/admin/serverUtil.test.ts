732506a68e2ae6c812c6479e5b7723d12fb97b55
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-11-29 14:01:43 +0000

    Suppress console output in exec() tests

--- test/admin/serverUtil.test.ts
+++ test/admin/serverUtil.test.ts
@@ -3,15 +3,19 @@ const { exec } = require("utils/server/serverUtil")
 describe("serverUtil", () => {
     describe("exec()", () => {
         it("should resolve when there is a zero exit code", () => {
-            expect(exec(`echo "it works"; exit 0`)).resolves.toEqual(
-                "it works\n"
-            )
+            expect(
+                exec(`echo "it works"; exit 0`, {
+                    silent: true
+                })
+            ).resolves.toEqual("it works\n")
         })
 
         it("should reject when there is a non-zero exit code", () => {
-            expect(exec(`echo "does not work"; exit 1`)).rejects.toBeInstanceOf(
-                Error
-            )
+            expect(
+                exec(`echo "does not work"; exit 1`, {
+                    silent: true
+                })
+            ).rejects.toBeInstanceOf(Error)
         })
     })
 })
