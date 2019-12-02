41c8457518ab8d8af62cd31a55e2ec6f428db827
Jason Crawford <jason@jasoncrawford.org>
2019-12-02 11:36:46 -0800

    Render a chart in the ExploreView
    Based on hardcoded dummy chart config for now.

--- charts/ExploreView.tsx
+++ charts/ExploreView.tsx
@@ -1,15 +1,34 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
+import { Bounds } from "./Bounds"
+import { ChartView } from "./ChartView"
+import { ChartConfig } from "./ChartConfig"
+
+// Hardcoding some dummy config for now so we can display a chart.
+// There will eventually be a list of these, downloaded from a static JSON file.
+// -@jasoncrawford 2 Dec 2019
+const DUMMY_JSON_CONFIG = {
+  "id":677,
+  "title":"Child mortality rate",
+  "subtitle":"Share of newborns who die before reaching the age of five.",
+  "sourceDesc":"IHME, Global Burden of Disease",
+  "note":"",
+  "dimensions":[{"display":{},"property":"y","variableId":104402}],
+  "selectedData":[{"index":0,"entityId":355}],
+}
+
 export class ExploreView extends React.Component {
   static bootstrap({containerNode}: {containerNode: HTMLElement}) {
-    return ReactDOM.render(<ExploreView/>, containerNode)
+    const rect = containerNode.getBoundingClientRect()
+    const bounds = Bounds.fromRect(rect)
+    return ReactDOM.render(<ExploreView bounds={bounds}/>, containerNode)
   }
 
   render() {
-    return <blockquote>
-      “If you want to build a ship, don't drum up the men to gather wood, divide the work, and give orders.
-      Instead, teach them to yearn for the vast and endless sea.”
-    </blockquote>
+    const chart = new ChartConfig(DUMMY_JSON_CONFIG)
+    return <div>
+      <ChartView chart={chart} bounds={this.props.bounds}/>
+    </div>
   }
 }
