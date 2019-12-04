b2fade965c3c050dc1a6364c072315d62aaddd9b
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-12-04 14:46:25 +0000

    Update tests for exec()

--- test/admin/serverUtil.test.ts
+++ test/admin/serverUtil.test.ts
@@ -2,20 +2,23 @@ const { exec } = require("utils/server/serverUtil")
 
 describe("serverUtil", () => {
     describe("exec()", () => {
-        it("should resolve when there is a zero exit code", () => {
-            expect(
-                exec(`echo "it works"; exit 0`, {
-                    silent: true
-                })
-            ).resolves.toEqual("it works\n")
+        it("should resolve when there is a zero exit code", async () => {
+            const result = await exec(`echo "it works"; exit 0`, {
+                silent: true
+            })
+            expect(result).toEqual("it works\n")
         })
 
-        it("should reject when there is a non-zero exit code", () => {
-            expect(
-                exec(`echo "does not work"; exit 1`, {
+        it("should reject when there is a non-zero exit code", async () => {
+            try {
+                await exec(`echo "does not work"; exit 1`, {
                     silent: true
                 })
-            ).rejects.toBeInstanceOf(Error)
+            } catch (err) {
+                expect(err).toBeInstanceOf(Error)
+            } finally {
+                expect.assertions(1)
+            }
         })
     })
 })
