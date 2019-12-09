5c55dbacb041d7e733800c4299826f4ffbda50d3
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-12-09 16:26:49 +0000

    Force all charts listed in spreadsheet to be explorable

--- utils/charts.ts
+++ utils/charts.ts
@@ -21,18 +21,68 @@ export const EXPLORABLE_CHART_TYPES: ChartTypeType[] = [
 // In order to avoid remarking the charts, I've added some chart IDs below to be
 // forced to be explorable, even if their config does not have the flag set.
 //
+// IDs extracted from the indicators spreadsheet:
+// https://docs.google.com/spreadsheets/d/1jo4fJ64GtM3xBK7o3necYCsR-a4fczbxfPeGg0gia34/edit#gid=0
+//
 // -@danielgavrilov 2019-12-09
 export const FORCE_EXPLORABLE_CHART_IDS: number[] = [
+    66,
+    132,
+    133,
+    230,
+    299,
+    389,
     421,
+    471,
     488,
+    586,
     598,
     677,
+    699,
+    703,
     712,
+    762,
+    792,
+    794,
+    844,
     970,
     1068,
     1372,
+    1375,
+    1491,
+    1500,
+    1546,
+    1547,
     1596,
-    2285
+    1917,
+    1965,
+    1966,
+    2285,
+    2286,
+    2469,
+    2699,
+    2726,
+    2836,
+    2837,
+    2968,
+    3401,
+    3482,
+    3485,
+    3487,
+    3553,
+    3570,
+    3594,
+    3603,
+    3772,
+    3798,
+    3799,
+    3800,
+    3801,
+    3872,
+    3873,
+    3874,
+    3875,
+    3876
 ]
 
 // A centralized predicate to test whether a chart can be explorable.
