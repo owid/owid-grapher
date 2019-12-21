6a908119b219f6f7f927dc20f9765a1582a36425
Matthieu Bergel <hello@matthieubergel.org>
2019-12-21 11:12:11 +0100

    chore: turn on graphql debugging on dev environment

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Configuration overrides for WP_ENV === 'development'
  */
@@ -10,6 +11,7 @@ Config::define('WP_DEBUG', true);
 Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
+Config::define('GRAPHQL_DEBUG', true);
 
 ini_set('display_errors', '1');
 
