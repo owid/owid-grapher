042654ba4de0e241677f250070234dce4706afc1
Matthieu Bergel <hello@matthieubergel.org>
2020-01-24 17:09:46 +0100

    refactor(sidebar): use IntersectionObserver for active item

--- site/client/TableOfContents.tsx
+++ site/client/TableOfContents.tsx
@@ -12,12 +12,29 @@ interface TableOfContentsData {
     pageTitle: string
 }
 
+const isRecordTopViewport = (record: IntersectionObserverEntry) => {
+    return (
+        record.rootBounds &&
+        record.boundingClientRect.top < record.rootBounds.height / 2
+    )
+}
+
+const getPreviousHeading = (
+    nextHeadingRecord: IntersectionObserverEntry | undefined,
+    previousHeadings: Array<{ slug: string; previous: string | null }>
+) => {
+    return previousHeadings.find(
+        heading => heading.slug === nextHeadingRecord?.target.id
+    )?.previous
+}
+
 export const TableOfContents = ({
     headings,
     pageTitle
 }: TableOfContentsData) => {
     const [isToggled, setIsToggled] = useState(false)
     const [isSticky, setIsSticky] = useState(false)
+    const [activeHeading, setActiveHeading] = useState("")
     const tocRef = useRef<HTMLElement>(null)
     const stickySentinelRef = useRef<HTMLDivElement>(null)
 
@@ -60,6 +77,67 @@ export const TableOfContents = ({
         }
     }, [])
 
+    useEffect(() => {
+        const previousHeadings = headings.map((heading, i) => ({
+            slug: heading.slug,
+            previous: i > 0 ? headings[i - 1].slug : null
+        }))
+
+        let currentHeadingRecord: IntersectionObserverEntry | undefined
+        let init = true
+
+        const observer = new IntersectionObserver(
+            records => {
+                let nextHeadingRecord: IntersectionObserverEntry | undefined
+
+                // Target headings going down
+                currentHeadingRecord = records.find(
+                    record =>
+                        // filter out records no longer intersecting (triggering on exit)
+                        record.isIntersecting &&
+                        // filter out records fully in the page (upcoming section)
+                        record.intersectionRatio !== 1 &&
+                        // filter out intersections happening at the bottom of the viewport
+                        isRecordTopViewport(record)
+                )
+
+                if (currentHeadingRecord) {
+                    setActiveHeading(currentHeadingRecord.target.id)
+                } else {
+                    // Target headings going up
+                    nextHeadingRecord = records.find(
+                        record =>
+                            isRecordTopViewport(record) &&
+                            record.intersectionRatio === 1
+                    )
+                    if (nextHeadingRecord) {
+                        setActiveHeading(
+                            getPreviousHeading(
+                                nextHeadingRecord,
+                                previousHeadings
+                            ) || ""
+                        )
+                    } else if (init) {
+                        currentHeadingRecord = records
+                            .reverse()
+                            .find(record => record.boundingClientRect.top < 0)
+                        setActiveHeading(currentHeadingRecord?.target.id || "")
+                    }
+                }
+                init = false
+            },
+            {
+                rootMargin: "-10px", // 10px offset to trigger intersection when landing exactly at the border when clicking an anchor
+                threshold: new Array(11).fill(0).map((v, i) => i / 10)
+            }
+        )
+
+        const contentHeadings = document.querySelectorAll("h2, h3")
+        contentHeadings.forEach(contentHeading => {
+            observer.observe(contentHeading)
+        })
+    }, [])
+
     return (
         <aside
             ref={tocRef}
@@ -83,7 +161,13 @@ export const TableOfContents = ({
                 </button>
                 <ul>
                     <li>
-                        <a onClick={toggle} href="#">
+                        <a
+                            onClick={() => {
+                                toggle()
+                                setActiveHeading("")
+                            }}
+                            href="#"
+                        >
                             {pageTitle}
                         </a>
                     </li>
@@ -91,7 +175,12 @@ export const TableOfContents = ({
                         <li
                             key={i}
                             className={
-                                heading.isSubheading ? "subsection" : "section"
+                                (heading.isSubheading
+                                    ? "subsection"
+                                    : "section") +
+                                (heading.slug === activeHeading
+                                    ? " active"
+                                    : "")
                             }
                         >
                             <a onClick={toggle} href={`#${heading.slug}`}>
