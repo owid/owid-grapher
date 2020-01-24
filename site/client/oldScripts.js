b02febe64bacf20e7326f767661ba31f3a0e94d1
Matthieu Bergel <hello@matthieubergel.org>
2020-01-24 17:33:38 +0100

    refactor(toc): remove old scroll and resize listeners

--- site/client/oldScripts.js
+++ site/client/oldScripts.js
@@ -1,59 +1,6 @@
 window.$ = window.jQuery = require("jquery")
 require("./tooltip.js")
 
-// Construct scroll-friendly nav sidebar for entries. Should be idempotent so it
-// can be called again to reconstruct on window resize
-// var OWIDScrollNav = function() {
-//     var $page = $("article.page"),
-//         $sidebar = $(".entry-sidebar")
-
-//     if (!$page.length || !$sidebar.length || $(".no-sidebar").length) return
-
-//     // Don't make sidebar unless there are enough headings
-//     if ($page.find("h2").length < 2) return
-
-//     // Cleanup any existing stuff
-//     $sidebar.attr("style", "")
-//     $(window).off("scroll.toc")
-
-//     // Keep track of sections so we can find the closest one
-//     var headings = $(
-//         ".article-header h1, .article-content h2, .article-content h3"
-//     ).map(function(i, el) {
-//         return $(el)
-//     })
-
-//     var currentHeadingIndex = null
-//     var onScroll = function() {
-//         var scrollTop = $(document).scrollTop()
-
-//         // Figure out where in the document we are
-//         var lastHeadingIndex = null
-//         headings.each(function(i, $heading) {
-//             // HACK (Mispy): The +50 is so being right on top of the heading after you
-//             // click a link in the TOC still counts as being under it
-//             if ($heading.offset().top <= scrollTop + 50) lastHeadingIndex = i
-//         })
-
-//         if (lastHeadingIndex != currentHeadingIndex) {
-//             $sidebar.find("li.active").removeClass("active")
-//             currentHeadingIndex = lastHeadingIndex
-//             if (currentHeadingIndex !== null)
-//                 $sidebar
-//                     .find("li")
-//                     .eq(currentHeadingIndex)
-//                     .addClass("active")
-//         }
-//     }
-
-//     onScroll()
-//     $(window).on("scroll.toc", onScroll)
-// }
-
-// OWIDScrollNav()
-
-// $(window).on("resize.toc", OWIDScrollNav)
-
 // Don't let user enter empty search query
 $(".search-form").on("submit", function(evt) {
     if (
