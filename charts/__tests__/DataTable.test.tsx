d0edd31ca8cd4ee8e3a29bea9be31c4e5bce8c59
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-01-26 15:20:14 +0000

    Fix old DataTable tests
    And leave some todos. Will focus on tests later.

--- charts/__tests__/DataTable.test.tsx
+++ charts/__tests__/DataTable.test.tsx
@@ -29,9 +29,14 @@ describe(DataTable, () => {
             expect(view.find("thead th.entity").text()).toBe("Country")
         })
 
-        it("renders a variable header", () => {
-            const cell = view.find("thead th.dimension")
-            expect(cell.text()).toBe("Child mortality")
+        it("renders a variable name in header", () => {
+            const cell = view.find("thead th.dimension .name")
+            expect(cell.text()).toContain("Child mortality")
+        })
+
+        it("renders a unit name in header", () => {
+            const cell = view.find("thead th.dimension .unit")
+            expect(cell.text()).toContain("percent")
         })
 
         it("renders a row for each country", () => {
@@ -40,13 +45,17 @@ describe(DataTable, () => {
 
         it("renders the name of each country", () => {
             const cell = view.find("tbody tr td.entity").first()
-            expect(cell.text()).toBe("Andorra")
+            expect(cell.text()).toBe("Afghanistan")
         })
 
         it("renders the value for each country", () => {
             const cell = view.find("tbody tr td.dimension").first()
-            expect(cell.text()).toBe("0.21%")
+            expect(cell.text()).toBe("5.58%")
         })
+
+        test.todo("renders the unit next to the value when the unit is '%'")
+        test.todo("renders start and end values if a range is selected")
+        test.todo("renders the unit in the header when it isn't '%'")
     })
 
     describe("when the table doesn't have data for all rows", () => {
@@ -55,14 +64,15 @@ describe(DataTable, () => {
             view = shallow(<DataTable chart={chart} />)
         })
 
-        it("omits empty rows", () => {
-            expect(view.find("tbody tr")).toHaveLength(13)
-        })
-
         it("renders data in every row", () => {
             const cells = view.find("tbody tr td.dimension")
             const texts = cells.map(td => td.text())
             expect(texts.filter(v => v.length === 0)).toHaveLength(0)
         })
+
+        test.todo("renders the closest value within the specified tolerance")
+        test.todo(
+            "fades out rows that don't have data or are outside the tolerance"
+        )
     })
 })
