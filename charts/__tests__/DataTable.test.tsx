66441017bddf72368f36d854009c52c67ea2e053
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-02-26 12:21:02 +0000

    Add DataTable tests

--- charts/__tests__/DataTable.test.tsx
+++ charts/__tests__/DataTable.test.tsx
@@ -1,12 +1,19 @@
 import * as React from "react"
 import { shallow, ShallowWrapper } from "enzyme"
 
-import { DataTable } from "../DataTable"
+import { DataTable, ClosestYearNotice } from "../DataTable"
 import { ChartConfig, ChartConfigProps } from "../ChartConfig"
 import * as fixtures from "test/fixtures"
 
-function setupChart(id: number, varId: number) {
-    const props = new ChartConfigProps(fixtures.readChart(id))
+function setupChart(
+    id: number,
+    varId: number,
+    configOverrides?: Partial<ChartConfigProps>
+) {
+    const props = new ChartConfigProps({
+        ...fixtures.readChart(id),
+        ...configOverrides
+    })
     const chart = new ChartConfig(props)
     chart.vardata.receiveData(fixtures.readVariable(varId))
     return chart
@@ -39,6 +46,11 @@ describe(DataTable, () => {
             expect(cell.text()).toContain("percent")
         })
 
+        it("renders 'percent' in the column header when unit is '%'", () => {
+            const cell = view.find("thead th.dimension .unit")
+            expect(cell.text()).toContain("percent")
+        })
+
         it("renders a row for each country", () => {
             expect(view.find("tbody tr")).toHaveLength(224)
         })
@@ -53,9 +65,51 @@ describe(DataTable, () => {
             expect(cell.text()).toBe("5.58%")
         })
 
-        test.todo("renders the unit next to the value when the unit is '%'")
-        test.todo("renders start and end values if a range is selected")
-        test.todo("renders the unit in the header when it isn't '%'")
+        it("renders the unit in cell values when the unit is '%'", () => {
+            const cell = view.find("tbody tr td.dimension").first()
+            expect(cell.text()).toContain("%")
+        })
+    })
+
+    describe("when you select a range of years", () => {
+        beforeAll(() => {
+            const chart = setupChart(677, 104402, {
+                type: "LineChart",
+                tab: "chart",
+                minTime: 1990,
+                maxTime: 2017
+            })
+            view = shallow(<DataTable chart={chart} />)
+        })
+
+        it("header is split into two rows", () => {
+            expect(view.find("thead tr")).toHaveLength(2)
+        })
+
+        it("entity header cell spans 2 rows", () => {
+            const cell = view.find("thead .entity").first()
+            expect(cell.prop("rowSpan")).toBe(2)
+        })
+
+        it("renders start values", () => {
+            const cell = view.find("tbody .dimension-start").first()
+            expect(cell.text()).toBe("14.76% in 1990")
+        })
+
+        it("renders end values", () => {
+            const cell = view.find("tbody .dimension-end").first()
+            expect(cell.text()).toBe("5.58% in 2017")
+        })
+
+        it("renders absolute change values", () => {
+            const cell = view.find("tbody .dimension-delta").first()
+            expect(cell.text()).toBe("-9.18%")
+        })
+
+        it("renders relative change values", () => {
+            const cell = view.find("tbody .dimension-deltaRatio").first()
+            expect(cell.text()).toBe("-62%")
+        })
     })
 
     describe("when the table doesn't have data for all rows", () => {
@@ -64,9 +118,21 @@ describe(DataTable, () => {
             view = shallow(<DataTable chart={chart} />)
         })
 
-        test.todo("renders the closest value within the specified tolerance")
-        test.todo(
-            "fades out rows that don't have data or are outside the tolerance"
-        )
+        it("renders no value when data is not available for years within the tolerance", () => {
+            const cell = view
+                .find("tbody .dimension")
+                .at(1)
+                .first()
+            expect(cell.text()).toBe("")
+        })
+
+        it("renders a tolerance notice when data is not from targetYear", () => {
+            const notice = view
+                .find("tbody tr td.dimension")
+                .first()
+                .find(ClosestYearNotice)
+            expect(notice.prop("year")).toBe(2013)
+            expect(notice.prop("targetYear")).toBe(2016)
+        })
     })
 })
