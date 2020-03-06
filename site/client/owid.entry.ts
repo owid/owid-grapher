452deff04018acb364f9f4ddc41fd725f98d7ba8
Breck Yunits <breck7@gmail.com>
2020-03-06 09:52:34 -1000

    Removed unreachable search tracker code and probably always reachable trackedLinkExists check

--- site/client/owid.entry.ts
+++ site/client/owid.entry.ts
@@ -55,51 +55,21 @@ new SmoothScroll('a[href*="#"][data-smooth-scroll]', {
     popstate: false
 })
 
-const search = document.querySelector("form#search-nav") as HTMLFormElement
-if (search) {
-    // Todo: Not seeing this event in Amplitude. Can this be removed?
-    const input = search.querySelector("input[type=search]") as HTMLInputElement
-    search.addEventListener("submit", ev => {
-        ev.preventDefault()
-        Analytics.logEvent("OWID_SITE_SEARCH", { query: input.value })
-            .then(() => search.submit())
-            .catch(() => search.submit())
-    })
-}
-
-const trackedLinkExists: boolean = !!document.querySelector(
-    "[data-track-click]"
-)
-
-function createFunctionWithTimeout(callback: () => void, timeout: number = 50) {
-    let called = false
-    function fn() {
-        if (!called) {
-            called = true
-            callback()
-        }
+document.addEventListener("click", async ev => {
+    const targetElement = ev.target as HTMLElement
+    const trackedElement = getParent(
+        targetElement,
+        (el: HTMLElement) => el.getAttribute("data-track-click") !== null
+    )
+    if (trackedElement) {
+        // Note that browsers will cancel all pending requests once a user
+        // navigates away from a page. An earlier implementation had a
+        // timeout to send the event before navigating, but it broke
+        // CMD+CLICK for opening a new tab.
+        Analytics.logEvent("OWID_SITE_CLICK", {
+            text: trackedElement.innerText,
+            href: trackedElement.getAttribute("href"),
+            note: trackedElement.getAttribute("data-track-note")
+        })
     }
-    setTimeout(fn, timeout)
-    return fn
-}
-
-if (trackedLinkExists) {
-    document.addEventListener("click", async ev => {
-        const targetElement = ev.target as HTMLElement
-        const trackedElement = getParent(
-            targetElement,
-            (el: HTMLElement) => el.getAttribute("data-track-click") != null
-        )
-        if (trackedElement) {
-            // Note that browsers will cancel all pending requests once a user
-            // navigates away from a page. An earlier implementation had a
-            // timeout to send the event before navigating, but it broke
-            // CMD+CLICK for opening a new tab.
-            Analytics.logEvent("OWID_SITE_CLICK", {
-                text: trackedElement.innerText,
-                href: trackedElement.getAttribute("href"),
-                note: trackedElement.getAttribute("data-track-note")
-            })
-        }
-    })
-}
+})
