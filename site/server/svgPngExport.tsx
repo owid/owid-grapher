ad3b525fe25ed6e326b84820a28e065556a90490
Marcel Gerber <mgerber59@gmail.com>
2020-03-31 15:27:20 +0200

    Minify svg exports using svgo

--- site/server/svgPngExport.tsx
+++ site/server/svgPngExport.tsx
@@ -1,12 +1,25 @@
 import * as fs from "fs-extra"
 import * as sharp from "sharp"
 import * as path from "path"
+import * as svgo from "svgo"
 
 declare var global: any
 global.window = { location: { search: "" } }
 global.App = { isEditor: false }
 
 import { ChartConfig, ChartConfigProps } from "charts/ChartConfig"
+
+const svgoConfig: svgo.Options = {
+    floatPrecision: 1,
+    plugins: [{ collapseGroups: false }, { removeViewBox: false }]
+}
+
+const SVGO = new svgo(svgoConfig)
+
+async function optimizeSvg(svgString: string): Promise<string> {
+    const optimizedSvg = await SVGO.optimize(svgString)
+    return optimizedSvg.data
+}
 
 export async function chartToSVG(
     jsonConfig: ChartConfigProps,
@@ -28,9 +41,11 @@ export async function bakeImageExports(
     chart.vardata.receiveData(vardata)
     const outPath = path.join(outDir, chart.props.slug as string)
 
+    const optimizedSvg = await optimizeSvg(chart.staticSVG)
+
     return Promise.all([
         fs
-            .writeFile(`${outPath}.svg`, chart.staticSVG)
+            .writeFile(`${outPath}.svg`, optimizedSvg)
             .then(_ => console.log(`${outPath}.svg`)),
         sharp(Buffer.from(chart.staticSVG), { density: 144 })
             .png()
