6048b687fd997a362668217a751ad91cfe222102
Matthieu Bergel <hello@matthieubergel.org>
2020-04-07 08:15:09 +0200

    feat: display alert banner by default
    Injection via Google Optimize seems to be blocked by some ad blockers.

--- site/server/views/AlertBanner.tsx
+++ site/server/views/AlertBanner.tsx
@@ -3,14 +3,13 @@ import * as React from "react"
 export const AlertBanner = () => {
     return (
         <div className="alert-banner">
-            {/* <div className="content">
+            <div className="content">
                 <div className="text">
-                    All our data and research on the
-                    <a href="/coronavirus"> Coronavirus pandemic </a>
-                    on one page.
+                    Our daily updated research and data on the coronavirus
+                    pandemic.
                 </div>
                 <a href="/coronavirus">Read more</a>
-            </div> */}
+            </div>
         </div>
     )
 }
