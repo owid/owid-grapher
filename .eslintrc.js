6d600f139a2d46b8d071e1f84eb62dbe280a52e3
Marcel Gerber <mgerber59@gmail.com>
2020-04-09 21:52:00 +0200

    Disable most rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
     extends: [
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
+        "prettier",
+        "prettier/react",
+        "prettier/@typescript-eslint"
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
@@ -15,25 +16,31 @@ module.exports = {
         sourceType: "module"
     },
     rules: {
-        "@typescript-eslint/camelcase": "warn",
+        // These rules are only preliminary to ease the migration from tslint, many of them might make sense to have enabled!
+        "@typescript-eslint/camelcase": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/interface-name-prefix": "warn",
-        "@typescript-eslint/no-empty-function": "warn",
+        "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-empty-interface": "warn",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-inferrable-types": "off",
-        "@typescript-eslint/no-namespace": "warn",
-        "@typescript-eslint/no-this-alias": "warn",
-        "@typescript-eslint/no-use-before-define": "warn",
-        "@typescript-eslint/no-var-requires": "warn",
-        "no-var": "warn",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-this-alias": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            { argsIgnorePattern: "^(res|req)$" }
+        ],
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "no-var": "off",
         "prefer-const": ["warn", { destructuring: "all" }],
-        "react/display-name": "warn",
-        "react/jsx-key": "warn",
-        "react/jsx-no-target-blank": "warn",
-        "react/no-render-return-value": "warn",
-        "react/no-unescaped-entities": "warn",
-        "react/prop-types": "warn"
+        "react/display-name": "off",
+        "react/jsx-key": "off",
+        "react/jsx-no-target-blank": "off",
+        "react/no-render-return-value": "off",
+        "react/no-unescaped-entities": "off",
+        "react/prop-types": "off"
     },
     settings: {
         react: {
