8aecae46c5428b599b83681e23cb3c8ccb9b48ea
Shahid Ahmad <ahmad.s.shahid@gmail.com>
2020-04-16 18:36:25 -0400

    empty test file

---
+++ charts/__tests__/DataTab.test.tsx
@@ -0,0 +1,10 @@
+#! /usr/bin/env jest
+import fs = require("fs")
+import path = require("path")
+import { encode } from "entities"
+
+describe("DataTab", () => {
+    test("shahid", () => {
+        // nothing yet
+    })
+})
