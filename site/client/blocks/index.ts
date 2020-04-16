7f1cffdc7ec4f126bf1290c2a46a28f978310233
Matthieu Bergel <hello@matthieubergel.org>
2020-04-16 10:42:16 +0200

    feat(help): hide on charts not interactive

--- site/client/blocks/index.ts
+++ site/client/blocks/index.ts
@@ -1,11 +1,19 @@
 import { hydrate as hydrateAdditionalInformation } from "./AdditionalInformation/AdditionalInformation"
 import { render as renderAdditionalInformation } from "./AdditionalInformation/AdditionalInformation"
 import { render as renderHelp } from "./Help/Help"
+import { shouldProgressiveEmbed } from "../Grapher"
 
 export const renderBlocks = ($: CheerioStatic) => {
     renderAdditionalInformation($)
     renderHelp($)
 }
 export const runBlocks = () => {
+    if (!shouldProgressiveEmbed()) {
+        // Used by Help blocks. Pierces encapsulation but considered not worth going through hydration / client side rendering for this.
+        // If hydration required for other purposes, then reassess.
+        document
+            .getElementsByTagName("body")[0]
+            .classList.add("is-not-chart-interactive")
+    }
     hydrateAdditionalInformation()
 }
