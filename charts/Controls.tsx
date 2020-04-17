1844103fa6ce9bad7f165a3cae5d1c2a15ce9c11
Marcel Gerber <mgerber59@gmail.com>
2020-04-17 22:59:00 +0200

    ShareMenu: Allow copying link to chart

--- charts/Controls.tsx
+++ charts/Controls.tsx
@@ -3,6 +3,7 @@ import { observable, computed, action } from "mobx"
 import { observer } from "mobx-react"
 import * as Cookies from "js-cookie"
 import Select, { ValueType, StylesConfig } from "react-select"
+import copy from "copy-to-clipboard"
 
 import { ChartConfig, ChartConfigProps } from "./ChartConfig"
 import { getQueryParams, getWindowQueryParams } from "utils/client/url"
@@ -15,6 +16,7 @@ import { ADMIN_BASE_URL, ENV } from "settings"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCode } from "@fortawesome/free-solid-svg-icons/faCode"
+import { faCopy } from "@fortawesome/free-solid-svg-icons/faCopy"
 import { faEdit } from "@fortawesome/free-solid-svg-icons/faEdit"
 import { faDownload } from "@fortawesome/free-solid-svg-icons/faDownload"
 import { faShareAlt } from "@fortawesome/free-solid-svg-icons/faShareAlt"
@@ -73,13 +75,27 @@ class EmbedMenu extends React.Component<{
     }
 }
 
-@observer
-class ShareMenu extends React.Component<{
+interface ShareMenuProps {
     chart: ChartConfig
     chartView: any
     onDismiss: () => void
-}> {
+}
+
+interface ShareMenuState {
+    copied: boolean
+}
+
+@observer
+class ShareMenu extends React.Component<ShareMenuProps, ShareMenuState> {
     dismissable = true
+
+    constructor(props: ShareMenuProps) {
+        super(props)
+
+        this.state = {
+            copied: false
+        }
+    }
 
     @computed get title(): string {
         return this.props.chart.data.currentTitle
@@ -151,6 +167,12 @@ class ShareMenu extends React.Component<{
         }
     }
 
+    @action.bound onCopy() {
+        if (this.canonicalUrl) {
+            if (copy(this.canonicalUrl)) this.setState({ copied: true })
+        }
+    }
+
     @computed get twitterHref(): string {
         let href =
             "https://twitter.com/intent/tweet/?text=" +
@@ -209,6 +231,14 @@ class ShareMenu extends React.Component<{
                         <FontAwesomeIcon icon={faShareAlt} /> Share via&hellip;
                     </a>
                 )}
+                <a
+                    className="btn"
+                    title="Copy link to clipboard"
+                    onClick={this.onCopy}
+                >
+                    <FontAwesomeIcon icon={faCopy} />
+                    {this.state.copied ? "Copied!" : "Copy link"}
+                </a>
                 {editUrl && (
                     <a
                         className="btn"
