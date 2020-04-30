042355f7fc8d3aab663656e7259f71446e9eb7f2
Matthieu Bergel <hello@matthieubergel.org>
2020-04-30 15:52:18 +0200

    fix(temp script): remove legacy has-2-columns class

---
+++ web/app/plugins/owid/src/remove_has_2_columns.php
@@ -0,0 +1,66 @@
+@@ -1,61 +0,0 @@
+<?php
+// Run this script as a logged-in user otherwise the revisions are created with author "O"
+// If using wp eval-file, pass "--user=[USER_ID]" flag (e.g. lando wp eval-file --user=35 /path/to/script.php)
+//
+// Check out the following commits for other instances of that scripts:
+// - format_wpautop.php 4b70d7557fb31882e63957777a72a62d6906e0ec 
+// - h5_to_h4.php 5b5b7b1e9caa2c631a4684506525618730088143
+?>
+
+<?php
+
+function get_post_admin_links($post)
+{
+  $name = $post->post_title;
+  $view = "https://ourworldindata.org/$post->post_name";
+  $preview = "https://owid.cloud/admin/posts/preview/$post->ID";
+  $edit = "https://owid.cloud/wp/wp-admin/post.php?post=$post->ID&action=edit";
+  // $view = "http://localhost:3099/$post->post_name";
+  // $edit = "http://our-world-in-data.lndo.site/wp/wp-admin/post.php?post=$post->ID&action=edit";
+
+  return "$name ; $view ; $preview ; $edit";
+}
+
+function remove_has_2_columns()
+{
+  $posts = get_posts(array('post_type' => ['post', 'page', 'wp_block'], 'numberposts' => -1)); //, 'include' => array(26199, 596)));
+
+  foreach ($posts as $post) {
+    $content = $post->post_content;
+
+    // $patterns = array("/\r\n/", "/\n+/", "/\n/");
+    // $patterns = $replacements = null
+
+    $patterns = array(
+      "/wp-block-columns has-2-columns/",
+      "/<!-- wp:columns {\"className\":\"has-2-columns\"} -->/",
+      "/<!-- wp:columns {\"className\":\"has-2-columns is-style-sticky-right\"} -->/"
+    );
+    $replacements = array(
+      "wp-block-columns",
+      "<!-- wp:columns -->",
+      "<!-- wp:columns {\"className\":\"is-style-sticky-right\"} -->"
+    );
+
+    $nb_replacements = 0;
+    $content = preg_replace($patterns, $replacements, $content, -1, $nb_replacements);
+
+    if ($nb_replacements !== 0) echo get_post_admin_links($post) . "\n";
+
+    $my_post = array(
+      'ID'           => $post->ID,
+      'post_content' => $content,
+    );
+    wp_update_post($my_post);
+    // $post_id = wp_update_post($my_post, true);
+    // if (is_wp_error($post_id)) {
+    //   $errors = $post_id->get_error_messages();
+    //   foreach ($errors as $error) {
+    //     echo $error;
+    //   }
+    // }
+  }
+}
+
+remove_has_2_columns();
