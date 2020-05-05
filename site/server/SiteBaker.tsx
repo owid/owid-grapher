6a10024d9cfa1cd86820b6c38471ef42fbf2986a
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-05-05 18:56:52 +0100

    Add COVID_DASHBOARD feature flag

--- site/server/SiteBaker.tsx
+++ site/server/SiteBaker.tsx
@@ -304,10 +304,12 @@ export class SiteBaker {
                 await renderExplorableIndicatorsJson()
             )
         }
-        await this.stageWrite(
-            `${BAKED_SITE_DIR}/covid-chart-builder.html`,
-            await renderCovidChartBuilderPage()
-        )
+        if (settings.COVID_DASHBOARD) {
+            await this.stageWrite(
+                `${BAKED_SITE_DIR}/covid-chart-builder.html`,
+                await renderCovidChartBuilderPage()
+            )
+        }
     }
 
     // Pages that are expected by google scholar for indexing
