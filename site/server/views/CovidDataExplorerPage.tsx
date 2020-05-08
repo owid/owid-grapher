09e349a545eafa690faa86a159fd2d37251b0924
Breck Yunits <breck7@gmail.com>
2020-05-07 18:23:05 -1000

    Hide header and footer if embedded

--- site/server/views/CovidDataExplorerPage.tsx
+++ site/server/views/CovidDataExplorerPage.tsx
@@ -7,12 +7,19 @@ import { SiteFooter } from "./SiteFooter"
 export const CovidDataExplorerPage = () => {
     const script = `window.CovidDataExplorer.bootstrap()`
 
+    const iframeScript = `
+    if (window != window.top) {
+        document.documentElement.classList.add('iframe')
+    }
+`
+
     return (
         <html>
             <Head
                 canonicalUrl={`${settings.BAKED_BASE_URL}/covid-data-explorer`}
                 pageTitle="Covid-19 Data Explorer"
             >
+                <script dangerouslySetInnerHTML={{ __html: iframeScript }} />
                 <link
                     rel="preload"
                     href="https://covid.ourworldindata.org/data/owid-covid-data.csv"
@@ -20,9 +27,9 @@ export const CovidDataExplorerPage = () => {
                     crossOrigin="anonymous"
                 />
             </Head>
-            <body>
+            <body className="ChartPage">
                 <SiteHeader />
-                <main id="covidDataExplorer"></main>
+                <main id="covidDataExplorerContainer"></main>
                 <SiteFooter />
                 <script dangerouslySetInnerHTML={{ __html: script }} />
             </body>
