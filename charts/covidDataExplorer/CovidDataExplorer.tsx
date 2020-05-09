4b9006230c81f09cd61a40e2c327a399351be238
Breck Yunits <breck7@gmail.com>
2020-05-08 20:31:16 -1000

    Add some analytics to explorer

--- charts/covidDataExplorer/CovidDataExplorer.tsx
+++ charts/covidDataExplorer/CovidDataExplorer.tsx
@@ -657,7 +657,7 @@ export class CovidDataExplorer extends React.Component<{
         this.chart.hideEntityControls = true
         this.chart.externalCsvLink = covidDataPath
         this.chart.url.externalBaseUrl = `${BAKED_BASE_URL}/covid-data-explorer`
-        this.updateChart()
+        this._updateChart()
         const win = window as any
         win.covidDataExplorer = this
     }
