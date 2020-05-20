74444eec9b60b7a827441bf233d966d08ca5aa21
Breck Yunits <breck7@gmail.com>
2020-05-19 15:41:51 -1000

    Add v2 link schema to support Facebook link sharing

--- charts/__tests__/ChartUrl.test.ts
+++ charts/__tests__/ChartUrl.test.ts
@@ -333,46 +333,66 @@ describe(ChartUrl, () => {
 
 describe(EntityUrlBuilder, () => {
     const encodeTests = [
-        { entities: ["USA", "GB"], queryString: "USA+GB" },
-        { entities: ["YouTube", "Google+"], queryString: "YouTube+Google%2B" },
+        { entities: ["USA", "GB"], queryString: "USA%7CGB" },
+        {
+            entities: ["YouTube", "Google+"],
+            queryString: "YouTube%7CGoogle%2B"
+        },
         {
             entities: [
                 "Bogebakken (Denmark); 4300 - 3800 BCE",
                 "British Columbia (30 sites); 3500 BCE - 1674 CE",
                 "Brittany; 6000 BCE"
             ],
-            delimiter: "|",
             queryString:
-                "Bogebakken%20(Denmark)%3B%204300%20-%203800%20BCE|British%20Columbia%20(30%20sites)%3B%203500%20BCE%20-%201674%20CE|Brittany%3B%206000%20BCE"
+                "Bogebakken%20(Denmark)%3B%204300%20-%203800%20BCE%7CBritish%20Columbia%20(30%20sites)%3B%203500%20BCE%20-%201674%20CE%7CBrittany%3B%206000%20BCE"
         },
-
         {
-            delimiter: "|",
+            entities: ["British Columbia (30 sites); 3500 BCE - 1674 CE"],
+            queryString:
+                "British%20Columbia%20(30%20sites)%3B%203500%20BCE%20-%201674%20CE"
+        },
+        {
+            entities: ["North America"],
+            queryString: "North%20America"
+        },
+        {
             entities: [
                 "Men and Women Ages 65+",
                 "Australia & New Zealand + (Total)"
             ],
             queryString:
-                "Men%20and%20Women%20Ages%2065%2B|Australia%20%26%20New%20Zealand%20%2B%20(Total)"
+                "Men%20and%20Women%20Ages%2065%2B%7CAustralia%20%26%20New%20Zealand%20%2B%20(Total)"
         }
     ]
 
     encodeTests.forEach(testCase => {
         it(`correctly encodes url strings`, () => {
             expect(
-                EntityUrlBuilder.entitiesToQueryParams(
-                    testCase.entities,
-                    testCase.delimiter
-                )
+                EntityUrlBuilder.entitiesToQueryParams(testCase.entities)
             ).toEqual(testCase.queryString)
         })
 
         it(`correctly decodes url strings`, () => {
             expect(
-                EntityUrlBuilder.queryParamToCountries(
-                    testCase.queryString,
-                    testCase.delimiter
-                )
+                EntityUrlBuilder.queryParamToCountries(testCase.queryString)
+            ).toEqual(testCase.entities)
+        })
+    })
+
+    const legacyLinks = [
+        {
+            entities: ["North America", "DOM"],
+            queryString: "North%20America+DOM"
+        },
+        { entities: ["USA", "GB"], queryString: "USA+GB" },
+        { entities: ["YouTube", "Google+"], queryString: "YouTube+Google%2B" }
+    ]
+
+    legacyLinks.forEach(testCase => {
+        it(`correctly decodes legacy url strings`, () => {
+            expect(
+                EntityUrlBuilder.queryParamToCountries(testCase.queryString)
             ).toEqual(testCase.entities)
         })
     })
