cad3308c965b498c649d0ae81bda1aa685a7b8b1
Marcel Gerber <mgerber59@gmail.com>
2020-05-25 13:11:06 +0200

    Show table of deployed commits for all servers by default

--- scripts/liveCommit.ts
+++ scripts/liveCommit.ts
@@ -1,12 +1,15 @@
+import { sortBy } from "lodash"
 import parseArgs from "minimist"
 import fetch from "node-fetch"
 import opener from "opener"
+import * as timeago from "timeago.js"
 import { promisifiedExec as exec } from "utils/server/serverUtil"
 
 /**
  * Retrieves information about the deployed commit on a live or staging server.
  * Usage examples:
- * - `yarn live-commit` will retrieve the commit that's live on https://ourworldindata.org and opens it in GitHub
+ * - `yarn live-commit` will retrieve information about the deployed commits for _all_ servers, and show a table
+ * - `yarn live-commit live` will retrieve the commit that's live on https://ourworldindata.org and opens it in GitHub
  *   That's equivalent to `yarn live-commit live --open`
  * - `yarn live-commit staging --show` will `git show` information about the commit deployed on https://staging-owid.netlify.app
  * - `yarn live-commit --show --tree` will both show a git tree and a `git show` of the deployed commits on https://ourworldindata.org
@@ -16,38 +19,113 @@ import { promisifiedExec as exec } from "utils/server/serverUtil"
  *  If it still doesn't work, the live commit is not pushed to GitHub yet. That should only happen on a staging server, never on live.
  */
 
+const servers = ["live", "staging", "explorer", "hans", "playfair"]
+
 const args = parseArgs(process.argv.slice(2))
 
 const showTree = args["tree"]
 const showCommit = args["show"]
 const openInBrowser = args["open"] || !(showCommit || showTree)
 
-let server = "https://ourworldindata.org"
-if (args._[0] && args._[0] !== "live") {
-    server = `https://${args._[0]}-owid.netlify.com`
+function getServerUrl(server: string): string {
+    if (server === "live") return "https://ourworldindata.org"
+    else return `https://${server}-owid.netlify.com`
 }
 
-fetch(`${server}/head.txt`)
-    .then(res => {
-        if (res.ok) return res
-        else throw Error(`Request rejected with status ${res.status}`)
-    })
-    .then(resp => resp.text())
-    .then(async headSha => {
-        if (showTree)
-            await exec(
-                `git log -10 --graph --oneline --decorate --color=always ${headSha}`
-            )
+async function fetchCommitSha(server: string): Promise<string> {
+    return fetch(`${getServerUrl(server)}/head.txt`)
+        .then(res => {
+            if (res.ok) return res
+            else throw Error(`Request rejected with status ${res.status}`)
+        })
+        .then(resp => resp.text())
+}
 
-        if (showTree && showCommit) console.log()
+interface ServerCommitInformation {
+    serverName: string
+    commitSha: string | undefined
+    commitDate: string | undefined
+    commitAuthor: string | undefined
+    commitMessage: string | undefined
+}
 
-        if (showCommit) await exec(`git show --stat --color=always ${headSha}`)
+async function fetchAll(): Promise<Array<ServerCommitInformation>> {
+    const commits = await Promise.all(
+        servers.map(async serverName => {
+            let commitSha = undefined
+            try {
+                commitSha = await fetchCommitSha(serverName)
+            } catch {
+                commitSha = undefined
+            }
 
-        if (openInBrowser)
-            opener(`https://github.com/owid/owid-grapher/commit/${headSha}`)
-    })
-    .catch(err =>
-        console.error(
-            `Could not retrieve live commit information from ${server}. ${err}`
-        )
+            return {
+                serverName,
+                commitSha,
+                commitDate: undefined,
+                commitAuthor: undefined,
+                commitMessage: undefined
+            }
+        })
     )
+
+    const commitsWithInformation = await Promise.all(
+        commits.map(async commit => {
+            if (!commit.commitSha) return commit
+
+            const apiResponse = (await fetch(
+                `https://api.github.com/repos/owid/owid-grapher/git/commits/${commit.commitSha}`
+            )) as any
+
+            const response = await apiResponse.json()
+
+            return {
+                ...commit,
+                commitSha: commit.commitSha.substr(0, 7),
+                commitDate:
+                    response?.author?.date && new Date(response?.author?.date),
+                commitAuthor: response?.author?.name,
+                commitMessage: response?.message?.split("\n")?.[0]
+            }
+        })
+    )
+
+    return sortBy(commitsWithInformation, c => c.commitDate ?? 0)
+        .reverse()
+        .map(commitInformation => ({
+            ...commitInformation,
+            commitDate:
+                commitInformation.commitDate &&
+                timeago.format(commitInformation.commitDate)
+        }))
+}
+
+if (args._[0]) {
+    // fetch information for one specific server
+    const server = args._[0]
+    fetchCommitSha(server)
+        .then(async headSha => {
+            if (showTree)
+                await exec(
+                    `git log -10 --graph --oneline --decorate --color=always ${headSha}`
+                )
+
+            if (showTree && showCommit) console.log()
+
+            if (showCommit)
+                await exec(`git show --stat --color=always ${headSha}`)
+
+            if (openInBrowser)
+                opener(`https://github.com/owid/owid-grapher/commit/${headSha}`)
+        })
+        .catch(err =>
+            console.error(
+                `Could not retrieve commit information from ${getServerUrl(
+                    server
+                )}. ${err}`
+            )
+        )
+} else {
+    // fetch information for _all_ servers
+    fetchAll().then(console.table)
+}
