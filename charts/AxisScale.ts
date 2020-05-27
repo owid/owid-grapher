8b25438a9ccdc81caaf4f34b69c8a63db8c22f15
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-05-27 12:08:51 +0100

    Add comment to explain reasoning

--- charts/AxisScale.ts
+++ charts/AxisScale.ts
@@ -99,9 +99,31 @@ export class AxisScale {
     }
 
     getTickFormattingOptions(): TickFormattingOptions {
+        // The chart's tick formatting function is used by default to format axis ticks. This means
+        // that the chart's `numDecimalPlaces` is also used by default to format the axis ticks.
+        //
+        // However, the author-specified decimal places are not always appropriate for rendering
+        // ticks, because:
+        // 1. Subsets of the data may require higher fidelity, e.g. users can use the timeline to
+        //    end up in a subset of the dataset where values happen to be much lower than usual.
+        // 2. Ticks may be rendered at granularities that may not exist in the data, e.g. the data
+        //    may only contain 0 and 1, but we may show ticks in between those values.
+        //
+        // Therefore, when formatting ticks, we determine the `numDecimalPlaces` automatically, by
+        // finding the smallest difference between any pair of ticks and making sure that we have
+        // sufficient decimal places to express the difference to the first significant figure (the
+        // first non-zero digit).
+        //
+        // One significant figure is sufficient because we use D3's ticks() and that creates
+        // "uniformly-spaced, nicely-rounded values [...] where each value is a power of ten
+        // multiplied by 1, 2 or 5"
+        // See: https://github.com/d3/d3-array/blob/master/README.md#ticks
+        //
+        // -@danielgavrilov, 2020-05-27
         const tickValues = this.getTickValues()
         const minDist = min(rollingMap(tickValues, (a, b) => Math.abs(a - b)))
         if (minDist !== undefined) {
+            // Find the decimal places required to reach the first non-zero digit
             const dp = Math.ceil(-Math.log10(minDist))
             if (isFinite(dp) && dp >= 0) return { numDecimalPlaces: dp }
         }
