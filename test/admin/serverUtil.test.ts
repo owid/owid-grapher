5e560c04f188e92e3ef4af061987c4d6f872a00e
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-07-08 14:11:58 +0100

    Fix exec() tests

--- test/admin/serverUtil.test.ts
+++ test/admin/serverUtil.test.ts
@@ -1,5 +1,7 @@
 #! /usr/bin/env yarn jest
 
+import { ExecError } from "utils/server/serverUtil"
+
 const { exec } = require("utils/server/serverUtil")
 
 describe("serverUtil", () => {
@@ -8,18 +10,25 @@ describe("serverUtil", () => {
             const result = await exec(`echo "it works"; exit 0`, {
                 silent: true
             })
-            expect(result).toEqual("it works\n")
+            expect(result).toEqual({
+                code: 0,
+                stdout: "it works\n",
+                stderr: ""
+            })
         })
 
         it("should reject when there is a non-zero exit code", async () => {
             try {
-                await exec(`echo "does not work"; exit 1`, {
+                await exec(`echo "begin"; echo "fail" 1>&2; exit 1`, {
                     silent: true
                 })
             } catch (err) {
-                expect(err).toBeInstanceOf(Error)
+                expect(err).toBeInstanceOf(ExecError)
+                expect(err.code).toEqual(1)
+                expect(err.stdout).toEqual("begin\n")
+                expect(err.stderr).toEqual("fail\n")
             } finally {
-                expect.assertions(1)
+                expect.assertions(4)
             }
         })
     })
