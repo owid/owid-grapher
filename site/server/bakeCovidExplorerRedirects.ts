48ce848b611df1bf48268cf75367605bf3426be8
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-07-14 11:31:58 +0100

    Populate redirects as suggested by authors
    From spreadsheet: https://docs.google.com/spreadsheets/d/1wNYjv2BHfdclccKHlWxh33HcE4F8SNgbUsaqMH_2WDE/edit#gid=529677154

--- site/server/bakeCovidExplorerRedirects.ts
+++ site/server/bakeCovidExplorerRedirects.ts
@@ -14,16 +14,123 @@ interface ChartExplorerRedirect {
 }
 
 export const chartExplorerRedirects: ChartExplorerRedirect[] = [
-    // {
-    //     id: 4018,
-    //     slugs: [
-    //         "total-cases-covid-19",
-    //         "total-confirmed-cases-of-covid-19",
-    //         "total-cases-covid-19-who"
-    //     ],
-    //     explorerQueryStr:
-    //         "zoomToSelection=true&casesMetric=true&totalFreq=true&smoothing=0&country=OWID_WRL"
-    // }
+    {
+        id: 4018,
+        slugs: [
+            "total-cases-covid-19",
+            "total-confirmed-cases-of-covid-19",
+            "total-cases-covid-19-who"
+        ],
+        explorerQueryStr:
+            "zoomToSelection=true&casesMetric=true&dailyFreq=true&smoothing=0&country=~OWID_WRL"
+    },
+    {
+        id: 4019,
+        slugs: [
+            "daily-cases-covid-19",
+            "daily-new-confirmed-cases-of-covid-19",
+            "daily-cases-covid-19-who"
+        ],
+        explorerQueryStr:
+            "zoomToSelection=true&casesMetric=true&dailyFreq=true&smoothing=0&country=~OWID_WRL"
+    },
+    {
+        id: 4020,
+        slugs: ["total-deaths-covid-19", "total-deaths-covid-19-who"],
+        explorerQueryStr:
+            "zoomToSelection=true&deathsMetric=true&totalFreq=true&smoothing=0&country=~OWID_WRL"
+    },
+    {
+        id: 4021,
+        slugs: [
+            "daily-deaths-covid-19",
+            "daily-new-confirmed-deaths-due-to-covid-19",
+            "daily-deaths-covid-19-who"
+        ],
+        explorerQueryStr:
+            "zoomToSelection=true&deathsMetric=true&dailyFreq=true&smoothing=0&country=~OWID_WRL"
+    },
+    {
+        id: 4025,
+        slugs: ["covid-confirmed-cases-per-million-since-1-per-million"],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&casesMetric=true&totalFreq=true&aligned=true&perCapita=true&smoothing=0&country="
+    },
+    {
+        id: 4037,
+        slugs: [
+            "covid-confirmed-deaths-since-5th-death",
+            "covid-confirmed-deaths-since-10th-death"
+        ],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&deathsMetric=true&totalFreq=true&aligned=true&smoothing=0&country="
+    },
+    {
+        id: 4039,
+        slugs: [
+            "covid-confirmed-cases-since-100th-case",
+            "covid-tests-since-100th-case"
+        ],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&casesMetric=true&totalFreq=true&aligned=true&smoothing=0&country="
+    },
+    {
+        id: 4049,
+        slugs: ["case-fatality-rate-covid-19"],
+        explorerQueryStr:
+            "zoomToSelection=true&time=2020-03-14..&cfrMetric=true&totalFreq=true&aligned=true&smoothing=0&country=OWID_WRL~USA~ITA~BRA~ESP~SWE~DEU~IND~IRN"
+    },
+    {
+        id: 4056,
+        slugs: ["coronavirus-cfr"],
+        explorerQueryStr:
+            "zoomToSelection=true&time=2020-03-14..&cfrMetric=true&totalFreq=true&aligned=true&smoothing=0&country=OWID_WRL~USA~ITA~BRA~ESP~SWE~DEU~IND~IRN"
+    },
+    {
+        id: 4057,
+        slugs: ["covid-deaths-days-since-per-million"],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&minPopulationFilter=1000000&deathsMetric=true&totalFreq=true&aligned=true&perCapita=true&smoothing=0&country="
+    },
+    {
+        id: 4063,
+        slugs: [
+            "covid-confirmed-daily-cases-epidemiological-trajectory",
+            "covid-confirmed-daily-cases-since-100th-case"
+        ],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&minPopulationFilter=1000000&casesMetric=true&dailyFreq=true&aligned=true&smoothing=7&country="
+    },
+    {
+        id: 4078,
+        slugs: ["covid-confirmed-daily-deaths-epidemiological-trajectory"],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&minPopulationFilter=1000000&deathsMetric=true&dailyFreq=true&aligned=true&smoothing=7&country="
+    },
+    {
+        id: 4101,
+        slugs: ["covid-confirmed-cases-since-100th-case"],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&casesMetric=true&totalFreq=true&aligned=true&smoothing=0&country="
+    },
+    {
+        id: 4106,
+        slugs: ["covid-daily-deaths-trajectory-per-million"],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&minPopulationFilter=1000000&deathsMetric=true&dailyFreq=true&aligned=true&perCapita=true&smoothing=7&country="
+    },
+    {
+        id: 4107,
+        slugs: ["covid-daily-cases-trajectory-per-million"],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&minPopulationFilter=1000000&casesMetric=true&dailyFreq=true&aligned=true&perCapita=true&smoothing=7&country="
+    },
+    {
+        id: 4129,
+        slugs: ["total-tests-per-thousand-since-per-cap-death-threshold"],
+        explorerQueryStr:
+            "yScale=log&zoomToSelection=true&testsMetric=true&totalFreq=true&aligned=true&perCapita=true&smoothing=0&country="
+    }
 ]
 
 export const chartExplorerRedirectsBySlug: Record<
