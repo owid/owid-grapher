44ee43bf2dc6f0ef06759265df54cec1ee360a37
Breck Yunits <breck7@gmail.com>
2020-07-15 09:09:26 -1000

    New: change default chart view

--- charts/covidDataExplorer/CovidConstants.ts
+++ charts/covidDataExplorer/CovidConstants.ts
@@ -3,7 +3,7 @@ export const coronaOpenGraphImagePath = "coronavirus-data-explorer.png"
 export const coronaWordpressElementAttribute = "data-coronavirus-data-explorer"
 export const covidDataExplorerContainerId = "covidDataExplorerContainer"
 export const coronaDefaultView =
-    "yScale=log&zoomToSelection=true&deathsMetric=true&dailyFreq=true&aligned=true&smoothing=7&country=USA~GBR~CAN~BRA~AUS~IND~DEU~FRA~MEX~CHL~ZAF~DZA~COL"
+    "yScale=log&zoomToSelection=true&casesMetric=true&dailyFreq=true&aligned=true&smoothing=7&country=USA~GBR~CAN~BRA~AUS~IND~DEU~MEX~ZAF~COL~KOR~NOR~UGA~NGA&pickerMetric=location&pickerSort=asc"
 export const covidDataPath =
     "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 export const covidLastUpdatedPath =
