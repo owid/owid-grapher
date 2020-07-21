5672ed99731b0da36869144f7e6fb1c2514a20f0
Breck Yunits <breck7@gmail.com>
2020-07-20 16:04:34 -1000

    New: add styling to keyboard help menu

---
+++ charts/covidDataExplorer/CommandPalette.tsx
@@ -0,0 +1,47 @@
+import { observer } from "mobx-react"
+import React from "react"
+
+declare type keyboardCombo = string
+
+export interface Command {
+    combo: keyboardCombo
+    fn: () => any
+    title: string
+    category: string
+}
+
+@observer
+export class CommandPalette extends React.Component<{
+    commands: Command[]
+    display: "none" | "block"
+}> {
+    render() {
+        const style: any = {
+            display: this.props.display
+        }
+        let lastCat = ""
+        const commands = this.props.commands.map(command => {
+            let cat = undefined
+            if (command.category !== lastCat) {
+                lastCat = command.category
+                cat = <div className="commandCategory">{lastCat}</div>
+            }
+            return (
+                <>
+                    {cat}
+                    <div className="commandOption">
+                        <span className="commandCombo">{command.combo}</span>
+                        <a onClick={() => command.fn()}>{command.title}</a>
+                    </div>
+                </>
+            )
+        })
+
+        return (
+            <div className="CommandPalette" style={style}>
+                <div className="paletteTitle">Keyboard Shortcuts</div>
+                {commands}
+            </div>
+        )
+    }
+}
