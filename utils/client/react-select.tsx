d47ac23673984656d2d40269b86dfde62a6f185a
Breck Yunits <breck7@gmail.com>
2020-07-28 11:41:10 -1000

    Style tweaks

--- utils/client/react-select.tsx
+++ utils/client/react-select.tsx
@@ -10,34 +10,61 @@ export function asArray<T>(value: ValueType<T>): T[] {
     else return [value]
 }
 
-export function getStylesForTargetHeight(targetHeight: number): StylesConfig {
+export function getStylesForTargetHeight(
+    targetHeight: number,
+    props: any = {}
+): StylesConfig {
     // Taken from https://github.com/JedWatson/react-select/issues/1322#issuecomment-591189551
+    const {
+        control,
+        valueContainer,
+        singleValue,
+        clearIndicator,
+        container,
+        dropdownIndicator,
+        option,
+        menu
+    } = props
     return {
+        container: (base: React.CSSProperties) => ({
+            ...base,
+            ...container
+        }),
         control: (base: React.CSSProperties) => ({
             ...base,
-            minHeight: "initial"
+            minHeight: "initial",
+            ...control
         }),
         valueContainer: (base: React.CSSProperties) => ({
             ...base,
             height: `${targetHeight - 1 - 1}px`,
-            padding: "0 4px"
+            padding: "0 4px",
+            ...valueContainer
+        }),
+        singleValue: (base: React.CSSProperties) => ({
+            ...base,
+            ...singleValue
         }),
         clearIndicator: (base: React.CSSProperties) => ({
             ...base,
-            padding: `${(targetHeight - 20 - 1 - 1) / 2}px`
+            padding: `${(targetHeight - 20 - 1 - 1) / 2}px`,
+            ...clearIndicator
         }),
         dropdownIndicator: (base: React.CSSProperties) => ({
             ...base,
-            padding: `${(targetHeight - 20 - 1 - 1) / 2}px`
+            padding: `${(targetHeight - 20 - 1 - 1) / 2}px`,
+            ...dropdownIndicator
         }),
         option: (base: React.CSSProperties) => ({
             ...base,
             paddingTop: "5px",
-            paddingBottom: "5px"
+            paddingBottom: "5px",
+            ...option
         }),
         menu: (base: React.CSSProperties) => ({
             ...base,
-            zIndex: 10000
+            zIndex: 10000,
+            ...menu
         })
     }
 }
