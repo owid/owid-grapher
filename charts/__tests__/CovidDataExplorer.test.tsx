e7c5afd12c102c302a7c583d3b9c145af6932a61
Shahid Ahmad <ahmad.s.shahid@gmail.com>
2020-08-07 17:14:58 -0400

    multimetric tests v1

--- charts/__tests__/CovidDataExplorer.test.tsx
+++ charts/__tests__/CovidDataExplorer.test.tsx
@@ -4,7 +4,8 @@ import { CovidDataExplorer } from "../covidDataExplorer/CovidDataExplorer"
 import { CovidQueryParams } from "charts/covidDataExplorer/CovidChartUrl"
 import { covidSampleRows } from "../../test/fixtures/CovidSampleRows"
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, mount, ReactWrapper } from "enzyme"
+import { MultiMetricExplorer } from "../../stories/covidDataExplorer.stories"
 
 const dummyMeta = {
     charts: {},
@@ -28,5 +29,57 @@ describe(CovidDataExplorer, () => {
         // Need to split it off because the lines are in separate elements
         expect(headerText).toContain("Coronavirus Pandemic")
         expect(headerText).toContain("Data Explorer")
+    })
+})
+
+describe.only("When you try to create a multimetric Data Explorer", () => {
+    let explorerView: ReactWrapper
+    beforeAll(() => {
+        explorerView = mount(<MultiMetricExplorer />)
+    })
+
+    it("renders a table", () => {
+        expect(explorerView.find("table")).toHaveLength(1)
+    })
+
+    it("renders correct table headers", () => {
+        const tableHeader = explorerView
+            .find("thead tr")
+            .first()
+            .find("th span.name")
+            .map(tableHeader => tableHeader.text())
+
+        expect(tableHeader).toEqual([
+            "Confirmed cases",
+            "Confirmed cases",
+            "Confirmed deaths",
+            "Confirmed deaths",
+            "Tests",
+            "Tests",
+            "Tests per confirmed case",
+            "Case fatality rate",
+            "Share of positive tests"
+        ])
+    })
+
+    it("renders correct table rows", () => {
+        const tableRows = explorerView.find("tbody tr")
+        const secondRow = tableRows
+            .at(1)
+            .find("td")
+            .map(td => td.text())
+
+        expect(secondRow).toEqual([
+            "United States",
+            "1.20 million",
+            "23,841",
+            "71,078",
+            "2,144",
+            "May 5 7.54 million",
+            "May 5 258,954",
+            "May 5 6",
+            "5.9",
+            "May 5 0.2"
+        ])
     })
 })
--- stories/covidDataExplorer.stories.tsx
+++ stories/covidDataExplorer.stories.tsx
@@ -1,6 +1,4 @@
 import * as React from "react"
-import "site/client/owid.scss"
-import "charts/client/chart.scss"
 import { CovidDataExplorer } from "charts/covidDataExplorer/CovidDataExplorer"
 import { covidSampleRows } from "test/fixtures/CovidSampleRows"
 import { CovidQueryParams } from "charts/covidDataExplorer/CovidChartUrl"
@@ -9,16 +7,17 @@ export default {
     title: "CovidDataExplorer"
 }
 
+const EMPTY_DUMMY_META = {
+    charts: {},
+    variables: {}
+}
+
 export const SingleExplorerWithKeyboardShortcuts = () => {
-    const dummyMeta = {
-        charts: {},
-        variables: {}
-    }
     return (
         <CovidDataExplorer
             data={covidSampleRows}
             params={new CovidQueryParams("")}
-            covidChartAndVariableMeta={dummyMeta}
+            covidChartAndVariableMeta={EMPTY_DUMMY_META}
             updated="2020-05-09T18:59:31"
             enableKeyboardShortcuts={true}
         />
@@ -26,24 +25,50 @@ export const SingleExplorerWithKeyboardShortcuts = () => {
 }
 
 export const MultipleExplorers = () => {
-    const dummyMeta = {
-        charts: {},
-        variables: {}
-    }
     return (
         <>
             <CovidDataExplorer
                 data={covidSampleRows}
                 params={new CovidQueryParams("")}
-                covidChartAndVariableMeta={dummyMeta}
+                covidChartAndVariableMeta={EMPTY_DUMMY_META}
                 updated="2020-05-09T18:59:31"
             />
             <CovidDataExplorer
                 data={covidSampleRows}
                 params={new CovidQueryParams("")}
-                covidChartAndVariableMeta={dummyMeta}
+                covidChartAndVariableMeta={EMPTY_DUMMY_META}
                 updated="2020-05-09T18:59:31"
             />
         </>
+    )
+}
+
+export const MultiMetricExplorer = () => {
+    const startingParams = new CovidQueryParams(
+        "?tab=table&tableMetrics=cases~deaths~tests~tests_per_case~case_fatality_rate~positive_test_rate"
+    )
+    return (
+        <CovidDataExplorer
+            data={covidSampleRows}
+            params={startingParams}
+            queryStr="?tab=table&time=2020-05-06"
+            covidChartAndVariableMeta={EMPTY_DUMMY_META}
+            updated="2020-05-09T18:59:31"
+        />
+    )
+}
+
+export const MultiMetricExplorerPerCapita = () => {
+    const startingParams = new CovidQueryParams(
+        "?tab=table&tableMetrics=cases~deaths~tests~tests_per_case~case_fatality_rate~positive_test_rate"
+    )
+    return (
+        <CovidDataExplorer
+            data={covidSampleRows}
+            params={startingParams}
+            queryStr="?tab=table&time=2020-05-06"
+            covidChartAndVariableMeta={EMPTY_DUMMY_META}
+            updated="2020-05-09T18:59:31"
+        />
     )
 }
