5d56d535e8c2bc9bb84940243833e66df4ea9a81
Marcel Gerber <mgerber59@gmail.com>
2020-08-13 11:55:26 +0200

    fix & add comparison line tests

--- charts/__tests__/ComparisonLineGenerator.test.ts
+++ charts/__tests__/ComparisonLineGenerator.test.ts
@@ -12,7 +12,7 @@ describe(generateComparisonLinePoints, () => {
                 "linear",
                 "linear"
             )
-            expect(points.length).toEqual(100)
+            expect(points.length).toEqual(500)
         })
 
         it("it clamps points if they exceed the y max", () => {
@@ -23,7 +23,31 @@ describe(generateComparisonLinePoints, () => {
                 "linear",
                 "linear"
             )
-            expect(points.length).toEqual(51)
+            expect(points.length).toEqual(251)
+        })
+    })
+
+    describe("For y = 50*x", () => {
+        it("returns the correct number of points", () => {
+            const points = generateComparisonLinePoints(
+                "50*x",
+                [0, 10],
+                [0, 10],
+                "linear",
+                "linear"
+            )
+            expect(points.length).toEqual(11)
+        })
+
+        it("returns the correct number of points for a log chart", () => {
+            const points = generateComparisonLinePoints(
+                "50*x",
+                [1e-6, 1e6],
+                [0, 10],
+                "log",
+                "linear"
+            )
+            expect(points.length).toEqual(221)
         })
     })
 })
