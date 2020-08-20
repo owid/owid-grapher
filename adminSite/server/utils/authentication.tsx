673356e423793f7a6da3af51b337b19e0b197d78
Breck Yunits <breck7@gmail.com>
2020-08-20 10:01:44 -1000

    Cleanup: path name cleanup

--- adminSite/server/utils/authentication.tsx
+++ adminSite/server/utils/authentication.tsx
@@ -2,7 +2,7 @@ import * as express from "express"
 import * as crypto from "crypto"
 import * as randomstring from "randomstring"
 import { User } from "db/model/User"
-import { BCryptHasher } from "../../../utils/hashers"
+import { BCryptHasher } from "utils/hashers"
 
 import * as db from "db/db"
 import { SECRET_KEY, SESSION_COOKIE_AGE } from "serverSettings"
