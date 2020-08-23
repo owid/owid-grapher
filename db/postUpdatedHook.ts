432e0aa20615f8abc0f2b73d722d15bd1d4f8222
Breck Yunits <breck7@gmail.com>
2020-08-23 09:45:00 -1000

    Dev: restore postUpdatedHook

---
+++ db/postUpdatedHook.ts
@@ -0,0 +1,30 @@
+// This is used by owid-wordpress
+
+import { syncPostToGrapher } from "db/model/Post"
+import parseArgs from "minimist"
+import { BAKE_ON_CHANGE } from "serverSettings"
+import { enqueueDeploy } from "deploy/queue"
+import { exit } from "db/cleanup"
+const argv = parseArgs(process.argv.slice(2))
+
+async function main(
+    email: string,
+    name: string,
+    postId: number,
+    postSlug: string
+) {
+    console.log(email, name, postId)
+    const slug = await syncPostToGrapher(postId)
+
+    if (BAKE_ON_CHANGE) {
+        await enqueueDeploy({
+            authorName: name,
+            authorEmail: email,
+            message: slug ? `Updating ${slug}` : `Deleting ${postSlug}`
+        })
+    }
+
+    exit()
+}
+
+main(argv._[0], argv._[1], parseInt(argv._[2]), argv._[3])
