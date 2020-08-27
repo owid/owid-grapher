320b5665c6aea386195b16465e3c9a196ddd8d5f
Shahid Ahmad <ahmad.s.shahid@gmail.com>
2020-08-27 01:30:35 -0400

    snap years based on "closest" years

--- charts/controls/Timeline.tsx
+++ charts/controls/Timeline.tsx
@@ -390,7 +390,7 @@ export class Timeline extends React.Component<TimelineProps> {
 
         // if handles within 1 year of each other, snap to closest year.
         if (
-            this.endYear - this.startYear < 1 &&
+            this.startYearClosest === this.endYearClosest &&
             this.startYear !== this.endYear
         ) {
             ;[this.startYearRaw, this.endYearRaw] = [
