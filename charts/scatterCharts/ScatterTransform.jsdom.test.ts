c1e654f26d921696135f68d93731193a9b4ad450
Breck Yunits <breck7@gmail.com>
2020-08-31 09:05:30 -1000

    Dev: start ScatterTransform unit tests

---
+++ charts/scatterCharts/ScatterTransform.jsdom.test.ts
@@ -0,0 +1,12 @@
+#! /usr/bin/env yarn jest
+
+import { ScatterTransform } from "charts/scatterCharts/ScatterTransform"
+import { ChartConfig } from "charts/core/ChartConfig"
+
+describe(ScatterTransform, () => {
+    it("can create a new transform", () => {
+        const chart = new ChartConfig()
+        const scatterT = new ScatterTransform(chart)
+        expect(scatterT.failMessage).toEqual("Missing Y axis variable")
+    })
+})
