b6fbcf2ad0e18ecc5781ccf8f0c1ef3c802fd586
Matthieu Bergel <hello@matthieubergel.org>
2020-09-03 10:33:37 +0200

    chore(testcheck): test for shallow WC for github actions

--- deploy/testcheck.sh
+++ deploy/testcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-# Run pretty-quick in git working directories (e.g. dev) and prettier in others (e.g. yarn deploy live -r)
-if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+# Run pretty-quick in full working directories (e.g. dev) and prettier in others (e.g. CI, "yarn deploy live -r")
+if ! $(git rev-parse --is-shallow-repository) > /dev/null 2>&1; then
   PRETTIFY_CMD="yarn prettify:check"
 else
   PRETTIFY_CMD="yarn prettify-all:check"
