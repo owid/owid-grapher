17197489c87ce527a967eb45d899324e193ad124
Breck Yunits <breck7@gmail.com>
2020-09-03 18:45:00 -1000

    Dev: unnest deploy script if statement

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -25,23 +25,25 @@ elif [ "$NAME" == "live" ]; then
   # Prompt for confirmation if deploying to live
   read -p "Are you sure you want to deploy to '$NAME'? " -n 1 -r
   echo
+  if [[ $REPLY =~ ^[^Yy]$ ]]; then
+    exit 0
+  fi
 else
   echo "Please select either live or a valid test target."
   exit 1
 fi
 
-if [[ $REPLY =~ ^[Yy]$ ]] || [ "$NAME" != "live" ]
-then
-  OLD_REPO_BACKUP="$ROOT/tmp/$NAME-old"
-  SYNC_TARGET="$ROOT/tmp/$NAME-$USER"
-  SYNC_TARGET_TESTS="$ROOT/tmp/$NAME-tests"
-  TMP_NEW="$ROOT/tmp/$NAME-$USER-tmp"
-  FINAL_TARGET="$ROOT/$NAME"
-  FINAL_DATA="$ROOT/$NAME-data"
 
-  GIT_EMAIL="$(git config user.email)"
-  GIT_NAME="$(git config user.name)"
-  GIT_HEAD="$(git rev-parse HEAD)"
+OLD_REPO_BACKUP="$ROOT/tmp/$NAME-old"
+SYNC_TARGET="$ROOT/tmp/$NAME-$USER"
+SYNC_TARGET_TESTS="$ROOT/tmp/$NAME-tests"
+TMP_NEW="$ROOT/tmp/$NAME-$USER-tmp"
+FINAL_TARGET="$ROOT/$NAME"
+FINAL_DATA="$ROOT/$NAME-data"
+
+GIT_EMAIL="$(git config user.email)"
+GIT_NAME="$(git config user.name)"
+GIT_HEAD="$(git rev-parse HEAD)"
 
 if [ "$2" == "-r" ]; then
   # Run pre-deploy checks remotely
@@ -60,55 +62,54 @@ else
 fi
 
 
-  # Write the current commit SHA to public/head.txt so we always know which commit is deployed
-  echo $GIT_HEAD > public/head.txt
+# Write the current commit SHA to public/head.txt so we always know which commit is deployed
+echo $GIT_HEAD > public/head.txt
 
-  # Ensure tmp/ directory exists
-  ssh $HOST mkdir -p $ROOT/tmp
+# Ensure tmp/ directory exists
+ssh $HOST mkdir -p $ROOT/tmp
 
-  # Rsync the local repository to a temporary location on the server
-  $RSYNC $DIR/ $HOST:$SYNC_TARGET
+# Rsync the local repository to a temporary location on the server
+$RSYNC $DIR/ $HOST:$SYNC_TARGET
 
-  ssh -t $HOST 'bash -e -s' <<EOF
-  # Remove any previous temporary repo
-  rm -rf $TMP_NEW
+ssh -t $HOST 'bash -e -s' <<EOF
+# Remove any previous temporary repo
+rm -rf $TMP_NEW
 
-  # Copy the synced repo-- this is because we're about to move it, and we want the
-  # original target to stay around to make future syncs faster
-  cp -r $SYNC_TARGET $TMP_NEW
+# Copy the synced repo-- this is because we're about to move it, and we want the
+# original target to stay around to make future syncs faster
+cp -r $SYNC_TARGET $TMP_NEW
 
-  # Link in all the persistent stuff that needs to stay around between versions
-  ln -sf $FINAL_DATA/.env $TMP_NEW/.env
-  mkdir -p $FINAL_DATA/bakedSite
-  ln -sf $FINAL_DATA/bakedSite $TMP_NEW/bakedSite
-  mkdir -p $FINAL_DATA/datasetsExport
-  ln -sf $FINAL_DATA/datasetsExport $TMP_NEW/datasetsExport
+# Link in all the persistent stuff that needs to stay around between versions
+ln -sf $FINAL_DATA/.env $TMP_NEW/.env
+mkdir -p $FINAL_DATA/bakedSite
+ln -sf $FINAL_DATA/bakedSite $TMP_NEW/bakedSite
+mkdir -p $FINAL_DATA/datasetsExport
+ln -sf $FINAL_DATA/datasetsExport $TMP_NEW/datasetsExport
 
-  # Install dependencies, build assets and migrate
-  cd $TMP_NEW
-  yarn install --production --frozen-lockfile
-  yarn build
-  yarn migrate
-  yarn tsn algolia/configureAlgolia.ts
+# Install dependencies, build assets and migrate
+cd $TMP_NEW
+yarn install --production --frozen-lockfile
+yarn build
+yarn migrate
+yarn tsn algolia/configureAlgolia.ts
 
-  # Create deploy queue file writable by any user
-  touch .queue
-  chmod 0666 .queue
+# Create deploy queue file writable by any user
+touch .queue
+chmod 0666 .queue
 
-  # Atomically swap the old and new versions
-  rm -rf $OLD_REPO_BACKUP
-  mv $FINAL_TARGET $OLD_REPO_BACKUP || true
-  mv $TMP_NEW $FINAL_TARGET
+# Atomically swap the old and new versions
+rm -rf $OLD_REPO_BACKUP
+mv $FINAL_TARGET $OLD_REPO_BACKUP || true
+mv $TMP_NEW $FINAL_TARGET
 
-  # Restart the admin
-  pm2 restart $NAME
-  pm2 stop $NAME-deploy-queue
+# Restart the admin
+pm2 restart $NAME
+pm2 stop $NAME-deploy-queue
 
-  # Static build to update the public frontend code
-  cd $FINAL_TARGET
-  yarn tsn deploy/bakeSite.ts "$GIT_EMAIL" "$GIT_NAME"
+# Static build to update the public frontend code
+cd $FINAL_TARGET
+yarn tsn deploy/bakeSite.ts "$GIT_EMAIL" "$GIT_NAME"
 
-  # Restart the deploy queue
-  pm2 start $NAME-deploy-queue
+# Restart the deploy queue
+pm2 start $NAME-deploy-queue
 EOF
-fi
