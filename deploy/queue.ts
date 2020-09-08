a04eab6d8eb5628aae23a027ca5565ed5e31e70c
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-09-08 12:05:18 +0100

    Make deploy queue function names clearer

--- deploy/queue.ts
+++ deploy/queue.ts
@@ -8,26 +8,25 @@ import { DeployChange, Deploy, DeployStatus } from "./types"
 
 const MAX_SUCCESSIVE_FAILURES = 2
 
-let deploying = false
+/** Whether a deploy is currently running (global variable) */
+let deploying: boolean = false
 
-function identity(x: any) {
-    return x
-}
+// File manipulation
 
-async function readQueueContent(): Promise<string> {
+async function readQueuedFile(): Promise<string> {
     return await fs.readFile(DEPLOY_QUEUE_FILE_PATH, "utf8")
 }
 
-async function readPendingContent(): Promise<string | undefined> {
-    if (fs.existsSync(DEPLOY_PENDING_FILE_PATH)) {
+async function readPendingFile(): Promise<string | undefined> {
+    if (await fs.pathExists(DEPLOY_PENDING_FILE_PATH)) {
         return await fs.readFile(DEPLOY_PENDING_FILE_PATH, "utf8")
     }
     return undefined
 }
 
-async function readQueueAndPendingContent(): Promise<string> {
-    const queueContent = await readQueueContent()
-    const pendingContent = await readPendingContent()
+async function readQueuedAndPendingFiles(): Promise<string> {
+    const queueContent = await readQueuedFile()
+    const pendingContent = await readPendingFile()
     // If any deploys didn't exit cleanly, DEPLOY_PENDING_FILE_PATH would exist.
     // Prepend that message to the current deploy.
     if (pendingContent) {
@@ -37,51 +36,21 @@ async function readQueueAndPendingContent(): Promise<string> {
     }
 }
 
-export async function getDeploys(): Promise<Deploy[]> {
-    const [queueContent, pendingContent] = await Promise.all([
-        readQueueContent(),
-        readPendingContent()
-    ])
-    const deploys: Deploy[] = []
-    if (queueContent) {
-        deploys.push({
-            status: DeployStatus.queued,
-            changes: parseQueueContent(queueContent)
-        })
-    }
-    if (pendingContent) {
-        deploys.push({
-            status: DeployStatus.pending,
-            changes: parseQueueContent(pendingContent)
-        })
-    }
-    return deploys
-}
-
 export async function enqueueDeploy(item: DeployChange) {
     await fs.appendFile(DEPLOY_QUEUE_FILE_PATH, JSON.stringify(item) + "\n")
 }
 
-async function eraseQueueContent() {
+async function clearQueueFile() {
     await fs.truncate(DEPLOY_QUEUE_FILE_PATH, 0)
 }
 
+// Parsing queue content
+
 export async function queueIsEmpty(): Promise<boolean> {
-    return !(await readQueueAndPendingContent())
+    return !(await readQueuedAndPendingFiles())
 }
 
-async function pullQueueContent(): Promise<string> {
-    // Read line-delimited JSON
-    const queueContent = await readQueueAndPendingContent()
-
-    // Truncate file immediately. It's still somewhat possible that another process
-    // writes to this file in the meantime...
-    await eraseQueueContent()
-
-    return queueContent
-}
-
-function parseQueueContent(content: string): DeployChange[] {
+function parseContent(content: string): DeployChange[] {
     // Parse all lines in file as JSON
     return content
         .split("\n")
@@ -92,7 +61,7 @@ function parseQueueContent(content: string): DeployChange[] {
                 return null
             }
         })
-        .filter(identity)
+        .filter(x => x)
 }
 
 function generateCommitMsg(queueItems: DeployChange[]): string {
@@ -113,23 +82,47 @@ function generateCommitMsg(queueItems: DeployChange[]): string {
     return `Deploy ${date}\n${message}\n\n\n${coauthors}`
 }
 
+export async function getDeploys(): Promise<Deploy[]> {
+    const [queueContent, pendingContent] = await Promise.all([
+        readQueuedFile(),
+        readPendingFile()
+    ])
+    const deploys: Deploy[] = []
+    if (queueContent) {
+        deploys.push({
+            status: DeployStatus.queued,
+            changes: parseContent(queueContent)
+        })
+    }
+    if (pendingContent) {
+        deploys.push({
+            status: DeployStatus.pending,
+            changes: parseContent(pendingContent)
+        })
+    }
+    return deploys
+}
+
 export async function triggerDeploy() {
     if (!deploying) {
         deploying = true
         let failures = 0
         while (!(await queueIsEmpty()) && failures < MAX_SUCCESSIVE_FAILURES) {
-            const deployContent = await pullQueueContent()
+            const deployContent = await readQueuedAndPendingFiles()
+            // Truncate file immediately. Ideally this would be an atomic action, otherwise it's
+            // possible that another process writes to this file in the meantime...
+            await clearQueueFile()
             // Write to `.deploying` file to be able to recover the deploy message
             // in case of failure.
             await fs.writeFile(DEPLOY_PENDING_FILE_PATH, deployContent)
-            const message = generateCommitMsg(parseQueueContent(deployContent))
+            const message = generateCommitMsg(parseContent(deployContent))
             console.log(`Deploying site...\n---\n${message}\n---`)
             try {
                 await deploy(message)
                 await fs.unlink(DEPLOY_PENDING_FILE_PATH)
             } catch (err) {
                 failures++
-                // The error will be logged and sent to Slack.
+                // The error is already sent to Slack inside the deploy() function.
                 // The deploy will be retried unless we've reached MAX_SUCCESSIVE_FAILURES.
             }
         }
