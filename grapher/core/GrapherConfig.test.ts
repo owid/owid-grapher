b5df47cc2cd591162860ca9ba9c9ed3ec630eadd
Breck Yunits <breck7@gmail.com>
2020-09-08 12:29:48 -1000

    Dev: add constructor to GrapherConfig and test

--- grapher/core/GrapherConfig.test.ts
+++ grapher/core/GrapherConfig.test.ts
@@ -8,4 +8,10 @@ describe(PersistableGrapher, () => {
         const json = script.toObject()
         expect(Object.keys(json).length).toBe(0)
     })
+
+    it("does not preserve defaults in the object", () => {
+        const script = new PersistableGrapher({ tab: "chart" })
+        const json = script.toObject()
+        expect(Object.keys(json).length).toBe(0)
+    })
 })
