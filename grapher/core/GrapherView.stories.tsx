50d5bb209ad232f944f5093a782d192cb70b9493
Breck Yunits <breck7@gmail.com>
2020-09-07 21:06:19 -1000

    Fix: add NoData scss link and fix GrapherView story

--- grapher/core/GrapherView.stories.tsx
+++ grapher/core/GrapherView.stories.tsx
@@ -4,56 +4,7 @@ import { observer } from "mobx-react"
 
 import { Bounds } from "grapher/utils/Bounds"
 import { GrapherView } from "grapher/core/GrapherView"
-import { GrapherConfigInterface } from "grapher/core/GrapherConfig"
-import { Grapher } from "grapher/core/Grapher"
-
-const variableSet = {
-    variables: {
-        "66287": {
-            years: [1543, 1548],
-            entities: [1, 1],
-            values: [32, 33],
-            id: 66287,
-            name:
-                "Life expectancy (Clio-Infra up to 1949; UN Population Division for 1950 to 2015)",
-            unit: "",
-            description:
-                "Clio-Infra data measures life expectancy at birth for the total population per country and year. UN Population Division figures estimate life expectancy at birth for both sexes combined.",
-            createdAt: "2018-01-04T15:25:44.000Z",
-            updatedAt: "2018-02-28T08:59:09.000Z",
-            code: null,
-            coverage: "",
-            timespan: "",
-            datasetId: 1892,
-            sourceId: 13901,
-            shortUnit: null,
-            display: {},
-            columnOrder: 0,
-            originalMetadata: null,
-            datasetName:
-                "Total Life Expectancy - Clio-Infra up to 1949 and UN figures from 1950 onwards (2015)",
-            s_id: 13901,
-            s_name:
-                "Clio-Infra estimates until 1949; UN Population Division from 1950 to 2015",
-            source: {
-                id: 13901,
-                name:
-                    "Clio-Infra estimates until 1949; UN Population Division from 1950 to 2015",
-                dataPublishedBy:
-                    "OWID has combined two data sources: Clio-Infra's dataset for life expectancy up until 1949; and the UN Population Division for data from 1950 to 2015.",
-                dataPublisherSource: "Census data.",
-                link:
-                    "https://datasets.socialhistory.org/dataset.xhtml?persistentId=hdl:10622/LKYT53  and https://esa.un.org/unpd/wpp/Download/Standard/Population/",
-                retrievedData: "",
-                additionalInfo:
-                    "For historical data up until 1949, Clio-Infra's dataset compiled by Zijdeman and Ribeira da Silva (2015) was used. Further information can be found using this link: https://datasets.socialhistory.org/dataset.xhtml?persistentId=hdl:10622/LKYT53. \nData from 1950 onward uses the UN Population Division data, further information can be found at https://esa.un.org/unpd/wpp/Download/Standard/Population/",
-            },
-        },
-    },
-    entityKey: {
-        "1": { name: "United Kingdom", code: "GBR" },
-    },
-}
+import { basicGdpGrapher } from "grapher/test/samples"
 
 // Wrapper for GrapherView that uses css on figure element to determine the bounds
 @observer
@@ -77,15 +28,7 @@ class GrapherViewStory extends React.Component {
     }
 
     render() {
-        const chart = new Grapher(
-            {
-                hasMapTab: true,
-                dimensions: [{ property: "y", variableId: 66287, display: {} }],
-                manuallyProvideData: true,
-            } as GrapherConfigInterface,
-            {}
-        )
-        chart.receiveData(variableSet as any)
+        const grapher = basicGdpGrapher()
 
         return (
             <figure
@@ -94,7 +37,7 @@ class GrapherViewStory extends React.Component {
                 ref={this.base}
             >
                 {this.bounds && (
-                    <GrapherView grapher={chart} bounds={this.bounds} />
+                    <GrapherView grapher={grapher} bounds={this.bounds} />
                 )}
             </figure>
         )
