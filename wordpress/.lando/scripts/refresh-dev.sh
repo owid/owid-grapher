2d757c1aa76335bc592220ca249f30f7755ef166
Matthieu Bergel <hello@matthieubergel.org>
2020-10-01 14:23:11 +0200

    chore(refresh): rename refresh scripts

--- wordpress/.lando/scripts/refresh-dev.sh
+++ wordpress/.lando/scripts/refresh-dev.sh
@@ -1,21 +1,16 @@
 #!/bin/bash -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+##################################################
+# Refresh dev from live content (run from Lando) #
+##################################################
 
-set -a && source $DIR/.env && set +a
-
-if [ "${WP_ENV}" != "staging" ]; then
-  echo "Please only run on staging."
-  exit 1
-fi
-
+# env variables are set by lando's env_file directive
 WORDPRESS_DB_HOST=$DB_HOST
 WORDPRESS_DB_NAME=$DB_NAME
-GRAPHER_DB_HOST=$GRAPHER_DB_HOST
-GRAPHER_DB_NAME=$GRAPHER_DB_NAME
-STAGING_SERVER_NAME=$(basename $DIR | cut -d '-' -f1)
-MYSQL="sudo mysql --default-character-set=utf8mb4"
-DL_FOLDER="/tmp"
+GRAPHER_DB_HOST=database-grapher
+GRAPHER_DB_NAME=grapher
+MYSQL="mysql -u root --default-character-set=utf8mb4"
+DL_FOLDER="."
 
 # Default options
 WITH_UPLOADS=false
@@ -43,7 +38,7 @@ while [ "$1" != "" ]; do
     -h | --help)
       usage
       exit
-      ;;    
+      ;;
     -s | --skip-db-dl)
       SKIP_DB_DL=true
       ;;
@@ -62,47 +57,46 @@ while [ "$1" != "" ]; do
     shift
 done
 
+
 purge_db(){
   $MYSQL -h $1 -e "DROP DATABASE $2;CREATE DATABASE $2"
 }
 
 import_db(){
-  pv $1 | $MYSQL -h $2 $3
+  pv $1 | gunzip | $MYSQL -h $2 $3
 }
 
 # Wordpress DB
 if [ "${SKIP_DB_DL}" = false ]; then
   echo "Downloading Wordress database (live_wordpress)"
-  ssh owid-live "sudo mysqldump --default-character-set=utf8mb4 live_wordpress -r /tmp/live_wordpress.sql"
-  rsync -hav --progress owid-live:/tmp/live_wordpress.sql $DL_FOLDER
+    ssh owid-live "sudo mysqldump --default-character-set=utf8mb4 live_wordpress -r /tmp/live_wordpress.sql && sudo gzip -f /tmp/live_wordpress.sql"
+    rsync -hav --progress owid-live:/tmp/live_wordpress.sql.gz $DL_FOLDER
 fi
 echo "Importing Wordress database (live_wordpress)"
 purge_db $WORDPRESS_DB_HOST $WORDPRESS_DB_NAME
-import_db $DL_FOLDER/live_wordpress.sql $WORDPRESS_DB_HOST $WORDPRESS_DB_NAME
+import_db $DL_FOLDER/live_wordpress.sql.gz $WORDPRESS_DB_HOST $WORDPRESS_DB_NAME
 
 # Wordpress uploads
 if [ "${WITH_UPLOADS}" = true ]; then
   echo "Downloading Wordress uploads"
-  rsync -hav --delete --progress owid-live:live-data/wordpress/uploads/ ~/$STAGING_SERVER_NAME-data/wordpress/uploads
+  rsync -havz --delete --exclude=/.gitkeep --progress owid-live:live-data/wordpress/uploads/ web/app/uploads
 fi
 
 # Grapher database (owid_metadata)
 if [ "${SKIP_DB_DL}" = false ]; then
   echo "Downloading live Grapher metadata database (owid_metadata)"
-  ssh owid-live "cd live && yarn tsn db/exportMetadata.ts --with-passwords /tmp/owid_metadata_with_passwords.sql"
-  rsync -hav --progress owid-live:/tmp/owid_metadata_with_passwords.sql $DL_FOLDER
+  curl -Lo $DL_FOLDER/owid_metadata.sql.gz https://files.ourworldindata.org/owid_metadata.sql.gz
 fi
 echo "Importing live Grapher metadata database (owid_metadata)"
 purge_db $GRAPHER_DB_HOST $GRAPHER_DB_NAME
-import_db $DL_FOLDER/owid_metadata_with_passwords.sql $GRAPHER_DB_HOST $GRAPHER_DB_NAME
+import_db $DL_FOLDER/owid_metadata.sql.gz $GRAPHER_DB_HOST $GRAPHER_DB_NAME
 
 # Grapher database (owid_chartdata)
 if [ "${WITH_CHARTDATA}" = true ]; then
   if [ "${SKIP_DB_DL}" = false ]; then
     echo "Downloading live Grapher chartdata database (owid_chartdata)"
     curl -Lo $DL_FOLDER/owid_chartdata.sql.gz https://files.ourworldindata.org/owid_chartdata.sql.gz
-    gunzip -f $DL_FOLDER/owid_chartdata.sql.gz
   fi
   echo "Importing live Grapher chartdata database (owid_chartdata)"
-  import_db $DL_FOLDER/owid_chartdata.sql $GRAPHER_DB_HOST $GRAPHER_DB_NAME
+  import_db $DL_FOLDER/owid_chartdata.sql.gz $GRAPHER_DB_HOST $GRAPHER_DB_NAME
 fi
