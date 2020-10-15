2ab8656c8e6712221da0237914d5325dcd67e8ee
Breck Yunits <breck7@gmail.com>
2020-10-15 11:28:13 -1000

    Fix: remove line in postcss config that was causing Invalid PostCSS Plugin found at: plugins[0]

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import { addParameters } from "@storybook/client-api"
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport"
 import "site/client/owid.scss"
 import "grapher/core/grapher.scss"
+import "handsontable/dist/handsontable.full.css"
 
 addParameters({
     viewport: {
