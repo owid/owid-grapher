7850eccec4e53d80d7556176d4b5468cfa08a904
Matthieu Bergel <hello@matthieubergel.org>
2020-10-22 14:50:29 +0200

    feat: hide site tools on scroll

--- site/client/SiteTools.tsx
+++ site/client/SiteTools.tsx
@@ -1,25 +1,37 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { FeedbackPrompt } from "./Feedback"
+import { ScrollDirection, useScrollDirection } from "./hooks"
 import {
     NewsletterSubscription,
     NewsletterSubscriptionForm,
     NewsletterSubscriptionContext,
 } from "./NewsletterSubscription"
 
+const SITE_TOOLS_CLASS = "site-tools"
+
 const SiteTools = () => {
+    const scrollDirection = useScrollDirection()
+
     return (
-        <>
+        <div
+            className={`hide-wrapper${
+                (scrollDirection === ScrollDirection.Down && " hide") || ""
+            }`}
+        >
             <NewsletterSubscription
                 context={NewsletterSubscriptionContext.Floating}
             />
             <FeedbackPrompt />
-        </>
+        </div>
     )
 }
 
 export const runSiteTools = () => {
-    ReactDOM.render(<SiteTools />, document.querySelector(".site-tools"))
+    ReactDOM.render(
+        <SiteTools />,
+        document.querySelector(`.${SITE_TOOLS_CLASS}`)
+    )
 
     const newsletterSubscriptionFormRootHomepage = document.querySelector(
         ".homepage-subscribe .newsletter-subscription .root"
