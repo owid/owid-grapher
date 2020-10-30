6c14eee701f236c866c19afb706dc680b04cd87c
Matthieu Bergel <hello@matthieubergel.org>
2020-10-30 07:55:55 +0100

    enhance(glossary): smoother visual transition to "hydrated" component

--- site/server/formatGlossary.tsx
+++ site/server/formatGlossary.tsx
@@ -26,7 +26,9 @@ export const GlossaryLink = ({
                 __html: JSON.stringify({ slug, excerpt }),
             }}
         ></script>
-        <a href={`/glossary#${slug}`}>{match}</a>
+        <a className="expandable-block-button" href={`/glossary#${slug}`}>
+            {match}
+        </a>
     </span>
 )
 
