e476a9889492808c7d6df1383af9060bec21ea42
Breck Yunits <breck7@gmail.com>
2020-10-30 11:57:28 -1000

    Perf: do not use ckmeans if more than 10k values

--- grapher/color/BinningStrategies.ts
+++ grapher/color/BinningStrategies.ts
@@ -63,7 +63,8 @@ export function getBinMaximums(args: GetBinMaximumsWithStrategyArgs): number[] {
 
     if (valueCount < 1 || binCount < 1) return []
 
-    if (binningStrategy === BinningStrategy.ckmeans) {
+    // ckmeans is currently (10/30/2020) n^2, so do not use it if more than 1e4 values
+    if (binningStrategy === BinningStrategy.ckmeans && valueCount < 1e4) {
         const clusters = ckmeans(
             sortedValues,
             binCount > valueCount ? valueCount : binCount
