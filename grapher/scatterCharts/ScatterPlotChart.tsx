9f6bb9bc88016eb08d783edb6027932211935817
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-10-30 16:38:36 +0000

    ScatterPlot color & size columns tolerance fixes

--- grapher/scatterCharts/ScatterPlotChart.tsx
+++ grapher/scatterCharts/ScatterPlotChart.tsx
@@ -80,9 +80,7 @@ export class ScatterPlotChart
 
     transformTable(table: OwidTable) {
         if (this.manager.matchingEntitiesOnly && this.colorColumnSlug)
-            table = table.dropRowsWithInvalidValuesForColumn(
-                this.colorColumnSlug
-            )
+            table = table.dropRowsWithErrorValuesForColumn(this.colorColumnSlug)
 
         if (this.manager.excludedEntities) {
             const excludedEntityIdsSet = new Set(this.manager.excludedEntities)
@@ -101,22 +99,21 @@ export class ScatterPlotChart
         if (this.yScaleType === ScaleType.log && this.yColumnSlug)
             table = table.replaceNonPositiveCellsForLogScale([this.yColumnSlug])
 
-        if (this.colorColumnSlug) {
-            table = table.interpolateColumnWithTolerance(
-                this.colorColumnSlug,
-                Infinity
-            )
-        }
-
         // To avoid injecting unnecessary rows in the interpolateColumnWithTolerance() transform,
         // we drop any rows which are blank for both X and Y values.
         // The common case is that the Population data goes back to 10,000 BCE and in almost every
         // case we don't have that data for X and Y.
         // -@danielgavrilov, 2020-10-22
-        table = table.dropRowsWithErrorValuesForAllColumns([
-            this.xColumnSlug,
-            this.yColumnSlug,
-        ])
+        //
+        // UPDATE: We cannot drop rows here, because it may lead to missing color/size data - those
+        // columns usually have Infinity tolerance. Leaving this note as we'll probably try to
+        // optimize this later.
+        // -@danielgavrilov, 2020-10-30
+        //
+        // table = table.dropRowsWithErrorValuesForAllColumns([
+        //     this.xColumnSlug,
+        //     this.yColumnSlug,
+        // ])
 
         if (this.xColumnSlug) {
             table = table.interpolateColumnWithTolerance(this.xColumnSlug)
@@ -126,7 +123,25 @@ export class ScatterPlotChart
             table = table.interpolateColumnWithTolerance(this.yColumnSlug)
         }
 
-        // Drop any rows which have an ErrorValues for either X or Y.
+        if (this.sizeColumnSlug) {
+            // The tolerance on the size column is ignored. If we want to change this in the future
+            // we need to check all charts for regressions.
+            table = table.interpolateColumnWithTolerance(
+                this.sizeColumnSlug,
+                Infinity
+            )
+        }
+
+        if (this.colorColumnSlug) {
+            const tolerance =
+                table.get(this.colorColumnSlug)?.display.tolerance ?? Infinity
+            table = table.interpolateColumnWithTolerance(
+                this.colorColumnSlug,
+                tolerance
+            )
+        }
+
+        // Drop any rows which have an invalid cell for either X or Y.
         // This needs to be done after the tolerance, because the tolerance may fill in some gaps.
         table = table
             .columnFilter(
@@ -560,8 +575,12 @@ export class ScatterPlotChart
         return this.transformedTable.get(this.xColumnSlug)
     }
 
+    @computed private get sizeColumnSlug() {
+        return this.manager.sizeColumnSlug
+    }
+
     @computed private get sizeColumn() {
-        return this.transformedTable.get(this.manager.sizeColumnSlug)
+        return this.transformedTable.get(this.sizeColumnSlug)
     }
 
     @computed get failMessage() {
