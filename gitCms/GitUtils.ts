35763157b0b7157a0939e949b66b0a6645ae9026
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-11-09 12:37:21 +0000

    Fix: getUserInfo() changes git config

--- gitCms/GitUtils.ts
+++ gitCms/GitUtils.ts
@@ -25,9 +25,21 @@ export const pullAndRebaseFromGit = async (dir: string) =>
     await execFormatted(`cd %s && git pull --rebase`, [dir], false)
 
 export const gitUserInfo = async (dir: string) => {
-    const email = await execFormatted("git config user.email", [dir], false)
-    const name = await execFormatted("git config user.name", [dir], false)
-    const head = await execFormatted("git rev-parse HEAD", [dir], false)
+    const email = await execFormatted(
+        "cd %s && git config user.email",
+        [dir],
+        false
+    )
+    const name = await execFormatted(
+        "cd %s && git config user.name",
+        [dir],
+        false
+    )
+    const head = await execFormatted(
+        "cd %s && git rev-parse HEAD",
+        [dir],
+        false
+    )
     return {
         email: email.stdout.trim(),
         name: name.stdout.trim(),
