83a28bc0c96a4d2ab41756e557953ad1138d70b2
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-11-16 11:49:08 +0000

    Update COVID annotations

--- explorer/covidExplorer/CovidConstants.ts
+++ explorer/covidExplorer/CovidConstants.ts
@@ -188,7 +188,8 @@ export const intervalsAvailableByMetric: Map<
 
 // todo: auto import from covid repo.
 export const covidAnnotations = `location,date,cases_annotations,deaths_annotations
-Spain,2020-11-04,,earlier deaths added`
+Spain,2020-11-04,,earlier deaths added
+Sweden,,,recent days may undercount deaths`
 
 // https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data-codebook.md
 export interface ParsedCovidCsvRow {
