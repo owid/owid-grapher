d46c962350d65adea1ec3f9d73f2126f34575b4c
Breck Yunits <breck7@gmail.com>
2020-11-21 13:09:29 -1000

    Dev: start tests file for url utils

---
+++ utils/client/url.test.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env yarn jest
+
+import { queryParamsToStr, strToQueryParams } from "./url"
+
+it("encodes correctly", () => {
+    const pairs = [
+        {
+            str: "?foo=bar",
+            params: {
+                foo: "bar",
+            },
+        },
+    ]
+
+    pairs.forEach((pair) => {
+        expect(queryParamsToStr(pair.params)).toEqual(pair.str)
+        expect(strToQueryParams(pair.str)).toEqual(pair.params)
+    })
+})
