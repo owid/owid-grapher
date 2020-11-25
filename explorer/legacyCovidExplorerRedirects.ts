b1013ee2d304670affd1e6424d30f22c46b7413a
Breck Yunits <breck7@gmail.com>
2020-11-25 10:17:18 -1000

    Dev: add comments and list of old covid urls that need to be supported

--- explorer/legacyCovidExplorerRedirects.ts
+++ explorer/legacyCovidExplorerRedirects.ts
@@ -56,6 +56,68 @@ export const legacyGrapherToCovidExplorerRedirectTable = new CoreTable(
     redirectTableTsv
 )
 
+// In addition to the query strings above, the below ones are ones we need to redirect to the new Covid Explorer.
+// It is about 80 different ones, but it may just be like a 10 liner map function that takes old params and converts
+// Test case:
+//
+// https://ourworldindata.org/coronavirus-data-explorer?country=~FRA&cfrMetric=true
+//
+// should redirect to:
+//
+// https://staging.owid.cloud/admin/explorers/preview/coronavirus-data-explorer?patch=selection-is-France-and-Metric%20Radio-is-Case%20Fatality%20Rate
+//
+// Obviously the final details of the new link will change, but if we have tests and do strings as consts should be easy to finalize once we settle on the new URL details.
+
+const queryStringsToRedirect = `casesMetric=true&interval=daily&aligned=true&perCapita=true&smoothing=0
+casesMetric=true&interval=daily&perCapita=true&smoothing=0
+casesMetric=true&interval=daily&aligned=true&smoothing=0
+casesMetric=true&interval=daily&smoothing=0
+casesMetric=true&interval=weekly&smoothing=7
+casesMetric=true&interval=total&aligned=true&perCapita=true&smoothing=0
+casesMetric=true&interval=total&perCapita=true&smoothing=0
+casesMetric=true&interval=total&aligned=true&smoothing=0
+casesMetric=true&interval=total&smoothing=0
+casesMetric=true&interval=smoothed&aligned=true&perCapita=true&smoothing=7
+casesMetric=true&interval=smoothed&perCapita=true&smoothing=7
+casesMetric=true&interval=smoothed&aligned=true&smoothing=7
+casesMetric=true&interval=smoothed&smoothing=7
+casesMetric=true&interval=biweekly&smoothing=14
+casesMetric=true&interval=weeklyChange&smoothing=7
+casesMetric=true&interval=biweeklyChange&smoothing=14
+deathsMetric=true&interval=daily&aligned=true&perCapita=true&smoothing=0
+deathsMetric=true&interval=daily&perCapita=true&smoothing=0
+deathsMetric=true&interval=daily&aligned=true&smoothing=0
+deathsMetric=true&interval=daily&smoothing=0
+deathsMetric=true&interval=weekly&smoothing=7
+deathsMetric=true&interval=total&aligned=true&perCapita=true&smoothing=0
+deathsMetric=true&interval=total&perCapita=true&smoothing=0
+deathsMetric=true&interval=total&aligned=true&smoothing=0
+deathsMetric=true&interval=total&smoothing=0
+deathsMetric=true&interval=smoothed&aligned=true&perCapita=true&smoothing=7
+deathsMetric=true&interval=smoothed&perCapita=true&smoothing=7
+deathsMetric=true&interval=smoothed&aligned=true&smoothing=7
+deathsMetric=true&interval=smoothed&smoothing=7
+deathsMetric=true&interval=biweekly&smoothing=14
+deathsMetric=true&interval=weeklyChange&smoothing=7
+deathsMetric=true&interval=biweeklyChange&smoothing=14
+cfrMetric=true&interval=total&smoothing=0
+testsMetric=true&interval=daily&aligned=true&perCapita=true&smoothing=0
+testsMetric=true&interval=daily&perCapita=true&smoothing=0
+testsMetric=true&interval=daily&aligned=true&smoothing=0
+testsMetric=true&interval=daily&smoothing=0
+testsMetric=true&interval=total&aligned=true&perCapita=true&smoothing=0
+testsMetric=true&interval=total&perCapita=true&smoothing=0
+testsMetric=true&interval=total&aligned=true&smoothing=0
+testsMetric=true&interval=total&smoothing=0
+testsMetric=true&interval=smoothed&aligned=true&perCapita=true&smoothing=7
+testsMetric=true&interval=smoothed&perCapita=true&smoothing=7
+testsMetric=true&interval=smoothed&aligned=true&smoothing=7
+testsMetric=true&interval=smoothed&smoothing=7
+testsPerCaseMetric=true&interval=smoothed&smoothing=7
+testsPerCaseMetric=true&interval=total&smoothing=0
+positiveTestRate=true&interval=smoothed&smoothing=7
+positiveTestRate=true&interval=total&smoothing=0`
+
 let cached: ExplorerProgram
 // todo: remove
 export const getLegacyCovidExplorerAsExplorerProgramForSlug = async (
