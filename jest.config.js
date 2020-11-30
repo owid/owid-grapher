7350bc31ff241b1ab5f80632e7268df9d9009ca0
Breck Yunits <breck7@gmail.com>
2020-11-30 11:38:56 -1000

    Dev: just for fun

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,14 @@
-// Build the project JS first, and then test the JS. that should be the default, since it is always faster.
-// todo: restore the other slow option as a secondary option—passing a ts file to jest directly
-// const common = {
-//     moduleNameMapper: {
-//         "^(site|grapher|gitCms|explorer|coreTable|gridLang|clientUtils|adminSiteServer|db|deploy|settings)/(.*)$":
-//             "<rootDir>/itsJustJavascript/$1/$2",
-//     },
-// }
-
 module.exports = {
     testTimeout: 1000,
     projects: [
         {
-            // ...common,
-            displayName: "node",
+            displayName: { name: "node", color: "magenta" },
             testEnvironment: "node",
             testPathIgnorePatterns: [".jsdom.test."],
             testMatch: ["**/*.test.(jsx|js)"],
         },
         {
-            // ...common,
-            displayName: "jsdom",
+            displayName: { name: "jsdom", color: "cyan" },
             testEnvironment: "jsdom",
             testMatch: ["**/*.jsdom.test.(jsx|js)"],
         },
