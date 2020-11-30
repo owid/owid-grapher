d821d46409157fdd1aad39a72db1b6307cadbd88
Breck Yunits <breck7@gmail.com>
2020-11-30 05:22:51 -1000

    All tests pass

---
+++ explorer/stories.jsdom.test.tsx
@@ -0,0 +1,26 @@
+#! /usr/bin/env jest
+import { configure, mount } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+configure({ adapter: new Adapter() })
+
+// This just does a sanity check that all the stories can mount.
+// This file might not be necessary as there may be a way to do something similar with Storybook/Jest.
+// For now, to get a list of all stories for updating this file:
+// git ls-tree -r master --name-only | grep .stories.tsx | sed 's/.tsx//'
+
+import * as Explorer from "explorer/Explorer.stories"
+
+const runTests = (storybook: any) => {
+    const defaults = storybook.default
+    Object.keys(storybook).forEach((key) => {
+        if (key === "default") return
+        describe(defaults.title, () => {
+            const args = {}
+            it(`should load ${key}`, () => {
+                expect(mount(storybook[key](args))).toBeTruthy()
+            })
+        })
+    })
+}
+
+runTests(Explorer)
