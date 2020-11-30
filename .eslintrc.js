72a7a152cd3917f475247ab9ee107a58d100eab7
Breck Yunits <breck7@gmail.com>
2020-11-30 13:02:51 -1000

    Eslint: remove warning for relative imports since we need to use those now

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,12 +39,6 @@ module.exports = {
         "@typescript-eslint/no-use-before-define": "off",
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/restrict-plus-operands": "warn",
-        "no-restricted-imports": [
-            "warn",
-            {
-                patterns: ["../*"],
-            },
-        ],
         "no-console": ["warn", { allow: ["warn", "error"] }],
         "no-var": "off",
         "prefer-const": ["warn", { destructuring: "all" }],
