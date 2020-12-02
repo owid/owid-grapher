bfdb719934d36e86e0aa23025630984e42aeec24
Breck Yunits <breck7@gmail.com>
2020-12-01 14:06:00 -1000

    Dev: build fixes

--- gitCms/GitCmsConstants.ts
+++ gitCms/GitCmsConstants.ts
@@ -1,9 +1,11 @@
 export const GIT_CMS_FILE_ROUTE = "/git-cms"
 export const GIT_CMS_DEFAULT_BRANCH = "master"
 export const GIT_CMS_REPO_URL = `https://github.com/owid/owid-content`
-export const GIT_CMS_DIR = __dirname + "/../../owid-content"
 export const GIT_CMS_PULL_ROUTE = `${GIT_CMS_FILE_ROUTE}/pull`
 export const GIT_CMS_GLOB_ROUTE = `${GIT_CMS_FILE_ROUTE}/glob`
+
+// todo: refactor GitCmsServer to be a class, and pass this in as a top level param
+export const GIT_CMS_DIR = __dirname + "/../../../owid-content"
 
 export interface WriteRequest {
     filepath: string
