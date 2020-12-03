cb6242e1e502ff0a8a4823cd56a9e20f6389ebd4
Breck Yunits <breck7@gmail.com>
2020-12-02 18:58:38 -1000

    Fix: dev baseDir path

--- settings/serverSettings.ts
+++ settings/serverSettings.ts
@@ -7,7 +7,7 @@ const DB_USER = "root"
 const DB_PASS = ""
 const DB_HOST = "localhost"
 const DB_PORT = 3306
-const BASE_DIR = __dirname + "/../"
+const BASE_DIR = __dirname + "/../../"
 
 const defaultSettings = {
     ENV: "development",
