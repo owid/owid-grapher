f3e0855daf58f4dc6646aece2d9ea1dbf3a28b1e
Breck Yunits <breck7@gmail.com>
2020-12-03 16:59:58 -1000

    Dev: bake local script for testing bakes locally

---
+++ baker/bakeLocal.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import parseArgs from "minimist"
+import { SiteBaker } from "./SiteBaker"
+
+const bakeDomainToFolder = async (baseUrl: string, dir: string) => {
+    const baker = new SiteBaker(dir, baseUrl)
+    console.log(`Baking site with baseUrl '${baseUrl}' to dir '${dir}'`)
+    await baker.bakeAll()
+}
+
+const args = parseArgs(process.argv.slice(2))
+const theArgs = args._
+if (theArgs.length !== 2) {
+    console.error(`Usage: yarn bake-custom http://owid.org /home/user/owid.org`)
+    process.exit()
+}
+
+bakeDomainToFolder(theArgs[0], theArgs[1])
