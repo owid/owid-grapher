b1c82a39fa8fa27d46f66fe0ef90b85972bdded6
Breck Yunits <breck7@gmail.com>
2020-12-08 05:45:05 -1000

    Dev: add test

--- gitCms/GitCmsClient.test.ts
+++ gitCms/GitCmsClient.test.ts
@@ -5,3 +5,11 @@ import { GitCmsClient } from "./GitCmsClient"
 it("can init", () => {
     expect(new GitCmsClient()).toBeTruthy()
 })
+
+it("validates input", async () => {
+    try {
+        await new GitCmsClient().deleteRemoteFile({ filepath: "foo~bar" })
+    } catch (err) {
+        expect(err).toBeTruthy()
+    }
+})
