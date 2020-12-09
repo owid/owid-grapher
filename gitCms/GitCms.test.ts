9313944d321e5e038fa27eb28557be23a49bd113
Breck Yunits <breck7@gmail.com>
2020-12-08 14:57:55 -1000

    Dev: try catch cleanup

--- gitCms/GitCms.test.ts
+++ gitCms/GitCms.test.ts
@@ -9,93 +9,89 @@ import * as nodeFetch from "node-fetch"
 
 describe("client/server integration tests", () => {
     const baseDir = __dirname + "/integrationTestTempDirectoryOkToDelete"
-    try {
-        // Arrange
-        const testPort = 3456
-        const expressApp = express()
-        expressApp.use(bodyParser.json())
 
-        const server = new GitCmsServer({ baseDir })
-        server.verbose = false
-        server.addToRouter(expressApp)
-        const expressServer = expressApp.listen(testPort)
+    // Arrange
+    const testPort = 3456
+    const expressApp = express()
+    expressApp.use(bodyParser.json())
 
-        beforeAll(async () => {
-            // todo: whats a better pattern?
-            const glob = global as any
-            glob.fetch = nodeFetch
+    const server = new GitCmsServer({ baseDir })
+    server.verbose = false
+    server.addToRouter(expressApp)
+    const expressServer = expressApp.listen(testPort)
 
-            await server.createDirAndInitIfNeeded()
-        })
+    beforeAll(async () => {
+        // todo: whats a better pattern?
+        const glob = global as any
+        glob.fetch = nodeFetch
 
-        afterAll(() => {
-            const glob = global as any
-            glob.fetch = undefined
-            expressServer.close()
-        })
+        await server.createDirAndInitIfNeeded()
+    })
 
-        const client = new GitCmsClient(`http://localhost:${testPort}`)
-        const filepath = "foo.txt"
-        const content = "bar"
-        expect(server).toBeTruthy()
-
-        it("can write a file", async () => {
-            const response = await client.writeRemoteFile({
-                filepath,
-                content,
-                commitMessage: "created file",
-            })
-            if (!response.success) console.log(response) // Dump for easier debugging in CI
-            expect(response.success).toBeTruthy()
-        })
-
-        it("fails write gracefully when given a bad path", async () => {
-            const response = await client.writeRemoteFile({
-                filepath: "../badpath",
-                content,
-                commitMessage: "test writing a bad path",
-            })
-            expect(response.success).toBeFalsy()
-        })
-
-        it("can read a file", async () => {
-            const response = await client.readRemoteFile({ filepath })
-            expect(response.success).toBeTruthy()
-            expect(response.content).toEqual(content)
-        })
-
-        it("can read multiple files", async () => {
-            const response = await client.readRemoteFiles({
-                glob: "*.txt",
-                folder: "",
-            })
-            expect(response.success).toBeTruthy()
-            expect(response.files.length).toEqual(1)
-            expect(response.files[0].content).toEqual(content)
-        })
-
-        it("can fail reading gracefully", async () => {
-            const response = await client.readRemoteFile({
-                filepath: "fail.txt",
-            })
-            expect(response.success).toBeFalsy()
-        })
-
-        it("can delete a file", async () => {
-            const response = await client.deleteRemoteFile({ filepath })
-            if (!response.success) console.log(response) // Dump for easier debugging in CI
-            expect(response.success).toBeTruthy()
-        })
-
-        it("can fail delete gracefully", async () => {
-            const response = await client.deleteRemoteFile({
-                filepath,
-            })
-            expect(response.success).toBeFalsy()
-        })
-    } catch (err) {
-        console.error(err)
-    } finally {
+    afterAll(() => {
+        const glob = global as any
+        glob.fetch = undefined
+        expressServer.close()
         removeSync(baseDir)
-    }
+    })
+
+    const client = new GitCmsClient(`http://localhost:${testPort}`)
+    const filepath = "foo.txt"
+    const content = "bar"
+    expect(server).toBeTruthy()
+
+    it("can write a file", async () => {
+        const response = await client.writeRemoteFile({
+            filepath,
+            content,
+            commitMessage: "created file",
+        })
+        if (!response.success) console.log(response) // Dump for easier debugging in CI
+        expect(response.success).toBeTruthy()
+    })
+
+    it("fails write gracefully when given a bad path", async () => {
+        const response = await client.writeRemoteFile({
+            filepath: "../badpath",
+            content,
+            commitMessage: "test writing a bad path",
+        })
+        expect(response.success).toBeFalsy()
+    })
+
+    it("can read a file", async () => {
+        const response = await client.readRemoteFile({ filepath })
+        expect(response.success).toBeTruthy()
+        expect(response.content).toEqual(content)
+    })
+
+    it("can read multiple files", async () => {
+        const response = await client.readRemoteFiles({
+            glob: "*.txt",
+            folder: "",
+        })
+        expect(response.success).toBeTruthy()
+        expect(response.files.length).toEqual(1)
+        expect(response.files[0].content).toEqual(content)
+    })
+
+    it("can fail reading gracefully", async () => {
+        const response = await client.readRemoteFile({
+            filepath: "fail.txt",
+        })
+        expect(response.success).toBeFalsy()
+    })
+
+    it("can delete a file", async () => {
+        const response = await client.deleteRemoteFile({ filepath })
+        if (!response.success) console.log(response) // Dump for easier debugging in CI
+        expect(response.success).toBeTruthy()
+    })
+
+    it("can fail delete gracefully", async () => {
+        const response = await client.deleteRemoteFile({
+            filepath,
+        })
+        expect(response.success).toBeFalsy()
+    })
 })
