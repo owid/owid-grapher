c3d04cc1477f81f70f78924243c948cc83a2267a
Marcel Gerber <mgerber59@gmail.com>
2021-02-01 19:12:40 +0100

    fix: CC-BY link in chart footer shows tooltip again

--- grapher/footer/Footer.tsx
+++ grapher/footer/Footer.tsx
@@ -179,7 +179,7 @@ export class Footer extends React.Component<{
         const cc = this.base.current!.querySelector(".cclogo")
         if (cc && cc.matches(":hover")) {
             const div = this.base.current as HTMLDivElement
-            const mouse = getRelativeMouse(div.closest(".chart"), e)
+            const mouse = getRelativeMouse(div.closest(".GrapherComponent"), e)
             this.tooltipTarget = { x: mouse.x, y: mouse.y }
         } else this.tooltipTarget = undefined
     }
