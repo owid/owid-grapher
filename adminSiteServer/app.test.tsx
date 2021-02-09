736b439de57476695da459d5a514fed5a16bf7cd
Marcel Gerber <mgerber59@gmail.com>
2021-02-09 21:34:41 +0100

    test: increase timeout for app server start

--- adminSiteServer/app.test.tsx
+++ adminSiteServer/app.test.tsx
@@ -8,6 +8,8 @@ describe(OwidAdminApp, () => {
     })
 
     it("should be able to start the app", async () => {
+        jest.setTimeout(10000) // wait for up to 10s for the app server to start
+
         await app.startListening(8765, "localhost")
         expect(app.server).toBeTruthy()
         app.stopListening()
