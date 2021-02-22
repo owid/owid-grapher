a1d2d80b5e0f97d7c6d5b399c1a755f0598b6552
Matthieu Bergel <hello@matthieubergel.org>
2021-02-22 16:11:27 +0100

    Revert "Merge pull request #804 from owid/move-subnav"
    This reverts commit f67ea72c63e35ba6dc47bea28f6e238b21a95f16, reversing
    changes made to 384846f4d893be5d6b849148425841170a370c1b.

--- site/server/views/LongFormPage.tsx
+++ site/server/views/LongFormPage.tsx
@@ -123,14 +123,6 @@ export const LongFormPage = (props: {
                             hasSidebar ? " with-sidebar" : " no-sidebar"
                         }${isPost ? " thin-banner" : " large-banner"}`}
                     >
-                        {formattingOptions.subnavId && (
-                            <SiteSubnavigation
-                                subnavId={formattingOptions.subnavId}
-                                subnavCurrentId={
-                                    formattingOptions.subnavCurrentId
-                                }
-                            />
-                        )}
                         <div className="offset-header">
                             <header className="article-header">
                                 <div className="article-titles">
@@ -203,6 +195,15 @@ export const LongFormPage = (props: {
                                 )}
                             </header>
                         </div>
+                        {formattingOptions.subnavId && (
+                            <SiteSubnavigation
+                                subnavId={formattingOptions.subnavId}
+                                subnavCurrentId={
+                                    formattingOptions.subnavCurrentId
+                                }
+                            />
+                        )}
+
                         <div className="content-wrapper">
                             {hasSidebar && (
                                 <div>
