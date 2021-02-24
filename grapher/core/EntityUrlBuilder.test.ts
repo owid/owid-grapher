9d1c5180d813a894eb85eb59012adec400b5bf8d
Daniel Gavrilov <dgavrilov95@gmail.com>
2021-02-24 12:00:09 +0000

    Refactor EntityUrlBuilder & fix tests

--- grapher/core/EntityUrlBuilder.test.ts
+++ grapher/core/EntityUrlBuilder.test.ts
@@ -1,22 +1,22 @@
 #! /usr/bin/env jest
 
+import { Url } from "../../clientUtils/urls/Url"
 import {
-    queryParamsToStr,
-    strToQueryParams,
-} from "../../clientUtils/urls/UrlUtils"
-import { EntityUrlBuilder, ENTITY_V2_DELIMITER } from "./EntityUrlBuilder"
+    ENTITY_V2_DELIMITER,
+    getCountryQueryParam,
+    setCountryQueryParam,
+} from "./EntityUrlBuilder"
 
 const encodeTests = [
     {
-        entities: ["USA", "GB"],
-        queryParam: { _original: "USA~GB", decoded: "USA~GB" },
+        entities: ["United States", "United Kingdom"],
+        inputQueryStr: "?country=USA~GBR",
+        outputQueryStr: "?selection=United+States~United+Kingdom",
     },
     {
         entities: ["YouTube", "Google+"],
-        queryParam: {
-            _original: "YouTube~Google%2B",
-            decoded: "YouTube~Google+",
-        },
+        inputQueryStr: "?country=YouTube~Google%2B",
+        outputQueryStr: "?selection=YouTube~Google%2B",
     },
     {
         entities: [
@@ -24,67 +24,56 @@ const encodeTests = [
             "British Columbia (30 sites); 3500 BCE - 1674 CE",
             "Brittany; 6000 BCE",
         ],
-        queryParam: {
-            _original:
-                "Bogebakken%20(Denmark)%3B%204300%20-%203800%20BCE~British%20Columbia%20(30%20sites)%3B%203500%20BCE%20-%201674%20CE~Brittany%3B%206000%20BCE",
-            decoded:
-                "Bogebakken (Denmark); 4300 - 3800 BCE~British Columbia (30 sites); 3500 BCE - 1674 CE~Brittany; 6000 BCE",
-        },
+        inputQueryStr:
+            "?country=Bogebakken%20(Denmark)%3B%204300%20-%203800%20BCE~British%20Columbia%20(30%20sites)%3B%203500%20BCE%20-%201674%20CE~Brittany%3B%206000%20BCE",
+        outputQueryStr:
+            "?selection=Bogebakken+%28Denmark%29%3B+4300+-+3800+BCE~British+Columbia+%2830+sites%29%3B+3500+BCE+-+1674+CE~Brittany%3B+6000+BCE",
     },
     {
         entities: ["British Columbia (30 sites); 3500 BCE - 1674 CE"],
-        queryParam: {
-            _original:
-                "~British%20Columbia%20(30%20sites)%3B%203500%20BCE%20-%201674%20CE",
-            decoded: "~British Columbia (30 sites); 3500 BCE - 1674 CE",
-        },
+        inputQueryStr:
+            "?country=~British%20Columbia%20(30%20sites)%3B%203500%20BCE%20-%201674%20CE",
+        outputQueryStr:
+            "?selection=~British+Columbia+%2830+sites%29%3B+3500+BCE+-+1674+CE",
     },
     {
         entities: ["Caribbean small states"],
-        queryParam: {
-            _original: "~Caribbean%20small%20states",
-            decoded: "~Caribbean small states",
-        },
+        inputQueryStr: "?country=~Caribbean%20small%20states",
+        outputQueryStr: "?selection=~Caribbean+small+states",
     },
     {
         entities: ["North America"],
-        queryParam: {
-            _original: "~North America",
-            decoded: "~North America",
-        },
+        inputQueryStr: "?country=~North%20America",
+        outputQueryStr: "?selection=~North+America",
     },
     {
         entities: [],
-        queryParam: {
-            _original: "",
-            decoded: "",
-        },
+        inputQueryStr: "?country=",
+        outputQueryStr: "?selection=",
     },
     {
         entities: [
             "Men and Women Ages 65+",
             "Australia & New Zealand + (Total)",
         ],
-        queryParam: {
-            _original:
-                "Men%20and%20Women%20Ages%2065%2B~Australia%20%26%20New%20Zealand%20%2B%20(Total)",
-            decoded: "Men and Women Ages 65+~Australia & New Zealand + (Total)",
-        },
+        inputQueryStr:
+            "?country=Men%20and%20Women%20Ages%2065%2B~Australia%20%26%20New%20Zealand%20%2B%20(Total)",
+        outputQueryStr:
+            "?selection=Men+and+Women+Ages+65%2B~Australia+%26+New+Zealand+%2B+%28Total%29",
     },
 ]
 
 encodeTests.forEach((testCase) => {
     it(`correctly encodes url strings`, () => {
         expect(
-            EntityUrlBuilder.entityNamesToDecodedQueryParam(testCase.entities)
-        ).toEqual(testCase.queryParam.decoded)
+            setCountryQueryParam(Url.fromQueryStr(""), testCase.entities)
+                .queryStr
+        ).toEqual(testCase.outputQueryStr)
     })
 
     it(`correctly decodes url strings`, () => {
         expect(
-            EntityUrlBuilder.encodedQueryParamToEntityNames(
-                testCase.queryParam._original
-            )
+            getCountryQueryParam(Url.fromQueryStr(testCase.inputQueryStr))
         ).toEqual(testCase.entities)
     })
 })
@@ -92,31 +81,23 @@ encodeTests.forEach((testCase) => {
 describe("legacyLinks", () => {
     const legacyLinks = [
         {
-            entities: ["North America", "DOM"],
-            queryParam: {
-                _original: "North%20America+DOM",
-                decoded: "North America DOM",
-            },
+            entities: ["North America", "Africa"],
+            queryStr: "?country=North%20America+Africa",
         },
         {
-            entities: ["USA", "GB"],
-            queryParam: { _original: "USA+GB", decoded: "USA GB" },
+            entities: ["United States", "United Kingdom"],
+            queryStr: "?country=USA+GBR",
         },
         {
             entities: ["YouTube", "Google+"],
-            queryParam: {
-                _original: "YouTube+Google%2B",
-                decoded: "YouTube Google ",
-            },
+            queryStr: "?country=YouTube+Google%2B",
         },
     ]
 
     legacyLinks.forEach((testCase) => {
         it(`correctly decodes legacy url strings`, () => {
             expect(
-                EntityUrlBuilder.encodedQueryParamToEntityNames(
-                    testCase.queryParam._original
-                )
+                getCountryQueryParam(Url.fromQueryStr(testCase.queryStr))
             ).toEqual(testCase.entities)
         })
     })
@@ -126,27 +107,22 @@ describe("facebook", () => {
     const facebookLinks = [
         {
             entities: ["Caribbean small states"],
-            queryParam: {
-                _original: "Caribbean+small+states~",
-                decoded: "Carribean small states~",
-            },
+            queryStr: "?country=Caribbean+small+states~",
         },
     ]
 
     facebookLinks.forEach((testCase) => {
         it(`correctly decodes Facebook altered links`, () => {
             expect(
-                EntityUrlBuilder.encodedQueryParamToEntityNames(
-                    testCase.queryParam._original
-                )
+                getCountryQueryParam(Url.fromQueryStr(testCase.queryStr))
             ).toEqual(testCase.entities)
         })
     })
 })
 
 describe("it can handle legacy urls with dimension in selection key", () => {
-    const queryStr = queryParamsToStr({
-        selection: [
+    const url = Url.fromQueryParams({
+        country: [
             "United States",
             "USA",
             "GBR-0",
@@ -155,19 +131,15 @@ describe("it can handle legacy urls with dimension in selection key", () => {
         ].join(ENTITY_V2_DELIMITER),
     })
 
-    const results = EntityUrlBuilder.migrateEncodedLegacyCountryParam(
-        strToQueryParams(queryStr)._original.selection!
-    )
+    const results = getCountryQueryParam(url)
 
-    expect(results).toEqual(
-        [
-            "United States",
-            "United States",
-            "United Kingdom",
-            "GBR-0",
-            "1980",
-            "1980-1989",
-            "NotFound",
-        ].join(ENTITY_V2_DELIMITER)
-    )
+    expect(results).toEqual([
+        "United States",
+        "United States",
+        "GBR-0",
+        "United Kingdom",
+        "1980-1989",
+        "1980",
+        "NotFound",
+    ])
 })
--- grapher/core/EntityUrlBuilder.ts
+++ grapher/core/EntityUrlBuilder.ts
@@ -9,16 +9,59 @@ export const ENTITY_V2_DELIMITER = "~"
 
 const LegacyDimensionRegex = /\-\d+$/
 
-const dropLegacyDimensionInEntity = (entityName: string) => {
+const dropLegacyDimensionInEntityNames = (
+    entityNames: EntityName[]
+): EntityName[] => {
     // If an entity has the old name-dimension encoding, removing the dimension part and add it as a new selection. So USA-1 becomes USA.
     // This is only run against the old `country` params
-    if (LegacyDimensionRegex.test(entityName))
-        return entityName.replace(LegacyDimensionRegex, "")
-    return entityName
+    const newNames: EntityName[] = []
+    entityNames.forEach((entityName) => {
+        newNames.push(entityName)
+        if (LegacyDimensionRegex.test(entityName)) {
+            const nonDimensionName = entityName.replace(
+                LegacyDimensionRegex,
+                ""
+            )
+            newNames.push(nonDimensionName)
+        }
+    })
+    return newNames
 }
 
 const codeToEntityName = (codeOrEntityName: string) => {
     return LegacyEntityCodesToEntityNames[codeOrEntityName] ?? codeOrEntityName
+}
+
+const entityNamesToDecodedQueryParam = (entityNames: EntityName[]): string => {
+    // Always include a v2Delimiter in a v2 link. When decoding we will drop any empty strings.
+    if (entityNames.length === 1) return ENTITY_V2_DELIMITER + entityNames[0]
+    return entityNames.join(ENTITY_V2_DELIMITER)
+}
+
+const isV1Link = (queryParam: string) => {
+    // No legacy entities have a v2Delimiter in their name, so if a v2Delimiter is present we know it's a v2 link.
+    return !decodeURIComponent(queryParam).includes(ENTITY_V2_DELIMITER)
+}
+
+const decodeV1Link = (queryParam: string): EntityName[] => {
+    return queryParam.split(V1_DELIMITER).map(decodeURIComponent)
+}
+
+const decodeV2Link = (queryParam: string): EntityName[] => {
+    // Facebook turns %20 into +. v2 links will never contain a +, so we can safely replace all of them with %20.
+    return decodeURIComponent(queryParam.replace(/\+/g, "%20"))
+        .split(ENTITY_V2_DELIMITER)
+        .filter((item) => item)
+}
+
+const encodedQueryParamToEntityNames = (queryParam = ""): EntityName[] => {
+    // First preserve handling of the old v1 country=USA+FRA style links. If a link does not
+    // include a v2Delimiter and includes a + we assume it's a v1 link. Unfortunately link sharing
+    // with v1 links did not work on Facebook because FB would replace %20 with "+".
+    if (queryParam === "") return []
+    return isV1Link(queryParam)
+        ? decodeV1Link(queryParam)
+        : decodeV2Link(queryParam)
 }
 
 /**
@@ -27,19 +70,17 @@ const codeToEntityName = (codeOrEntityName: string) => {
  * Important: Only ever pass not-yet-decoded URI params in here, otherwise the migration will give wrong results for legacy URLs.
  */
 const migrateEncodedLegacyCountryParam = (countryParam: string) => {
-    const entityNames = EntityUrlBuilder.encodedQueryParamToEntityNames(
-        countryParam
-    )
-        .map(dropLegacyDimensionInEntity)
-        .map(codeToEntityName)
-    return EntityUrlBuilder.entityNamesToDecodedQueryParam(entityNames)
+    const entityNames = dropLegacyDimensionInEntityNames(
+        encodedQueryParamToEntityNames(countryParam)
+    ).map(codeToEntityName)
+    return entityNamesToDecodedQueryParam(entityNames)
 }
 
 export const migrateCountryQueryParam = (url: Url) => {
     // need to use `_original` (still-encoded) URL params because we need to
     // distinguish between `+` and `%20` in legacy URLs
     const { country } = url.queryParams._original
-    if (!country) return url
+    if (country === undefined) return url
     return url.updateQueryParams({
         country: undefined,
         selection: migrateEncodedLegacyCountryParam(country),
@@ -48,8 +89,8 @@ export const migrateCountryQueryParam = (url: Url) => {
 
 /** NOTE: Ensure country query param is migrated first! */
 export const getCountryQueryParam = (url: Url): EntityName[] | undefined => {
-    return url.queryParams.decoded.selection
-        ?.split(ENTITY_V2_DELIMITER)
+    return migrateCountryQueryParam(url)
+        .queryParams.decoded.selection?.split(ENTITY_V2_DELIMITER)
         .filter((entityName) => entityName)
 }
 
@@ -57,45 +98,9 @@ export const setCountryQueryParam = (
     url: Url,
     entityNames: EntityName[] | undefined
 ) => {
-    return url.updateQueryParams({
-        selection:
-            entityNames && entityNames.length > 0
-                ? EntityUrlBuilder.entityNamesToDecodedQueryParam(entityNames)
-                : undefined,
+    return migrateCountryQueryParam(url).updateQueryParams({
+        selection: entityNames
+            ? entityNamesToDecodedQueryParam(entityNames)
+            : undefined,
     })
-}
-
-export class EntityUrlBuilder {
-    static entityNamesToDecodedQueryParam(entityNames: EntityName[]) {
-        // Always include a v2Delimiter in a v2 link. When decoding we will drop any empty strings.
-        if (entityNames.length === 1)
-            return ENTITY_V2_DELIMITER + entityNames[0]
-        return entityNames.join(ENTITY_V2_DELIMITER)
-    }
-
-    static encodedQueryParamToEntityNames(queryParam = ""): EntityName[] {
-        // First preserve handling of the old v1 country=USA+FRA style links. If a link does not
-        // include a v2Delimiter and includes a + we assume it's a v1 link. Unfortunately link sharing
-        // with v1 links did not work on Facebook because FB would replace %20 with "+".
-        if (queryParam === "") return []
-        return this.isV1Link(queryParam)
-            ? this.decodeV1Link(queryParam)
-            : this.decodeV2Link(queryParam)
-    }
-
-    private static isV1Link(queryParam: string) {
-        // No legacy entities have a v2Delimiter in their name, so if a v2Delimiter is present we know it's a v2 link.
-        return !decodeURIComponent(queryParam).includes(ENTITY_V2_DELIMITER)
-    }
-
-    private static decodeV1Link(queryParam: string) {
-        return queryParam.split(V1_DELIMITER).map(decodeURIComponent)
-    }
-
-    private static decodeV2Link(queryParam: string) {
-        // Facebook turns %20 into +. v2 links will never contain a +, so we can safely replace all of them with %20.
-        return decodeURIComponent(queryParam.replace(/\+/g, "%20"))
-            .split(ENTITY_V2_DELIMITER)
-            .filter((item) => item)
-    }
 }
