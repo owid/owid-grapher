e3d525b417f3eb286f63af5b356304bae44846a0
Marcel Gerber <mgerber59@gmail.com>
2021-03-03 17:55:04 +0100

    feat(config): use `dotenv` for config

--- settings/clientSettings.ts
+++ settings/clientSettings.ts
@@ -4,32 +4,33 @@
 // webpack is configured to provide our clientSettings as `process.env.clientSettings`
 // (through DefinePlugin), so we can just use these here, and fall back to the defaults
 // if a setting is not set in clientSettings.json
-const clientSettings: any = process.env.clientSettings ?? {}
 
-export const ADMIN_SERVER_PORT = clientSettings.ADMIN_SERVER_PORT ?? 3030
-export const ADMIN_SERVER_HOST = clientSettings.ADMIN_SERVER_HOST ?? "localhost"
+import dotenv from "dotenv"
+dotenv.config()
+
+export const ENV = process.env.ENV ?? "development"
+
+export const ADMIN_SERVER_PORT = process.env.ADMIN_SERVER_PORT ?? 3030
+export const ADMIN_SERVER_HOST = process.env.ADMIN_SERVER_HOST ?? "localhost"
 export const BAKED_BASE_URL =
-    clientSettings.BAKED_BASE_URL ??
+    process.env.BAKED_BASE_URL ??
     `http://${ADMIN_SERVER_HOST}:${ADMIN_SERVER_PORT}`
 
-export const ENV = clientSettings.ENV ?? "development"
 export const BAKED_GRAPHER_URL =
-    clientSettings.BAKED_GRAPHER_URL ?? `${BAKED_BASE_URL}/grapher`
+    process.env.BAKED_GRAPHER_URL ?? `${BAKED_BASE_URL}/grapher`
 export const ADMIN_BASE_URL =
-    clientSettings.ADMIN_BASE_URL ??
+    process.env.ADMIN_BASE_URL ??
     `http://${ADMIN_SERVER_HOST}:${ADMIN_SERVER_PORT}`
-export const WORDPRESS_URL =
-    clientSettings.WORDPRESS_URL ?? "https://owid.cloud"
+export const WORDPRESS_URL = process.env.WORDPRESS_URL ?? "https://owid.cloud"
 
-export const ALGOLIA_ID = clientSettings.ALGOLIA_ID ?? ""
-export const ALGOLIA_SEARCH_KEY = clientSettings.ALGOLIA_SEARCH_KEY ?? ""
+export const ALGOLIA_ID = process.env.ALGOLIA_ID ?? ""
+export const ALGOLIA_SEARCH_KEY = process.env.ALGOLIA_SEARCH_KEY ?? ""
 
 export const STRIPE_PUBLIC_KEY =
-    clientSettings.STRIPE_PUBLIC_KEY ?? "pk_test_nIHvmH37zsoltpw3xMssPIYq"
+    process.env.STRIPE_PUBLIC_KEY ?? "pk_test_nIHvmH37zsoltpw3xMssPIYq"
 export const DONATE_API_URL =
-    clientSettings.DONATE_API_URL ?? "http://localhost:9000/donate"
+    process.env.DONATE_API_URL ?? "http://localhost:9000/donate"
 
 export const RECAPTCHA_SITE_KEY =
-    clientSettings.RECAPTCHA_SITE_KEY ??
-    "6LcJl5YUAAAAAATQ6F4vl9dAWRZeKPBm15MAZj4Q"
-export const OPTIMIZE_SVG_EXPORTS = clientSettings.OPTIMIZE_SVG_EXPORTS ?? false
+    process.env.RECAPTCHA_SITE_KEY ?? "6LcJl5YUAAAAAATQ6F4vl9dAWRZeKPBm15MAZj4Q"
+export const OPTIMIZE_SVG_EXPORTS = process.env.OPTIMIZE_SVG_EXPORTS ?? false
--- settings/serverSettings.ts
+++ settings/serverSettings.ts
@@ -1,94 +1,83 @@
 // This is where server-side only, potentially sensitive settings enter from the environment
 // DO NOT store sensitive strings in this file itself, as it is checked in to git!
 
-import fs from "fs-extra"
 import path from "path"
+import dotenv from "dotenv"
+dotenv.config()
+
+import * as clientSettings from "./clientSettings"
+
+const serverSettings: any = process.env ?? {}
 
 export const BASE_DIR = path.resolve(__dirname, "../..")
-const absoluteSettingsPath = path.resolve(BASE_DIR, "serverSettings.json")
-const localOverrides: any = fs.existsSync(absoluteSettingsPath)
-    ? fs.readJsonSync(absoluteSettingsPath)
-    : {}
 
-export const ADMIN_SERVER_PORT = localOverrides.ADMIN_SERVER_PORT ?? 3030
-export const ADMIN_SERVER_HOST = localOverrides.ADMIN_SERVER_HOST ?? "localhost"
-export const BAKED_BASE_URL =
-    localOverrides.BAKED_BASE_URL ??
-    `http://${ADMIN_SERVER_HOST}:${ADMIN_SERVER_PORT}`
+export const ADMIN_SERVER_PORT = clientSettings.ADMIN_SERVER_PORT
+export const ADMIN_SERVER_HOST = clientSettings.ADMIN_SERVER_HOST
+export const BAKED_BASE_URL = clientSettings.BAKED_BASE_URL
+
+export const ADMIN_BASE_URL = clientSettings.ADMIN_BASE_URL
+export const WORDPRESS_URL = clientSettings.WORDPRESS_URL
 
 export const BAKED_GRAPHER_URL =
-    localOverrides.BAKED_GRAPHER_URL ?? `${BAKED_BASE_URL}/grapher`
+    serverSettings.BAKED_GRAPHER_URL ?? `${BAKED_BASE_URL}/grapher`
 
-export const ADMIN_BASE_URL =
-    localOverrides.ADMIN_BASE_URL ??
-    `http://${ADMIN_SERVER_HOST}:${ADMIN_SERVER_PORT}`
+export const OPTIMIZE_SVG_EXPORTS = serverSettings.OPTIMIZE_SVG_EXPORTS ?? false
 
-export const WORDPRESS_URL =
-    localOverrides.WORDPRESS_URL ?? "https://owid.cloud"
-
-export const ALGOLIA_ID = localOverrides.ALGOLIA_ID ?? ""
-
-export const RECAPTCHA_SITE_KEY =
-    localOverrides.RECAPTCHA_SITE_KEY ??
-    "6LcJl5YUAAAAAATQ6F4vl9dAWRZeKPBm15MAZj4Q"
-
-export const OPTIMIZE_SVG_EXPORTS = localOverrides.OPTIMIZE_SVG_EXPORTS ?? false
-
-export const GITHUB_USERNAME = localOverrides.GITHUB_USERNAME ?? "owid-test"
+export const GITHUB_USERNAME = serverSettings.GITHUB_USERNAME ?? "owid-test"
 export const GIT_DEFAULT_USERNAME =
-    localOverrides.GIT_DEFAULT_USERNAME ?? "Our World in Data"
+    serverSettings.GIT_DEFAULT_USERNAME ?? "Our World in Data"
 export const GIT_DEFAULT_EMAIL =
-    localOverrides.GIT_DEFAULT_EMAIL ?? "info@ourworldindata.org"
+    serverSettings.GIT_DEFAULT_EMAIL ?? "info@ourworldindata.org"
 
-export const BLOG_POSTS_PER_PAGE = localOverrides.BLOG_POSTS_PER_PAGE ?? 21
-export const BLOG_SLUG = localOverrides.BLOG_SLUG ?? "blog"
+export const BLOG_POSTS_PER_PAGE = serverSettings.BLOG_POSTS_PER_PAGE ?? 21
+export const BLOG_SLUG = serverSettings.BLOG_SLUG ?? "blog"
 
-export const DB_NAME = localOverrides.DB_NAME ?? "owid"
-export const DB_USER = localOverrides.DB_USER ?? "root"
-export const DB_PASS = localOverrides.DB_PASS ?? ""
-export const DB_HOST = localOverrides.DB_HOST ?? "localhost"
-export const DB_PORT = localOverrides.DB_PORT ?? 3306
+export const DB_NAME = serverSettings.DB_NAME ?? "owid"
+export const DB_USER = serverSettings.DB_USER ?? "root"
+export const DB_PASS = serverSettings.DB_PASS ?? ""
+export const DB_HOST = serverSettings.DB_HOST ?? "localhost"
+export const DB_PORT = serverSettings.DB_PORT ?? 3306
 
-export const ENV = localOverrides.ENV ?? "development"
+export const ENV = serverSettings.ENV ?? "development"
 export const BAKED_SITE_DIR =
-    localOverrides.BAKED_SITE_DIR ?? `${BASE_DIR}/bakedSite` // Where the static build output goes
+    serverSettings.BAKED_SITE_DIR ?? `${BASE_DIR}/bakedSite` // Where the static build output goes
 export const SECRET_KEY =
-    localOverrides.SECRET_KEY ??
+    serverSettings.SECRET_KEY ??
     "fejwiaof jewiafo jeioa fjieowajf isa fjidosajfgj"
-export const WORDPRESS_DB_NAME = localOverrides.WORDPRESS_DB_NAME ?? DB_NAME
-export const WORDPRESS_DB_USER = localOverrides.WORDPRESS_DB_USER ?? DB_USER
-export const WORDPRESS_DB_PASS = localOverrides.WORDPRESS_DB_PASS ?? DB_PASS
-export const WORDPRESS_DB_HOST = localOverrides.WORDPRESS_DB_HOST ?? DB_HOST
-export const WORDPRESS_DB_PORT = localOverrides.WORDPRESS_DB_PORT ?? DB_PORT
-export const WORDPRESS_API_USER = localOverrides.WORDPRESS_API_USER ?? ""
-export const WORDPRESS_API_PASS = localOverrides.WORDPRESS_API_PASS ?? ""
-export const SESSION_COOKIE_AGE = localOverrides.SESSION_COOKIE_AGE ?? 1209600
-export const ALGOLIA_SECRET_KEY = localOverrides.ALGOLIA_SECRET_KEY ?? ""
-export const STRIPE_SECRET_KEY = localOverrides.STRIPE_SECRET_KEY ?? ""
+export const WORDPRESS_DB_NAME = serverSettings.WORDPRESS_DB_NAME ?? DB_NAME
+export const WORDPRESS_DB_USER = serverSettings.WORDPRESS_DB_USER ?? DB_USER
+export const WORDPRESS_DB_PASS = serverSettings.WORDPRESS_DB_PASS ?? DB_PASS
+export const WORDPRESS_DB_HOST = serverSettings.WORDPRESS_DB_HOST ?? DB_HOST
+export const WORDPRESS_DB_PORT = serverSettings.WORDPRESS_DB_PORT ?? DB_PORT
+export const WORDPRESS_API_USER = serverSettings.WORDPRESS_API_USER ?? ""
+export const WORDPRESS_API_PASS = serverSettings.WORDPRESS_API_PASS ?? ""
+export const SESSION_COOKIE_AGE = serverSettings.SESSION_COOKIE_AGE ?? 1209600
+export const ALGOLIA_SECRET_KEY = serverSettings.ALGOLIA_SECRET_KEY ?? ""
+export const STRIPE_SECRET_KEY = serverSettings.STRIPE_SECRET_KEY ?? ""
 
 // Settings for automated email sending, e.g. for admin invite
-export const EMAIL_HOST = localOverrides.EMAIL_HOST ?? "smtp.mail.com"
-export const EMAIL_PORT = localOverrides.EMAIL_PORT ?? 443
-export const EMAIL_HOST_USER = localOverrides.EMAIL_HOST_USER ?? "user"
+export const EMAIL_HOST = serverSettings.EMAIL_HOST ?? "smtp.mail.com"
+export const EMAIL_PORT = serverSettings.EMAIL_PORT ?? 443
+export const EMAIL_HOST_USER = serverSettings.EMAIL_HOST_USER ?? "user"
 export const EMAIL_HOST_PASSWORD =
-    localOverrides.EMAIL_HOST_PASSWORD ?? "password"
+    serverSettings.EMAIL_HOST_PASSWORD ?? "password"
 
 // Wordpress target setting
-export const WORDPRESS_DIR = localOverrides.WORDPRESS_DIR ?? ""
-export const HTTPS_ONLY = localOverrides.HTTPS_ONLY ?? true
+export const WORDPRESS_DIR = serverSettings.WORDPRESS_DIR ?? ""
+export const HTTPS_ONLY = serverSettings.HTTPS_ONLY ?? true
 
 // Node slack webhook to report errors to using express-error-slac
 export const SLACK_ERRORS_WEBHOOK_URL =
-    localOverrides.SLACK_ERRORS_WEBHOOK_URL ?? ""
+    serverSettings.SLACK_ERRORS_WEBHOOK_URL ?? ""
 export const GIT_DATASETS_DIR =
-    localOverrides.GIT_DATASETS_DIR ?? `${BASE_DIR}/datasetsExport` //  Where the git exports go
-export const TMP_DIR = localOverrides.TMP_DIR ?? "/tmp"
-export const UNCATEGORIZED_TAG_ID = localOverrides.UNCATEGORIZED_TAG_ID ?? 375
+    serverSettings.GIT_DATASETS_DIR ?? `${BASE_DIR}/datasetsExport` //  Where the git exports go
+export const TMP_DIR = serverSettings.TMP_DIR ?? "/tmp"
+export const UNCATEGORIZED_TAG_ID = serverSettings.UNCATEGORIZED_TAG_ID ?? 375
 
 // Should the static site output be baked when relevant database items change
-export const BAKE_ON_CHANGE = localOverrides.BAKE_ON_CHANGE ?? false
+export const BAKE_ON_CHANGE = serverSettings.BAKE_ON_CHANGE ?? false
 export const DEPLOY_QUEUE_FILE_PATH =
-    localOverrides.DEPLOY_QUEUE_FILE_PATH ?? `${BASE_DIR}/.queue`
+    serverSettings.DEPLOY_QUEUE_FILE_PATH ?? `${BASE_DIR}/.queue`
 export const DEPLOY_PENDING_FILE_PATH =
-    localOverrides.DEPLOY_PENDING_FILE_PATH ?? `${BASE_DIR}/.pending`
-export const CLOUDFLARE_AUD = localOverrides.CLOUDFLARE_AUD ?? ""
+    serverSettings.DEPLOY_PENDING_FILE_PATH ?? `${BASE_DIR}/.pending`
+export const CLOUDFLARE_AUD = serverSettings.CLOUDFLARE_AUD ?? ""
