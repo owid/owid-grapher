5806c2f3307ad9491411e14a8f70f9f6402a8138
Daniel Gavrilov <dgavrilov95@gmail.com>
2021-03-03 11:51:37 +0000

    fix: add types to settings

--- settings/clientSettings.ts
+++ settings/clientSettings.ts
@@ -6,30 +6,34 @@
 // if a setting is not set in clientSettings.json
 const clientSettings: any = process.env.clientSettings ?? {}
 
-export const ADMIN_SERVER_PORT = clientSettings.ADMIN_SERVER_PORT ?? 3030
-export const ADMIN_SERVER_HOST = clientSettings.ADMIN_SERVER_HOST ?? "localhost"
-export const BAKED_BASE_URL =
+export const ADMIN_SERVER_PORT: number =
+    clientSettings.ADMIN_SERVER_PORT ?? 3030
+export const ADMIN_SERVER_HOST: string =
+    clientSettings.ADMIN_SERVER_HOST ?? "localhost"
+export const BAKED_BASE_URL: string =
     clientSettings.BAKED_BASE_URL ??
     `http://${ADMIN_SERVER_HOST}:${ADMIN_SERVER_PORT}`
 
-export const ENV = clientSettings.ENV ?? "development"
-export const BAKED_GRAPHER_URL =
+export const ENV: string = clientSettings.ENV ?? "development"
+export const BAKED_GRAPHER_URL: string =
     clientSettings.BAKED_GRAPHER_URL ?? `${BAKED_BASE_URL}/grapher`
-export const ADMIN_BASE_URL =
+export const ADMIN_BASE_URL: string =
     clientSettings.ADMIN_BASE_URL ??
     `http://${ADMIN_SERVER_HOST}:${ADMIN_SERVER_PORT}`
-export const WORDPRESS_URL =
+export const WORDPRESS_URL: string =
     clientSettings.WORDPRESS_URL ?? "https://owid.cloud"
 
-export const ALGOLIA_ID = clientSettings.ALGOLIA_ID ?? ""
-export const ALGOLIA_SEARCH_KEY = clientSettings.ALGOLIA_SEARCH_KEY ?? ""
+export const ALGOLIA_ID: string = clientSettings.ALGOLIA_ID ?? ""
+export const ALGOLIA_SEARCH_KEY: string =
+    clientSettings.ALGOLIA_SEARCH_KEY ?? ""
 
-export const STRIPE_PUBLIC_KEY =
+export const STRIPE_PUBLIC_KEY: string =
     clientSettings.STRIPE_PUBLIC_KEY ?? "pk_test_nIHvmH37zsoltpw3xMssPIYq"
-export const DONATE_API_URL =
+export const DONATE_API_URL: string =
     clientSettings.DONATE_API_URL ?? "http://localhost:9000/donate"
 
-export const RECAPTCHA_SITE_KEY =
+export const RECAPTCHA_SITE_KEY: string =
     clientSettings.RECAPTCHA_SITE_KEY ??
     "6LcJl5YUAAAAAATQ6F4vl9dAWRZeKPBm15MAZj4Q"
-export const OPTIMIZE_SVG_EXPORTS = clientSettings.OPTIMIZE_SVG_EXPORTS ?? false
+export const OPTIMIZE_SVG_EXPORTS: boolean =
+    clientSettings.OPTIMIZE_SVG_EXPORTS ?? false
--- settings/serverSettings.ts
+++ settings/serverSettings.ts
@@ -10,85 +10,100 @@ const localOverrides: any = fs.existsSync(absoluteSettingsPath)
     ? fs.readJsonSync(absoluteSettingsPath)
     : {}
 
-export const ADMIN_SERVER_PORT = localOverrides.ADMIN_SERVER_PORT ?? 3030
-export const ADMIN_SERVER_HOST = localOverrides.ADMIN_SERVER_HOST ?? "localhost"
-export const BAKED_BASE_URL =
+export const ADMIN_SERVER_PORT: number =
+    localOverrides.ADMIN_SERVER_PORT ?? 3030
+export const ADMIN_SERVER_HOST: string =
+    localOverrides.ADMIN_SERVER_HOST ?? "localhost"
+export const BAKED_BASE_URL: string =
     localOverrides.BAKED_BASE_URL ??
     `http://${ADMIN_SERVER_HOST}:${ADMIN_SERVER_PORT}`
 
-export const BAKED_GRAPHER_URL =
+export const BAKED_GRAPHER_URL: string =
     localOverrides.BAKED_GRAPHER_URL ?? `${BAKED_BASE_URL}/grapher`
 
-export const ADMIN_BASE_URL =
+export const ADMIN_BASE_URL: string =
     localOverrides.ADMIN_BASE_URL ??
     `http://${ADMIN_SERVER_HOST}:${ADMIN_SERVER_PORT}`
 
-export const WORDPRESS_URL =
+export const WORDPRESS_URL: string =
     localOverrides.WORDPRESS_URL ?? "https://owid.cloud"
 
-export const ALGOLIA_ID = localOverrides.ALGOLIA_ID ?? ""
+export const ALGOLIA_ID: string = localOverrides.ALGOLIA_ID ?? ""
 
-export const RECAPTCHA_SITE_KEY =
+export const RECAPTCHA_SITE_KEY: string =
     localOverrides.RECAPTCHA_SITE_KEY ??
     "6LcJl5YUAAAAAATQ6F4vl9dAWRZeKPBm15MAZj4Q"
 
-export const OPTIMIZE_SVG_EXPORTS = localOverrides.OPTIMIZE_SVG_EXPORTS ?? false
+export const OPTIMIZE_SVG_EXPORTS: boolean =
+    localOverrides.OPTIMIZE_SVG_EXPORTS ?? false
 
-export const GITHUB_USERNAME = localOverrides.GITHUB_USERNAME ?? "owid-test"
-export const GIT_DEFAULT_USERNAME =
+export const GITHUB_USERNAME: string =
+    localOverrides.GITHUB_USERNAME ?? "owid-test"
+export const GIT_DEFAULT_USERNAME: string =
     localOverrides.GIT_DEFAULT_USERNAME ?? "Our World in Data"
-export const GIT_DEFAULT_EMAIL =
+export const GIT_DEFAULT_EMAIL: string =
     localOverrides.GIT_DEFAULT_EMAIL ?? "info@ourworldindata.org"
 
-export const BLOG_POSTS_PER_PAGE = localOverrides.BLOG_POSTS_PER_PAGE ?? 21
-export const BLOG_SLUG = localOverrides.BLOG_SLUG ?? "blog"
+export const BLOG_POSTS_PER_PAGE: number =
+    localOverrides.BLOG_POSTS_PER_PAGE ?? 21
+export const BLOG_SLUG: string = localOverrides.BLOG_SLUG ?? "blog"
 
-export const DB_NAME = localOverrides.DB_NAME ?? "owid"
-export const DB_USER = localOverrides.DB_USER ?? "root"
-export const DB_PASS = localOverrides.DB_PASS ?? ""
-export const DB_HOST = localOverrides.DB_HOST ?? "localhost"
-export const DB_PORT = localOverrides.DB_PORT ?? 3306
+export const DB_NAME: string = localOverrides.DB_NAME ?? "owid"
+export const DB_USER: string = localOverrides.DB_USER ?? "root"
+export const DB_PASS: string = localOverrides.DB_PASS ?? ""
+export const DB_HOST: string = localOverrides.DB_HOST ?? "localhost"
+export const DB_PORT: number = localOverrides.DB_PORT ?? 3306
 
-export const ENV = localOverrides.ENV ?? "development"
-export const BAKED_SITE_DIR =
+export const ENV: string = localOverrides.ENV ?? "development"
+export const BAKED_SITE_DIR: string =
     localOverrides.BAKED_SITE_DIR ?? `${BASE_DIR}/bakedSite` // Where the static build output goes
-export const SECRET_KEY =
+export const SECRET_KEY: string =
     localOverrides.SECRET_KEY ??
     "fejwiaof jewiafo jeioa fjieowajf isa fjidosajfgj"
-export const WORDPRESS_DB_NAME = localOverrides.WORDPRESS_DB_NAME ?? DB_NAME
-export const WORDPRESS_DB_USER = localOverrides.WORDPRESS_DB_USER ?? DB_USER
-export const WORDPRESS_DB_PASS = localOverrides.WORDPRESS_DB_PASS ?? DB_PASS
-export const WORDPRESS_DB_HOST = localOverrides.WORDPRESS_DB_HOST ?? DB_HOST
-export const WORDPRESS_DB_PORT = localOverrides.WORDPRESS_DB_PORT ?? DB_PORT
-export const WORDPRESS_API_USER = localOverrides.WORDPRESS_API_USER ?? ""
-export const WORDPRESS_API_PASS = localOverrides.WORDPRESS_API_PASS ?? ""
-export const SESSION_COOKIE_AGE = localOverrides.SESSION_COOKIE_AGE ?? 1209600
-export const ALGOLIA_SECRET_KEY = localOverrides.ALGOLIA_SECRET_KEY ?? ""
-export const STRIPE_SECRET_KEY = localOverrides.STRIPE_SECRET_KEY ?? ""
+export const WORDPRESS_DB_NAME: string =
+    localOverrides.WORDPRESS_DB_NAME ?? DB_NAME
+export const WORDPRESS_DB_USER: string =
+    localOverrides.WORDPRESS_DB_USER ?? DB_USER
+export const WORDPRESS_DB_PASS: string =
+    localOverrides.WORDPRESS_DB_PASS ?? DB_PASS
+export const WORDPRESS_DB_HOST: string =
+    localOverrides.WORDPRESS_DB_HOST ?? DB_HOST
+export const WORDPRESS_DB_PORT: number =
+    localOverrides.WORDPRESS_DB_PORT ?? DB_PORT
+export const WORDPRESS_API_USER: string =
+    localOverrides.WORDPRESS_API_USER ?? ""
+export const WORDPRESS_API_PASS: string =
+    localOverrides.WORDPRESS_API_PASS ?? ""
+export const SESSION_COOKIE_AGE: number =
+    localOverrides.SESSION_COOKIE_AGE ?? 1209600
+export const ALGOLIA_SECRET_KEY: string =
+    localOverrides.ALGOLIA_SECRET_KEY ?? ""
+export const STRIPE_SECRET_KEY: string = localOverrides.STRIPE_SECRET_KEY ?? ""
 
 // Settings for automated email sending, e.g. for admin invite
-export const EMAIL_HOST = localOverrides.EMAIL_HOST ?? "smtp.mail.com"
-export const EMAIL_PORT = localOverrides.EMAIL_PORT ?? 443
-export const EMAIL_HOST_USER = localOverrides.EMAIL_HOST_USER ?? "user"
-export const EMAIL_HOST_PASSWORD =
+export const EMAIL_HOST: string = localOverrides.EMAIL_HOST ?? "smtp.mail.com"
+export const EMAIL_PORT: number = localOverrides.EMAIL_PORT ?? 443
+export const EMAIL_HOST_USER: string = localOverrides.EMAIL_HOST_USER ?? "user"
+export const EMAIL_HOST_PASSWORD: string =
     localOverrides.EMAIL_HOST_PASSWORD ?? "password"
 
 // Wordpress target setting
-export const WORDPRESS_DIR = localOverrides.WORDPRESS_DIR ?? ""
-export const HTTPS_ONLY = localOverrides.HTTPS_ONLY ?? true
+export const WORDPRESS_DIR: string = localOverrides.WORDPRESS_DIR ?? ""
+export const HTTPS_ONLY: boolean = localOverrides.HTTPS_ONLY ?? true
 
 // Node slack webhook to report errors to using express-error-slac
-export const SLACK_ERRORS_WEBHOOK_URL =
+export const SLACK_ERRORS_WEBHOOK_URL: string =
     localOverrides.SLACK_ERRORS_WEBHOOK_URL ?? ""
-export const GIT_DATASETS_DIR =
+export const GIT_DATASETS_DIR: string =
     localOverrides.GIT_DATASETS_DIR ?? `${BASE_DIR}/datasetsExport` //  Where the git exports go
-export const TMP_DIR = localOverrides.TMP_DIR ?? "/tmp"
-export const UNCATEGORIZED_TAG_ID = localOverrides.UNCATEGORIZED_TAG_ID ?? 375
+export const TMP_DIR: string = localOverrides.TMP_DIR ?? "/tmp"
+export const UNCATEGORIZED_TAG_ID: number =
+    localOverrides.UNCATEGORIZED_TAG_ID ?? 375
 
 // Should the static site output be baked when relevant database items change
-export const BAKE_ON_CHANGE = localOverrides.BAKE_ON_CHANGE ?? false
-export const DEPLOY_QUEUE_FILE_PATH =
+export const BAKE_ON_CHANGE: boolean = localOverrides.BAKE_ON_CHANGE ?? false
+export const DEPLOY_QUEUE_FILE_PATH: string =
     localOverrides.DEPLOY_QUEUE_FILE_PATH ?? `${BASE_DIR}/.queue`
-export const DEPLOY_PENDING_FILE_PATH =
+export const DEPLOY_PENDING_FILE_PATH: string =
     localOverrides.DEPLOY_PENDING_FILE_PATH ?? `${BASE_DIR}/.pending`
-export const CLOUDFLARE_AUD = localOverrides.CLOUDFLARE_AUD ?? ""
+export const CLOUDFLARE_AUD: string = localOverrides.CLOUDFLARE_AUD ?? ""
