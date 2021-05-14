698ae9b1d72b5cba2ae7487fee80a7dbac3e5eb3
Lars Yencken <lars@yencken.org>
2021-05-14 16:46:17 +0200

    chore: Update db import to match lando settings

--- db/downloadAndCreateDatabase.sh
+++ db/downloadAndCreateDatabase.sh
@@ -1,5 +1,11 @@
-mysql -e "CREATE DATABASE owid;"
+#!/bin/bash
+
+set -e
+
+MYSQL='mysql -h 127.0.0.1 -u root'
+
+$MYSQL -e "CREATE DATABASE owid;"
 curl -Lo /tmp/owid_metadata.sql.gz https://files.ourworldindata.org/owid_metadata.sql.gz
-gunzip < /tmp/owid_metadata.sql.gz | mysql -D owid
+gunzip < /tmp/owid_metadata.sql.gz | $MYSQL -D owid
 curl -Lo /tmp/owid_chartdata.sql.gz https://files.ourworldindata.org/owid_chartdata.sql.gz
-gunzip < /tmp/owid_chartdata.sql.gz | mysql -D owid
+gunzip < /tmp/owid_chartdata.sql.gz | $MYSQL -D owid
