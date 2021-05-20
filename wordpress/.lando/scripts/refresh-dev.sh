64b0e1b8a19887178918eedb3eaff8b94c4acc60
Lars Yencken <lars@yencken.org>
2021-05-20 12:27:39 +0200

    doc: fix typos in refresh script

--- wordpress/.lando/scripts/refresh-dev.sh
+++ wordpress/.lando/scripts/refresh-dev.sh
@@ -19,7 +19,7 @@ SKIP_DB_DL=false
 
 usage()
 {
-  echo "Refreshes content. At the minimum, both Wordress and Grapher databases are cleared and populated after downloading the latest archives."
+  echo "Refreshes content. At the minimum, both Wordpress and Grapher databases are cleared and populated after downloading the latest archives."
   echo "The Grapher database is only populated with owid_metadata by default. Add --with-chartdata to have access to the full content."
   echo "Usage: refresh [options...]"
   echo ""
@@ -68,17 +68,17 @@ import_db(){
 
 # Wordpress DB
 if [ "${SKIP_DB_DL}" = false ]; then
-  echo "Downloading Wordress database (live_wordpress)"
+  echo "Downloading Wordpress database (live_wordpress)"
     ssh owid-live "sudo mysqldump --default-character-set=utf8mb4 live_wordpress -r /tmp/live_wordpress.sql && sudo gzip -f /tmp/live_wordpress.sql"
     rsync -hav --progress owid-live:/tmp/live_wordpress.sql.gz $DL_FOLDER
 fi
-echo "Importing Wordress database (live_wordpress)"
+echo "Importing Wordpress database (live_wordpress)"
 purge_db $WORDPRESS_DB_HOST $WORDPRESS_DB_NAME
 import_db $DL_FOLDER/live_wordpress.sql.gz $WORDPRESS_DB_HOST $WORDPRESS_DB_NAME
 
 # Wordpress uploads
 if [ "${WITH_UPLOADS}" = true ]; then
-  echo "Downloading Wordress uploads"
+  echo "Downloading Wordpress uploads"
   rsync -havz --delete --exclude=/.gitkeep --progress owid-live:live-data/wordpress/uploads/ web/app/uploads
 fi
 
