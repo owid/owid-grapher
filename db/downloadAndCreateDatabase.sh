da2d444c954bf192b08b2c75f2769de76af76c0a
Matthieu Bergel <hello@matthieubergel.org>
2021-05-21 18:18:48 +0200

    Revert "Merge pull request #893 from owid/one-shot-makefile"
    This reverts commit c5dde2a6b4a72c514b8b1504e7ec95dba139a8df

--- db/downloadAndCreateDatabase.sh
+++ db/downloadAndCreateDatabase.sh
@@ -1,20 +1,5 @@
-#!/bin/bash
-#
-#  downloadAndCreateDatabase.sh
-#
-#  Download and ingest the publicly shared subset of the production grapher 
-#  database, containing data for every published graph.
-#
-
-set -e
-
-DB_NAME=grapher
-MYSQL="mysql -h 127.0.0.1 -u root"
-
-# database should not exist for a fresh lando spin-up
-$MYSQL -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-
+mysql -e "CREATE DATABASE owid;"
 curl -Lo /tmp/owid_metadata.sql.gz https://files.ourworldindata.org/owid_metadata.sql.gz
-gunzip < /tmp/owid_metadata.sql.gz | $MYSQL -D ${DB_NAME}
+gunzip < /tmp/owid_metadata.sql.gz | mysql -D owid
 curl -Lo /tmp/owid_chartdata.sql.gz https://files.ourworldindata.org/owid_chartdata.sql.gz
-gunzip < /tmp/owid_chartdata.sql.gz | $MYSQL -D ${DB_NAME}
+gunzip < /tmp/owid_chartdata.sql.gz | mysql -D owid
