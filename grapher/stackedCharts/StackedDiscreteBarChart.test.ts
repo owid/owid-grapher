f54bb23e02f9056e5df8ee6a545810e3f9224b48
Marcel Gerber <mgerber59@gmail.com>
2021-05-30 22:09:10 +0200

    fix: stacked discrete bar chart handles mising values for some columns

--- grapher/stackedCharts/StackedDiscreteBarChart.test.ts
+++ grapher/stackedCharts/StackedDiscreteBarChart.test.ts
@@ -53,12 +53,90 @@ it("can display a StackedDiscreteBar chart in relative mode", () => {
     expect(chart.failMessage).toEqual("")
     expect(chart.series.length).toEqual(2)
     expect(chart.series[0].points).toEqual([
-        { position: "France", value: 40, valueOffset: 0, time: 2000 },
-        { position: "Spain", value: 30, valueOffset: 0, time: 2000 },
+        {
+            position: "France",
+            value: 40,
+            valueOffset: 0,
+            time: 2000,
+            fake: false,
+        },
+        {
+            position: "Spain",
+            value: 30,
+            valueOffset: 0,
+            time: 2000,
+            fake: false,
+        },
     ])
     expect(chart.series[1].points).toEqual([
-        { position: "France", value: 60, valueOffset: 40, time: 2000 },
-        { position: "Spain", value: 70, valueOffset: 30, time: 2000 },
+        {
+            position: "France",
+            value: 60,
+            valueOffset: 40,
+            time: 2000,
+            fake: false,
+        },
+        {
+            position: "Spain",
+            value: 70,
+            valueOffset: 30,
+            time: 2000,
+            fake: false,
+        },
+    ])
+})
+
+it("can display a chart with missing variable data for some entities", () => {
+    const csv = `coal,gas,year,entityName
+    20,,2000,France
+    ,14,2000,Spain`
+    const table = new OwidTable(csv, [
+        { slug: "coal", type: ColumnTypeNames.Numeric },
+        { slug: "gas", type: ColumnTypeNames.Numeric },
+        { slug: "year", type: ColumnTypeNames.Year },
+    ])
+
+    const manager: ChartManager = {
+        table,
+        selection: table.availableEntityNames,
+        yColumnSlugs: ["coal", "gas"],
+    }
+    const chart = new StackedDiscreteBarChart({ manager })
+
+    // Check that our absolute values get properly transformed into percentages
+    expect(chart.failMessage).toEqual("")
+    expect(chart.series.length).toEqual(2)
+    expect(chart.series[0].points).toEqual([
+        {
+            position: "France",
+            value: 20,
+            valueOffset: 0,
+            time: 2000,
+            fake: false,
+        },
+        {
+            position: "Spain",
+            value: 0,
+            valueOffset: 0,
+            time: 0,
+            fake: true,
+        },
+    ])
+    expect(chart.series[1].points).toEqual([
+        {
+            position: "France",
+            value: 0,
+            valueOffset: 20,
+            time: 0,
+            fake: true,
+        },
+        {
+            position: "Spain",
+            value: 14,
+            valueOffset: 0,
+            time: 2000,
+            fake: false,
+        },
     ])
 })
 
