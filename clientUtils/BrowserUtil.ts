72dd3ed0968e65ca57be95c6bc1fafbdb760cfdd
Marcel Gerber <mgerber59@gmail.com>
2021-06-07 22:18:08 +0200

    fix: `prefersReducedMotion` works in node environments

--- clientUtils/BrowserUtil.ts
+++ clientUtils/BrowserUtil.ts
@@ -1,3 +1,4 @@
 // Taken from https://since1979.dev/respecting-prefers-reduced-motion-with-javascript-and-react/
 export const prefersReducedMotion = (): boolean =>
-    window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    typeof window !== "undefined" &&
+    window.matchMedia?.("(prefers-reduced-motion: reduce)").matches
