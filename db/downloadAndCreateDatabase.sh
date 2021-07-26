83c1e2aa99a49f633224e7e40acbf5c47fe7946e
Daniel Gavrilov <dgavrilov95@gmail.com>
2021-07-26 09:57:41 +0100

    chore: fail on error in downloadAndCreateDatabase.sh

--- db/downloadAndCreateDatabase.sh
+++ db/downloadAndCreateDatabase.sh
@@ -1,3 +1,5 @@
+set -ex
+
 mysql -e "CREATE DATABASE owid;"
 curl -Lo /tmp/owid_metadata.sql.gz https://files.ourworldindata.org/owid_metadata.sql.gz
 gunzip < /tmp/owid_metadata.sql.gz | mysql -D owid
