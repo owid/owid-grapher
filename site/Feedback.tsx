43ed2b0563c4897275df0149ed517d103a64636a
Daniel Gavrilov <dgavrilov95@gmail.com>
2021-09-02 11:22:44 +0100

    chore(feedback): replace hardcoded URLs

--- site/Feedback.tsx
+++ site/Feedback.tsx
@@ -73,7 +73,7 @@ const topicMatchers = [
 const vaccineNotice = (
     <a
         key="vaccineNotice"
-        href="https://ourworldindata.org/covid-vaccinations#frequently-asked-questions"
+        href={`${BAKED_BASE_URL}/covid-vaccinations#frequently-asked-questions"`}
         target="_blank"
         rel="noreferrer"
     >
@@ -84,7 +84,7 @@ const vaccineNotice = (
 const copyrightNotice = (
     <a
         key="copyrightNotice"
-        href="https://ourworldindata.org/faqs#how-is-your-work-copyrighted"
+        href={`${BAKED_BASE_URL}/faqs#how-is-your-work-copyrighted"`}
         target="_blank"
         rel="noreferrer"
     >
@@ -94,7 +94,7 @@ const copyrightNotice = (
 const citationNotice = (
     <a
         key="citationNotice"
-        href="https://ourworldindata.org/faqs#how-should-i-cite-your-work"
+        href={`${BAKED_BASE_URL}/faqs#how-should-i-cite-your-work"`}
         target="_blank"
         rel="noreferrer"
     >
@@ -104,7 +104,7 @@ const citationNotice = (
 const translateNotice = (
     <a
         key="translateNotice"
-        href="https://ourworldindata.org/faqs#can-i-translate-your-work-into-another-language"
+        href={`${BAKED_BASE_URL}/faqs#can-i-translate-your-work-into-another-language"`}
         target="_blank"
         rel="noreferrer"
     >
