0aa12c3d477a776387dbe617d4091af9c0b41708
Daniel Bachler <daniel@danielbachler.de>
2021-11-12 09:50:24 +0100

    enhance: add some comments

--- devTools/schemaProcessor/schemaProcessor.ts
+++ devTools/schemaProcessor/schemaProcessor.ts
@@ -25,8 +25,29 @@ function isPlainTypeStringOrNull(item: string) {
     return isPlainTypeString(item) || item === "null"
 }
 
-function extractSchema(schema: any, path: string, items: FieldDescription[]) {
-    // for primitives we have nothing to do
+/** This function takes a json schema that has been read and converted from json to a
+    JS object and processes it into a list of FieldDescriptions. A FieldDescription
+    contains just the information we will need for creating the columns for the Bulk FASTT
+    editor.
+
+    On a high level, what we do here is walk the recursive document structure of the json
+    schema and look at every level. If a property "type" exists and it is object then we
+    want to recurse over the properties (we are usually only interested in properties in
+    leaf position in the object graph because these are the ones we want to map to a single
+    spreadsheet cell). If the type is a primitive then we just create a FieldDescription entry.
+
+    There are some complications, for example:
+        * type can be an array to express a union of null | string
+        * anyof creates a fork that has to be combined into a single entry
+ *
+ */
+function extractSchema(
+    schema: any,
+    path: string,
+    items: FieldDescription[]
+): void {
+    // We shouldn't encounter primitives in the schema itself (since we recurse
+    // only into expected structures which are almost always objects)
     if (
         _.isNull(schema) ||
         _.isUndefined(schema) ||
@@ -38,6 +59,9 @@ function extractSchema(schema: any, path: string, items: FieldDescription[]) {
         console.log("shouldn't come here?", path)
         return
     } else if (_.isPlainObject(schema)) {
+        // If the current schema fragment describes a normal object
+        // then do not emit anything directly and recurse over the
+        // described properties
         if (
             schema.hasOwnProperty("type") &&
             schema.type === "object" &&
@@ -48,6 +72,14 @@ function extractSchema(schema: any, path: string, items: FieldDescription[]) {
                 extractSchema(schema.properties[key], newPath, items)
             }
         } else if (
+            // if we have an object that uses patternProperties to
+            // describe arbitrary properties then we special case
+            // if all such properties are of primitive type. In our
+            // schema we use such constructs for mapping arbitrary
+            // country names to colors for example where the
+            // paternProperties is ".*" and the type of those
+            // is string (interpreted as a color)
+            // We yield something like this as a single entry
             schema.hasOwnProperty("type") &&
             schema.type === "object" &&
             schema.hasOwnProperty("patternProperties") &&
@@ -64,9 +96,12 @@ function extractSchema(schema: any, path: string, items: FieldDescription[]) {
                 path: path,
                 default: schema.default,
                 needsSpecialEditor: true,
+                enumOptions: schema.enum,
             })
         } else if (schema.type === "array") {
-            // TODO: probably special handling for these
+            // For now if we have an array we add "[*]" to the path
+            // and recurse on the items. Since we want to write arrays
+            // as one go we will probably change this soon
             const newPath = `${path}[*]`
             extractSchema(schema.items, newPath, items)
         } else if (
@@ -74,13 +109,23 @@ function extractSchema(schema: any, path: string, items: FieldDescription[]) {
             (_.isArray(schema.type) &&
                 _.every(schema.type, isPlainTypeStringOrNull))
         ) {
+            // If the object describes a primitive type or is
+            // an array of only primitive types or null (e.g.
+            // because the type is ["string", "null"]) then
+            // we yield a single element
             items.push({
                 type: schema.type,
                 path: path,
                 default: schema.default,
                 needsSpecialEditor: false,
+                enumOptions: schema.enum,
             })
         } else if (
+            // If we have a oneOf description then we need to
+            // check if all of the cases have a type field with
+            // a primitive type. If so we collect all the types
+            // and yield a single FieldDefinition with the merged
+            // type
             schema.hasOwnProperty("oneOf") &&
             _.isArray(schema.oneOf) &&
             _.every(
