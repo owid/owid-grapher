0d7fdd03e3d21919196038f49836565e3b27fde4
Matthieu Bergel <hello@matthieubergel.org>
2022-01-20 13:37:14 +0100

    tests(explorer): move renderExplorerPage test to baker

--- explorerAdminServer/ExplorerAdminServer.test.ts
+++ explorerAdminServer/ExplorerAdminServer.test.ts
@@ -2,18 +2,11 @@
 
 jest.setTimeout(10000) // wait up to 10s
 
-import { ExplorerProgram } from "../explorer/ExplorerProgram"
 import { ExplorerAdminServer } from "./ExplorerAdminServer"
 
 it("can init", async () => {
-    const server = new ExplorerAdminServer(__dirname, "https://example.com")
+    const server = new ExplorerAdminServer(__dirname)
     expect(server).toBeTruthy()
-
-    expect(
-        await server.renderExplorerPage(
-            new ExplorerProgram("foo", "explorerTitle helloWorld")
-        )
-    ).toContain("helloWorld")
 
     const allExplorersResult = await server.getAllExplorersCommand()
     expect(allExplorersResult.success).toBeTruthy()
