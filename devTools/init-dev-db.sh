10538a3367f0d32ac7b756ae5a0110f384a73409
Daniel Bachler <daniel@danielbachler.de>
2022-01-28 16:58:43 +0100

    WIP enhance(docker-compose): first attempt at full docker-compose

--- devTools/init-dev-db.sh
+++ devTools/init-dev-db.sh
@@ -4,4 +4,4 @@ set -o pipefail
 set -o nounset
 
 # Run the init-dev-db.sql script inside the mysql container as root to create the two users
-docker exec -i owid-grapher_db_1 sh -c 'exec mysql -uroot -p"weeniest-stretch-contaminate-gnarl"' < init-dev-db.sql
+docker exec -i owid-grapher_db_1 sh -c 'exec mysql -uroot -p"weeniest-stretch-contaminate-gnarl"' < "$(dirname ${BASH_SOURCE})/init-dev-db.sql"
