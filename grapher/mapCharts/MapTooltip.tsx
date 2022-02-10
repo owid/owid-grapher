9af305f97512a1229411b6765e151d467d9ae589
Daniel Gavrilov <dgavrilov95@gmail.com>
2022-02-10 11:02:41 +0000

    feat(map): use sparklines in tooltip instead of sparkbars

--- grapher/mapCharts/MapTooltip.tsx
+++ grapher/mapCharts/MapTooltip.tsx
@@ -2,113 +2,106 @@ import React from "react"
 import { computed } from "mobx"
 import { observer } from "mobx-react"
 import { Tooltip } from "../tooltip/Tooltip.js"
-import { takeWhile, last, first, isMobile } from "../../clientUtils/Util.js"
-import {
-    SparkBars,
-    SparkBarsDatum,
-    SparkBarsProps,
-} from "../sparkBars/SparkBars.js"
-import { SparkBarTimeSeriesValue } from "../sparkBars/SparkBarTimeSeriesValue.js"
-import { MapChartManager, ChoroplethSeries } from "./MapChartConstants.js"
-import { ColorScale } from "../color/ColorScale.js"
+import { MapChartManager } from "./MapChartConstants.js"
+import { ColorScale, ColorScaleManager } from "../color/ColorScale.js"
 import { Time } from "../../coreTable/CoreTableConstants.js"
 import { ChartTypeName } from "../core/GrapherConstants.js"
 import { OwidTable } from "../../coreTable/OwidTable.js"
-import { CoreColumn } from "../../coreTable/CoreTableColumns.js"
+import {
+    EntityName,
+    OwidVariableRow,
+} from "../../coreTable/OwidTableConstants.js"
+import { LineChart } from "../lineCharts/LineChart.js"
+import { PrimitiveType } from "../../clientUtils/owidTypes.js"
+import classNames from "classnames"
+import { Bounds } from "../../clientUtils/Bounds.js"
+import { LineChartManager } from "../lineCharts/LineChartConstants.js"
+import {
+    darkenColorForHighContrastText,
+    darkenColorForLine,
+    darkenColorForText,
+} from "../color/ColorUtils.js"
+import { getPreposition } from "../../coreTable/CoreTableUtils.js"
+import { isNumber } from "../../clientUtils/Util.js"
 
 interface MapTooltipProps {
-    tooltipDatum?: ChoroplethSeries
-    tooltipTarget?: { x: number; y: number; featureId: string }
-    isEntityClickable?: boolean
+    entityName: EntityName
     manager: MapChartManager
-    colorScale?: ColorScale
+    colorScaleManager: ColorScaleManager
+    formatValue: (d: PrimitiveType) => string
+    timeSeriesTable: OwidTable
+    tooltipTarget: { x: number; y: number; featureId: string }
     targetTime?: Time
+    isEntityClickable?: boolean
 }
+
+const SPARKLINE_WIDTH = 140
+const SPARKLINE_HEIGHT = 60
 
 @observer
 export class MapTooltip extends React.Component<MapTooltipProps> {
-    private sparkBarsDatumXAccessor = (d: SparkBarsDatum): number => d.time
-
-    @computed private get sparkBarsToDisplay(): number {
-        return isMobile() ? 13 : 20
-    }
-
-    @computed private get sparkBarsProps(): SparkBarsProps<SparkBarsDatum> {
-        return {
-            data: this.sparkBarsData,
-            x: this.sparkBarsDatumXAccessor,
-            y: (d: SparkBarsDatum): number => d.value,
-            xDomain: this.sparkBarsDomain,
-        }
-    }
-
-    @computed get inputTable(): OwidTable {
-        return this.props.manager.table
-    }
-
     @computed private get mapColumnSlug(): string | undefined {
         return this.props.manager.mapColumnSlug
     }
 
-    // Uses the rootTable because if a target year is set, we filter the years at the grapher level.
-    // Todo: might want to do all filtering a step below the Grapher level?
-    @computed private get sparkBarColumn(): CoreColumn {
-        return this.inputTable.rootTable.get(this.mapColumnSlug)
+    @computed private get entityName(): EntityName {
+        return this.props.entityName
     }
 
-    @computed private get sparkBarsData(): SparkBarsDatum[] {
-        const tooltipDatum = this.props.tooltipDatum
-        if (!tooltipDatum) return []
-
-        const sparkBarValues: SparkBarsDatum[] = []
-        this.sparkBarColumn.valueByEntityNameAndOriginalTime
-            .get(tooltipDatum.seriesName)
-            ?.forEach((value, key) => {
-                sparkBarValues.push({
-                    time: key,
-                    value: value as number,
-                })
-            })
-
-        return takeWhile(
-            sparkBarValues,
-            (d) => d.time <= tooltipDatum.time
-        ).slice(-this.sparkBarsToDisplay)
+    // Table pre-filtered by targetTime, exlcudes time series
+    @computed private get mapTable(): OwidTable {
+        const table =
+            this.props.manager.transformedTable ?? this.props.manager.table
+        return table.filterByEntityNames([this.entityName])
     }
 
-    @computed private get sparkBarsDomain(): [number, number] {
-        const lastVal = last(this.sparkBarsData)
-
-        const end = lastVal ? this.sparkBarsDatumXAccessor(lastVal) : 0
-        const start = end > 0 ? end - this.sparkBarsToDisplay + 1 : 0
-
-        return [start, end]
+    @computed private get timeSeriesTable(): OwidTable | undefined {
+        if (this.mapColumnSlug === undefined) return undefined
+        return this.props.timeSeriesTable
+            .filterByEntityNames([this.entityName])
+            .columnFilter(
+                this.mapColumnSlug,
+                isNumber,
+                "Drop rows with non-number values in Y column"
+            )
     }
 
-    @computed private get currentSparkBar(): number | undefined {
-        const lastVal = last(this.sparkBarsData)
-        return lastVal ? this.sparkBarsDatumXAccessor(lastVal) : undefined
+    @computed private get datum(): OwidVariableRow<PrimitiveType> | undefined {
+        return this.mapTable.get(this.mapColumnSlug).owidRows[0]
     }
 
-    colorScale = this.props.colorScale ?? new ColorScale()
-
-    @computed private get renderSparkBars(): boolean | undefined {
-        return this.props.manager.mapIsClickable
+    @computed private get hasTimeSeriesData(): boolean {
+        return this.timeSeriesTable !== undefined
+            ? this.timeSeriesTable.numRows > 1
+            : false
     }
 
-    @computed private get darkestColorInColorScheme(): string | undefined {
-        const { colorScale } = this
-        return colorScale.isColorSchemeInverted
-            ? first(colorScale.baseColors)
-            : last(colorScale.baseColors)
+    @computed private get lineColorScale(): ColorScale {
+        const manager = this.props.colorScaleManager
+        return new ColorScale({
+            colorScaleConfig: manager.colorScaleConfig,
+            hasNoDataBin: manager.hasNoDataBin,
+            defaultNoDataColor: manager.defaultNoDataColor,
+            defaultBaseColorScheme: manager.defaultBaseColorScheme,
+            colorScaleColumn: manager.colorScaleColumn,
+            transformColor: darkenColorForLine,
+        })
     }
 
-    @computed private get barColor(): string | undefined {
-        const { colorScale } = this
-        return colorScale.singleColorScale &&
-            !colorScale.customNumericColorsActive
-            ? this.darkestColorInColorScheme
-            : undefined
+    @computed private get textColorScale(): ColorScale {
+        const manager = this.props.colorScaleManager
+        return new ColorScale({
+            colorScaleConfig: manager.colorScaleConfig,
+            hasNoDataBin: manager.hasNoDataBin,
+            defaultNoDataColor: manager.defaultNoDataColor,
+            defaultBaseColorScheme: manager.defaultBaseColorScheme,
+            colorScaleColumn: manager.colorScaleColumn,
+            transformColor: darkenColorForText,
+        })
+    }
+
+    @computed private get showSparkline(): boolean {
+        return this.hasTimeSeriesData
     }
 
     @computed private get tooltipTarget(): {
@@ -125,25 +118,74 @@ export class MapTooltip extends React.Component<MapTooltipProps> {
         )
     }
 
-    render(): JSX.Element {
-        const { tooltipDatum, isEntityClickable, targetTime, manager } =
-            this.props
+    // Line chart fields
+    @computed private get sparklineTable(): OwidTable {
+        return this.timeSeriesTable ?? new OwidTable()
+    }
+    @computed private get sparklineManager(): LineChartManager {
+        return {
+            table: this.sparklineTable,
+            transformedTable: this.sparklineTable,
+            yColumnSlug: this.mapColumnSlug,
+            colorColumnSlug: this.mapColumnSlug,
+            selection: [this.entityName],
+            colorScaleOverride: this.lineColorScale,
+            hideLegend: true,
+            hidePoints: true,
+            baseFontSize: 11,
+            disableIntroAnimation: true,
+            lineStrokeWidth: 3.5,
+            annotation: {
+                entityName: this.entityName,
+                year: this.datum?.time,
+            },
+            yAxisConfig: {
+                hideAxis: true,
+                hideGridlines: false,
+                compactLabels: true,
+                ticks: [
+                    { value: 0, priority: 1 },
+                    // { value: -Infinity, priority: 2 },
+                    // { value: Infinity, priority: 2 },
+                ],
+            },
+            xAxisConfig: {
+                hideAxis: false,
+                hideGridlines: true,
+                compactLabels: true,
+                // Always show up to the target time on the X axis
+                min: this.props.targetTime,
+                max: this.props.targetTime,
+                // Horizontal axes by default add the start & end ticks,
+                // and we don't need any further ticks.
+                maxTicks: 0,
+            },
+        }
+    }
 
+    render(): JSX.Element {
+        const { tooltipTarget, mapTable, datum, textColorScale } = this
+        const { isEntityClickable, targetTime, manager } = this.props
+
+        // Only LineChart and ScatterPlot allow `mapIsClickable`
         const clickToSelectMessage =
             manager.type === ChartTypeName.LineChart
                 ? "Click for change over time"
                 : "Click to select"
 
-        const { timeColumn } = this.inputTable
-        const { renderSparkBars, barColor, tooltipTarget } = this
-
+        // TODO simplify?
+        const { timeColumn } = mapTable
+        const preposition = getPreposition(timeColumn)
         const displayTime = !timeColumn.isMissing
             ? timeColumn.formatValue(targetTime)
             : targetTime
         const displayDatumTime =
-            timeColumn && tooltipDatum
-                ? timeColumn.formatValue(tooltipDatum.time)
-                : tooltipDatum?.time.toString() ?? ""
+            timeColumn && datum
+                ? timeColumn.formatValue(datum?.time)
+                : datum?.time.toString() ?? ""
+        const valueColor: string | undefined = darkenColorForHighContrastText(
+            textColorScale?.getColor(datum?.value) ?? "#333"
+        )
 
         return (
             <Tooltip
@@ -168,43 +210,55 @@ export class MapTooltip extends React.Component<MapTooltipProps> {
                         tooltipTarget.featureId.replace(/_/g, " ")}
                 </h3>
                 <div
+                    className="map-tooltip"
                     style={{
                         margin: 0,
                         padding: "0.3em 0.3em",
                     }}
                 >
-                    {tooltipDatum ? (
-                        <div className="map-tooltip">
-                            <div className="trend">
-                                {renderSparkBars && (
-                                    <div className="plot">
-                                        <SparkBars<SparkBarsDatum>
-                                            {...this.sparkBarsProps}
-                                            currentX={this.currentSparkBar}
-                                            color={barColor}
-                                        />
-                                    </div>
-                                )}
-                                <div
-                                    className={
-                                        "value" +
-                                        (renderSparkBars ? "" : " no-plot")
-                                    }
+                    <div className="map-value-with-sparkline">
+                        {this.showSparkline && (
+                            <div className="sparkline">
+                                <svg
+                                    className="plot"
+                                    width={SPARKLINE_WIDTH}
+                                    height={SPARKLINE_HEIGHT}
                                 >
-                                    <SparkBarTimeSeriesValue
-                                        className="current"
-                                        value={tooltipDatum.displayValue}
-                                        displayDate={displayDatumTime}
-                                        valueColor={
-                                            renderSparkBars ? barColor : "black"
-                                        }
+                                    <LineChart
+                                        manager={this.sparklineManager}
+                                        // Add padding so that the edges of the plot doesn't get clipped.
+                                        // The plot can go out of boundaries due to line stroke thickness.
+                                        bounds={new Bounds(
+                                            0,
+                                            0,
+                                            SPARKLINE_WIDTH,
+                                            SPARKLINE_HEIGHT
+                                        ).pad({ top: 7, left: 7, right: 7 })}
                                     />
-                                </div>
+                                </svg>
+                            </div>
+                        )}
+
+                        <div
+                            className={classNames({
+                                "value-wrapper": true,
+                                "no-plot": !this.showSparkline,
+                            })}
+                        >
+                            <div
+                                className="value"
+                                style={{ color: valueColor }}
+                            >
+                                {datum
+                                    ? this.props.formatValue(datum.value)
+                                    : "No data"}
+                            </div>
+                            <div className="time">
+                                {preposition}{" "}
+                                {datum ? displayDatumTime : displayTime}
                             </div>
                         </div>
-                    ) : (
-                        `No data for ${displayTime}`
-                    )}
+                    </div>
                 </div>
                 {isEntityClickable && (
                     <div>
