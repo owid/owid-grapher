a510cb55c7e6cf846a3ae97f0eff1b66d664d571
Daniel Gavrilov <dgavrilov95@gmail.com>
2022-03-08 12:46:15 +0000

    chore(docker): create `tmp-downloads` folder if doesn't exist

--- devTools/docker/download-grapher-mysql.sh
+++ devTools/docker/download-grapher-mysql.sh
@@ -5,6 +5,8 @@ set -o nounset
 
 FOLDER="${DATA_FOLDER:-./tmp-downloads}"
 
+mkdir -p $FOLDER
+
 echo "Downloading live Grapher metadata database (owid_metadata)"
 curl -Lo $FOLDER/owid_metadata.sql.gz https://files.ourworldindata.org/owid_metadata.sql.gz
 
--- devTools/docker/download-wordpress-mysql.sh
+++ devTools/docker/download-wordpress-mysql.sh
@@ -9,5 +9,7 @@ set -o nounset
 
 FOLDER="${DATA_FOLDER:-./tmp-downloads}"
 
+mkdir -p $FOLDER
+
 ssh owid-live "sudo mysqldump --default-character-set=utf8mb4 live_wordpress -r /tmp/live_wordpress.sql && sudo gzip -f /tmp/live_wordpress.sql"
 rsync -hav --progress owid-live:/tmp/live_wordpress.sql.gz $FOLDER
