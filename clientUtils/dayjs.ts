ee44f9571a56ab2c1fc9be9a84c080f87c21e989
Marcel Gerber <mgerber59@gmail.com>
2022-03-09 11:41:08 +0100

    refactor(dayjs): add customParseFormat plugin

--- clientUtils/dayjs.ts
+++ clientUtils/dayjs.ts
@@ -1,16 +1,19 @@
 // Imports dayjs and loads the plugins we need. Then exports it with the correct types.
 
 import dayjs, { Dayjs } from "dayjs"
-import utc from "dayjs/plugin/utc"
+import customParseFormat from "dayjs/plugin/customParseFormat"
 import relativeTime from "dayjs/plugin/relativeTime"
+import utc from "dayjs/plugin/utc"
 
-dayjs.extend(utc)
+dayjs.extend(customParseFormat)
 dayjs.extend(relativeTime)
+dayjs.extend(utc)
 
 export default dayjs
 
 // We need these explicit plugin type imports _and exports_ to get the right Dayjs type down the line
-import type utcType from "dayjs/plugin/utc"
+import type customParseFormatType from "dayjs/plugin/customParseFormat"
 import type relativeTimeType from "dayjs/plugin/relativeTime"
+import type utcType from "dayjs/plugin/utc"
 
-export type { Dayjs, utcType, relativeTimeType }
+export type { Dayjs, customParseFormatType, relativeTimeType, utcType }
