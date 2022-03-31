83a2b13caa19428e621592b787000b73b58da2fd
Matthieu Bergel <hello@matthieubergel.org>
2022-03-31 10:31:35 +0200

    refactor(format): remove unused "references" property

--- clientUtils/owidTypes.ts
+++ clientUtils/owidTypes.ts
@@ -66,7 +66,6 @@ export interface FormattedPost extends FullPost {
     info?: string
     html: string
     style?: string
-    references: Record<string, unknown>[]
     footnotes: string[]
     tocHeadings: TocHeading[]
     pageDesc: string
