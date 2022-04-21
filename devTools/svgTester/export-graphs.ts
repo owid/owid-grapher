d111d935e63af806c186f5dbffed1d47af8383cb
ikesau <ikesau@protonmail.com>
2022-04-21 16:03:35 +0000

    improve(export-graphs.ts): add batch comparisons

--- devTools/svgTester/export-graphs.ts
+++ devTools/svgTester/export-graphs.ts
@@ -9,8 +9,16 @@ import workerpool from "workerpool"
 async function main(parsedArgs: parseArgs.ParsedArgs) {
     try {
         const inDir = parsedArgs["i"] ?? "grapherData"
-        const outDir = parsedArgs["o"] ?? "grapherSvgs"
-        const targetConfig = parsedArgs["c"]
+        let outDir = parsedArgs["o"] ?? "grapherSvgs"
+        const targetConfigs: string[] = (parsedArgs["c"] ?? "")
+            .split(",")
+            .map(String)
+            .filter((entry: string) => entry)
+
+        if (targetConfigs.length) {
+            outDir = path.join(outDir, "comparisons")
+        }
+
         if (!fs.existsSync(inDir))
             throw `Input directory does not exist ${inDir}`
         if (!fs.existsSync(outDir)) fs.mkdirSync(outDir, { recursive: true })
@@ -19,9 +27,9 @@ async function main(parsedArgs: parseArgs.ParsedArgs) {
         const directories = []
         for await (const entry of dir) {
             if (entry.isDirectory()) {
-                if (String(targetConfig) === entry.name) {
+                if (!targetConfigs.length) {
                     directories.push(entry.name)
-                } else if (!targetConfig) {
+                } else if (targetConfigs.includes(entry.name)) {
                     directories.push(entry.name)
                 }
             }
@@ -38,6 +46,27 @@ async function main(parsedArgs: parseArgs.ParsedArgs) {
         const svgRecords: utils.SvgRecord[] = await Promise.all(
             jobDescriptions.map((job) => pool.exec("renderSvgAndSave", [job]))
         )
+
+        // Copy over copies from master for easy comparing
+        if (targetConfigs.length) {
+            const comparisonDir = await fs.opendir(outDir)
+            const filenames: string[] = []
+            for await (const file of comparisonDir) {
+                if (file.name.includes("svg")) {
+                    filenames.push(file.name)
+                }
+            }
+            const svgPath = path.join(inDir, "..", "svg")
+            const masterDir = await fs.opendir(svgPath)
+            for await (const file of masterDir) {
+                if (filenames.includes(file.name)) {
+                    await fs.copyFile(
+                        path.join(svgPath, file.name),
+                        path.join(outDir, file.name.replace(".svg", "_old.svg"))
+                    )
+                }
+            }
+        }
 
         await utils.writeResultsCsvFile(outDir, svgRecords)
         // This call to exit is necessary for some unknown reason to make sure that the process terminates. It
@@ -61,7 +90,7 @@ Usage:
 Options:
     -i DIR         Input directory containing the data. [default: grapherData]
     -o DIR         Output directory that will contain the csv file and one svg file per grapher [default: grapherSvgs]
-    -c ID          A specific config ID that you want to run instead of generating SVGs from all configs [default: undefined] 
+    -c ID          A comma-separated list of config IDs that you want to run instead of generating SVGs from all configs [default: undefined] 
     `)
     process.exit(0)
 } else {
