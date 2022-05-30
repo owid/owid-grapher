988715d5c7ac02b772d876ac0b967611dfc00cd1
ikesau <ikesau@protonmail.com>
2022-05-30 21:01:03 +0000

    feature(DetailsOnDemand.tsx): add API call

--- adminSiteClient/DetailsOnDemand.tsx
+++ adminSiteClient/DetailsOnDemand.tsx
@@ -1,8 +1,30 @@
 import React from "react"
+import { observable } from "mobx"
+import { bind } from "decko"
 import { observer } from "mobx-react"
 import { AdminLayout } from "./AdminLayout.js"
+
+interface Detail {
+    category: string
+    term: string
+    title: string
+    content: string
+    id: number
+}
+
 @observer
 export class DetailsOnDemandPage extends React.Component {
+    @observable details: Detail[] = []
+
+    @bind async getDetails() {
+        const json = await this.context.admin.getJSON("/api/details")
+        this.details = json.details
+    }
+
+    componentDidMount() {
+        this.getDetails()
+    }
+
     render() {
         return (
             <AdminLayout title="Details on Demand">
