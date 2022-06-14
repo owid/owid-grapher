13bc6e64d2f676a39da0d8be139a0056fc25bf00
Marcel Gerber <mgerber59@gmail.com>
2022-06-14 12:35:13 +0200

    style: replace tabs with spaces

--- db/migration/1654553380446-AddSourceChecksum.ts
+++ db/migration/1654553380446-AddSourceChecksum.ts
@@ -3,16 +3,16 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class AddSourceChecksum1654553380446 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-			ALTER TABLE datasets 
-			ADD COLUMN sourceChecksum VARCHAR(64) 
-			DEFAULT NULL;
-    	`)
+            ALTER TABLE datasets 
+            ADD COLUMN sourceChecksum VARCHAR(64) 
+            DEFAULT NULL;
+        `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-			ALTER TABLE datasets 
-			DROP COLUMN sourceChecksum;
-    	`)
+            ALTER TABLE datasets 
+            DROP COLUMN sourceChecksum;
+        `)
     }
 }
