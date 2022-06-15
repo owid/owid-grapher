416684e6c96a17ca20f54bb0c8556112af5fbab9
Matthieu Bergel <hello@matthieubergel.org>
2022-06-15 14:58:48 +0200

    feat(iframe): detect showContext param

--- grapher/core/GrapherConstants.ts
+++ grapher/core/GrapherConstants.ts
@@ -20,6 +20,9 @@ export const GRAPHER_PAGE_BODY_CLASS = "StandaloneGrapherOrExplorerPage"
 
 export const GRAPHER_IS_IN_IFRAME_CLASS = "IsInIframe"
 
+export const GRAPHER_SHOW_CONTEXT_CLASS = "show-context"
+export const GRAPHER_SHOW_CONTEXT_PARAM = "showContext"
+
 export const DEFAULT_GRAPHER_WIDTH = 850
 export const DEFAULT_GRAPHER_HEIGHT = 600
 
--- site/IframeDetector.tsx
+++ site/IframeDetector.tsx
@@ -1,10 +1,18 @@
-import { GRAPHER_IS_IN_IFRAME_CLASS } from "../grapher/core/GrapherConstants.js"
+import {
+    GRAPHER_IS_IN_IFRAME_CLASS,
+    GRAPHER_SHOW_CONTEXT_CLASS,
+    GRAPHER_SHOW_CONTEXT_PARAM,
+} from "../grapher/core/GrapherConstants.js"
 import React from "react"
 
 export const IFrameDetector = () => (
     <script
         dangerouslySetInnerHTML={{
-            __html: `if (window != window.top) document.documentElement.classList.add('${GRAPHER_IS_IN_IFRAME_CLASS}')`,
+            __html: `
+            if (window != window.top) document.documentElement.classList.add('${GRAPHER_IS_IN_IFRAME_CLASS}')
+            const params = new URLSearchParams(window.location.search)
+            if(params.get("${GRAPHER_SHOW_CONTEXT_PARAM}")) document.documentElement.classList.add('${GRAPHER_SHOW_CONTEXT_CLASS}')
+            `,
         }}
     />
 )
