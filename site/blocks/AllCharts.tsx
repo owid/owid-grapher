0c03f9fe924f196d41272d245ec143b142c1bca1
Matthieu Bergel <hello@matthieubergel.org>
2022-06-17 14:29:45 +0200

    feat(gallery): add all charts gallery

---
+++ site/blocks/AllCharts.tsx
@@ -0,0 +1,104 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { useState } from "react"
+import { RelatedChart } from "../../clientUtils/owidTypes.js"
+import { BAKED_GRAPHER_EXPORTS_BASE_URL } from "../../settings/clientSettings.js"
+import {
+    DEFAULT_GRAPHER_HEIGHT,
+    DEFAULT_GRAPHER_WIDTH,
+} from "../../grapher/core/GrapherConstants.js"
+import { Modal } from "../Modal.js"
+import { GalleryArrow, GalleryArrowDirection } from "../GalleryArrow.js"
+
+export const ALL_CHARTS_CLASS_NAME = "wp-block-all-charts"
+
+export const AllCharts = ({ charts }: { charts: RelatedChart[] }) => {
+    const [isGalleryOpen, setGalleryOpen] = useState(false)
+    const [activeSlide, setActiveSlide] = useState(0)
+
+    const isFirstSlideActive = activeSlide === 0
+    const isLastSlideActive = activeSlide === charts.length - 1
+
+    return (
+        <div className={ALL_CHARTS_CLASS_NAME}>
+            <ul>
+                {charts.map((chart, idx) => (
+                    <li key={chart.slug}>
+                        <a
+                            href={`/grapher/${chart.slug}`}
+                            onClick={(event) => {
+                                // Allow opening charts in new tab/window with ⌘+CLICK
+                                if (
+                                    !event.metaKey &&
+                                    !event.shiftKey &&
+                                    !event.ctrlKey
+                                ) {
+                                    event.preventDefault()
+                                    setActiveSlide(idx)
+                                    setGalleryOpen(!isGalleryOpen)
+                                }
+                            }}
+                        >
+                            <img
+                                src={`${BAKED_GRAPHER_EXPORTS_BASE_URL}/${chart.slug}.svg`}
+                                loading="lazy"
+                                data-no-lightbox
+                                data-no-img-formatting
+                                width={DEFAULT_GRAPHER_WIDTH}
+                                height={DEFAULT_GRAPHER_HEIGHT}
+                            ></img>
+                            <span>{chart.title}</span>
+                        </a>
+                        {chart.variantName ? (
+                            <span className="variantName">
+                                {chart.variantName}
+                            </span>
+                        ) : null}
+                    </li>
+                ))}
+            </ul>
+            {isGalleryOpen && (
+                <Modal onClose={() => setGalleryOpen(false)}>
+                    <div className="all-charts-gallery">
+                        <iframe
+                            src={`https://ourworldindata.org/grapher/${charts[activeSlide].slug}`}
+                            loading="lazy"
+                        />
+                        <div className="navigation">
+                            <GalleryArrow
+                                disabled={isFirstSlideActive}
+                                onClick={() => setActiveSlide(activeSlide - 1)}
+                                label={
+                                    !isFirstSlideActive
+                                        ? charts[activeSlide - 1].title
+                                        : ""
+                                }
+                                direction={GalleryArrowDirection.prev}
+                            ></GalleryArrow>
+                            <GalleryArrow
+                                disabled={isLastSlideActive}
+                                onClick={() => setActiveSlide(activeSlide + 1)}
+                                label={
+                                    !isLastSlideActive
+                                        ? charts[activeSlide + 1].title
+                                        : ""
+                                }
+                                direction={GalleryArrowDirection.next}
+                            ></GalleryArrow>
+                        </div>
+                    </div>
+                </Modal>
+            )}
+        </div>
+    )
+}
+
+export const runAllCharts = (charts: RelatedChart[]) => {
+    const relatedChartsEl = document.querySelector<HTMLElement>(
+        `.${ALL_CHARTS_CLASS_NAME}`
+    )
+    if (relatedChartsEl) {
+        const relatedChartsWrapper = relatedChartsEl.parentElement
+        ReactDOM.hydrate(<AllCharts charts={charts} />, relatedChartsWrapper)
+    }
+}
