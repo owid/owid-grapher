ec2e6e1facf1f3b6a2d9f75e77b7848a37ed7668
ikesau <ikesau@protonmail.com>
2022-06-24 15:48:39 -0400

    feat(parser): add mdParser

---
+++ grapher/text/parser.test.ts
@@ -0,0 +1,322 @@
+import { mdParser } from "./parser.js"
+
+describe("mdast parsers", () => {
+    it("mdParser.text can parse a word", () => {
+        expect(mdParser.text.parse("word")).toEqual({
+            status: true,
+            value: {
+                type: "text",
+                value: "word",
+            },
+        })
+    })
+    it("mdParser.text can parse words with punctuation", () => {
+        expect(mdParser.text.parse("can't?")).toEqual({
+            status: true,
+            value: {
+                type: "text",
+                value: "can't?",
+            },
+        })
+
+        expect(mdParser.text.parse("'mid-west'")).toEqual({
+            status: true,
+            value: {
+                type: "text",
+                value: "'mid-west'",
+            },
+        })
+    })
+    it("mdParser.bold can parse a word with bold", () => {
+        expect(mdParser.bold.parse("**I'm bold as brass**")).toEqual({
+            status: true,
+            value: {
+                type: "bold",
+                children: [
+                    { type: "text", value: "I'm" },
+                    { type: "text", value: "bold" },
+                    { type: "text", value: "as" },
+                    { type: "text", value: "brass" },
+                ],
+            },
+        })
+    })
+    it("mdParser.italic can parse a phrase with italics", () => {
+        expect(mdParser.italic.parse("_Mamma mia!_")).toEqual({
+            status: true,
+            value: {
+                type: "italic",
+                children: [
+                    { type: "text", value: "Mamma" },
+                    { type: "text", value: "mia!" },
+                ],
+            },
+        })
+    })
+    it("mdParser.url can parse URLs", () => {
+        expect(mdParser.url.parse("www.google.com")).toEqual({
+            status: true,
+            value: {
+                type: "url",
+                content: "www.google.com",
+                href: "www.google.com",
+            },
+        })
+        expect(mdParser.url.parse("[test](www.google.com)")).toEqual({
+            status: true,
+            value: {
+                type: "url",
+                children: [{ type: "text", value: "test" }],
+                href: "www.google.com",
+            },
+        })
+    })
+
+    it("mdParser.detailOnDemand can parse detail on demand syntax", () => {
+        expect(
+            mdParser.detailOnDemand.parse("[**dod**](hover::general::thing)")
+        ).toEqual({
+            status: true,
+            value: {
+                type: "detailOnDemand",
+                children: [
+                    {
+                        type: "bold",
+                        children: [
+                            {
+                                type: "text",
+                                value: "dod",
+                            },
+                        ],
+                    },
+                ],
+                category: "general",
+                term: "thing",
+            },
+        })
+        expect(
+            mdParser.detailOnDemand.parse(
+                "[a dod with multiple words](hover::general::thing)"
+            )
+        ).toEqual({
+            status: true,
+            value: {
+                type: "detailOnDemand",
+                children: [
+                    {
+                        type: "text",
+                        value: "a",
+                    },
+                    {
+                        type: "text",
+                        value: "dod",
+                    },
+                    {
+                        type: "text",
+                        value: "with",
+                    },
+                    {
+                        type: "text",
+                        value: "multiple",
+                    },
+                    {
+                        type: "text",
+                        value: "words",
+                    },
+                ],
+                category: "general",
+                term: "thing",
+            },
+        })
+    })
+    it("mdParser.value can parse words and newlines", () => {
+        expect(
+            mdParser.value.parse(`hello
+        
+        world
+        
+        how **are** you?`)
+        ).toEqual({
+            status: true,
+            value: [
+                {
+                    type: "text",
+                    value: "hello",
+                },
+                {
+                    type: "newline",
+                },
+                {
+                    type: "newline",
+                },
+                {
+                    type: "text",
+                    value: "world",
+                },
+                {
+                    type: "newline",
+                },
+                {
+                    type: "newline",
+                },
+                {
+                    type: "text",
+                    value: "how",
+                },
+                {
+                    children: [
+                        {
+                            type: "text",
+                            value: "are",
+                        },
+                    ],
+                    type: "bold",
+                },
+                {
+                    type: "text",
+                    value: "you?",
+                },
+            ],
+        })
+    })
+
+    it("mdParser can parse nested bold and italics", () => {
+        expect(
+            mdParser.value.parse(
+                "Hello _I am italicized and **I am bolded and italicized**_"
+            )
+        ).toEqual({
+            status: true,
+            value: [
+                {
+                    type: "text",
+                    value: "Hello",
+                },
+                {
+                    children: [
+                        {
+                            type: "text",
+                            value: "I",
+                        },
+                        {
+                            type: "text",
+                            value: "am",
+                        },
+                        {
+                            type: "text",
+                            value: "italicized",
+                        },
+                        {
+                            type: "text",
+                            value: "and",
+                        },
+                        {
+                            children: [
+                                {
+                                    type: "text",
+                                    value: "I",
+                                },
+                                {
+                                    type: "text",
+                                    value: "am",
+                                },
+                                {
+                                    type: "text",
+                                    value: "bolded",
+                                },
+                                {
+                                    type: "text",
+                                    value: "and",
+                                },
+                                {
+                                    type: "text",
+                                    value: "italicized",
+                                },
+                            ],
+                            type: "bold",
+                        },
+                    ],
+                    type: "italic",
+                },
+            ],
+        })
+    })
+
+    it("mdParser.value can parse links inside bold and italics", () => {
+        expect(
+            mdParser.value.parse(
+                "**_[bold and italic](www.ourworldindata.org)_**"
+            )
+        ).toEqual({
+            status: true,
+            value: [
+                {
+                    children: [
+                        {
+                            children: [
+                                {
+                                    children: [
+                                        {
+                                            type: "text",
+                                            value: "bold",
+                                        },
+                                        {
+                                            type: "text",
+                                            value: "and",
+                                        },
+                                        {
+                                            type: "text",
+                                            value: "italic",
+                                        },
+                                    ],
+                                    href: "www.ourworldindata.org",
+                                    type: "url",
+                                },
+                            ],
+                            type: "italic",
+                        },
+                    ],
+                    type: "bold",
+                },
+            ],
+        })
+
+        expect(
+            mdParser.value.parse(
+                "_**[italic and bold](www.ourworldindata.org)**_"
+            )
+        ).toEqual({
+            status: true,
+            value: [
+                {
+                    children: [
+                        {
+                            children: [
+                                {
+                                    children: [
+                                        {
+                                            type: "text",
+                                            value: "italic",
+                                        },
+                                        {
+                                            type: "text",
+                                            value: "and",
+                                        },
+                                        {
+                                            type: "text",
+                                            value: "bold",
+                                        },
+                                    ],
+                                    href: "www.ourworldindata.org",
+                                    type: "url",
+                                },
+                            ],
+                            type: "bold",
+                        },
+                    ],
+                    type: "italic",
+                },
+            ],
+        })
+    })
+})
---
+++ grapher/text/parser.ts
@@ -0,0 +1,110 @@
+import P, { Language } from "parsimmon"
+
+// An AST inspired by MDAST
+// Deviates because we want to track individual words and newlines to use with TextWrap and our SVG exporter
+
+// The root literal
+interface Text {
+    type: "text"
+    value: string
+}
+
+export const textParser = (): P.Parser<Text> =>
+    P.regex(/[A-Za-z'\-":!?,.]+/).map((value) => ({ type: "text", value }))
+
+// A special type that we'll use when building lines with TextWrap
+interface Newline {
+    type: "newline"
+}
+
+const newlineParser = (): P.Parser<Newline> =>
+    P.regex(/\n/).map(() => ({
+        type: "newline",
+    }))
+
+// A less greedy version of P.whitespace that doesn't consume newlines
+const whitespaceParser = P.regex(/ */)
+
+//
+interface MarkdownNode {
+    type: string
+    children: (Text | MarkdownNode)[]
+}
+
+const boldParser = (r: P.Language) =>
+    P.seqObj<MarkdownNode>(
+        P.string("**"),
+        ["children", r.value],
+        P.string("**")
+    ).map(({ children }) => ({
+        type: "bold",
+        children,
+    }))
+
+const italicParser = (r: P.Language) =>
+    P.seqObj<MarkdownNode>(
+        P.string("_"),
+        ["children", r.value],
+        P.string("_")
+    ).map(({ children }) => ({
+        type: "italic",
+        children,
+    }))
+
+// https://urlregex.com
+const urlRegex =
+    /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+
+const markdownUrlParser = (r: Language) =>
+    P.seqObj<{ children: MarkdownNode[]; href: string }>(
+        P.string("["),
+        ["children", r.phrasing.sepBy(whitespaceParser)],
+        P.string("]("),
+        ["href", P.regex(urlRegex)],
+        P.string(")")
+    ).map(({ children, href }) => ({
+        type: "url",
+        children,
+        href,
+    }))
+
+const plainURLParser = P.regex(urlRegex).map((result) => ({
+    type: "url",
+    content: result,
+    href: result,
+}))
+
+const urlParser = (r: Language) => P.alt(markdownUrlParser(r), plainURLParser)
+
+const detailOnDemandParser = (r: Language) =>
+    P.seqObj<{
+        category: string
+        term: string
+        children: MarkdownNode[]
+    }>(
+        P.string("["),
+        ["children", r.phrasing.sepBy(whitespaceParser)],
+        P.string("](hover::"),
+        ["category", P.letters],
+        P.string("::"),
+        ["term", P.letters],
+        P.string(")")
+    ).map(({ children, category, term }) => ({
+        type: "detailOnDemand",
+        category,
+        term,
+        children,
+    }))
+
+export const mdParser = P.createLanguage({
+    phrasing: (r) => P.alt(r.bold, r.italic, r.text),
+    wrappers: (r) => P.alt(r.detailOnDemand, r.url),
+    newline: newlineParser,
+    text: textParser,
+    bold: boldParser,
+    italic: italicParser,
+    detailOnDemand: detailOnDemandParser,
+    url: urlParser,
+    value: (r) =>
+        P.alt(r.newline, r.phrasing, r.wrappers).sepBy(whitespaceParser),
+})
