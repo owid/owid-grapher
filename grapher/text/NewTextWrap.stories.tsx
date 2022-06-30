ea95d05fb4646f0d08e731cd6a3be8adaa3b3f67
Daniel Gavrilov <dgavrilov95@gmail.com>
2022-06-30 14:39:43 +0100

    WIP: a flawed text wrapping method

--- grapher/text/NewTextWrap.stories.tsx
+++ grapher/text/NewTextWrap.stories.tsx
@@ -7,6 +7,7 @@ import {
     IRLineBreak,
     IRLink,
     IRText,
+    IRWhitespace,
     splitIntoLines,
 } from "./NewTextWrap.js"
 
@@ -31,7 +32,11 @@ class MarkdownViewer extends React.Component {
         return (
             <div
                 onMouseMove={this.onMouseMove}
-                style={{ position: "relative", minHeight: "600px" }}
+                style={{
+                    position: "relative",
+                    minHeight: "600px",
+                    fontFamily: "Arial",
+                }}
             >
                 <div
                     style={{
@@ -46,38 +51,88 @@ class MarkdownViewer extends React.Component {
                 <div>
                     {splitIntoLines(
                         [
-                            new IRText("Hello! "),
+                            new IRText("I"),
+                            new IRWhitespace(),
+                            new IRText("am"),
+                            new IRWhitespace(),
+                            new IRText("bold"),
+                            new IRText("."),
+                            new IRWhitespace(),
+                            new IRText("and"),
+                            new IRWhitespace(),
+                            new IRText("that"),
+                            new IRText("'s"),
+                            new IRWhitespace(),
+                            new IRText("fine."),
+                            new IRWhitespace(),
+                            new IRLineBreak(),
+                            new IRLineBreak(),
+                            new IRLineBreak(),
+                            new IRText("Hello!"),
+                            new IRWhitespace(),
+                            new IRText("Testing"),
+                            new IRWhitespace(),
+                            new IRText("this"),
+                            new IRWhitespace(),
+                            new IRText("somewhat"),
+                            new IRWhitespace(),
+                            new IRText("long"),
+                            new IRWhitespace(),
+                            new IRText("line"),
+                            new IRText("."),
+                            new IRWhitespace(),
                             new IRBold([
-                                new IRText("I am bold, and this is "),
-                                new IRItalic([
-                                    new IRText("both bold and italic"),
-                                ]),
-                                new IRText(" and can even have custom "),
+                                new IRText("I"),
+                                new IRWhitespace(),
+                                new IRText("am"),
+                                new IRWhitespace(),
+                                new IRText("bold"),
+                                new IRItalic([new IRText("-ish")]),
+                                new IRText("."),
+                                new IRWhitespace(),
+                                new IRText("and"),
+                                new IRWhitespace(),
+                                new IRText("that"),
+                                new IRText("'s"),
+                                new IRWhitespace(),
+                                new IRText("fine."),
                                 new IRLineBreak(),
                                 new IRLineBreak(),
                                 new IRText("newlines"),
                                 new IRLineBreak(),
                                 new IRLineBreak(),
                                 new IRLink("http://example.com", [
-                                    new IRText("and links can contain "),
+                                    new IRText("links"),
+                                    new IRWhitespace(),
+                                    new IRText("can"),
+                                    new IRWhitespace(),
+                                    new IRText("contain"),
+                                    new IRWhitespace(),
                                     new IRItalic([new IRText("formatting")]),
-                                    new IRText(" too. "),
+                                    new IRWhitespace(),
+                                    new IRText("too."),
                                 ]),
                             ]),
                             new IRText(
-                                "Averylongtokenthatcantbesplitbutshouldbeincludedanyway. "
+                                "Averylongtokenthatcantbesplitbutshouldbeincludedanyway."
                             ),
                             new IRBold([new IRText("Ifthereisbold")]),
                             new IRText("itwillgetalinebreak."),
                             new IRLineBreak(),
                             new IRLineBreak(),
-                            new IRItalic([new IRText("THE END")]),
+                            new IRItalic([
+                                new IRText("THE"),
+                                new IRWhitespace(),
+                                new IRText("END"),
+                            ]),
                         ],
                         this.width
                     ).map((tokens, i) => (
                         <div key={i}>
                             {tokens.length ? (
-                                tokens.map((token) => token.toHTML())
+                                <span style={{ border: "1px solid #ddd" }}>
+                                    {tokens.map((token) => token.toHTML())}
+                                </span>
                             ) : (
                                 <br />
                             )}
