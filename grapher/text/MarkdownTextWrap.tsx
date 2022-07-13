8016819b7dc180eadc295441e82f3e5a6b275a6e
ikesau <ikesau@protonmail.com>
2022-07-13 20:57:55 +0000

    improve(MarkdownTextWrap.tsx): display links on SVG export

--- grapher/text/MarkdownTextWrap.tsx
+++ grapher/text/MarkdownTextWrap.tsx
@@ -222,15 +222,38 @@ export class IRLink extends IRElement {
         )
     }
     toSVG(key?: React.Key): JSX.Element {
+        // When we have a plainUrl just render the link as an <a> tag
+        if (
+            this.children.length === 1 &&
+            this.children[0].constructor.name === "IRText" &&
+            this.children[0].toPlaintext() === this.href
+        ) {
+            return (
+                <a
+                    key={key}
+                    href={this.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {this.href}
+                </a>
+            )
+        }
+
+        // Otherwise add the <a> tag in brackets after the text
         return (
-            <a
-                key={key}
-                href={this.href}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
+            <>
                 {this.children.map((child, i) => child.toSVG(i))}
-            </a>
+                <a
+                    key={key}
+                    href={this.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {" "}
+                    ({this.href})
+                </a>
+            </>
         )
     }
 }
