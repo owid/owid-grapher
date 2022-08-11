abe7f7a2b357056b36abb85e3c60395c8a17272c
ikesau <ikesau@protonmail.com>
2022-08-11 13:32:41 -0400

    improve(ExpandableParagraph): convert to functional component

--- site/blocks/ExpandableParagraph.tsx
+++ site/blocks/ExpandableParagraph.tsx
@@ -1,53 +1,41 @@
+import React, { useState } from "react"
 import classnames from "classnames"
-import React from "react"
 import ReactDOM from "react-dom"
 
-export class ExpandableParagraph extends React.Component<
-    | {
-          children: React.ReactNode
-          dangerouslySetInnerHTML?: undefined
-      }
-    | {
-          children?: undefined
-          dangerouslySetInnerHTML: {
-              __html: string
+export const ExpandableParagraph = (
+    props:
+        | {
+              children: React.ReactNode
+              dangerouslySetInnerHTML?: undefined
           }
-      },
-    {
-        isExpanded: boolean
-    }
-> {
-    state = {
-        isExpanded: false,
-    }
+        | {
+              children?: undefined
+              dangerouslySetInnerHTML: {
+                  __html: string
+              }
+          }
+) => {
+    const [isExpanded, setIsExpanded] = useState(false)
 
-    render() {
-        const { isExpanded } = this.state
-
-        return (
-            <>
-                <div
-                    className={classnames("expandable-paragraph", {
-                        "expandable-paragraph--is-expanded": isExpanded,
-                    })}
-                    // Either pass children or dangerouslySetInnerHTML
-                    {...this.props}
-                />
-                {!isExpanded && (
-                    <button
-                        className="expandable-paragraph__expand-button"
-                        onClick={() =>
-                            this.setState({
-                                isExpanded: true,
-                            })
-                        }
-                    >
-                        Continue reading
-                    </button>
-                )}
-            </>
-        )
-    }
+    return (
+        <>
+            <div
+                className={classnames("expandable-paragraph", {
+                    "expandable-paragraph--is-expanded": isExpanded,
+                })}
+                // Either pass children or dangerouslySetInnerHTML
+                {...props}
+            />
+            {!isExpanded && (
+                <button
+                    className="expandable-paragraph__expand-button"
+                    onClick={() => setIsExpanded(true)}
+                >
+                    Continue reading
+                </button>
+            )}
+        </>
+    )
 }
 
 export const hydrateExpandableParagraphs = () => {
