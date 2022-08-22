b32afb973bee7d20caa8c8ec52f3a80811c05fdd
Marcel Gerber <mgerber59@gmail.com>
2022-08-23 00:45:20 +0200

    fix: format authors for bibtex

--- site/formatting.tsx
+++ site/formatting.tsx
@@ -35,13 +35,15 @@ export const formatUrls = (html: string) =>
 export const formatAuthors = ({
     authors,
     requireMax,
+    forBibtex,
 }: {
     authors: string[]
     requireMax?: boolean
+    forBibtex?: boolean
 }) => {
     if (requireMax && !authors.includes("Max Roser")) authors.push("Max Roser")
 
-    let authorsText = authors.slice(0, -1).join(", ")
+    let authorsText = authors.slice(0, -1).join(forBibtex ? " and " : ", ")
     if (authorsText.length === 0) authorsText = authors[0]
     else authorsText += ` and ${last(authors)}`
 
