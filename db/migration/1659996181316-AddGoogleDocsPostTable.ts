1b650734e0414a0c9a3970f989c4b4af35cc1843
Matthieu Bergel <hello@matthieubergel.org>
2022-09-07 13:56:00 +0200

    wip(gdocs): use gdoc id as DB id

--- db/migration/1659996181316-AddGoogleDocsPostTable.ts
+++ db/migration/1659996181316-AddGoogleDocsPostTable.ts
@@ -3,11 +3,10 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class AddGoogleDocsPostTable1659996181316 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`posts_gdocs\` (
-            \`id\` int NOT NULL AUTO_INCREMENT,
+            \`id\` varchar(255) NOT NULL,
             \`slug\` varchar(255) DEFAULT NULL,
             \`content\` json DEFAULT NULL,
             \`published\` tinyint DEFAULT 0 NOT NULL,
-            \`documentId\` varchar(255) NULL,
             \`createdAt\` datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
             \`updatedAt\` datetime NULL ON UPDATE CURRENT_TIMESTAMP,
             PRIMARY KEY (\`id\`),
