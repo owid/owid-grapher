b258c8b5bc70ad3cd8694d3deaff1e745f597346
Daniel Bachler <daniel@danielbachler.de>
2022-09-08 19:08:32 +0200

    wip: start updating continents

--- grapher/color/ColorConstants.ts
+++ grapher/color/ColorConstants.ts
@@ -1,15 +1,5 @@
 import { Color } from "../../coreTable/CoreTableConstants.js"
 
-export const ContinentColors = {
-    Africa: "#923E8B",
-    Antarctica: "#5887A1",
-    Asia: "#2D8587",
-    Europe: "#4C5C78",
-    "North America": "#E04E4B",
-    Oceania: "#A8633C",
-    "South America": "#932834",
-}
-
 export interface ColorSchemeInterface {
     name: string
     colorSets: Color[][] // Different color sets depending on how many distinct colors you want
