b658171ed4608db5c66a008d8492940b7f463fa7
Matthieu Bergel <hello@matthieubergel.org>
2022-09-15 14:04:53 +0200

    wip(gdocs): add edit page header

--- adminSiteClient/GdocsEditPage.tsx
+++ adminSiteClient/GdocsEditPage.tsx
@@ -1,14 +1,18 @@
 import React, { useContext, useEffect } from "react"
 import { AdminLayout } from "./AdminLayout.js"
 import { GdocsMatchProps } from "./GdocsIndexPage.js"
-import { GdocsSettings } from "./GdocsSettings.js"
+import { GdocsSettings } from "./GdocsSettingsForm.js"
 import { OwidArticle } from "../site/gdocs/owid-article.js"
 import { AdminAppContext } from "./AdminAppContext.js"
 import { OwidArticleType } from "../clientUtils/owidTypes.js"
+import { Button, Drawer, PageHeader, Switch, Tag } from "antd"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/index.js"
+import { faGear } from "@fortawesome/free-solid-svg-icons/faGear"
 
 export const GdocsEditPage = ({ match }: GdocsMatchProps) => {
     const { id } = match.params
     const [gdoc, setGdoc] = React.useState<OwidArticleType>()
+    const [isSettingsOpen, setIsSettingsOpen] = React.useState(false)
 
     const { admin } = useContext(AdminAppContext)
     useEffect(() => {
@@ -22,12 +26,47 @@ export const GdocsEditPage = ({ match }: GdocsMatchProps) => {
     }, [id, admin])
 
     return gdoc ? (
-        <AdminLayout title="Google Docs - Edit">
-            <main className="d-flex">
-                <GdocsSettings gdoc={gdoc} setGdoc={setGdoc} />
-                <div className="flex-grow-1">
-                    <OwidArticle {...gdoc} />
-                </div>
+        <AdminLayout title="Google Docs - Edit" noSidebar>
+            <main className="GdocsEditPage">
+                <PageHeader
+                    title={gdoc.title}
+                    subTitle={
+                        <Button
+                            type="default"
+                            size="small"
+                            onClick={() => setIsSettingsOpen(true)}
+                        >
+                            Edit title
+                        </Button>
+                    }
+                    className="site-page-header"
+                    // tags={<Tag color="blue">Live</Tag>}
+                    extra={[
+                        // <Switch key="2" defaultChecked />,
+                        <Button key="1" type="primary">
+                            Publish
+                        </Button>,
+                        //   <DropdownMenu key="more" />,
+                    ]}
+                    // breadcrumb={{ routes }}
+                ></PageHeader>
+                <Drawer
+                    title="Settings"
+                    placement="right"
+                    onClose={() => setIsSettingsOpen(false)}
+                    visible={isSettingsOpen}
+                >
+                    <GdocsSettings gdoc={gdoc} setGdoc={setGdoc} />
+                </Drawer>
+
+                <OwidArticle {...gdoc} />
+                <Button
+                    type="primary"
+                    onClick={() => setIsSettingsOpen(true)}
+                    className="settings-toggle"
+                >
+                    <FontAwesomeIcon icon={faGear} />
+                </Button>
             </main>
         </AdminLayout>
     ) : null
