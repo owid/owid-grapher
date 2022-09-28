0f986c6f352210e0d904217c558527914a1daa6d
Matthieu Bergel <hello@matthieubergel.org>
2022-09-28 11:26:29 +0200

    wip(gdocs): modal to confirm unpublishing

--- adminSiteClient/GdocsMoreMenu.tsx
+++ adminSiteClient/GdocsMoreMenu.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Dropdown, Menu, Button } from "antd"
+import React, { useState } from "react"
+import { Dropdown, Menu, Button, Modal } from "antd"
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons/faEllipsisVertical"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/index.js"
 import { OwidArticleType } from "../clientUtils/owidTypes.js"
@@ -10,35 +10,56 @@ export const GdocsMoreMenu = ({
 }: {
     gdoc: OwidArticleType
     onUnpublish: VoidFunction
-}) => (
-    <Dropdown
-        trigger={["click"]}
-        overlay={
-            <Menu
-                onClick={({ key }) => {
-                    switch (key) {
-                        case "unpublish":
-                            onUnpublish()
-                            break
+}) => {
+    const [isModalOpen, setModalOpen] = useState(false)
 
-                        default:
-                            break
-                    }
+    const closeModal = () => {
+        setModalOpen(false)
+    }
+    return (
+        <>
+            <Dropdown
+                trigger={["click"]}
+                overlay={
+                    <Menu
+                        onClick={({ key }) => {
+                            switch (key) {
+                                case "unpublish":
+                                    setModalOpen(true)
+                                default:
+                                    break
+                            }
+                        }}
+                        items={[
+                            {
+                                key: "unpublish",
+                                label: "Unpublish",
+                                danger: gdoc.published,
+                                disabled: !gdoc.published,
+                            },
+                        ]}
+                    />
+                }
+                placement="bottomRight"
+            >
+                <Button>
+                    <FontAwesomeIcon icon={faEllipsisVertical} />
+                </Button>
+            </Dropdown>
+            <Modal
+                title="Are you sure you want to unpublish?"
+                open={isModalOpen}
+                okText="Unpublish"
+                okButtonProps={{ danger: true }}
+                onCancel={closeModal}
+                onOk={() => {
+                    onUnpublish()
+                    closeModal()
                 }}
-                items={[
-                    {
-                        key: "unpublish",
-                        label: "Unpublish",
-                        danger: gdoc.published,
-                        disabled: !gdoc.published,
-                    },
-                ]}
-            />
-        }
-        placement="bottomRight"
-    >
-        <Button>
-            <FontAwesomeIcon icon={faEllipsisVertical} />
-        </Button>
-    </Dropdown>
-)
+            >
+                By confirming, the article will no longer be visible to the
+                public.
+            </Modal>
+        </>
+    )
+}
