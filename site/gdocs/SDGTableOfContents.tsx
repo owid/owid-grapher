eaae167b2f1a54077755d420fdc5df6077c3c76a
Matthieu Bergel <hello@matthieubergel.org>
2022-10-28 22:00:40 +0200

    feat(sdg): style open / close

--- site/gdocs/SDGTableOfContents.tsx
+++ site/gdocs/SDGTableOfContents.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import { TocHeading } from "../../clientUtils/owidTypes.js"
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons/faArrowDown"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/index.js"
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus"
+import { faMinus } from "@fortawesome/free-solid-svg-icons/faMinus"
 
 const VERTICAL_TAB_CHAR = "\u000b"
 
@@ -25,9 +27,16 @@ export default function SDGTableOfContents({ toc }: { toc: TocHeading[] }) {
 
     return (
         <nav className="sdg-toc">
-            <span onClick={toggleIsOpen} data-track-note="sdg-toc-toggle">
-                Index
-            </span>
+            <div
+                onClick={toggleIsOpen}
+                className="sdg-toc-toggle"
+                data-track-note="sdg-toc-toggle"
+            >
+                <div>Index</div>
+                <div>
+                    <FontAwesomeIcon icon={isOpen ? faMinus : faPlus} />
+                </div>
+            </div>
             {isOpen && (
                 <ul>
                     {tocHeadingsWithSupertitle.map(
