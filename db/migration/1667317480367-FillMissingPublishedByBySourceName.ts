d14f82e4b65d13bf2330d690ca52b50d05fafb32
Marigold <mojmir.vinkler@gmail.com>
2022-11-01 16:58:22 +0100

    migration(sources): fill missing dataPublishedBy with source nae

---
+++ db/migration/1667317480367-FillMissingPublishedByBySourceName.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class FillMissingPublishedByBySourceName1667317480367
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            UPDATE sources
+            SET description = JSON_SET(description, '$.dataPublishedBy', name)
+            where
+            (
+                description->'$.dataPublishedBy' = CAST('null' AS JSON) or
+                description->'$.dataPublishedBy' is null or
+                description->'$.dataPublishedBy' = ""
+            )`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // no going back...
+    }
+}
