c95c08cda4bfa752405271c10073f0e3e6b8bdbc
ikesau <ikesau@protonmail.com>
2022-11-09 13:07:00 -0500

    fix(gdoc rendering): bold in lists

--- site/gdocs/List.tsx
+++ site/gdocs/List.tsx
@@ -5,7 +5,9 @@ export default function List({ d }: { d: OwidArticleBlock }) {
     return (
         <ul className={"list"}>
             {d.value.map((_d: string, i: number) => {
-                return <li key={i}>{_d}</li>
+                return (
+                    <li key={i} dangerouslySetInnerHTML={{ __html: _d }}></li>
+                )
             })}
         </ul>
     )
