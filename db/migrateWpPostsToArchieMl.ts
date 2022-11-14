7a4f8119d38ae7750ced90cf41a836f4ae351bd8
Daniel Bachler <daniel@danielbachler.de>
2022-11-14 23:09:50 +0100

    wip:

--- db/migrateWpPostsToArchieMl.ts
+++ db/migrateWpPostsToArchieMl.ts
@@ -9,6 +9,8 @@ import {
     OwidRawArticleBlock,
     RawBlockImage,
     RawBlockText,
+    OwidEnrichedArticleBlock,
+    EnrichedBlockPullQuote,
 } from "@ourworldindata/utils"
 import { match } from "ts-pattern"
 import { cheerioToSpan, spansToHtmlString } from "./gdocUtils.js"
@@ -29,7 +31,7 @@ import { cheerioToSpan, spansToHtmlString } from "./gdocUtils.js"
 
 function unwrapNode(
     node: CheerioElement
-): ArchieMlTransformationResult<OwidRawArticleBlock> {
+): ArchieMlTransformationResult<OwidEnrichedArticleBlock> {
     const children = node.children.map(cheerioToArchieML)
     return joinArchieMLTransformationResults(children)
 }
@@ -77,128 +79,124 @@ function findRecursive(
 
 function cheerioToArchieML(
     node: CheerioElement
-): ArchieMlTransformationResult<OwidRawArticleBlock> {
+): ArchieMlTransformationResult<OwidEnrichedArticleBlock> {
     if (node.type === "comment") return { errors: [], content: [] }
     const span = cheerioToSpan(node)
     if (span)
         return {
             errors: [],
             // TODO: below should be a list of spans and a rich text block
-            content: [{ type: "text", value: spansToHtmlString([span]) }],
+            content: [{ type: "text", value: [span], parseErrors: [] }],
         }
     else if (node.type === "tag") {
-        const result: ArchieMlTransformationResult<OwidRawArticleBlock> = match(
-            node
-        )
-            .with({ tagName: "address" }, unwrapNode)
-            .with(
-                { tagName: "blockquote" },
-                (): ArchieMlTransformationResult<RawBlockPullQuote> => {
-                    const childElements = joinArchieMLTransformationResults(
-                        node.children.map(cheerioToArchieML)
-                    )
-                    // TODO: put this in place again
-                    // const cleanedChildElements = consolidateSpans(
-                    //     childElements.content
-                    // )
-                    if (
-                        childElements.content.length !== 1 ||
-                        childElements.content[0].type !== "text"
-                    )
-                        return {
-                            errors: [
-                                {
-                                    name: "blockquote content is not just text",
-                                    details: ``,
-                                },
-                            ],
-                            content: [],
-                        }
-
-                    return {
-                        errors: [],
-                        content: [
-                            {
-                                type: "pull-quote",
-                                // TODO: this is incomplete - needs to match to all text-ish elements like StructuredText
-                                value: [
+        const result: ArchieMlTransformationResult<OwidEnrichedArticleBlock> =
+            match(node)
+                .with({ tagName: "address" }, unwrapNode)
+                .with(
+                    { tagName: "blockquote" },
+                    (): ArchieMlTransformationResult<EnrichedBlockPullQuote> => {
+                        const childElements = joinArchieMLTransformationResults(
+                            node.children.map(cheerioToArchieML)
+                        )
+                        // TODO: put this in place again
+                        // const cleanedChildElements = consolidateSpans(
+                        //     childElements.content
+                        // )
+                        if (
+                            childElements.content.length !== 1 ||
+                            childElements.content[0].type !== "text"
+                        )
+                            return {
+                                errors: [
                                     {
-                                        type: "text",
-                                        value: childElements.content[0].value,
+                                        name: "blockquote content is not just text",
+                                        details: ``,
                                     },
                                 ],
-                            },
-                        ],
-                    }
-                }
-            )
-            .with({ tagName: "body" }, unwrapNode)
-            .with({ tagName: "center" }, unwrapNode) // might want to translate this to a block with a centered style?
-            .with({ tagName: "details" }, unwrapNode)
-            .with({ tagName: "div" }, unwrapNode)
-            .with({ tagName: "figcaption" }, unwrapNode)
-            .with(
-                { tagName: "figure" },
-                (): ArchieMlTransformationResult<RawBlockImage> => {
-                    const errors: ArchieMlTransformationError[] = []
-                    const [figcaptionChildren, otherChildren] = _.partition(
-                        node.children,
-                        (n) => n.tagName === "figcaption"
-                    )
-                    let figcaptionElement: RawBlockText | undefined = undefined
-                    if (figcaptionChildren.length > 1) {
-                        errors.push({
-                            name: "too many figcaption elements",
-                            details: `Found ${figcaptionChildren.length} elements`,
-                        })
-                    } else {
-                        const figCaption =
-                            figcaptionChildren.length > 0
-                                ? cheerioToArchieML(figcaptionChildren[0])
-                                : undefined
-                        if (figCaption)
-                            if (figCaption.content.length > 1)
-                                errors.push({
-                                    name: "too many figcaption elements after archieml transform",
-                                    details: `Found ${figCaption.content.length} elements after transforming to archieml`,
-                                })
-                            else {
-                                const element = figCaption.content[0]
-                                if (element?.type === "text")
-                                    figcaptionElement = element
-                                else
-                                    errors.push({
-                                        name: "figcaption element is not structured text",
-                                        details: `Found ${element?.type} element after transforming to archieml`,
-                                    })
+                                content: [],
                             }
-                    }
-                    const image = findRecursive(otherChildren, "img")
-                    if (!image) {
-                        // TODO: this is a legitimate case, there may be other content in a figure
-                        // but for now we treat it as an error and see how often this error happens
-                        errors.push({
-                            name: "too many figcaption elements after archieml transform",
-                            details: `Found ${otherChildren.length} elements`,
-                        })
-                    }
 
-                    return {
-                        errors,
-                        content: [
-                            {
-                                type: "image",
-                                value: {
-                                    src: image?.attribs.src ?? "",
-                                    caption: figcaptionElement?.value,
+                        return {
+                            errors: [],
+                            content: [
+                                {
+                                    type: "pull-quote",
+                                    // TODO: this is incomplete - needs to match to all text-ish elements like StructuredText
+                                    text: childElements.content[0].value,
+                                    parseErrors: [],
                                 },
-                            },
-                        ],
+                            ],
+                        }
                     }
-                }
-            )
-            // TODO: this is missing a lot of html tags still
-            .otherwise(() => ({ errors: [], content: [] }))
+                )
+                .with({ tagName: "body" }, unwrapNode)
+                .with({ tagName: "center" }, unwrapNode) // might want to translate this to a block with a centered style?
+                .with({ tagName: "details" }, unwrapNode)
+                .with({ tagName: "div" }, unwrapNode)
+                .with({ tagName: "figcaption" }, unwrapNode)
+                .with(
+                    { tagName: "figure" },
+                    (): ArchieMlTransformationResult<RawBlockImage> => {
+                        const errors: ArchieMlTransformationError[] = []
+                        const [figcaptionChildren, otherChildren] = _.partition(
+                            node.children,
+                            (n) => n.tagName === "figcaption"
+                        )
+                        let figcaptionElement: RawBlockText | undefined =
+                            undefined
+                        if (figcaptionChildren.length > 1) {
+                            errors.push({
+                                name: "too many figcaption elements",
+                                details: `Found ${figcaptionChildren.length} elements`,
+                            })
+                        } else {
+                            const figCaption =
+                                figcaptionChildren.length > 0
+                                    ? cheerioToArchieML(figcaptionChildren[0])
+                                    : undefined
+                            if (figCaption)
+                                if (figCaption.content.length > 1)
+                                    errors.push({
+                                        name: "too many figcaption elements after archieml transform",
+                                        details: `Found ${figCaption.content.length} elements after transforming to archieml`,
+                                    })
+                                else {
+                                    const element = figCaption.content[0]
+                                    if (element?.type === "text")
+                                        figcaptionElement = element
+                                    else
+                                        errors.push({
+                                            name: "figcaption element is not structured text",
+                                            details: `Found ${element?.type} element after transforming to archieml`,
+                                        })
+                                }
+                        }
+                        const image = findRecursive(otherChildren, "img")
+                        if (!image) {
+                            // TODO: this is a legitimate case, there may be other content in a figure
+                            // but for now we treat it as an error and see how often this error happens
+                            errors.push({
+                                name: "too many figcaption elements after archieml transform",
+                                details: `Found ${otherChildren.length} elements`,
+                            })
+                        }
+
+                        return {
+                            errors,
+                            content: [
+                                {
+                                    type: "image",
+                                    value: {
+                                        src: image?.attribs.src ?? "",
+                                        caption: figcaptionElement?.value,
+                                    },
+                                },
+                            ],
+                        }
+                    }
+                )
+                // TODO: this is missing a lot of html tags still
+                .otherwise(() => ({ errors: [], content: [] }))
         return result
     } else
         return {
