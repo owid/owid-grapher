ff365f3dd007cd119099bff3ff657eff45cc1a0b
ikesau <ikesau@protonmail.com>
2023-01-20 20:32:57 +0000

    feat(images): add posts_gdocs_x_images table

--- db/migration/1674238961678-GdocPostsXImages.ts
+++ db/migration/1674238961678-GdocPostsXImages.ts
@@ -1,20 +1,18 @@
-// import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from "typeorm"
 
-// export class GdocPostsXImages1674238961678 implements MigrationInterface {
-//     public async up(queryRunner: QueryRunner): Promise<void> {
-//         await queryRunner.query("ALTER TABLE posts_gdocs ADD UNIQUE (id)")
+export class GdocPostsXImages1674238961678 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`posts_gdocs_x_images\` (
+            \`id\` int NOT NULL AUTO_INCREMENT,
+            \`docId\` int NOT NULL,
+            \`imageId\` int NOT NULL,
+            PRIMARY KEY (\`id\`),
+            CONSTRAINT FOREIGN KEY (\`docId\`) REFERENCES \`posts_gdocs\` (\`id\`),
+            CONSTRAINT FOREIGN KEY (\`imageId\`) REFERENCES \`images\` (\`id\`)
+        )`)
+    }
 
-//         await queryRunner.query(`CREATE TABLE \`posts_gdocs_x_images\` (
-//             \`id\` int NOT NULL AUTO_INCREMENT,
-//             \`docId\` varchar(255) NOT NULL,
-//             \`imageId\` int NOT NULL,
-//             PRIMARY KEY (\`id\`),
-//             CONSTRAINT FOREIGN KEY (\`docId\`) REFERENCES \`posts_gdocs\` (\`id\`),
-//             CONSTRAINT FOREIGN KEY (\`imageId\`) REFERENCES \`images\` (\`id\`)
-//         )`)
-//     }
-
-//     public async down(queryRunner: QueryRunner): Promise<void> {
-//         await queryRunner.query("DROP TABLE IF EXISTS `posts_gdocs_x_images`")
-//     }
-// }
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("DROP TABLE IF EXISTS `posts_gdocs_x_images`")
+    }
+}
