494e6f9aecd383049438f23cd9d6ec8ad25f1e62
Matthieu <matthieu@ourworldindata.org>
2023-01-26 15:41:41 +0000

    poc(search): add tags and partial sync with topics refinements

--- site/SearchAutocomplete.tsx
+++ site/SearchAutocomplete.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { createElement, Fragment, useEffect, useRef, useState } from "react"
 import { render } from "react-dom"
-
 import { usePagination, useSearchBox } from "react-instantsearch-hooks"
 import { autocomplete, getAlgoliaFacets } from "@algolia/autocomplete-js"
 import { BaseItem } from "@algolia/autocomplete-core"
 import type { AutocompleteOptions, Render } from "@algolia/autocomplete-js"
 import "@algolia/autocomplete-theme-classic"
+import "@algolia/autocomplete-plugin-tags/dist/theme.min.css"
 import { SearchClient } from "algoliasearch/lite.js"
+import { BaseTag, createTagsPlugin } from "@algolia/autocomplete-plugin-tags"
+import { useInstantSearch } from "react-instantsearch-hooks-web"
 
 type AutocompleteProps = Partial<AutocompleteOptions<BaseItem>> & {
     className?: string
@@ -16,6 +18,7 @@ type AutocompleteProps = Partial<AutocompleteOptions<BaseItem>> & {
 
 type SetInstantSearchUiStateOptions = {
     query: string
+    topics?: BaseTag[]
 }
 
 export function SearchAutocomplete({
@@ -27,6 +30,7 @@ export function SearchAutocomplete({
 
     const { query, refine: setQuery } = useSearchBox()
     const { refine: setPage } = usePagination()
+    const { indexUiState, setIndexUiState } = useInstantSearch()
 
     const [instantSearchUiState, setInstantSearchUiState] =
         useState<SetInstantSearchUiStateOptions>({ query })
@@ -34,6 +38,17 @@ export function SearchAutocomplete({
     useEffect(() => {
         setQuery(instantSearchUiState.query)
         setPage(0)
+        setIndexUiState((prevIndexUiState: any) => {
+            if (!instantSearchUiState.topics) return prevIndexUiState
+
+            return {
+                ...prevIndexUiState,
+                refinementList: {
+                    ...prevIndexUiState.refinementList,
+                    _tags: instantSearchUiState.topics,
+                },
+            }
+        })
     }, [instantSearchUiState])
 
     useEffect(() => {
@@ -51,15 +66,18 @@ export function SearchAutocomplete({
             onSubmit({ state }) {
                 setInstantSearchUiState({ query: state.query })
             },
-            onStateChange({ prevState, state }) {
-                if (prevState.query !== state.query) {
-                    setInstantSearchUiState({
-                        query: state.query,
-                    })
-                }
+            onStateChange({ prevState, state }: any) {
+                // todo debounce
+                setInstantSearchUiState({
+                    query: state.query,
+                    topics: state.context.tagsPlugin.tags.map(
+                        (tag: BaseTag) => tag.label
+                    ),
+                })
             },
             renderer: { createElement, Fragment, render: render as Render },
             getSources: () => getSources(searchClient),
+            plugins: [tagsPlugin],
         })
 
         return () => autocompleteInstance.destroy()
@@ -67,6 +85,19 @@ export function SearchAutocomplete({
 
     return <div className={className} ref={autocompleteContainer} />
 }
+
+const tagsPlugin = createTagsPlugin({
+    getTagsSubscribers() {
+        return [
+            {
+                sourceId: "topics",
+                getTag({ item }: any) {
+                    return item
+                },
+            },
+        ]
+    },
+})
 
 function getSources(searchClient: SearchClient) {
     return [
