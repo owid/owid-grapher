54482aabf58d2fb68fbcb1b2d124d9b5762192ad
Marcel Gerber <mgerber59@gmail.com>
2023-01-26 21:27:08 +0100

    perf: adapt recommendations from d3-geo
    see https://github.com/d3/d3-geo/pull/272

--- packages/@ourworldindata/grapher/src/mapCharts/GeoPathRoundingContext.ts
+++ packages/@ourworldindata/grapher/src/mapCharts/GeoPathRoundingContext.ts
@@ -4,19 +4,24 @@ import { GeoContext } from "d3-geo"
 // In contrast to what d3-geo does by default, it will round all coordinates to one decimal place
 // Adapted from https://github.com/d3/d3-geo/blob/8d3f3a98c034b087e2c808f752d2381d51c30015/src/path/string.js
 
+// FUTURE: Once we update to d3-geo@^3.10, d3-geo can do this by default: https://github.com/d3/d3-geo/pull/272
+
+// Rounds to one decimal place
+const round = (x: number): number => Math.round(x * 10) / 10
+
 export class GeoPathRoundingContext implements GeoContext {
-    _string: string[] = []
+    _string: string = ""
 
     beginPath(): void {
-        this._string = []
+        this._string = ""
     }
 
     moveTo(x: number, y: number): void {
-        this._string.push("M" + x.toFixed(1) + "," + y.toFixed(1))
+        this._string += `M${round(x)},${round(y)}`
     }
 
     lineTo(x: number, y: number): void {
-        this._string.push("L" + x.toFixed(1) + "," + y.toFixed(1))
+        this._string += `L${round(x)},${round(y)}`
     }
 
     arc(_x: number, _y: number, _radius: number): void {
@@ -24,10 +29,10 @@ export class GeoPathRoundingContext implements GeoContext {
     }
 
     closePath(): void {
-        this._string.push("Z")
+        this._string += "Z"
     }
 
     result(): string {
-        return this._string.join("")
+        return this._string
     }
 }
