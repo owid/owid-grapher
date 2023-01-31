5309441a3d119e0dc65b5ba477c931cf0204b325
Daniel Bachler <daniel@danielbachler.de>
2023-01-31 21:34:53 +0100

    enhance: enforce round trip testing for all components at the type level

--- db/model/Gdoc/exampleEnrichedBlocks.ts
+++ db/model/Gdoc/exampleEnrichedBlocks.ts
@@ -45,15 +45,18 @@ const enrichedChart: EnrichedBlockChart = {
     parseErrors: [],
 }
 
-export const enrichedBlockExamples: OwidEnrichedArticleBlock[] = [
-    enrichedBlockText,
-    {
+export const enrichedBlockExamples: Record<
+    OwidEnrichedArticleBlock["type"],
+    OwidEnrichedArticleBlock
+> = {
+    text: enrichedBlockText,
+    aside: {
         type: "aside",
         position: "right",
         caption: boldLinkExampleText,
         parseErrors: [],
     },
-    {
+    chart: {
         type: "chart",
         url: "https://ourworldindata.org/grapher/total-cases-covid-19",
         height: "400",
@@ -63,7 +66,7 @@ export const enrichedBlockExamples: OwidEnrichedArticleBlock[] = [
         caption: boldLinkExampleText,
         parseErrors: [],
     },
-    {
+    scroller: {
         type: "scroller",
         blocks: [
             {
@@ -73,7 +76,7 @@ export const enrichedBlockExamples: OwidEnrichedArticleBlock[] = [
         ],
         parseErrors: [],
     },
-    {
+    "chart-story": {
         type: "chart-story",
         items: [
             {
@@ -84,45 +87,44 @@ export const enrichedBlockExamples: OwidEnrichedArticleBlock[] = [
         ],
         parseErrors: [],
     },
-    {
+    "additional-charts": {
         type: "additional-charts",
         items: [boldLinkExampleText],
         parseErrors: [],
     },
-    {
+    "fixed-graphic": {
         type: "fixed-graphic",
         graphic: enrichedChart,
         text: [enrichedBlockText],
         position: "right",
         parseErrors: [],
     },
-    {
+    image: {
         type: "image",
         src: "https://ourworldindata.org/uploads/2022/03/Future-as-triangles-of-an-hour-glass-01.png",
         caption: boldLinkExampleText,
         parseErrors: [],
     },
-    {
+    list: {
         type: "list",
         items: [enrichedBlockText],
         parseErrors: [],
     },
-    {
+    "numbered-list": {
         type: "numbered-list",
         items: [enrichedBlockText],
         parseErrors: [],
     },
-    {
+    "pull-quote": {
         type: "pull-quote",
         text: [spanSimpleText],
         parseErrors: [],
     },
-    {
+    "horizontal-rule": {
         type: "horizontal-rule",
-        value: {},
         parseErrors: [],
     },
-    {
+    recirc: {
         type: "recirc",
         title: spanSimpleText,
         items: [
@@ -134,19 +136,19 @@ export const enrichedBlockExamples: OwidEnrichedArticleBlock[] = [
         ],
         parseErrors: [],
     },
-    {
+    html: {
         type: "html",
         value: "<p>This is a paragraph</p>",
         parseErrors: [],
     },
-    {
+    heading: {
         type: "heading",
         level: 1,
         text: boldLinkExampleText,
         supertitle: boldLinkExampleText,
         parseErrors: [],
     },
-    {
+    "sdg-grid": {
         type: "sdg-grid",
         items: [
             {
@@ -156,49 +158,42 @@ export const enrichedBlockExamples: OwidEnrichedArticleBlock[] = [
         ],
         parseErrors: [],
     },
-    {
+    "sticky-right": {
         type: "sticky-right",
         left: [enrichedBlockText],
         right: [enrichedBlockText],
         parseErrors: [],
     },
-    {
+    "sticky-left": {
         type: "sticky-left",
         left: [enrichedBlockText],
         right: [enrichedBlockText],
         parseErrors: [],
     },
-    {
+    "side-by-side": {
         type: "side-by-side",
         left: [enrichedBlockText],
         right: [enrichedBlockText],
         parseErrors: [],
     },
-    {
+    "gray-section": {
         type: "gray-section",
         items: [enrichedBlockText],
         parseErrors: [],
     },
-    {
+    "prominent-link": {
         type: "prominent-link",
         url: "https://ourworldindata.org/grapher/total-cases-covid-19",
         title: "A test title",
         description: "A test description",
         parseErrors: [],
     },
-    {
+    "sdg-toc": {
         type: "sdg-toc",
-        value: {},
         parseErrors: [],
     },
-    {
+    "missing-data": {
         type: "missing-data",
-        value: {},
         parseErrors: [],
     },
-    {
-        type: "additional-charts",
-        items: [boldLinkExampleText],
-        parseErrors: [],
-    },
-]
+}
