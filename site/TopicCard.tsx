f291eb5a178c2511a4d7eee8d07d762fdd79fd2f
Matthieu <matthieu@ourworldindata.org>
2023-01-31 12:23:01 +0000

    poc(search): styles and links topics card

--- site/TopicCard.tsx
+++ site/TopicCard.tsx
@@ -1,20 +1,24 @@
 import React from "react"
 import { Hit as AlgoliaHit } from "instantsearch.js/es/types"
 import { Highlight } from "react-instantsearch-hooks-web"
+import { BAKED_BASE_URL } from "../settings/clientSettings.js"
 
 type HitProps = {
     hit: AlgoliaHit<{
         title: string
         _tags: string[]
+        slug: string
     }>
 }
 
 export const TopicCard = ({ hit }: HitProps) => {
     return (
-        <div className="TopicCard">
-            <div className="TopicCard__title">
-                <Highlight hit={hit} attribute="title" />
+        <a href={`${BAKED_BASE_URL}/${hit.slug}`}>
+            <div className="TopicCard">
+                <div className="TopicCard__title">
+                    <Highlight hit={hit} attribute="title" />
+                </div>
             </div>
-        </div>
+        </a>
     )
 }
