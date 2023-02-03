6ffdf2e197838407dcb9a99fd364460f039e8365
Marcel Gerber <mgerber59@gmail.com>
2023-02-03 15:40:50 +0100

    migrate(chart_tags): rename column isKey -> isKeyChart

--- db/migration/1675374345914-ChartTagIsKeyChartNonNull.ts
+++ db/migration/1675374345914-ChartTagIsKeyChartNonNull.ts
@@ -1,6 +1,8 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class ChartTagIsKeyNonNull1675374345914 implements MigrationInterface {
+export class ChartTagIsKeyChartNonNull1675374345914
+    implements MigrationInterface
+{
     public async up(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query(`
         UPDATE chart_tags
@@ -10,9 +12,17 @@ export class ChartTagIsKeyNonNull1675374345914 implements MigrationInterface {
         queryRunner.query(`
         ALTER TABLE chart_tags
         MODIFY COLUMN isKey TINYINT UNSIGNED NOT NULL DEFAULT FALSE`)
+
+        queryRunner.query(`
+        ALTER TABLE chart_tags
+        RENAME COLUMN isKey TO isKeyChart`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+        ALTER TABLE chart_tags
+        RENAME COLUMN isKeyChart TO isKey`)
+
         queryRunner.query(`
         ALTER TABLE chart_tags
         MODIFY COLUMN isKey TINYINT UNSIGNED`)
