6236fbdd62c3d50ae7948c54bd88fa7f59697bcd
Matthieu <matthieu@ourworldindata.org>
2023-02-03 09:15:32 +0000

    wip(header): clean up nav structure

--- site/SiteHeader.tsx
+++ site/SiteHeader.tsx
@@ -4,6 +4,10 @@ import { faBars } from "@fortawesome/free-solid-svg-icons/faBars"
 import { faEnvelopeOpenText } from "@fortawesome/free-solid-svg-icons/faEnvelopeOpenText"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/index.js"
 import { AlertBanner } from "./AlertBanner.js"
+import {
+    NewsletterSubscription,
+    NewsletterSubscriptionContext,
+} from "./NewsletterSubscription.js"
 
 interface SiteHeaderProps {
     hideAlertBanner?: boolean
@@ -42,102 +46,46 @@ export const SiteHeader = (props: SiteHeaderProps) => (
                         </a>
                     </div>
                 </div>
-                <nav className="site-navigation">
-                    <div className="topics-button-wrapper">
-                        <a href="/#entries" className="topics-button">
-                            <div className="label">
-                                Articles <br />
-                                <strong>by topic</strong>
-                            </div>
-                            <div className="icon">
-                                <svg width="12" height="6">
-                                    <path
-                                        d="M0,0 L12,0 L6,6 Z"
-                                        fill="currentColor"
-                                    />
-                                </svg>
-                            </div>
-                        </a>
-                    </div>
-                    <div>
-                        <div className="site-primary-navigation">
-                            <form
-                                className="HeaderSearch"
-                                action="/search"
-                                method="GET"
+                <nav className="site-primary-links">
+                    <ul>
+                        <li>Browse by topic</li>
+                        <li>
+                            <a href="/blog" data-track-note="header-navigation">
+                                Latest
+                            </a>
+                        </li>
+                        <li>Resources</li>
+                        <li>
+                            <a
+                                href="/about"
+                                data-track-note="header-navigation"
                             >
-                                <input
-                                    type="search"
-                                    name="q"
-                                    placeholder="Search..."
-                                />
-                                <div className="icon">
-                                    <FontAwesomeIcon icon={faSearch} />
-                                </div>
-                            </form>
-                            <ul className="site-primary-links">
-                                <li>
-                                    <a
-                                        href="/blog"
-                                        data-track-note="header-navigation"
-                                    >
-                                        Latest
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="/about"
-                                        data-track-note="header-navigation"
-                                    >
-                                        About
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="/donate"
-                                        data-track-note="header-navigation"
-                                    >
-                                        Donate
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="site-secondary-navigation">
-                            <ul className="site-secondary-links">
-                                <li>
-                                    <a
-                                        href="/charts"
-                                        data-track-note="header-navigation"
-                                    >
-                                        All charts
-                                    </a>
-                                </li>
-                                {/* <li><a href="/teaching"  data-track-note="header-navigation">Teaching Hub</a></li> */}
-                                {/* <li>
-                                    <a
-                                        href="https://sdg-tracker.org"
-                                        data-track-note="header-navigation"
-                                    >
-                                        Sustainable Development Goals Tracker
-                                    </a>
-                                </li> */}
-                            </ul>
-                        </div>
-                    </div>
+                                About
+                            </a>
+                        </li>
+                    </ul>
                 </nav>
-                <div className="mobile-site-navigation">
-                    <button data-track-note="mobile-search-button">
-                        <FontAwesomeIcon icon={faSearch} />
-                    </button>
-                    <button data-track-note="mobile-newsletter-button">
-                        <FontAwesomeIcon icon={faEnvelopeOpenText} />
-                    </button>
-                    <button data-track-note="mobile-hamburger-button">
-                        <FontAwesomeIcon icon={faBars} />
-                    </button>
+                <div className="site-search-cta">
+                    <form
+                        className="HeaderSearch"
+                        action="/search"
+                        method="GET"
+                    >
+                        <input type="search" name="q" placeholder="Search..." />
+                        <div className="icon">
+                            <FontAwesomeIcon icon={faSearch} />
+                        </div>
+                    </form>
+                    <NewsletterSubscription
+                        context={NewsletterSubscriptionContext.Floating}
+                    />
+                    <a href="/donate" data-track-note="header-navigation">
+                        Donate
+                    </a>
                 </div>
             </div>
         </header>
         {props.hideAlertBanner !== true && <AlertBanner />}
     </>
+)
 )
