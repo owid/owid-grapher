463579e3dfe4c6fbff8df47324102eceab25829e
ikesau <ikesau@protonmail.com>
2023-02-08 19:04:24 +0000

    feat(images): add NOT NULLABLE to more Image fields

--- db/migration/1673967387686-AddImagesTable.ts
+++ db/migration/1673967387686-AddImagesTable.ts
@@ -2,15 +2,15 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class AddImagesTable1673967387686 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        // TODO: probably shouldn't have DEFAULT NULLs
+        // A record of images that have been uploaded to this server's S3 instance
         // originalWidth can be null because of SVGs
         // googleId isn't UNIQUE because people can rename files and in that case we want to re-upload the photo
         // without breaking previously published articles
         await queryRunner.query(`CREATE TABLE \`images\` (
             \`id\` int NOT NULL AUTO_INCREMENT,
-            \`googleId\` varchar(511) DEFAULT NULL,
-            \`filename\` varchar(255) DEFAULT NULL,
-            \`defaultAlt\` varchar(1023) DEFAULT NULL,
+            \`googleId\` varchar(511) NOT NULL,
+            \`filename\` varchar(255) NOT NULL,
+            \`defaultAlt\` varchar(1023) NOT NULL,
             \`originalWidth\` int DEFAULT NULL,
             \`updatedAt\` BIGINT,
             PRIMARY KEY (\`id\`),
