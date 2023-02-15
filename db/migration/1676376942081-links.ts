974fe1d213fbeb3876020f7d95ae8af637504e19
ikesau <ikesau@protonmail.com>
2023-02-15 17:42:57 +0000

    feat(links): rename source to sourceId

--- db/migration/1676376942081-links.ts
+++ db/migration/1676376942081-links.ts
@@ -5,13 +5,13 @@ export class links1676376942081 implements MigrationInterface {
         queryRunner.query(`
         CREATE TABLE links (
             id INT NOT NULL AUTO_INCREMENT,
-            source VARCHAR(255),
+            sourceId VARCHAR(255),
             target VARCHAR(2047) NOT NULL,
             type ENUM("gdoc", "url") NOT NULL,
             context VARCHAR(255) NOT NULL,
             text VARCHAR(255) NOT NULL,
             PRIMARY KEY(id),
-            CONSTRAINT FOREIGN KEY (source) REFERENCES posts_gdocs (id)
+            CONSTRAINT FOREIGN KEY (sourceId) REFERENCES posts_gdocs (id)
         )`)
     }
 
