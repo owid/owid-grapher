fe825079c135e71cd9598b9ea082cc50a0681bed
Matthieu <matthieu@ourworldindata.org>
2023-02-18 20:41:17 +0000

    wip(nav): toggle search in place

--- site/SiteSearchInput.tsx
+++ site/SiteSearchInput.tsx
@@ -1,11 +1,27 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/index.js"
 import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch"
+import classnames from "classnames"
 
-export const SiteSearchInput = () => {
+export const SiteSearchInput = ({
+    isActive,
+    onClick,
+}: {
+    isActive: boolean
+    onClick: VoidFunction
+}) => {
     return (
-        <form className="SiteSearchInput" action="/search" method="GET">
-            <input name="search" placeholder="Search for a topic or chart..." />
+        <form
+            className={classnames("SiteSearchInput", { active: isActive })}
+            action="/search"
+            method="GET"
+        >
+            <input
+                name="search"
+                placeholder="Search for a topic or chart..."
+                onClick={onClick}
+                className={classnames({ active: isActive })}
+            />
             <div className="icon">
                 <FontAwesomeIcon icon={faSearch} />
             </div>
