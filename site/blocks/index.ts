72f938ef564a03491dd241f3bde8d1edccc3ba28
Matthieu <matthieu@ourworldindata.org>
2023-02-27 18:03:20 +0000

    feat(help): bring help boxes back on mobile Nowadays not only used next to non-interactive charts but also next to interactive charts (explorers) and in other article contexts, making it overall more desirable to show them.

--- site/blocks/index.ts
+++ site/blocks/index.ts
@@ -2,20 +2,12 @@ import { hydrate as hydrateAdditionalInformation } from "./AdditionalInformation
 import { runSearchCountry } from "../../site/SearchCountry.js"
 import { runExpandableInlineBlock } from "../../site/ExpandableInlineBlock.js"
 import { runDataTokens } from "../../site/runDataTokens.js"
-import { shouldProgressiveEmbed } from "../../site/multiembedder/MultiEmbedder.js"
 import { hydrateKeyInsights } from "./KeyInsights.js"
 import { hydrateStickyNav } from "./StickyNav.js"
 import { hydrateExpandableParagraphs } from "./ExpandableParagraph.js"
 import { hydrateCodeSnippets } from "./CodeSnippet.js"
 
 export const runBlocks = () => {
-    if (!shouldProgressiveEmbed()) {
-        // Used by Help blocks. Pierces encapsulation but considered not worth going through hydration / client side rendering for this.
-        // If hydration required for other purposes, then reassess.
-        document
-            .getElementsByTagName("body")[0]
-            .classList.add("is-not-chart-interactive")
-    }
     runDataTokens()
     runExpandableInlineBlock()
     runSearchCountry()
