e866c39d0c17cc59df6520b06ab50d778882d1c2
Ike Saunders <ikesau@protonmail.com>
2023-03-16 14:14:32 -0400

    fix: restore collation for links migration

--- db/migration/1676376942081-PostsGdocsLinks.ts
+++ db/migration/1676376942081-PostsGdocsLinks.ts
@@ -5,7 +5,7 @@ export class PostsGdocsLinks1676376942081 implements MigrationInterface {
         queryRunner.query(`
         CREATE TABLE posts_gdocs_links (
             id INT NOT NULL AUTO_INCREMENT,
-            sourceId VARCHAR(255),
+            sourceId VARCHAR(255) COLLATE utf8mb4_0900_as_cs,
             target VARCHAR(2047) NOT NULL,
             linkType ENUM("gdoc", "url") NOT NULL,
             componentType VARCHAR(255) NOT NULL,
