b776a9311eccd6e10171aae41f120a4fe9110dff
Ike Saunders <ikesau@protonmail.com>
2023-03-23 11:01:13 -0400

    enhance: sync-s3-images script PR feedback

--- devTools/docker/sync-s3-images.sh
+++ devTools/docker/sync-s3-images.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env  bash
 
-# This script assumes that you have ssh access to the OWID production db server at live-db.owid.io It will
-# fail if you don't have these credentials
-
 set -o errexit
 set -o pipefail
 set -o nounset
+
+if [[ -n "$IMAGE_HOSTING_BUCKET_PATH" ]]; then
+  echo 'Please set IMAGE_HOSTING_BUCKET_PATH in .env'
+  exit 1
+fi
+
+# IMAGE_HOSTING_BUCKET_PATH should be your environment's folder in the owid-image-upload bucket
+# e.g. owid-image-upload/staging
+# for local development, it should be owid-image-upload/local-yourname
+# at least until we decide to instead host images locally, if ever
 
 s3cmd sync s3://owid-image-upload/production/ s3://$IMAGE_HOSTING_BUCKET_PATH/
