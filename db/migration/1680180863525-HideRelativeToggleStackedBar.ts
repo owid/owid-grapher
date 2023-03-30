8733d3fecc2fa02ca9694042978385ea7f8355d6
Marcel Gerber <mgerber59@gmail.com>
2023-03-30 15:07:15 +0200

    migrate: enable `hideRelativeToggle` in all existing StackedBar charts

---
+++ db/migration/1680180863525-HideRelativeToggleStackedBar.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class HideRelativeToggleStackedBar1680180863525
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+        UPDATE charts
+        SET config = JSON_SET(config, "$.hideRelativeToggle", TRUE)
+        WHERE config->>"$.type" = "StackedBar"
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+        UPDATE charts
+        SET config = JSON_REMOVE(config, "$.hideRelativeToggle")
+        WHERE config->>"$.type" = "StackedBar"
+        `)
+    }
+}
