6c8abd13f333453f3f8c57295414f383d192f077
Matthieu <matthieu@ourworldindata.org>
2023-04-13 12:51:13 +0000

    feat(dp): add static export while grapher loads

--- site/GrapherWithFallback.tsx
+++ site/GrapherWithFallback.tsx
@@ -1,4 +1,8 @@
-import { Grapher } from "@ourworldindata/grapher"
+import {
+    DEFAULT_GRAPHER_HEIGHT,
+    DEFAULT_GRAPHER_WIDTH,
+    Grapher,
+} from "@ourworldindata/grapher"
 import React from "react"
 import { GrapherFigureView } from "./GrapherFigureView.js"
 import { BAKED_GRAPHER_EXPORTS_BASE_URL } from "../settings/clientSettings.js"
@@ -19,18 +23,21 @@ export const GrapherWithFallback = ({
                 {grapher ? (
                     <GrapherFigureView grapher={grapher} />
                 ) : (
-                    // Given GrapherFigureView also renders a figure which gets
-                    // the same styling and height, rendering an empty <figure>
-                    // while Grapher is instanciated probably helps with
-                    // cumulative layout shift (CLS)
-                    <figure data-grapher-src></figure>
+                    // Render fallback svg when javascript disabled or while
+                    // grapher is loading
+                    <figure
+                        data-grapher-src
+                        className="GrapherWithFallback__fallback"
+                    >
+                        <img
+                            src={`${BAKED_GRAPHER_EXPORTS_BASE_URL}/${slug}.svg`}
+                            width={DEFAULT_GRAPHER_WIDTH}
+                            height={DEFAULT_GRAPHER_HEIGHT}
+                            loading="lazy"
+                        />
+                    </figure>
                 )}
             </>
-
-            <noscript className="GrapherWithFallback__fallback">
-                <img src={`${BAKED_GRAPHER_EXPORTS_BASE_URL}/${slug}.svg`} />
-                <p>Interactive visualization requires JavaScript</p>
-            </noscript>
         </div>
     ) : null
 }
