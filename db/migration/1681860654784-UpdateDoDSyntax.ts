8c0e716241d8e8ac9342f76a0d542ed99c23fdc0
Ike Saunders <ikesau@protonmail.com>
2023-04-19 17:31:07 -0400

    enhance: migrate dods in chart_revisions and suggested_chart_revisions

--- db/migration/1681860654784-UpdateDoDSyntax.ts
+++ db/migration/1681860654784-UpdateDoDSyntax.ts
@@ -1,24 +1,59 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
+const tables = {
+    charts: ["config"],
+    chart_revisions: ["config"],
+    suggested_chart_revisions: ["suggestedConfig", "originalConfig"],
+}
+const fields = ["subtitle", "note"]
+
 export class UpdateDoDSyntax1681860654784 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`        
-            UPDATE charts SET config = JSON_SET(config, '$.subtitle', JSON_UNQUOTE(REGEXP_REPLACE(JSON_EXTRACT(config, '$.subtitle'), 'hover::[a-z]+::', '#dod:')));
-        `)
-        await queryRunner.query(`        
-            UPDATE charts SET config = JSON_SET(config, '$.note', JSON_UNQUOTE(REGEXP_REPLACE(JSON_EXTRACT(config, '$.note'), 'hover::[a-z]+::', '#dod:')));
-        `)
+        for (const [table, columns] of Object.entries(tables)) {
+            for (const column of columns) {
+                for (const field of fields) {
+                    await queryRunner.query(`        
+                        UPDATE ${table} SET ${column} = JSON_SET(
+                            ${column},
+                            '$.${field}',
+                            JSON_UNQUOTE(
+                                REGEXP_REPLACE(
+                                    ${column}->>'$.${field}',
+                                    'hover::[a-z]+::',
+                                    '#dod:'
+                                )
+                            )
+                        )
+                        WHERE ${column}->>'$.${field}' IS NOT NULL
+                    `)
+                }
+            }
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         // This migration removes category information so it can't be perfectly undone
         // e.g. hover::energy::nuclear -> #dod:nuclear
         // The best we can do is set all the categories to "general"
-        await queryRunner.query(`        
-            UPDATE charts SET config = JSON_SET(config, '$.subtitle', JSON_UNQUOTE(REGEXP_REPLACE(JSON_EXTRACT(config, '$.subtitle'), '#dod:', 'hover::general::')));
-        `)
-        await queryRunner.query(`        
-            UPDATE charts SET config = JSON_SET(config, '$.note', JSON_UNQUOTE(REGEXP_REPLACE(JSON_EXTRACT(config, '$.note'), '#dod:', 'hover::general::')));
-        `)
+        for (const [table, columns] of Object.entries(tables)) {
+            for (const column of columns) {
+                for (const field of fields) {
+                    await queryRunner.query(`        
+                        UPDATE ${table} SET ${column} = JSON_SET(
+                            ${column},
+                            '$.${field}',
+                            JSON_UNQUOTE(
+                                REGEXP_REPLACE(
+                                    ${column}->>'$.${field}',
+                                    '#dod:',
+                                    'hover::general::'
+                                )
+                            )
+                        )
+                        WHERE ${column}->>'$.${field}' IS NOT NULL
+                    `)
+                }
+            }
+        }
     }
 }
