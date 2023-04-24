700a01ce200c4693d80d5cfb4f24ef540551e3f1
Christian Swinehart <drafting@samizdat.co>
2023-04-24 17:01:51 -0400

    use updated ETL data & endpoint
    merge in search aliases from `countries.ts`

--- devTools/regionsImporter/update.ts
+++ devTools/regionsImporter/update.ts
@@ -5,56 +5,67 @@ import { parse } from 'papaparse'
 import fetch from 'node-fetch'
 import _ from 'lodash'
 
-const ETL_REGIONS_URL = "https://catalog-staging.ourworldindata.org/grapher/regions/2023-01-01/regions/regions.csv",
+const ETL_REGIONS_URL = "https://catalog-staging.ourworldindata.org/grapher/regions/latest/regions/regions.csv",
       GRAPHER_ROOT = __dirname.replace(/\/(itsJustJavascript\/)?devTools.*/, ""),
       GRAPHER_REGIONS_PATH = `${GRAPHER_ROOT}/packages/@ourworldindata/utils/src/regions.json`,
       ADDITIONAL_CONTINENT_MEMBERS = {
         Africa: [ 'OWID_SML', 'OWID_ZAN' ],
         Asia: [ 'OWID_ABK', 'OWID_AKD', 'OWID_NAG', 'OWID_CYN', 'OWID_SOS' ],
         Europe: [ 'OWID_CIS', 'SJM', 'OWID_TRS' ],
+      },
+      SEARCH_ALIASES = {
+        ARE: ["UAE"],
+        CZE: ["Czech Republic"],
+        GBR: ["UK"],
+        MKD: ["Macedonia"],
+        SWZ: ["Swaziland"],
+        USA: ["US", "USA"],
       }
 
+function csvToJson(val:string, col:string){
+  switch (col){
+    case 'is_mappable':
+    case 'is_historical':
+    case 'omit_country_page':
+      return val==='True'
 
+    case 'members':
+      // use eval since pandas produces non-json arrays using single-quoted strings
+      return val ? eval(val) : undefined
 
+    default:
+      return val
+  }
+}
 
 async function main(){
   // fetch csv and js-ify non-string fields
   console.log(`Fetching ${ETL_REGIONS_URL}`)
   let response = await fetch(ETL_REGIONS_URL),
-      {data, errors, meta} = parse(await response.text(), {
-        header:true,
-        transform: (val, col) => {
-          switch (col){
-            case 'is_mappable':
-            case 'omit_country_page':
-              return val==='True'
+      {data, errors, meta} = parse(await response.text(), {header:true, transform:csvToJson})
 
-            case 'members':
-              return val ? JSON.parse(val.replace(/'/g, '"')) : undefined
-
-            default:
-              return val
-          }
-        }
-      })
+  // strip out empty rows and make sure entities are sorted
+  data = _.sortBy(data, 'code').filter((c:any) => !!c.code)
 
   let entities = data.map((row:any) => {
     // drop unused/redundant attrs
     let entity = _.pickBy(row, val => !!val)
     if (entity.short_name===entity.name) delete entity.short_name
-    if (entity.region_type==='country'){
-      delete entity.region_type
-    }else{
-      delete entity.is_mappable
-    }
+    if (entity.region_type!=='country') delete entity.is_mappable
 
     // add back countries removed from the ETL's continents list
     if (entity.region_type==='continent'){
       entity.members = [...entity.members, ..._.get(ADDITIONAL_CONTINENT_MEMBERS, entity.name, [])]
     }
 
-    // convert keys to camelCase
-    return _.mapKeys(entity, (val, key) => _.camelCase(key))
+    return _(entity).mapKeys((val, key) =>
+      // convert keys to camelCase
+      key==='omit_country_page' ? 'isUnlisted' : _.camelCase(key)
+    ).pick(
+      // give keys a consistent ordering
+      "code", "shortCode", "name", "shortName", "slug", "regionType",
+      "isMappable", "isHistorical", "isUnlisted", "members"
+    ).value()
   })
 
   // generate new regions.json file and report changes (if any)
