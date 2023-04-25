0223cbeb27878c8b78b7375fbc1c522c62c53ea2
Christian Swinehart <drafting@samizdat.co>
2023-04-25 19:22:46 -0400

    include type info for `WorldRegionToProjection`

--- packages/@ourworldindata/grapher/src/mapCharts/WorldRegionsToProjection.ts
+++ packages/@ourworldindata/grapher/src/mapCharts/WorldRegionsToProjection.ts
@@ -1,17 +1,18 @@
 import { regions, continents } from "@ourworldindata/utils"
 import { MapProjectionName } from "./MapProjections"
 
-export const WorldRegionToProjection = Object.fromEntries(
-    continents
-        .map(({ name, members }) =>
-            members
-                .map((code) => [
-                    regions.find((c) => c.code === code)?.name,
-                    name.replace(/ /, "") as MapProjectionName,
-                ])
-                .filter(([name, _projection]) => !!name)
-        )
-        .flat()
-)
+export const WorldRegionToProjection: Map<string, MapProjectionName> =
+    Object.fromEntries(
+        continents
+            .map(({ name, members }) =>
+                members
+                    .map((code) => [
+                        regions.find((c) => c.code === code)?.name,
+                        name.replace(/ /, "") as MapProjectionName,
+                    ])
+                    .filter(([name, _projection]) => !!name)
+            )
+            .flat()
+    )
 
 export type WorldRegionName = keyof typeof WorldRegionToProjection
