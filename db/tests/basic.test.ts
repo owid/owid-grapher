c9290f1822f3c7845709079123e034a2a2551ac1
Marcel Gerber <mgerber59@gmail.com>
2023-04-25 13:54:50 +0200

    chore(esm): fix imports

--- db/tests/basic.test.ts
+++ db/tests/basic.test.ts
@@ -2,7 +2,7 @@
 import sqlFixtures from "sql-fixtures"
 import { dbTestConfig } from "./dbTestConfig.js"
 import { dataSource } from "./dataSource.dbtests.js"
-import { knex, Knex } from "knex"
+import knex, { Knex } from "knex"
 import { getConnection } from "../db.js"
 import typeorm from "typeorm"
 import { User } from "../model/User.js"
