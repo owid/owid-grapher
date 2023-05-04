4b79e2bff0d1b31935f8b18ed4440c37309ef25d
Christian Swinehart <drafting@samizdat.co>
2023-05-04 11:51:14 -0400

    switch to using production catalog

--- devTools/regionsImporter/update.ts
+++ devTools/regionsImporter/update.ts
@@ -13,7 +13,7 @@ import fetch from "node-fetch"
 import _ from "lodash"
 
 const ETL_REGIONS_URL =
-        "https://catalog-staging.ourworldindata.org/grapher/regions/latest/regions/regions.csv",
+        "https://catalog.ourworldindata.org/grapher/regions/latest/regions/regions.csv",
     GEO_JSON_URL =
         "https://raw.githubusercontent.com/alexabruck/worldmap-sensitive/master/dist/world.geo.json",
     GRAPHER_ROOT = __dirname.replace(/\/(itsJustJavascript\/)?devTools.*/, ""),
