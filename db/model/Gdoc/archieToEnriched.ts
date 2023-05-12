11cc87a7774984b79f23d8ddb3b0b738d4fc459c
Daniel Bachler <daniel@danielbachler.de>
2023-05-12 16:39:02 +0200

    :hammer: extract lowercaseObjectKeys utility function

--- db/model/Gdoc/archieToEnriched.ts
+++ db/model/Gdoc/archieToEnriched.ts
@@ -55,15 +55,11 @@ export const archieToEnriched = (text: string): OwidGdocContent => {
     )
 
     // Inside .body all keys will be sanitized to lowercase but
-    // for the frontmatter this doesn't happen - do it now so
+    // for the frontmatter this doesn't happen down there - do it now so
     // that "Title: bla" works as well as "title: bla"
     const parsed_unsanitized = load(noLeadingWSLinks)
-    const parsed: any = Object.fromEntries(
-        Object.entries(parsed_unsanitized).map(([key, value]) => [
-            key.toLowerCase(),
-            value,
-        ])
-    )
+    const parsed: any = lowercaseObjectKeys(parsed_unsanitized)
+
     const toc: TocHeadingWithTitleSupertitle[] = []
     let pointer: Array<string | number> = []
     // archie doesn't have a nested list structure. it treats as a series of text blocks
@@ -123,10 +119,7 @@ export const archieToEnriched = (text: string): OwidGdocContent => {
     // track h2s and h3s for the SDG table of contents
     traverseBlocks(parsed.body, (child: OwidRawGdocBlock) => {
         // ensure keys are lowercase
-        child = Object.entries(child).reduce(
-            (acc, [key, value]) => ({ ...acc, [key.toLowerCase()]: value }),
-            {} as OwidRawGdocBlock
-        )
+        child = lowercaseObjectKeys(child)
 
         // nest list items
         if (child.type === "text" && child.value.startsWith("* ")) {
@@ -187,4 +180,7 @@ export const archieToEnriched = (text: string): OwidGdocContent => {
             : citation.map(htmlToSimpleTextBlock)
     parsed.toc = toc
     return parsed
+}
+function lowercaseObjectKeys(parsed_unsanitized: any): any {
+    throw new Error("Function not implemented.")
 }
