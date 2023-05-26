3b31ddaa89928149350e5727dbfbdd4619140c30
Ike Saunders <ikesau@protonmail.com>
2023-05-26 17:12:12 +0000

    :construction: move the error location

--- baker/buildAndDeploySite.ts
+++ baker/buildAndDeploySite.ts
@@ -8,8 +8,6 @@ import path from "path"
 import { BakeStep, bakeSteps } from "./SiteBaker.js"
 import { DeployTarget } from "./DeployTarget.js"
 
-throw new Error("This deploy should fail fast")
-
 yargs(hideBin(process.argv))
     .command<{
         target: DeployTarget
@@ -29,6 +27,7 @@ yargs(hideBin(process.argv))
                 })
         },
         async ({ target, skipChecks, runChecksRemotely, steps }) => {
+            throw new Error("This deploy should fail fast")
             const bakeSteps = steps ? new Set(steps as BakeStep[]) : undefined
             const deployer = new Deployer({
                 target: target as any,
