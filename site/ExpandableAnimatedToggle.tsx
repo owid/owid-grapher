d539b1cd27181747b23b3dfcb8609fecb14dbf17
Matthieu <matthieu@ourworldindata.org>
2023-06-13 13:14:28 +0000

    ✨(exp anim toggle): remove animation

--- site/ExpandableAnimatedToggle.tsx
+++ site/ExpandableAnimatedToggle.tsx
@@ -2,7 +2,6 @@ import { faMinus } from "@fortawesome/free-solid-svg-icons/faMinus"
 import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/index.js"
 import React, { useState } from "react"
-import AnimateHeight from "react-animate-height"
 import cx from "classnames"
 
 export const ExpandableAnimatedToggle = ({
@@ -16,12 +15,10 @@ export const ExpandableAnimatedToggle = ({
     isExpandedDefault?: boolean
     isStacked?: boolean
 }) => {
-    const [height, setHeight] = useState<"auto" | 0>(
-        isExpandedDefault ? "auto" : 0
-    )
+    const [isOpen, setOpen] = useState(isExpandedDefault)
 
     const toggle = () => {
-        setHeight(height === 0 ? "auto" : 0)
+        setOpen(!isOpen)
     }
 
     return (
@@ -34,12 +31,16 @@ export const ExpandableAnimatedToggle = ({
                 <h4>{label}</h4>
                 <FontAwesomeIcon
                     className="ExpandableAnimatedToggle__icon"
-                    icon={height === 0 ? faPlus : faMinus}
+                    icon={!isOpen ? faPlus : faMinus}
                 />
             </button>
-            <AnimateHeight height={height} animateOpacity>
-                <div className="content-wrapper">{content}</div>
-            </AnimateHeight>
+            <div
+                className={cx("ExpandableAnimatedToggle__content", {
+                    "ExpandableAnimatedToggle__content--open": isOpen,
+                })}
+            >
+                {content}
+            </div>
         </div>
     )
 }
