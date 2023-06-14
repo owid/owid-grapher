5240e522708005b0f5ed52ada77591efe4c16965
Marcel Gerber <mgerber59@gmail.com>
2023-06-14 11:04:20 +0200

    refactor(analytics): change "Explorer customize chart" event name

--- explorer/Explorer.tsx
+++ explorer/Explorer.tsx
@@ -597,7 +597,7 @@ export class Explorer
             <a
                 className="btn btn-primary mobile-button"
                 onClick={this.toggleMobileControls}
-                data-track-note="covid_customize_chart"
+                data-track-note="explorer_customize-chart_mobile"
             >
                 <FontAwesomeIcon icon={faChartLine} /> Customize chart
             </a>
