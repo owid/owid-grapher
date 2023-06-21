b14731064d4dca917580d8a232694ee8c5f0d6ca
Ike Saunders <ikesau@protonmail.com>
2023-06-21 17:20:45 -0400

    🎉 update scrollY when collapsing expandable-parapraph

--- site/blocks/ExpandableParagraph.tsx
+++ site/blocks/ExpandableParagraph.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useLayoutEffect, useRef, useState } from "react"
 import cx from "classnames"
 import ReactDOM from "react-dom"
 import ReactDOMServer from "react-dom/server.js"
@@ -22,13 +22,35 @@ export const ExpandableParagraph = (
 ) => {
     const CLOSED_HEIGHT = 100
     const [height, setHeight] = useState<"auto" | number>(CLOSED_HEIGHT)
+    const buttonRef = useRef<HTMLButtonElement>(null)
+    const prevTopRef = useRef<number>(0)
 
     const isClosed = height === CLOSED_HEIGHT
     const { className, buttonVariant = "full", ...propsWithoutStyles } = props
 
     const toggleExpanded = () => {
+        if (!isClosed && buttonRef.current) {
+            // Store where in the screen the button was before closing
+            prevTopRef.current = buttonRef.current.getBoundingClientRect().top
+        }
         setHeight(isClosed ? "auto" : CLOSED_HEIGHT)
     }
+
+    useLayoutEffect(() => {
+        const currentButton = buttonRef.current
+        const prevTop = prevTopRef.current
+
+        // prevTop === 0 is the intial render, where we don't want to do anything
+        if (prevTop && currentButton && isClosed) {
+            // Store where in the screen the button is now (likely a negative number i.e. off-screen, above)
+            const currentTop = currentButton.getBoundingClientRect().top
+            window.scrollTo({
+                // Scroll up by the difference between where the button used to be and where it is now
+                // e.g. prevTop = 1000, currentTop = -500, scroll up by 1500 pixels so that currentTop will equal 1000 again
+                top: window.scrollY - (prevTop - currentTop),
+            })
+        }
+    }, [buttonRef, height, prevTopRef, isClosed])
 
     const maskColor = isClosed ? "transparent" : "#000"
     const contentStyles = {
@@ -46,6 +68,7 @@ export const ExpandableParagraph = (
             />
 
             <button
+                ref={buttonRef}
                 className={cx(
                     "expandable-paragraph__expand-button",
                     `expandable-paragraph__expand-button--${buttonVariant}`
