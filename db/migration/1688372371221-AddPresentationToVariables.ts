34fd1fc7ad99cef944e6a82a5b6c0ec99b0ab3c6
Marigold <mojmir.vinkler@gmail.com>
2023-07-04 11:48:32 +0200

    flatten presentation field

--- db/migration/1688372371221-AddPresentationToVariables.ts
+++ db/migration/1688372371221-AddPresentationToVariables.ts
@@ -6,26 +6,50 @@ export class AddPresentationToVariables1688372371221
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `ALTER TABLE variables
-              ADD COLUMN schemaVersion int,
-              ADD COLUMN processingLevel varchar(255),
-              ADD COLUMN presentation JSON;
-            `
+                ADD COLUMN schemaVersion INT,
+                ADD COLUMN processingLevel ENUM('minor', 'medium', 'major'),
+                ADD COLUMN processingLog JSON,
+                ADD COLUMN titlePublic TEXT,
+                ADD COLUMN titleVariant TEXT,
+                ADD COLUMN producerShort TEXT,
+                ADD COLUMN citationInline TEXT,
+                ADD COLUMN descriptionShort TEXT,
+                ADD COLUMN descriptionFromProducer TEXT,
+                -- ADD COLUMN topicTagsLinks JSON,
+                ADD COLUMN keyInfoText TEXT,
+                ADD COLUMN processingInfo TEXT,
+                ADD COLUMN licenses JSON;`
         )
-
-        await queryRunner.query(
-            `ALTER TABLE variables
-            ADD CONSTRAINT processing_level_check
-            CHECK (processingLevel IN ('minor', 'medium', 'major'));
-            `
-        )
+        await queryRunner.query(`
+        CREATE TABLE posts_gdocs_variables (
+            gdocId varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs NOT NULL,
+            variableId INT NOT NULL,
+            fragmentId VARCHAR(255) NOT NULL,
+            PRIMARY KEY (gdocId, fragmentId, variableId),
+            FOREIGN KEY (gdocId) REFERENCES posts_gdocs(id),
+            FOREIGN KEY (variableId) REFERENCES variables(id)
+        )`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+        DROP TABLE IF EXISTS posts_gdocs_variables;`)
+
         await queryRunner.query(
             `ALTER TABLE variables
-              DROP COLUMN schemaVersion,
-              DROP COLUMN processingLevel,
-              DROP COLUMN presentation;
+                DROP COLUMN schemaVersion,
+                DROP COLUMN processingLevel,
+                DROP COLUMN processingLog,
+                DROP COLUMN titlePublic,
+                DROP COLUMN titleVariant,
+                DROP COLUMN producerShort,
+                DROP COLUMN citationInline,
+                DROP COLUMN descriptionShort,
+                DROP COLUMN descriptionFromProducer,
+                -- DROP COLUMN topicTagsLinks,
+                DROP COLUMN keyInfoText,
+                DROP COLUMN processingInfo,
+                DROP COLUMN licenses;
             `
         )
     }
