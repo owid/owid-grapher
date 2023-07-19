b6557e2d76387ed7a6c82f538c78297ab5cd1517
sophiamersmann <sophia.mersmann1@gmail.com>
2023-07-19 12:10:13 +0000

    refactor(footer): clean up footer manager

--- packages/@ourworldindata/grapher/src/footer/FooterManager.ts
+++ packages/@ourworldindata/grapher/src/footer/FooterManager.ts
@@ -1,20 +1,12 @@
 import { TooltipManager } from "../tooltip/TooltipProps"
-import { Bounds } from "@ourworldindata/utils"
 import { GrapherInterface } from "../core/GrapherInterface"
-import {
-    GrapherTabOption,
-    GrapherTabOverlayOption,
-} from "../core/GrapherConstants"
 
-export interface FooterManager {
+export interface FooterManager extends TooltipManager {
     fontSize?: number
     sourcesLine?: string
     note?: string
     hasOWIDLogo?: boolean
     originUrlWithProtocol?: string
-    currentTab?: GrapherTabOption | GrapherTabOverlayOption
-    tooltips?: TooltipManager["tooltips"]
-    tabBounds?: Bounds
     details?: GrapherInterface["details"]
     detailsOrderedByReference?: Set<string>
     shouldIncludeDetailsInStaticExport?: boolean
