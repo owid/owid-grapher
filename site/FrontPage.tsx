68f2728de3240f681a05215892087ac441119745
Matthew Conlen <mc@mathisonian.com>
2023-07-19 18:00:39 -0400

    remove topic section on hp

--- site/FrontPage.tsx
+++ site/FrontPage.tsx
@@ -7,7 +7,6 @@ import {
     faAngleRight,
     faExternalLinkAlt,
     faAngleDoubleDown,
-    faArrowRight,
 } from "@fortawesome/free-solid-svg-icons"
 import {
     faTwitter,
@@ -20,27 +19,16 @@ import {
     NewsletterSubscriptionContext,
 } from "../site/NewsletterSubscription.js"
 import {
-    CategoryWithEntries,
-    EntryNode,
     IndexPost,
 } from "@ourworldindata/utils"
 import PostCard from "./PostCard/PostCard.js"
 
-const splitOnLastWord = (str: string) => {
-    const endIndex = (str.lastIndexOf(" ") as number) + 1
-    return {
-        start: endIndex === 0 ? "" : str.substring(0, endIndex),
-        end: str.substring(endIndex),
-    }
-}
-
 export const FrontPage = (props: {
-    entries: CategoryWithEntries[]
     totalCharts: number
     baseUrl: string
     featuredWork: IndexPost[]
 }) => {
-    const { entries, totalCharts, baseUrl, featuredWork } = props
+    const { totalCharts, baseUrl, featuredWork } = props
 
     // Structured data for google
     const structuredMarkup = {
@@ -52,43 +40,6 @@ export const FrontPage = (props: {
             target: `${baseUrl}/search?q={search_term_string}`,
             "query-input": "required name=search_term_string",
         },
-    }
-
-    const renderEntry = (entry: EntryNode, categorySlug: string) => {
-        const { start: titleStart, end: titleEnd } = splitOnLastWord(
-            entry.title
-        )
-        return (
-            <a
-                key={entry.slug}
-                href={`/${entry.slug}`}
-                className={`entry-item-container ${categorySlug}-color`}
-                data-track-note="homepage_entries"
-            >
-                <div className="entry-item">
-                    <div className="top">
-                        {entry.kpi && (
-                            <div
-                                className="kpi"
-                                dangerouslySetInnerHTML={{
-                                    __html: entry.kpi,
-                                }}
-                            />
-                        )}
-                        <p className="excerpt">{entry.excerpt}</p>
-                    </div>
-                    <div className="bottom">
-                        <h5>
-                            {titleStart}
-                            <span>
-                                {titleEnd}
-                                <FontAwesomeIcon icon={faArrowRight} />
-                            </span>
-                        </h5>
-                    </div>
-                </div>
-            </a>
-        )
     }
 
     return (
@@ -373,53 +324,6 @@ export const FrontPage = (props: {
                         </div>
                     </div>
                 </section>
-
-                <section id="entries" className="homepage-entries">
-                    <div className="wrapper">
-                        <h2>
-                            All of our pages on global problems and global
-                            changes
-                        </h2>
-                        {entries.map((category) => (
-                            <div
-                                key={category.slug}
-                                className="category-wrapper"
-                            >
-                                <h3
-                                    className={`${category.slug}-color`}
-                                    id={category.slug}
-                                >
-                                    {category.name}
-                                </h3>
-                                {!!category.entries.length && (
-                                    <div className="category-entries">
-                                        {category.entries.map((entry) =>
-                                            renderEntry(entry, category.slug)
-                                        )}
-                                    </div>
-                                )}
-                                {category.subcategories.map((subcategory) => (
-                                    <div key={subcategory.slug}>
-                                        <h4
-                                            className={`${category.slug}-color`}
-                                        >
-                                            {subcategory.name}
-                                        </h4>
-                                        <div className="category-entries">
-                                            {subcategory.entries.map((entry) =>
-                                                renderEntry(
-                                                    entry,
-                                                    category.slug
-                                                )
-                                            )}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
-                    </div>
-                </section>
-
                 <SiteFooter baseUrl={baseUrl} />
             </body>
         </html>
