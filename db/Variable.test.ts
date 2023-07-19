f2b8c050d8e48dc7992d730cb3783d412bce0e53
Marigold <mojmir.vinkler@gmail.com>
2023-07-19 09:12:55 +0200

    linter

--- db/Variable.test.ts
+++ db/Variable.test.ts
@@ -1,11 +1,6 @@
 #! /usr/bin/env yarn jest
 
-import {
-    writeVariableCSV,
-    getVariableData,
-    VariableQueryRow,
-    _dataAsDFfromS3,
-} from "./model/Variable.js"
+import { writeVariableCSV, _dataAsDFfromS3 } from "./model/Variable.js"
 import * as db from "./db.js"
 import * as Variable from "./model/Variable.js"
 import pl from "nodejs-polars"
