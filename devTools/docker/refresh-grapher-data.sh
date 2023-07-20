f56cdd9cc1748e6a79188c3a9ed6a88e7555385f
Daniel Bachler <daniel@danielbachler.de>
2023-07-20 18:59:14 +0200

    :bug: fix DB default collation on dev/staging servers

--- devTools/docker/refresh-grapher-data.sh
+++ devTools/docker/refresh-grapher-data.sh
@@ -21,7 +21,7 @@ import_db(){
 
 fillGrapherDb() {
     echo "==> Refreshing grapher database"
-    _mysql -e "DROP DATABASE IF EXISTS $GRAPHER_DB_NAME;CREATE DATABASE $GRAPHER_DB_NAME;"
+    _mysql -e "DROP DATABASE IF EXISTS $GRAPHER_DB_NAME;CREATE DATABASE $GRAPHER_DB_NAME;ALTER DATABASE $GRAPHER_DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;"
 
     if [ -f "${DATA_FOLDER}/owid_metadata.sql.gz" ]; then
         echo "Importing live Grapher metadata database (owid_metadata)"
