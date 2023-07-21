310f1819dbd61eaeaadb5736ee74d6dc44ac98aa
Christian Swinehart <drafting@samizdat.co>
2023-07-21 10:51:43 -0400

    :sparkles: (tooltip): add `striped` flag to table rows

--- packages/@ourworldindata/grapher/src/tooltip/TooltipProps.ts
+++ packages/@ourworldindata/grapher/src/tooltip/TooltipProps.ts
@@ -55,6 +55,7 @@ export interface TooltipTableRow {
     swatch?: string // css color string for the series's swatch
     focused?: boolean // highlighted (based on hovered series in chart)
     blurred?: boolean // greyed out (typically due to missing data)
+    striped?: boolean // use textured swatch (to show data is extrapolated)
     notice?: string | number // actual year data was drawn (when ≠ target year)
     values: (string | number | undefined)[]
 }
