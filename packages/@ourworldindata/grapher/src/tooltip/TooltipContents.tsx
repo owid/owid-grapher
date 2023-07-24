d8e96a0f676600951e9412cfea82121fa1c2c097
Christian Swinehart <drafting@samizdat.co>
2023-07-24 16:08:45 -0400

    :lipstick: (tooltip): remove unused regex capture

--- packages/@ourworldindata/grapher/src/tooltip/TooltipContents.tsx
+++ packages/@ourworldindata/grapher/src/tooltip/TooltipContents.tsx
@@ -177,7 +177,7 @@ export class TooltipTable extends React.Component<TooltipTableProps> {
                             swatch = "transparent",
                         } = row
                         const [_m, seriesName, seriesParenthetical] =
-                            name.match(/^(.*?)(\(([^()]*)\))?$/) ?? []
+                            name.match(/^(.*?)(\([^()]*\))?$/) ?? []
 
                         return (
                             <tr
