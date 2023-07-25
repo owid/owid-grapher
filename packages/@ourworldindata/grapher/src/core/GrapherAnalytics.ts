f50b02d5f21b918605e4192fc2d9d486c71aff79
Marcel Gerber <mgerber59@gmail.com>
2023-07-25 16:57:23 +0200

    feat: add method to update GA consent

--- packages/@ourworldindata/grapher/src/core/GrapherAnalytics.ts
+++ packages/@ourworldindata/grapher/src/core/GrapherAnalytics.ts
@@ -4,7 +4,7 @@ const DEBUG = false
 
 // Add type information for dataLayer global provided by Google Tag Manager
 type WindowWithDataLayer = Window & {
-    dataLayer?: GAEvent[]
+    dataLayer?: (GAEvent | GAConsent)[]
 }
 declare const window: WindowWithDataLayer
 
@@ -42,6 +42,16 @@ interface GAEvent {
     eventTarget?: string
     grapherPath?: string
 }
+
+// taken from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/de66435d18fbdb2684947d16b5cd3a77f876324c/types/gtag.js/index.d.ts#L151-L156
+interface GAConsentParams {
+    ad_storage?: "granted" | "denied" | undefined
+    analytics_storage?: "granted" | "denied" | undefined
+    wait_for_update?: number | undefined
+    region?: string[] | undefined
+}
+
+type GAConsent = ["consent", "default" | "update", GAConsentParams]
 
 // Note: consent-based blocking dealt with at the Google Tag Manager level.
 // Events are discarded if consent not given.
@@ -162,5 +172,9 @@ export class GrapherAnalytics {
         }
 
         window.dataLayer?.push(event)
+    }
+
+    updateGAConsentSettings(consent: GAConsentParams): void {
+        window.dataLayer?.push(["consent", "update", consent])
     }
 }
