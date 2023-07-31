a9b193cc938ed9b76f4fccd809054155479367b9
Daniel Bachler <daniel@danielbachler.de>
2023-07-31 11:44:21 +0200

    :hammer: incorporate PR feedback

---
+++ db/migration/1690795517408-FixChartConfigDisplayShortUnitNull.ts
@@ -0,0 +1,67 @@
+import { cloneDeep } from "@ourworldindata/utils"
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class FixChartConfigDisplayShortUnitNull1690795517408
+    implements MigrationInterface
+{
+    private fixConfigs(
+        charts: { id: number; config: Record<string, any> }[]
+    ): { id: number; config: string }[] {
+        const fixedCharts: { id: number; config: string }[] = []
+        for (const oldChart of charts) {
+            const chart = cloneDeep(oldChart)
+            let chartModified = false
+            // if any of the dimensions has a shortUnit field that is null, remove it
+            if (chart.config.dimensions) {
+                for (const dimension of chart.config.dimensions) {
+                    if (dimension.display) {
+                        if (dimension.display.shortUnit === null) {
+                            delete dimension.display.shortUnit
+                            chartModified = true
+                        }
+                    }
+                }
+            }
+            if (chartModified) {
+                fixedCharts.push({
+                    id: chart.id,
+                    config: JSON.stringify(chart.config),
+                })
+            }
+        }
+        return fixedCharts
+    }
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const tables = {
+            suggested_chart_revisions: "suggestedConfig",
+            chart_revisions: "config",
+            charts: "config",
+        }
+
+        for (const [tableName, columnName] of Object.entries(tables)) {
+            // where the .display.shortUnit field contains null, remove the field
+            const chartsPrimitive: { id: number; config: string }[] =
+                await queryRunner.query(
+                    `select id, ${columnName} as config from ${tableName}`
+                )
+            const charts: { id: number; config: Record<string, any> }[] =
+                chartsPrimitive.map((chart) => ({
+                    id: chart.id,
+                    config: JSON.parse(chart.config),
+                }))
+
+            const fixedCharts = this.fixConfigs(charts)
+            // update the charts
+            for (const chart of fixedCharts) {
+                await queryRunner.query(
+                    `update ${tableName} set ${columnName} = ? where id = ?`,
+                    [chart.config, chart.id]
+                )
+            }
+        }
+    }
+
+    public async down(_queryRunner: QueryRunner): Promise<void> {
+        return
+    }
+}
