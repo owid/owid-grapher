14352ac8aecd198ae4fdec6a09238ca9021adf05
Marcel Gerber <mgerber59@gmail.com>
2023-08-22 14:03:07 +0200

    test(regions): add test cases

--- packages/@ourworldindata/utils/src/regions.test.ts
+++ packages/@ourworldindata/utils/src/regions.test.ts
@@ -1,1 +1,17 @@
 #! /usr/bin/env jest
+
+import { isCountryName, getCountryBySlug } from "./regions.js"
+
+it("isCountryName", () => {
+    expect(isCountryName("United States")).toEqual(true)
+    expect(isCountryName("Not a country")).toEqual(false)
+})
+
+it("getCountryBySlug", () => {
+    expect(getCountryBySlug("united-states")).toMatchObject({
+        name: "United States",
+        slug: "united-states",
+        code: "USA",
+    })
+    expect(getCountryBySlug("not-a-country")).toEqual(undefined)
+})
