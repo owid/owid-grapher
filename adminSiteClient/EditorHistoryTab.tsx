8408f7123c568220c053ffe3661c6efa75984e23
Daniel Bachler <daniel@danielbachler.de>
2023-08-23 19:42:06 +0200

    :sparkles: add copy to yaml button on chart admin reference tab

--- adminSiteClient/EditorHistoryTab.tsx
+++ adminSiteClient/EditorHistoryTab.tsx
@@ -3,6 +3,8 @@ import { observer } from "mobx-react"
 import { ChartEditor, Log } from "./ChartEditor.js"
 import { Section, Timeago } from "./Forms.js"
 import { computed, action } from "mobx"
+import { dump } from "js-yaml"
+import { notification } from "antd"
 
 @observer
 class LogRenderer extends React.Component<{
@@ -60,6 +62,25 @@ export class EditorHistoryTab extends React.Component<{ editor: ChartEditor }> {
         grapher.rebuildInputOwidTable()
     }
 
+    @action.bound copyYamlToClipboard() {
+        // Use the Clipboard API to copy the config into the users clipboard
+        const chartConfigObject = {
+            ...this.props.editor.grapher.object,
+        }
+        delete chartConfigObject.id
+        delete chartConfigObject.dimensions
+        delete chartConfigObject.version
+        delete chartConfigObject.isPublished
+        const chartConfigAsYaml = dump(chartConfigObject)
+        navigator.clipboard.writeText(chartConfigAsYaml)
+        notification["success"]({
+            message: "Copied YAML to clipboard",
+            description: "You can now paste this into the ETL",
+            placement: "bottomRight",
+            closeIcon: <></>,
+        })
+    }
+
     render() {
         // Avoid modifying the original JSON object
         // Due to mobx memoizing computed values, the JSON can be mutated.
@@ -77,6 +98,12 @@ export class EditorHistoryTab extends React.Component<{ editor: ChartEditor }> {
                     </ul>
                 ))}
                 <Section name="Debug Version">
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.copyYamlToClipboard}
+                    >
+                        Copy YAML for ETL
+                    </button>
                     <textarea
                         rows={7}
                         readOnly
