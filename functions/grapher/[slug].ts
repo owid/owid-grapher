355d319f906fd4ac2fe8e8b69b760f2b6668eb19
Marcel Gerber <mgerber59@gmail.com>
2023-08-24 16:55:26 +0200

    enhance(cf): use HTMLRewriter in grapher function

--- functions/grapher/[slug].ts
+++ functions/grapher/[slug].ts
@@ -2,13 +2,28 @@ export const onRequestGet: PagesFunction = async ({ request, params, env }) => {
     const slug = params.slug as string
     const url = new URL(request.url)
     const { search } = url
-    const grapherPageResp = await env.ASSETS.fetch(url)
-    const grapherPageHtml = await grapherPageResp.text()
 
-    const transformedHtml = grapherPageHtml.replace(
-        /"[^"]+\/default-grapher-thumbnail.png"/g,
-        `"https://thumbnails.owid.io/grapher/${slug}.png${search}"`
-    )
+    const grapherPageResp = await env.ASSETS.fetch(url, { redirect: "manual" })
 
-    return new Response(transformedHtml, grapherPageResp)
+    // For local testing
+    // const grapherPageResp = await fetch(
+    //     `https://ourworldindata.org/grapher/${slug}`,
+    //     { redirect: "manual" }
+    // )
+
+    if (grapherPageResp.status !== 200) return grapherPageResp
+
+    const thumbnailUrl = `https://thumbnails.owid.io/grapher/${slug}.png${search}`
+
+    const rewriter = new HTMLRewriter().on("meta", {
+        element: (element) => {
+            if (
+                element.getAttribute("property") === "og:image" ||
+                element.getAttribute("name") === "twitter:image"
+            )
+                element.setAttribute("content", thumbnailUrl)
+        },
+    })
+
+    return rewriter.transform(grapherPageResp)
 }
