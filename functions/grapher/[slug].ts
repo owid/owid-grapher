5c1ea4531bcd843c365aefa357f2e74d82657048
Marcel Gerber <mgerber59@gmail.com>
2023-08-28 19:58:21 +0200

    enhance(cf): serve underlying asset as-is in case of exception

--- functions/grapher/[slug].ts
+++ functions/grapher/[slug].ts
@@ -1,4 +1,10 @@
-export const onRequestGet: PagesFunction = async ({ request, params, env }) => {
+export const onRequestGet: PagesFunction = async (context) => {
+    // Makes it so that if there's an error, we will just deliver the original page before the HTML rewrite.
+    // Only caveat is that redirects will not be taken into account for some reason; but on the other hand the worker is so simple that it's unlikely to fail.
+    context.passThroughOnException()
+
+    const { request, env, params } = context
+
     const slug = params.slug as string
     const url = new URL(request.url)
     const { search } = url
@@ -11,19 +17,21 @@ export const onRequestGet: PagesFunction = async ({ request, params, env }) => {
     //     { redirect: "manual" }
     // )
 
+    // A non-200 status code is most likely a redirect (301 or 302) or a 404, all of which we want to pass through as-is.
+    // In the case of the redirect, the browser will then request the new URL which will again be handled by this worker.
     if (grapherPageResp.status !== 200) return grapherPageResp
 
     const thumbnailUrl = `https://thumbnails.owid.io/grapher/${slug}.png${search}`
 
-    const rewriter = new HTMLRewriter().on("meta", {
-        element: (element) => {
-            if (
-                element.getAttribute("property") === "og:image" ||
-                element.getAttribute("name") === "twitter:image"
-            )
+    // Rewrite the two meta tags that are used for a social media preview image.
+    const rewriter = new HTMLRewriter().on(
+        'meta[property="og:image"], meta[name="twitter:image"]',
+        {
+            element: (element) => {
                 element.setAttribute("content", thumbnailUrl)
-        },
-    })
+            },
+        }
+    )
 
     return rewriter.transform(grapherPageResp)
 }
