df52ddd18a09344ad15788b0977d3126a46579be
Ike Saunders <ikesau@protonmail.com>
2023-09-13 17:44:00 -0400

    🐛 de-dupe posts in /latest

--- baker/siteRenderers.tsx
+++ baker/siteRenderers.tsx
@@ -226,21 +226,7 @@ export const renderPost = async (
 }
 
 export const renderFrontPage = async () => {
-    const wpPosts = await Promise.all(
-        (await getPosts()).map((post) => getFullPost(post, true))
-    )
-    const gdocPosts = await Gdoc.getPublishedGdocs().then(
-        mapGdocsToWordpressPosts
-    )
-    const gdocSlugs = new Set(gdocPosts.map(({ slug }) => slug))
-    const posts = [...gdocPosts]
-    // Only adding each wpPost if there isn't already a gdoc with the same slug,
-    // to make sure we use the most up-to-date metadata
-    for (const wpPost of wpPosts) {
-        if (!gdocSlugs.has(wpPost.slug)) {
-            posts.push(wpPost)
-        }
-    }
+    const posts = await getBlogIndex()
 
     const NUM_FEATURED_POSTS = 6
 
@@ -290,6 +276,10 @@ export const renderFrontPage = async () => {
                     if (post) {
                         return post
                     }
+                    console.log(
+                        "Featured work error: could not find listed post with slug: ",
+                        manuallySetPost.slug
+                    )
                 }
                 return filteredPosts[missingPosts++]
             })
