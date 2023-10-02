56901f5ec3e7d3f73d14d1bf417ddcf7c901a122
Marcel Gerber <mgerber59@gmail.com>
2023-10-02 12:20:58 +0200

    db: recreate active_datasets view
    ... to include new columns `version` and `shortName`

---
+++ db/migration/1696240121675-RecreateActiveDatasetsViewToIncludeNewColumns.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class RecreateActiveDatasetsViewToIncludeNewColumns1696240121675
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+        ALTER VIEW active_datasets AS SELECT * FROM datasets WHERE not isArchived;
+      `)
+    }
+
+    public async down(_queryRunner: QueryRunner): Promise<void> {
+        // Empty
+    }
+}
