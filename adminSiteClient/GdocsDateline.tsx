a047675ac15ce50c26faf484cac9fb639fa6c2db
Matthieu <matthieu@ourworldindata.org>
2023-10-04 07:57:23 +0000

    fix(gdoc): remove "Today" button in datepicker

--- adminSiteClient/GdocsDateline.tsx
+++ adminSiteClient/GdocsDateline.tsx
@@ -49,6 +49,12 @@ export const GdocsDateline = ({
                         format="ddd, MMM D, YYYY"
                         id="publishedAt"
                         status={publishedAtError?.type}
+                        // The "Today" button has been disabled because it sets
+                        // the time to the current time. This time change makes
+                        // it all the way to the atom feed, which is then
+                        // interpreted by MailChimp's RSS-to-Email as a new
+                        // article.
+                        showToday={false}
                     />
                     <GdocsErrorHelp
                         error={publishedAtError}
