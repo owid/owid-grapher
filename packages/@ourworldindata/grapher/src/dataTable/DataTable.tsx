294f286416de87ec23e7e3aa8de57e3fc687fa55
sophiamersmann <sophia.mersmann1@gmail.com>
2023-10-10 09:10:38 +0000

    refactor(data-table): reomve transformTable method

--- packages/@ourworldindata/grapher/src/dataTable/DataTable.tsx
+++ packages/@ourworldindata/grapher/src/dataTable/DataTable.tsx
@@ -94,19 +94,6 @@ export class DataTable extends React.Component<{
     manager?: DataTableManager
     bounds?: Bounds
 }> {
-    transformTable(table: OwidTable): OwidTable {
-        if (this.manager.showSelectionOnlyInDataTable) {
-            table = table.filterByEntityNames(
-                this.selectionArray.selectedEntityNames
-            )
-        }
-        return table
-    }
-
-    @computed get transformedTable(): OwidTable {
-        return this.transformTable(this.manager.table)
-    }
-
     @observable private storedState: DataTableState = {
         sort: DEFAULT_SORT_STATE,
     }
@@ -144,11 +131,13 @@ export class DataTable extends React.Component<{
     }
 
     @computed get table(): OwidTable {
-        return this.transformedTable
-    }
-
-    @computed get inputTable(): OwidTable {
-        return this.manager.table
+        let table = this.manager.table
+        if (this.manager.showSelectionOnlyInDataTable) {
+            table = table.filterByEntityNames(
+                this.selectionArray.selectedEntityNames
+            )
+        }
+        return table
     }
 
     @computed get manager(): DataTableManager {
