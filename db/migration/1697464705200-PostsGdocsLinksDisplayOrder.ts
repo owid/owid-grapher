934b1fe894a88a48727ff2179d16dd2cdd85a871
Marigold <mojmir.vinkler@gmail.com>
2023-10-16 16:10:36 +0200

    :sparkles: add displayOrder column to posts_gdocs_variables_faqs table

---
+++ db/migration/1697464705200-PostsGdocsLinksDisplayOrder.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class PostsGdocsLinksDisplayOrder1697464705200
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(
+            `ALTER TABLE posts_gdocs_variables_faqs
+            ADD COLUMN displayOrder SMALLINT NOT NULL DEFAULT 0;`
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(
+            `ALTER TABLE posts_gdocs_variables_faqs
+            DROP COLUMN display_order;`
+        )
+    }
+}
