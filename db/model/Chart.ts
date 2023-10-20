99996be9a9ca21c1b97712c372eb928b0554f7aa
Matthieu <matthieu@ourworldindata.org>
2023-10-20 14:34:06 +0000

    feat: add tags to list of public parent tags

--- db/model/Chart.ts
+++ db/model/Chart.ts
@@ -26,7 +26,7 @@ import { OPENAI_API_KEY } from "../../settings/serverSettings.js"
 // XXX hardcoded filtering to public parent tags
 export const PUBLIC_TAG_PARENT_IDS = [
     1515, 1507, 1513, 1504, 1502, 1509, 1506, 1501, 1514, 1511, 1500, 1503,
-    1505, 1508, 1512, 1510,
+    1505, 1508, 1512, 1510, 1834, 1835,
 ]
 
 @Entity("charts")
