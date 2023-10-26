53fa95cc7ade2906c00b134ff63e660f09e52854
sophiamersmann <sophia.mersmann1@gmail.com>
2023-10-26 11:23:46 +0000

    fix(data-table): fix type error

--- packages/@ourworldindata/grapher/src/dataTable/DataTable.tsx
+++ packages/@ourworldindata/grapher/src/dataTable/DataTable.tsx
@@ -771,6 +771,9 @@ export class DataTable extends React.Component<{
                     end !== undefined &&
                     typeof start.value === "number" &&
                     typeof end.value === "number" &&
+                    // sanity check: start time should always be <= end time
+                    start.time !== undefined &&
+                    end.time !== undefined &&
                     start.time <= end.time
                 ) {
                     const deltaValue = end.value - start.value
