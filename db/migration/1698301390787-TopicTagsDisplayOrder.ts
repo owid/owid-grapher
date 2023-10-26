ac80c1cf6b73972c0db012d53e181c88625c850d
Marigold <mojmir.vinkler@gmail.com>
2023-10-26 08:25:26 +0200

    ✨ add displayOrder column to tags_variables_topic_tags table

---
+++ db/migration/1698301390787-TopicTagsDisplayOrder.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class TopicTagsDisplayOrder1698301390787 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(
+            `ALTER TABLE tags_variables_topic_tags
+            ADD COLUMN displayOrder SMALLINT NOT NULL DEFAULT 0;`
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(
+            `ALTER TABLE tags_variables_topic_tags
+            DROP COLUMN display_order;`
+        )
+    }
+}
