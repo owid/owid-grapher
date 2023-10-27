735b80c40512b89ed94f44b0dec1566d22c0b569
Daniel Bachler <daniel@danielbachler.de>
2023-10-27 21:35:59 +0200

    markdown-transform-dod-links

--- packages/@ourworldindata/components/src/SimpleMarkdownText.tsx
+++ packages/@ourworldindata/components/src/SimpleMarkdownText.tsx
@@ -7,12 +7,42 @@ type SimpleMarkdownTextProps = {
     text: string
 }
 
+function transformDodLinks() {
+    return function (tree: any) {
+        visit(tree, "element", function (node) {
+            if (
+                node.tagName === "a" &&
+                typeof node.properties.href === "string" &&
+                node.properties.href.startsWith("#dod:")
+            ) {
+                // use a regex to split the #dod: part from the term of the href. Use a named capture group
+                // to capture the term
+                const match = node.properties.href.match(/#dod:(?<term>.+)/)
+                if (match) {
+                    node.properties.class = "dod-span"
+                    node.properties["data-id"] = match.groups?.term
+                    node.properties["aria-expanded"] = "false"
+                    delete node.properties.href
+                }
+                //node.children.push(
+            }
+        })
+    }
+}
+
 export class SimpleMarkdownText extends React.Component<SimpleMarkdownTextProps> {
     @computed get text(): string {
         return this.props.text
     }
 
     render(): JSX.Element | null {
-        return <Remark remarkPlugins={[remarkPlainLinks]}>{this.text}</Remark>
+        return (
+            <Remark
+                rehypePlugins={[transformDodLinks]}
+                remarkPlugins={[remarkPlainLinks]}
+            >
+                {this.text}
+            </Remark>
+        )
     }
 }
