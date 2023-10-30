ad303b6c1e9d8875b0bfd6ee207327eb4f1a578b
Daniel Bachler <daniel@danielbachler.de>
2023-10-30 09:46:56 +0100

    :hammer: fix minor rebase issues

--- packages/@ourworldindata/components/src/SimpleMarkdownText.tsx
+++ packages/@ourworldindata/components/src/SimpleMarkdownText.tsx
@@ -35,20 +35,6 @@ export class SimpleMarkdownText extends React.Component<SimpleMarkdownTextProps>
         return this.props.text
     }
 
-    @computed get ast(): MarkdownRoot["children"] {
-        if (!this.text) return []
-        const result = mdParser.markdown.parse(this.props.text)
-        if (result.status) {
-            return result.value.children
-        }
-        return []
-    }
-
-    @computed get tokens(): IRToken[] {
-        const tokens = parsimmonToTextTokens(this.ast, {})
-        return recursiveMergeTextTokens(tokens)
-    }
-
     render(): JSX.Element | null {
         return <Remark rehypePlugins={[transformDodLinks]}>{this.text}</Remark>
     }
