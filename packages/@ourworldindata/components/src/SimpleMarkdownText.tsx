4d3f11a3dd8982609468fdb6d8f204f87f293b4f
Daniel Bachler <daniel@danielbachler.de>
2023-10-30 15:27:00 +0100

    :hammer: downgrade hast dependencies and fix dod modification

--- packages/@ourworldindata/components/src/SimpleMarkdownText.tsx
+++ packages/@ourworldindata/components/src/SimpleMarkdownText.tsx
@@ -8,24 +8,27 @@ type SimpleMarkdownTextProps = {
 }
 
 function transformDodLinks() {
+    // TODO: try to type this properly when we have upgraded to a recent version with ESM. This is
+    // going to be a HAST tree and with that typing it should be able to make the access in there typesafe.
     return function (tree: any) {
-        visit(tree, "element", function (node) {
-            if (
-                node.tagName === "a" &&
-                typeof node.properties.href === "string" &&
-                node.properties.href.startsWith("#dod:")
-            ) {
-                // use a regex to split the #dod: part from the term of the href. Use a named capture group
-                // to capture the term
-                const match = node.properties.href.match(/#dod:(?<term>.+)/)
-                if (match) {
-                    node.properties.class = "dod-span"
-                    node.properties["data-id"] = match.groups?.term
-                    node.properties["aria-expanded"] = "false"
-                    delete node.properties.href
+        visit(tree, "element", function (node: any) {
+            if (node.tagName === "a")
+                if (
+                    node.properties &&
+                    typeof node.properties.href === "string" &&
+                    node.properties.href.startsWith("#dod:")
+                ) {
+                    // use a regex to split the #dod: part from the term of the href. Use a named capture group
+                    // to capture the term
+                    const match = node.properties.href.match(/#dod:(?<term>.+)/)
+                    if (match) {
+                        node.properties.class = "dod-span"
+                        node.properties["data-id"] = match.groups?.term
+                        node.properties["aria-expanded"] = "false"
+                        delete node.properties.href
+                    }
+                    //node.children.push(
                 }
-                //node.children.push(
-            }
         })
     }
 }
