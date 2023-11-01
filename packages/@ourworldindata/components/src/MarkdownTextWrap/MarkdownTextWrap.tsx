a285df56235c837febebd28c83fed891b72a0e53
Daniel Bachler <daniel@danielbachler.de>
2023-11-01 13:02:31 +0100

    :honeybee: remove dead code

--- packages/@ourworldindata/components/src/MarkdownTextWrap/MarkdownTextWrap.tsx
+++ packages/@ourworldindata/components/src/MarkdownTextWrap/MarkdownTextWrap.tsx
@@ -495,63 +495,6 @@ export const sumTextWrapHeights = (
     sum(elements.map((element) => element.height)) +
     (elements.length - 1) * spacer
 
-// export function parsimmonToTextTokens(
-//     nodes: EveryMarkdownChildNode[],
-//     fontParams?: IRFontParams
-// ): IRToken[] {
-//     return nodes.map((node): IRToken => {
-//         if (node.type === "text") {
-//             return new IRText(node.value, fontParams)
-//         } else if (node.type === "newline") {
-//             return new IRLineBreak()
-//         } else if (node.type === "whitespace") {
-//             return new IRWhitespace(fontParams)
-//         } else if (
-//             node.type === "bold" ||
-//             node.type === "plainBold" ||
-//             node.type === "boldWithoutItalic"
-//         ) {
-//             return new IRBold(
-//                 parsimmonToTextTokens(node.children, {
-//                     ...fontParams,
-//                     fontWeight: 700,
-//                 })
-//             )
-//         } else if (
-//             node.type === "italic" ||
-//             node.type === "plainItalic" ||
-//             node.type === "italicWithoutBold"
-//         ) {
-//             return new IRItalic(
-//                 parsimmonToTextTokens(node.children, {
-//                     ...fontParams,
-//                     isItalic: true,
-//                 })
-//             )
-//         } else if (node.type === "plainUrl") {
-//             return new IRLink(
-//                 node.href,
-//                 parsimmonToTextTokens(
-//                     [{ type: "text", value: node.href }],
-//                     fontParams
-//                 )
-//             )
-//         } else if (node.type === "markdownLink") {
-//             return new IRLink(
-//                 node.href,
-//                 parsimmonToTextTokens(node.children, fontParams)
-//             )
-//         } else if (node.type === "detailOnDemand") {
-//             return new IRDetailOnDemand(
-//                 node.term,
-//                 parsimmonToTextTokens(node.children, fontParams)
-//             )
-//         } else {
-//             throw new Error(`Unknown node type: ${(node as any).type}`)
-//         }
-//     })
-// }
-
 type MarkdownTextWrapProps = {
     text: string
     fontSize: number
