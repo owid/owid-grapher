207a9a7eac2b05558a0f62ac92e5774f3bd11c56
sophiamersmann <sophia.mersmann1@gmail.com>
2023-11-03 10:01:56 +0000

    refactor(sources-modal): clean up css of the processing section

--- packages/@ourworldindata/grapher/src/modal/SourcesModal.tsx
+++ packages/@ourworldindata/grapher/src/modal/SourcesModal.tsx
@@ -220,7 +220,7 @@ export class Source extends React.Component<{
                             />
                         </>
                     )}
-                <h3 className="heading">
+                <h3 className="heading heading--tight">
                     How we process data at Our World in Data:
                 </h3>
                 <IndicatorProcessing
