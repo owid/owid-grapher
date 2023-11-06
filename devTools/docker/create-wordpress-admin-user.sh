9ce46ed397415bb462778e6dc2c4ff6067327380
Lars Yencken <lars@yencken.org>
2023-11-06 16:42:14 +0000

    :honeybee: Fix "make refresh.wp" error when updating permissions
    The step creating an admin user did not appear to be specifying the
    database, and would error out.
    
    Since "make refresh" uses this, that was breaking too.

--- devTools/docker/create-wordpress-admin-user.sh
+++ devTools/docker/create-wordpress-admin-user.sh
@@ -10,19 +10,19 @@ set -o nounset
 : "${WORDPRESS_DB_HOST:?Need to set WORDPRESS_DB_HOST non-empty}"
 
 _mysql() {
-    mysql --default-character-set=utf8mb4 -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASS -h $WORDPRESS_DB_HOST --port="${WORDPRESS_DB_PORT}" -e "$1" $WORDPRESS_DB_NAME
+    mysql --default-character-set=utf8mb4 --database="${WORDPRESS_DB_NAME}" -h"${WORDPRESS_DB_HOST}" -u"${WORDPRESS_DB_USER}" -p"${WORDPRESS_DB_PASS}" --port "${WORDPRESS_DB_PORT}" "$@"
 }
 
 createWordpressAdminUser() {
     echo "Clearing wordpress password hashes"
-    _mysql 'UPDATE wp_users SET user_pass = "";'
+    _mysql -e 'UPDATE wp_users SET user_pass = "";'
 
     echo "Adding the user admin@example.com with password 'admin'"
-    _mysql 'INSERT INTO wp_users (user_login, user_email, user_pass, user_registered, user_nicename) VALUES ("admin", "admin@example.com", "$2y$10$2ilzpLslIA29cZezVXJTDOqLlkGyXK6YcNvr2QPvn95WdmVdnxl2S", NOW(), "Admin");'
+    _mysql -e 'INSERT INTO wp_users (user_login, user_email, user_pass, user_registered, user_nicename) VALUES ("admin", "admin@example.com", "$2y$10$2ilzpLslIA29cZezVXJTDOqLlkGyXK6YcNvr2QPvn95WdmVdnxl2S", NOW(), "Admin");'
 
     echo "Giving the admin user administrator privileges"
-    _mysql 'INSERT INTO wp_usermeta (user_id, meta_key, meta_value) VALUES ((SELECT id FROM wp_users WHERE user_email = "admin@example.com"), "wp_capabilities", "a:1:{s:13:\"administrator\";s:1:\"1\";}");'
-    _mysql 'INSERT INTO wp_usermeta (user_id, meta_key, meta_value) VALUES ((SELECT id FROM wp_users WHERE user_email = "admin@example.com"), "rich_editing", "true");'
+    _mysql -e 'INSERT INTO wp_usermeta (user_id, meta_key, meta_value) VALUES ((SELECT id FROM wp_users WHERE user_email = "admin@example.com"), "wp_capabilities", "a:1:{s:13:\"administrator\";s:1:\"1\";}");'
+    _mysql -e 'INSERT INTO wp_usermeta (user_id, meta_key, meta_value) VALUES ((SELECT id FROM wp_users WHERE user_email = "admin@example.com"), "rich_editing", "true");'
 
     return 0
 }
