944d5b2ad048e77eb16d31bc10e18e976968bb20
Daniel Bachler <daniel@danielbachler.de>
2023-11-07 19:48:41 +0100

    :hammer: simplify regex solution for urls with trailing period etc

--- packages/@ourworldindata/components/src/markdown/remarkPlainLinks.ts
+++ packages/@ourworldindata/components/src/markdown/remarkPlainLinks.ts
@@ -9,47 +9,26 @@ import findAndReplace from "mdast-util-find-and-replace"
 //   The path, query string and fragment: A forward slash followed by any word or numeric character (unicode classes so umlauts like ö match
 //       as well as any of the following: .+?:%&=~#) zero or more times. Note that we exclude space even though that is valid in a URL but it tends
 //       to make the match too greedy.
-//   Note that this URL will tend to match too much at the end - if there is a URL at the end of a sentence, the period will
-//   be matched by the regex. We accept this and handle it below when we create the link.
-//   This URL could be made to not match trailing characters like .?: but AFAIK this requires a negative lookbehind which are not supported in
-//   Safari before 16.4 which came out in Spring 2023
+//       We match the same subgroup [\p{L}\p{N}_\-.\+/?:%&=~#] twice, once with a * and then excactly once but without interpuncation characters .?:
+//       This is to make sure that we don't match trailing punctuation as part of the URL ("This is an http://example.com." - note that the leading
+//       period should not be part of the URL)
+//       Finally, the very last part is a lone forward slash which would not be matched by the previous subgroup.
 export const urlRegex =
-    /https?:\/\/([\w-]+\.)+[\w-]+(\/[\p{L}\p{N}_\-.\+/?:%&=~#]*)?/gu
+    /https?:\/\/([\w-]+\.)+[\w-]+((\/[\p{L}\p{N}_\-.\+/?:%&=~#]*[\p{L}\p{N}_\-\+/%&=~#])|\/)?/gu
 
 export function remarkPlainLinks() {
     const turnIntoLink = (value: any, _match: string) => {
-        // Split off any trailing .?: characters and add them back after the link
-        const isSentenceInterpunctation = (c: string) =>
-            c === "." || c === "?" || c === ":"
-        let i
-        for (
-            i = value.length - 1;
-            i >= 0 && isSentenceInterpunctation(value[i]);
-            i--
-        ) {}
-        const link = value.slice(0, i + 1)
-        const rest = value.slice(i + 1)
-        const restText =
-            rest.length > 0
-                ? [
-                      {
-                          type: "text",
-                          value: rest,
-                      },
-                  ]
-                : []
         return [
             {
                 type: "link",
-                url: link,
+                url: value,
                 children: [
                     {
                         type: "text",
-                        value: link,
+                        value: value,
                     },
                 ],
             },
-            ...restText,
         ]
     }
     return (tree: any) => {
