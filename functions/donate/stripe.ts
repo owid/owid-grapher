6180e869a68bd3f8f7005d11f36b51f8f982697b
Matthieu <matthieu@ourworldindata.org>
2023-12-12 17:10:42 +0000

    💬 (donate) update custom text on stripe checkout

--- functions/donate/stripe.ts
+++ functions/donate/stripe.ts
@@ -69,11 +69,11 @@ export async function createCheckoutSession(
 
     const messageInterval =
         interval === "monthly"
-            ? "You will be charged monthly and can cancel any time by writing us at donate@ourworldindata.org."
+            ? "You will be charged monthly and can cancel any time by writing to us at donate@ourworldindata.org."
             : "You will only be charged once."
     const message = showOnList
-        ? `You chose for your donation to be publicly attributed to "${metadata.name}. It will appear on our list of donors next time we update it. The donation amount will not be disclosed. ${messageInterval}`
-        : `You chose to remain anonymous, your name won't be shown on our list of donors. ${messageInterval}`
+        ? `You chose for your donation to be publicly listed as "${metadata.name}. Your name will appear on our list of donors next time we update it. The donation amount will not be disclosed. ${messageInterval}`
+        : `You chose to remain anonymous, your name won't be shown on our list of supporters. ${messageInterval}`
 
     if (interval === "monthly") {
         options.mode = "subscription"
