f6a1b571f97f3b8d5c1b8b0180dd490d6306a902
Matthieu <matthieu@ourworldindata.org>
2023-12-14 07:43:50 +0000

    🚚 (donate) move donate form

--- site/DonateForm.tsx
+++ site/DonateForm.tsx
@@ -9,7 +9,7 @@ import {
     DONATE_API_URL,
     BAKED_BASE_URL,
     RECAPTCHA_SITE_KEY,
-} from "../../settings/clientSettings.js"
+} from "../settings/clientSettings.js"
 import {
     Tippy,
     stringifyUnknownError,
