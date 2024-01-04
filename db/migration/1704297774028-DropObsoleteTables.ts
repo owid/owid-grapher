36e39c5fe5983ba5aead999d97414d4fb3e9be38
Daniel Bachler <daniel@danielbachler.de>
2024-01-04 13:44:06 +0100

    🔨 remove obsolete tables and country standardizer

---
+++ db/migration/1704297774028-DropObsoleteTables.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class DropObsoleteTables1704297774028 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // drop tables country_name_tool_continent, country_name_tool_countrydata, country_name_tool_countryname
+        await queryRunner.query(
+            `DROP TABLE IF EXISTS country_name_tool_continent`
+        )
+        await queryRunner.query(
+            `DROP TABLE IF EXISTS country_name_tool_countrydata`
+        )
+        await queryRunner.query(
+            `DROP TABLE IF EXISTS country_name_tool_countryname`
+        )
+        // drop importer_importhistory
+        await queryRunner.query(`DROP TABLE IF EXISTS importer_importhistory`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
