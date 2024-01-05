7b34197793bdc38e62732918c507bbd179df8862
Matthieu <matthieu@ourworldindata.org>
2024-01-05 10:33:20 +0000

    🔧 (sentry) add support for environment

--- functions/donation/_middleware.ts
+++ functions/donation/_middleware.ts
@@ -3,16 +3,21 @@ import { CaptureConsole } from "@sentry/integrations"
 
 interface SentryEnvVars {
     SENTRY_DSN: string
+    ENV: "production" | "development"
 }
 
 const hasSentryEnvVars = (env: any): env is SentryEnvVars => {
-    return !!env.SENTRY_DSN
+    return (
+        !!env.SENTRY_DSN &&
+        !!env.ENV &&
+        ["production", "development"].includes(env.ENV)
+    )
 }
 
 export const onRequest: PagesFunction = (context) => {
     if (!hasSentryEnvVars(context.env)) {
         console.error(
-            "Missing SENTRY_DSN environment variable. Continuing without error logging..."
+            "Missing Sentry environment variables. Continuing without error logging..."
         )
         // Gracefully continue if Sentry is not configured.
         return context.next()
@@ -21,5 +26,6 @@ export const onRequest: PagesFunction = (context) => {
     return sentryPlugin({
         dsn: context.env.SENTRY_DSN,
         integrations: [new CaptureConsole()],
+        environment: context.env.ENV,
     })(context)
 }
