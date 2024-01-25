e7ea05657f95f80db625dcd53c7984dda1663bf7
Marigold <mojmir.vinkler@gmail.com>
2024-01-25 09:13:29 +0100

    add correct urls to ETL services

--- settings/clientSettings.ts
+++ settings/clientSettings.ts
@@ -75,11 +75,11 @@ export const IMAGE_HOSTING_BUCKET_SUBFOLDER_PATH: string =
 
 // Link to production wizard.  You need Tailscale to access it in production.
 export const ETL_WIZARD_URL: string =
-    process.env.ETL_WIZARD_URL ?? "http://localhost:8053/"
+    process.env.ETL_WIZARD_URL ?? `${ADMIN_BASE_URL}:8053/`
 
 // Production ETL API runs on http://etl-prod-2:8083/v1 (you need Tailscale to access it)
 export const ETL_API_URL: string =
-    process.env.ETL_API_URL ?? `${ADMIN_BASE_URL}:8000/api/v1`
+    process.env.ETL_API_URL ?? `${ADMIN_BASE_URL}:8081/api/v1`
 
 export const GDOCS_DETAILS_ON_DEMAND_ID: string =
     process.env.GDOCS_DETAILS_ON_DEMAND_ID ?? ""
