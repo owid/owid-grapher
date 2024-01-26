4527da713b58cfce123022d485ab3332c9131052
Matthieu Bergel <matthieu@ourworldindata.org>
2024-01-26 20:33:29 +0000

    (wp) fix pageOverrides tests

--- baker/pageOverrides.test.ts
+++ baker/pageOverrides.test.ts
@@ -1,24 +1,43 @@
 import {
-    FullPost,
+    DbEnrichedPost,
     WP_PostType,
     extractFormattingOptions,
 } from "@ourworldindata/utils"
+import * as Post from "../db/model/Post.js"
 import * as pageOverrides from "./pageOverrides.js"
 
-const mockCreatePost = (slug: string): FullPost => {
+import { jest } from "@jest/globals"
+
+const mockCreatePost = (slug: string): DbEnrichedPost => {
     return {
         id: 123,
         type: WP_PostType.Post,
         slug: slug,
         title: "",
-        date: new Date(0),
-        modifiedDate: new Date(0),
         authors: [],
         content: "",
+        status: "",
+        isListed: false,
+        published_at: new Date(0),
+        updated_at: new Date(0),
+        updated_at_in_wordpress: new Date(0),
+        gdocSuccessorId: "",
+        excerpt: "",
+        created_at_in_wordpress: new Date(0),
+        featured_image: "",
+        formattingOptions: null,
+        archieml: null,
+        archieml_update_statistics: null,
+        markdown: "",
     }
 }
 
 const forestLandingSlug = "forests-and-deforestation"
+
+const getPostEnrichedBySlug = jest.spyOn(Post, "getPostEnrichedBySlug")
+getPostEnrichedBySlug.mockImplementation((landingSlug) =>
+    Promise.resolve(mockCreatePost(landingSlug))
+)
 
 it("gets parent landing", async () => {
     const formattingOptions = extractFormattingOptions(
@@ -26,10 +45,7 @@ it("gets parent landing", async () => {
     )
 
     await expect(
-        pageOverrides.getLandingOnlyIfParent(
-            mockCreatePost("forest-area"),
-            formattingOptions
-        )
+        pageOverrides.getLandingOnlyIfParent("forest-area", formattingOptions)
     ).resolves.toEqual(mockCreatePost(forestLandingSlug))
 })
 
@@ -39,10 +55,7 @@ it("does not get parent landing (subnavId invalid)", async () => {
     )
 
     await expect(
-        pageOverrides.getLandingOnlyIfParent(
-            mockCreatePost("forest-area"),
-            formattingOptions
-        )
+        pageOverrides.getLandingOnlyIfParent("forest-area", formattingOptions)
     ).resolves.toEqual(undefined)
 })
 
@@ -53,7 +66,7 @@ it("does not get parent landing (post is already a landing)", async () => {
 
     await expect(
         pageOverrides.getLandingOnlyIfParent(
-            mockCreatePost(forestLandingSlug),
+            forestLandingSlug,
             formattingOptions
         )
     ).resolves.toEqual(undefined)
@@ -64,10 +77,11 @@ it("does not get parent landing and logs (landing post not found)", async () => 
         "<!-- formatting-options subnavId:forests subnavCurrentId:forest-area -->"
     )
 
+    getPostEnrichedBySlug.mockImplementationOnce(() =>
+        Promise.resolve(undefined)
+    )
+
     await expect(
-        pageOverrides.getLandingOnlyIfParent(
-            mockCreatePost("forest-area"),
-            formattingOptions
-        )
+        pageOverrides.getLandingOnlyIfParent("forest-area", formattingOptions)
     ).resolves.toEqual(undefined)
 })
