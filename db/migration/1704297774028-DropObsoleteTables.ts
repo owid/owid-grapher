009def8cdd0164be2aa8de49f282ef4381d2340f
Daniel Bachler <daniel@danielbachler.de>
2024-01-27 02:15:17 +0100

    🔨 fix order of tables to drop

--- db/migration/1704297774028-DropObsoleteTables.ts
+++ db/migration/1704297774028-DropObsoleteTables.ts
@@ -4,13 +4,13 @@ export class DropObsoleteTables1704297774028 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         // drop tables country_name_tool_continent, country_name_tool_countrydata, country_name_tool_countryname
         await queryRunner.query(
-            `DROP TABLE IF EXISTS country_name_tool_continent`
+            `DROP TABLE IF EXISTS country_name_tool_countryname`
         )
         await queryRunner.query(
             `DROP TABLE IF EXISTS country_name_tool_countrydata`
         )
         await queryRunner.query(
-            `DROP TABLE IF EXISTS country_name_tool_countryname`
+            `DROP TABLE IF EXISTS country_name_tool_continent`
         )
         // drop importer_importhistory
         await queryRunner.query(`DROP TABLE IF EXISTS importer_importhistory`)
