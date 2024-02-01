34d433f8ef9ee6b1b83bbe79f8f65b141efafec2
Ike Saunders <ikesau@protonmail.com>
2024-02-01 23:02:47 +0000

    ✨ update homepage searchbar heading copy

--- site/gdocs/components/HomepageSearch.tsx
+++ site/gdocs/components/HomepageSearch.tsx
@@ -12,12 +12,9 @@ export function HomepageSearch(props: { className?: string }) {
             : "Thousand of charts across 200 topics - All free: open access and open source"
     return (
         <div className={props.className}>
-            <h2 className="h2-bold span-cols-14 homepage-search__mobile-headline">
+            <h2 className="h1-semibold span-cols-6 col-start-5 span-md-cols-10 col-md-start-3 span-sm-cols-12 col-sm-start-2">
                 Research and data to make progress against the world’s largest
                 problems.
-            </h2>
-            <h2 className="h2-bold span-cols-14 homepage-search__search-heading">
-                Search Our World in Data
             </h2>
             <Autocomplete
                 placeholder="Try “COVID-19”, “GDP”, “Energy”, “CO2 emissions per capita”…"
