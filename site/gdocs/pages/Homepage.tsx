a8a539f09b38784a85246cd69966e3026b5c3231
Ike Saunders <ikesau@protonmail.com>
2024-02-08 22:44:23 +0000

    🎉 add homepage all topics section

--- site/gdocs/pages/Homepage.tsx
+++ site/gdocs/pages/Homepage.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faRss, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
+import { faRss } from "@fortawesome/free-solid-svg-icons"
 import {
     faTwitter,
     faFacebookSquare,
@@ -13,9 +13,156 @@ import {
 } from "../../NewsletterSubscription.js"
 import { ArticleBlocks } from "../components/ArticleBlocks.js"
 import { OwidGdocHomepageContent } from "@ourworldindata/types"
+import { SiteNavigationStatic } from "../../SiteNavigation.js"
 
 export interface HomepageProps {
     content: OwidGdocHomepageContent
+}
+
+const SocialSection = () => {
+    return (
+        <section
+            className="grid grid-cols-12-full-width span-cols-14"
+            id="subscribe"
+        >
+            <section className="homepage-social-ribbon span-cols-8 col-start-2">
+                <h2 className="h2-semibold">Subscribe to our newsletter</h2>
+                <div id="newsletter-subscription-root">
+                    {/* Hydrated in runSiteTools() */}
+                    <NewsletterSubscriptionForm
+                        context={NewsletterSubscriptionContext.Homepage}
+                    />
+                </div>
+            </section>
+            <section className="homepage-social-ribbon__social-media span-cols-4">
+                <h2 className="h2-semibold">Follow us</h2>
+                <ul className="homepage-social-ribbon__social-list">
+                    <li>
+                        <a
+                            href="https://twitter.com/ourworldindata"
+                            className="list-item"
+                            title="Twitter"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            data-track-note="homepage_follow_us"
+                        >
+                            <span className="icon">
+                                <FontAwesomeIcon icon={faTwitter} />
+                            </span>
+                            <span className="label">Twitter</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://facebook.com/ourworldindata"
+                            className="list-item"
+                            title="Facebook"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            data-track-note="homepage_follow_us"
+                        >
+                            <span className="icon">
+                                <FontAwesomeIcon icon={faFacebookSquare} />
+                            </span>
+                            <span className="label">Facebook</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.instagram.com/ourworldindata/"
+                            className="list-item"
+                            title="Instagram"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            data-track-note="homepage_follow_us"
+                        >
+                            <span className="icon">
+                                <FontAwesomeIcon icon={faInstagram} />
+                            </span>
+                            <span className="label">Instagram</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.threads.net/@ourworldindata"
+                            className="list-item"
+                            title="Threads"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            data-track-note="homepage_follow_us"
+                        >
+                            <span className="icon">
+                                <FontAwesomeIcon icon={faThreads} />
+                            </span>
+                            <span className="label">Threads</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/feed"
+                            className="list-item"
+                            title="RSS"
+                            target="_blank"
+                            data-track-note="homepage_follow_us"
+                        >
+                            <span className="icon">
+                                <FontAwesomeIcon icon={faRss} />
+                            </span>
+                            <span className="label">RSS Feed</span>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </section>
+    )
+}
+
+const AllTopicsSection = () => {
+    const { categories } = SiteNavigationStatic
+    return (
+        <section
+            id="all-topics"
+            className="grid span-cols-12 col-start-2 homepage-topics"
+        >
+            <h2 className="h2-bold span-cols-12">All our topics</h2>
+            <p className="body-2-regular span-cols-12">
+                All our data, research, and writing — topic by topic.
+            </p>
+            {categories.map((category) => (
+                <section
+                    key={category.slug}
+                    className="homepage-topic span-cols-12"
+                >
+                    <h2 className="homepage-topic__topic-name h3-bold">
+                        {category.name}
+                    </h2>
+                    <ul className="homepage-topic__topic-list display-3-regular">
+                        {category?.subcategories?.map(
+                            // We have to flatten the list because otherwise the text won't wrap the way we need
+                            ({ slug, name, entries }) => (
+                                <>
+                                    <li
+                                        className="homepage-topic__subtopic"
+                                        key={slug}
+                                    >
+                                        {name}:
+                                    </li>
+                                    {entries.map(({ slug, title }) => (
+                                        <li
+                                            className="homepage-topic__subtopic-entry"
+                                            key={slug}
+                                        >
+                                            <a href={`${slug}`}>{title}</a>
+                                        </li>
+                                    ))}
+                                </>
+                            )
+                        )}
+                    </ul>
+                </section>
+            ))}
+        </section>
+    )
 }
 
 export const Homepage = (props: HomepageProps): JSX.Element => {
@@ -24,99 +171,8 @@ export const Homepage = (props: HomepageProps): JSX.Element => {
     return (
         <div className="grid grid-cols-12-full-width">
             <ArticleBlocks blocks={content.body} />
-            <section
-                className="grid grid-cols-12-full-width span-cols-14"
-                id="subscribe"
-            >
-                <section className="homepage-social-ribbon span-cols-8 col-start-2">
-                    <h2 className="h2-semibold">Subscribe to our newsletter</h2>
-                    <div id="newsletter-subscription-root">
-                        {/* Hydrated in runSiteTools() */}
-                        <NewsletterSubscriptionForm
-                            context={NewsletterSubscriptionContext.Homepage}
-                        />
-                    </div>
-                </section>
-                <section className="homepage-social-ribbon__social-media span-cols-4">
-                    <h2 className="h2-semibold">Follow us</h2>
-                    <ul className="homepage-social-ribbon__social-list">
-                        <li>
-                            <a
-                                href="https://twitter.com/ourworldindata"
-                                className="list-item"
-                                title="Twitter"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                data-track-note="homepage_follow_us"
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={faTwitter} />
-                                </span>
-                                <span className="label">Twitter</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://facebook.com/ourworldindata"
-                                className="list-item"
-                                title="Facebook"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                data-track-note="homepage_follow_us"
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={faFacebookSquare} />
-                                </span>
-                                <span className="label">Facebook</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://www.instagram.com/ourworldindata/"
-                                className="list-item"
-                                title="Instagram"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                data-track-note="homepage_follow_us"
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={faInstagram} />
-                                </span>
-                                <span className="label">Instagram</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://www.threads.net/@ourworldindata"
-                                className="list-item"
-                                title="Threads"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                data-track-note="homepage_follow_us"
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={faThreads} />
-                                </span>
-                                <span className="label">Threads</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/feed"
-                                className="list-item"
-                                title="RSS"
-                                target="_blank"
-                                data-track-note="homepage_follow_us"
-                            >
-                                <span className="icon">
-                                    <FontAwesomeIcon icon={faRss} />
-                                </span>
-                                <span className="label">RSS Feed</span>
-                            </a>
-                        </li>
-                    </ul>
-                </section>
-            </section>
+            <SocialSection />
+            <AllTopicsSection />
         </div>
     )
 }
