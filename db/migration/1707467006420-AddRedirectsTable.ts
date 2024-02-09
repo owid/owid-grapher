2bf6eebb785fd1eff503a666313cd88af80077d5
Matthieu Bergel <matthieu@ourworldindata.org>
2024-02-09 08:36:21 +0000

    wip: add redirects table migration

---
+++ db/migration/1707467006420-AddRedirectsTable.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddRedirectsTable1707467006420 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE redirects (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                source TEXT NOT NULL,
+                target TEXT NOT NULL,
+                code ENUM('301', '302') DEFAULT '301',
+                createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            );
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE redirects`)
+    }
+}
