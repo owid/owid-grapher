2f347c5344bd843beebeacf149756b57dbd6ae3a
Marigold <mojmir.vinkler@gmail.com>
2024-02-15 09:25:36 +0100

    :bug: continue correctly on 304 from image fetching

--- baker/GDriveImagesBaker.tsx
+++ baker/GDriveImagesBaker.tsx
@@ -53,13 +53,21 @@ export const bakeDriveImages = async (bakedSiteDir: string) => {
                             "If-None-Match": existingEtag,
                         },
                     }).then((response) => {
-                        // If the response status is 404, throw an error to trigger retry
-                        if (!response.ok) {
-                            throw new Error(
-                                `Fetching image failed: ${response.status} ${response.statusText} ${response.url}`
+                        if (response.status === 304) {
+                            // Image has not been modified, skip without logging
+                            return response
+                        } else if (response.ok) {
+                            // Log fetched images if it was success but wasn't 304
+                            console.log(
+                                `Fetching image ${image.filename} from ${remoteFilePath} using etag ${existingEtag}...`
                             )
+                            return response
+                        } else {
+                            // If the response status is 404, throw an error to trigger retry
+                            const msg = `Fetching image failed: ${response.status} ${response.statusText} ${response.url}`
+                            console.log(msg)
+                            throw new Error(msg)
                         }
-                        return response
                     }),
                 { maxRetries: 5, exponentialBackoff: true, initialDelay: 1000 }
             )
@@ -67,11 +75,6 @@ export const bakeDriveImages = async (bakedSiteDir: string) => {
             // Image has not been modified, skip
             if (response.status === 304) {
                 return
-            } else {
-                // Log fetched images, this should be pretty rare as most images should return 304
-                console.log(
-                    `Fetching image ${image.filename} from ${remoteFilePath} using etag ${existingEtag}...`
-                )
             }
 
             let buffer = Buffer.from(await response.arrayBuffer())
