7f2524503528c9d34f5e0a9d0900a3f4bdd24818
sophiamersmann <sophia.mersmann1@gmail.com>
2024-02-23 13:19:58 +0000

    🐝 restore static chart baker behaviour

--- baker/GrapherBaker.tsx
+++ baker/GrapherBaker.tsx
@@ -394,8 +394,7 @@ const bakeGrapherPageAndVariablesPngAndSVGIfChanged = async (
     await fs.mkdirp(`${bakedSiteDir}/grapher/exports/`)
     const svgPath = `${bakedSiteDir}/grapher/exports/${grapher.slug}.svg`
     const pngPath = `${bakedSiteDir}/grapher/exports/${grapher.slug}.png`
-    // if (!isSameVersion || !fs.existsSync(svgPath) || !fs.existsSync(pngPath)) {
-    if (true) {
+    if (!isSameVersion || !fs.existsSync(svgPath) || !fs.existsSync(pngPath)) {
         const loadDataMetadataPromises: Promise<OwidVariableDataMetadataDimensions>[] =
             variableIds.map(getVariableData)
         const variableDataMetadata = await Promise.all(loadDataMetadataPromises)
