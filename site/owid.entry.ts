08fbb3e8513258912c1d65b768511de9ce79bf22
Matthieu Bergel <matthieu@ourworldindata.org>
2024-03-02 18:12:17 +0000

    fix(vite): reorder dev css to match production css

--- site/owid.entry.ts
+++ site/owid.entry.ts
@@ -1,5 +1,5 @@
-import "./owid.scss"
 import "@ourworldindata/grapher/src/core/grapher.scss"
+import "./owid.scss"
 // From https://fontawesome.com/how-to-use/on-the-web/other-topics/server-side-rendering:
 // "If the CSS is missing when this icon displays in the browser it will flash
 // from a very large icon down to a properly sized one a moment later."
