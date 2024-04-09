528583c57b060c60126886f92c18600ce94ebb20
Marcel Gerber <mgerber59@gmail.com>
2024-04-09 10:33:04 +0200

    refactor: move LogCompareModal into own component

--- adminSiteClient/EditorHistoryTab.tsx
+++ adminSiteClient/EditorHistoryTab.tsx
@@ -8,6 +8,50 @@ import { dump } from "js-yaml"
 import { notification, Modal } from "antd"
 import ReactDiffViewer, { DiffMethod } from "react-diff-viewer"
 
+function LogCompareModal({
+    log,
+    previousLog,
+    isOpen,
+    onClose,
+}: {
+    log: Log
+    previousLog: Log
+    isOpen: boolean
+    onClose: () => void
+}) {
+    const titleForLog = (log: Log) => {
+        const user = log.userName || log.userId.toString()
+        return <Timeago time={log.createdAt} by={user} />
+    }
+
+    return (
+        <Modal
+            open={isOpen}
+            centered
+            width="80vw"
+            onOk={onClose}
+            onCancel={onClose}
+            cancelButtonProps={{ style: { display: "none" } }}
+        >
+            <div style={{ maxHeight: "50vh", overflowY: "auto" }}>
+                <ReactDiffViewer
+                    newValue={JSON.stringify(log.config, null, 2)}
+                    oldValue={JSON.stringify(previousLog.config, null, 2)}
+                    leftTitle={titleForLog(previousLog)}
+                    rightTitle={titleForLog(log)}
+                    compareMethod={DiffMethod.WORDS_WITH_SPACE}
+                    styles={{
+                        contentText: {
+                            wordBreak: "break-word",
+                        },
+                    }}
+                    extraLinesSurroundingDiff={2}
+                />
+            </div>
+        </Modal>
+    )
+}
+
 @observer
 class LogRenderer extends React.Component<{
     log: Log
@@ -16,7 +60,8 @@ class LogRenderer extends React.Component<{
 }> {
     @observable isCompareModalOpen = false
 
-    titleForLog(log: Log) {
+    @computed get title() {
+        const { log } = this.props
         const user = log.userName || log.userId.toString()
         return (
             <>
@@ -25,55 +70,27 @@ class LogRenderer extends React.Component<{
         )
     }
 
-    @computed get title() {
-        return this.titleForLog(this.props.log)
-    }
-
-    @computed get compareModal() {
-        const { log, previousLog } = this.props
-
-        return (
-            <Modal
-                open={this.isCompareModalOpen}
-                centered
-                width="80vw"
-                onOk={() => (this.isCompareModalOpen = false)}
-                onCancel={() => (this.isCompareModalOpen = false)}
-                cancelButtonProps={{ style: { display: "none" } }}
-            >
-                <div style={{ overflowY: "auto", maxHeight: "50vh" }}>
-                    <ReactDiffViewer
-                        newValue={JSON.stringify(log.config, null, 2)}
-                        oldValue={JSON.stringify(previousLog?.config, null, 2)}
-                        leftTitle={
-                            previousLog ? this.titleForLog(previousLog) : ""
-                        }
-                        rightTitle={this.title}
-                        compareMethod={DiffMethod.WORDS}
-                        styles={{
-                            contentText: {
-                                wordBreak: "break-word",
-                            },
-                        }}
-                    />
-                </div>
-            </Modal>
-        )
-    }
-
     render() {
         const { log } = this.props
         const { title } = this
+        const hasCompareButton = !!this.props.previousLog
 
         return (
             <li
                 className="list-group-item d-flex justify-content-between"
                 style={{ alignItems: "center" }}
             >
-                {this.compareModal}
+                {hasCompareButton && (
+                    <LogCompareModal
+                        log={log}
+                        previousLog={this.props.previousLog}
+                        isOpen={this.isCompareModalOpen}
+                        onClose={() => (this.isCompareModalOpen = false)}
+                    />
+                )}
                 <span>{title}</span>
                 <div className="d-flex" style={{ gap: 6 }}>
-                    {!!this.props.previousLog && (
+                    {hasCompareButton && (
                         <button
                             className="btn btn-secondary"
                             onClick={() => (this.isCompareModalOpen = true)}
