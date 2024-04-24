f4c5508f413531f76dfa630ec10d78c45dd2f033
Daniel Bachler <daniel@danielbachler.de>
2024-04-24 12:01:49 +0200

    ✨ add button to show/hide the preview

--- explorerAdminClient/ExplorerCreatePage.tsx
+++ explorerAdminClient/ExplorerCreatePage.tsx
@@ -49,6 +49,8 @@ export class ExplorerCreatePage extends React.Component<{
 }> {
     disposers: Array<() => void> = []
 
+    @observable showPreview: boolean = true
+
     @computed private get manager() {
         return this.props.manager ?? {}
     }
@@ -212,6 +214,10 @@ export class ExplorerCreatePage extends React.Component<{
         else if (this.isModified) void this.save()
     }
 
+    @action.bound private onShowPreviewChanged() {
+        this.showPreview = !this.showPreview
+    }
+
     render() {
         if (!this.isReady) return <LoadingIndicator />
 
@@ -265,6 +271,21 @@ export class ExplorerCreatePage extends React.Component<{
             ? "Are you sure you want to leave? You have unsaved changes."
             : "" // todo: provide an explanation of how many cells are modified.
 
+        const showPreviewCheckbox = (
+            <div className="form-check">
+                <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="showPreview"
+                    checked={this.showPreview}
+                    onChange={() => this.onShowPreviewChanged()}
+                ></input>
+                <label className="form-check-label" htmlFor="showPreview">
+                    Show Preview
+                </label>
+            </div>
+        )
+
         return (
             <>
                 <Prompt when={isModified} message={modifiedMessage} />
@@ -281,6 +302,7 @@ export class ExplorerCreatePage extends React.Component<{
                                 isNewFile={isNewFile}
                             />
                         </div>
+                        {showPreviewCheckbox}
                         <div style={{ textAlign: "right" }}>{buttons}</div>
                     </div>
                     <HotEditor
@@ -288,7 +310,9 @@ export class ExplorerCreatePage extends React.Component<{
                         program={program}
                         programOnDisk={this.programOnDisk}
                     />
-                    <PictureInPicture previewLink={previewLink} />
+                    {this.showPreview && (
+                        <PictureInPicture previewLink={previewLink} />
+                    )}
                     <a className="PreviewLink" href={previewLink}>
                         Visit preview
                     </a>
