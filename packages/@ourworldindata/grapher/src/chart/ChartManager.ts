a5e4a33c3c5d8f5612302b3891a9b9d1ffe7330b
sophiamersmann <sophia.mersmann1@gmail.com>
2024-05-07 14:55:35 +0000

    🐝 fix rebase

--- packages/@ourworldindata/grapher/src/chart/ChartManager.ts
+++ packages/@ourworldindata/grapher/src/chart/ChartManager.ts
@@ -34,15 +34,7 @@ export interface ChartManager {
     table: OwidTable
     transformedTable?: OwidTable
 
-<<<<<<< HEAD
-||||||| parent of 4ee28c795 (Review markups)
-    isSelectingData?: boolean
-    startSelectingWhenLineClicked?: boolean // used by lineLabels
-=======
     isGlobe?: boolean
-    isSelectingData?: boolean
-    startSelectingWhenLineClicked?: boolean // used by lineLabels
->>>>>>> 4ee28c795 (Review markups)
     isExportingToSvgOrPng?: boolean
     isRelativeMode?: boolean
     comparisonLines?: ComparisonLineConfig[]
