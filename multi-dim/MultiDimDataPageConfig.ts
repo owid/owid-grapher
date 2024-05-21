297125646e997306d4863074866801edfd55f65e
Marcel Gerber <mgerber59@gmail.com>
2024-05-21 17:16:34 +0200

    feat(multi-dim): config class

---
+++ multi-dim/MultiDimDataPageConfig.ts
@@ -0,0 +1,76 @@
+import fs from "fs-extra"
+import YAML from "yaml"
+import { MultiDimDataPageConfigType, View } from "./MultiDimDataPageTypes.js"
+import { DimensionProperty, keyBy } from "@ourworldindata/utils"
+
+export class MultiDimDataPageConfig {
+    private constructor(public readonly config: MultiDimDataPageConfigType) {}
+
+    static fromYaml(yaml: string) {
+        return new MultiDimDataPageConfig(YAML.parse(yaml))
+    }
+
+    static fromObject(obj: MultiDimDataPageConfigType) {
+        return new MultiDimDataPageConfig(obj)
+    }
+
+    get dimensions() {
+        const dimensionsWithChoicesBySlug = this.config.dimensions.map(
+            (dimension) => ({
+                ...dimension,
+                choices: keyBy(dimension.choices, "slug"),
+            })
+        )
+        return keyBy(dimensionsWithChoicesBySlug, "slug")
+    }
+
+    filterViewsByDimensions(dimensions: Record<string, string>): View[] {
+        return this.config.views.filter((view) => {
+            for (const [dimensionSlug, choiceSlug] of Object.entries(
+                dimensions
+            )) {
+                if (view.dimensions[dimensionSlug] !== choiceSlug) return false
+            }
+            return true
+        })
+    }
+
+    findViewByDimensions(dimensions: Record<string, string>) {
+        const matchingViews = this.filterViewsByDimensions(dimensions)
+        if (matchingViews.length === 0) return undefined
+        if (matchingViews.length > 1) {
+            throw new Error(
+                `Multiple views found for dimensions ${JSON.stringify(
+                    dimensions
+                )}`
+            )
+        }
+        return matchingViews[0]
+    }
+
+    static transformIndicatorPathObj(
+        indicatorPathObj: Record<string, DimensionProperty>
+    ): Record<DimensionProperty, string[]> {
+        const emptyObj: Record<DimensionProperty, string[]> = {
+            x: [],
+            y: [],
+            color: [],
+            size: [],
+            table: [],
+        }
+        return Object.entries(indicatorPathObj).reduce(
+            (result, [indicatorPath, dimension]) => {
+                result[dimension].push(indicatorPath)
+                return result
+            },
+            emptyObj
+        )
+    }
+}
+
+if (require.main === module) {
+    const CONFIG_PLAIN = fs.readFileSync(__dirname + "/config.yml", "utf-8")
+
+    const config = MultiDimDataPageConfig.fromYaml(CONFIG_PLAIN)
+    console.log(config.dimensions)
+}
