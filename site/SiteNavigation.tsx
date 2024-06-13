108a8cb8488796355e2ff6277636c2f4985f0485
Edouard Mathieu <edmat@pm.me>
2024-06-13 16:37:18 +0200

    🎉 add neglected tropical diseases page to menu

--- site/SiteNavigation.tsx
+++ site/SiteNavigation.tsx
@@ -422,6 +422,10 @@ export const SiteNavigationStatic: { categories: CategoryWithEntries[] } = {
                             slug: "smallpox",
                             title: "Smallpox",
                         },
+                        {
+                            slug: "neglected-tropical-diseases",
+                            title: "Neglected Tropical Diseases",
+                        },
                     ],
                 },
                 {
