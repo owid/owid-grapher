c55673dbaed7ba1898c0b37967dceac141e1fce1
Marcel Gerber <mgerber59@gmail.com>
2024-08-05 18:39:28 +0100

    feat(flags): script to update flags from `flag-icons`

---
+++ devTools/flagUpdater/update.ts
@@ -0,0 +1,61 @@
+import { countries, Country } from "@ourworldindata/utils"
+
+import path from "path"
+import fs from "fs-extra"
+import { glob } from "glob"
+import findProjectBaseDir from "../../settings/findBaseDir.ts"
+
+const BASE_DIR = findProjectBaseDir(__dirname)
+if (!BASE_DIR) throw new Error("Could not find project base directory")
+const FLAG_BASE_PATH = path.join(BASE_DIR, "node_modules/flag-icons/flags/4x3")
+const FLAG_TARGET_DIR = path.join(BASE_DIR, "public/images/flags")
+
+const main = async () => {
+    const skippedBecauseMissingShortCode: Country[] = []
+    const failedBecauseNoFlag: Country[] = []
+    let successfulCount: number = 0
+
+    for (const f of await glob(`${FLAG_TARGET_DIR}/*.svg`)) {
+        await fs.remove(f)
+    }
+
+    await fs.ensureDir(FLAG_TARGET_DIR)
+
+    for (const country of countries) {
+        if (!country.shortCode) {
+            skippedBecauseMissingShortCode.push(country)
+            continue
+        }
+
+        const flagPath = path.join(
+            FLAG_BASE_PATH,
+            `${country.shortCode.toLowerCase()}.svg`
+        )
+        const exists = await fs.pathExists(flagPath)
+        if (!exists) {
+            failedBecauseNoFlag.push(country)
+            continue
+        }
+
+        const targetPath = path.join(FLAG_TARGET_DIR, `${country.code}.svg`)
+        await fs.copy(flagPath, targetPath)
+        successfulCount++
+    }
+
+    console.log(`Successfully copied ${successfulCount} flags.`)
+
+    if (skippedBecauseMissingShortCode.length > 0) {
+        console.log(
+            `Skipped ${skippedBecauseMissingShortCode.length} countries because they had no short code:`,
+            skippedBecauseMissingShortCode.map((c) => c.name)
+        )
+    }
+    if (failedBecauseNoFlag.length > 0) {
+        console.log(
+            `Failed to copy flags for ${failedBecauseNoFlag.length} countries because the flag was missing:`,
+            failedBecauseNoFlag.map((c) => c.name)
+        )
+    }
+}
+
+main()
