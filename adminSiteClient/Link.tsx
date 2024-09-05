f7a9616e994755a2fb93b9815cc2b9216f3e76db
Martin Račák <martin.racak@ourworldindata.org>
2024-09-05 17:51:11 +0200

    Add active state to admin nav

--- adminSiteClient/Link.tsx
+++ adminSiteClient/Link.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link as ReactLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { AdminAppContext, AdminAppContextType } from "./AdminAppContext.js"
 
 interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
@@ -15,6 +15,6 @@ export class Link extends React.Component<LinkProps> {
     render() {
         const { native, to, ...rest } = this.props
         if (native) return <a href={this.context.admin.url(to)} {...rest} />
-        else return <ReactLink to={to} {...rest} />
+        else return <NavLink to={to} {...rest} />
     }
 }
