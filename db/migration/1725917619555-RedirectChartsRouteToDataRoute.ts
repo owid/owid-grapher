2b309fa2500405a854c818ce4179b315091f8517
Ike Saunders <ikesau@protonmail.com>
2024-09-09 21:40:10 +0000

    🎉 move /charts to /data

---
+++ db/migration/1725917619555-RedirectChartsRouteToDataRoute.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class RedirectChartsRouteToDataRoute1725917619555
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`-- sql
+            INSERT INTO redirects (source, target) VALUES ("/charts", "/data")
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`-- sql
+            DELETE FROM redirects WHERE source = "/charts"
+        `)
+    }
+}
