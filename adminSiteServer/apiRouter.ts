63621f9527b0fe72a9b7ed45cef83eca6e53d6f4
Daniel Bachler <daniel@danielbachler.de>
2024-09-09 13:37:50 +0200

    🐝 fix TS complaint with old configs and restore removed workflow comment

--- adminSiteServer/apiRouter.ts
+++ adminSiteServer/apiRouter.ts
@@ -436,7 +436,7 @@ const saveGrapher = async (
         referencedVariablesMightChange = true,
     }: {
         user: DbPlainUser
-        newConfig: GrapherInterface
+        newConfig: GrapherInterface // Note that it is valid for newConfig to be of an older schema version which means that GrapherInterface as a type is slightly misleading
         existingConfig?: GrapherInterface
         // if undefined, keep inheritance as is.
         // if true or false, enable or disable inheritance
@@ -527,8 +527,10 @@ const saveGrapher = async (
         // TODO: find a more principled way to do schema upgrades
 
         // grapher-schema.004 -> grapher-schema.005 removed the obsolete hideLinesOutsideTolerance field
-        delete newConfig.hideLinesOutsideTolerance
-        newConfig.$schema = defaultGrapherConfig.$schema
+        const configForMigration = newConfig as any
+        delete configForMigration.hideLinesOutsideTolerance
+        configForMigration.$schema = defaultGrapherConfig.$schema
+        newConfig = configForMigration
     }
 
     // add the isPublished field if is missing
