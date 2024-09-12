e2796a308b64fc3d3d6cb76756f972de3b362e34
Ike Saunders <ikesau@protonmail.com>
2024-09-12 16:56:22 -0400

    🐛 remove old /data redirect in data-catalog migration

--- db/migration/1725917619555-RedirectChartsRouteToDataRoute.ts
+++ db/migration/1725917619555-RedirectChartsRouteToDataRoute.ts
@@ -5,11 +5,17 @@ export class RedirectChartsRouteToDataRoute1725917619555
 {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`-- sql
+            DELETE FROM redirects WHERE source = "/data"
+        `)
+        await queryRunner.query(`-- sql
             INSERT INTO redirects (source, target) VALUES ("/charts", "/data")
         `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`-- sql
+            INSERT INTO redirects (source, target) VALUES ("/data", "#entries")
+        `)
         await queryRunner.query(`-- sql
             DELETE FROM redirects WHERE source = "/charts"
         `)
