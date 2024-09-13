a8f9586bbb8f136795dc2035a4ae116966567f88
Martin Račák <rakyi@users.noreply.github.com>
2024-09-13 11:37:43 +0200

    Update 404 page to the new design (#3948)
    * Update 404 page to the new design
    
    * Use owid-link-90 for links

--- site/NotFoundPageMain.tsx
+++ site/NotFoundPageMain.tsx
@@ -1,14 +1,14 @@
+import * as React from "react"
+import { hydrate } from "react-dom"
+import NotFoundPageForm from "./NotFoundPageForm.js"
 import { SiteAnalytics } from "./SiteAnalytics.js"
 
 const analytics = new SiteAnalytics()
 
 export function runNotFoundPage() {
-    const query = window.location.pathname.split("/")
-    const searchInput = document.getElementById("search_q") as HTMLInputElement
-    if (searchInput && query.length)
-        searchInput.value = decodeURIComponent(query[query.length - 1]).replace(
-            /[\-_\+|]/g,
-            " "
-        )
     analytics.logPageNotFoundError(window.location.href)
+    hydrate(
+        <NotFoundPageForm />,
+        document.getElementById("not-found-page-form")
+    )
 }
