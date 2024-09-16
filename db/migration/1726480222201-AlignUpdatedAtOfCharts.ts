a813b4ab9f5ab5325f54d0d94826194b139c107c
Marigold <mojmir.vinkler@gmail.com>
2024-09-16 11:58:06 +0200

    wip

---
+++ db/migration/1726480222201-AlignUpdatedAtOfCharts.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AlignUpdatedAtOfCharts1726480222201 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            UPDATE chart_configs cf
+            INNER JOIN charts c ON c.configId = cf.id
+            SET cf.updatedAt = LEAST(cf.updatedAt, c.updatedAt)
+            WHERE c.updatedAt IS NOT NULL;
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        throw new Error(
+            "Cannot automatically revert migration 'AlignUpdatedAtOfCharts1726480222201'"
+        )
+    }
+}
