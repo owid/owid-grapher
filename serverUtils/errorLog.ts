d1455cfda1dc80236096c10f0a6de9e28b51f879
Ike Saunders <ikesau@protonmail.com>
2024-10-09 13:42:24 -0400

    🐛 disaggregate site-baker bugsnag errors

--- serverUtils/errorLog.ts
+++ serverUtils/errorLog.ts
@@ -1,11 +1,19 @@
 import Bugsnag from "@bugsnag/js"
 
-export const logErrorAndMaybeSendToBugsnag = async (err: any, req?: any) => {
+export const logErrorAndMaybeSendToBugsnag = async (
+    err: any,
+    req?: any,
+    // Sometimes Bugsnag's default grouping algorithm gets it wrong (e.g. all SiteBaker errors get grouped together).
+    // Set a hash here to ensure that errors with the same hash will be grouped together / excluded from group they would otherwise be in.
+    groupingHash?: string
+) => {
     console.error(err)
     if (req) {
         req.bugsnag.notify(err)
     } else {
-        Bugsnag.notify(err)
+        Bugsnag.notify(err, (event) => {
+            if (groupingHash) event.groupingHash = groupingHash
+        })
     }
 }
 
