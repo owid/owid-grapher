3138296e6c789c71f8310106258577b867db6c2e
sophiamersmann <sophia.mersmann1@gmail.com>
2024-11-27 12:06:48 +0100

    ✨ (slope) add Change in to title in relative mode

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -1626,7 +1626,7 @@ export class Grapher
             !this.hideAnnotationFieldsInTitle?.changeInPrefix
         return (
             !this.forceHideAnnotationFieldsInTitle?.changeInPrefix &&
-            this.isOnLineChartTab &&
+            (this.isOnLineChartTab || this.isOnSlopeChartTab) &&
             this.isRelativeMode &&
             showChangeInPrefix
         )
