5fe9bdf11b4c04f61d5554d8beda44cc72776025
Ike Saunders <ikesau@protonmail.com>
2024-11-29 16:23:04 -0500

    ✨ PR feedback

---
+++ db/migration/1732833869225-CloudflareImagesAuthors.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CloudflareImagesAuthors1732833869225
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {}
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
