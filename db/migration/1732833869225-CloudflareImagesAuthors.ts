4e2efc41b4c6b5f7668cec59080db572d752f563
Ike Saunders <ikesau@protonmail.com>
2024-12-01 17:16:50 -0500

    🔨 remove unused migration

--- db/migration/1732833869225-CloudflareImagesAuthors.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
-
-export class CloudflareImagesAuthors1732833869225
-    implements MigrationInterface
-{
-    public async up(queryRunner: QueryRunner): Promise<void> {}
-
-    public async down(queryRunner: QueryRunner): Promise<void> {}
-}
