adf6f17a598f127cd8961f76f08c4be3704503f0
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-01-03 10:36:21 +0100

    🔨 remove relative mode from discrete bar chart configs

---
+++ db/migration/1735896576517-RemoveRelativeModeFromDiscreteBarCharts.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class RemoveRelativeModeFromDiscreteBarCharts1735896576517 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            -- sql
+            update chart_configs
+            set
+                patch = json_remove(patch, '$.stackMode'),
+                full = json_remove(full, '$.stackMode')
+            where
+                chartType = 'DiscreteBar'
+                and full ->> '$.stackMode' = 'relative';
+        `)
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    public async down(): Promise<void> {}
+}
