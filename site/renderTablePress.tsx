848d72962e3c8a529a6269b4a69fb4e8706f56e3
Martin Račák <martin.racak@ourworldindata.org>
2025-01-06 16:10:41 +0100

    Remove Tablepress
    It should not be used anywhere.

--- site/renderTablePress.tsx
+++
@@ -1,6 +0,0 @@
-import { renderToStaticMarkup } from "react-dom/server"
-import Tablepress from "./Tablepress.js"
-
-export const renderTablePress = (table: string[][]) => {
-    return renderToStaticMarkup(<Tablepress data={table} />)
-}
