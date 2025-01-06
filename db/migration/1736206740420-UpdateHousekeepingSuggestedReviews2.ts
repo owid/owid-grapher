ebd2f9834736120fd8d1d71665b834d54880eb95
lucasrodes <lucasrodes@users.noreply.github.com>
2025-01-07 00:41:52 +0100

    migration

---
+++ db/migration/1736206740420-UpdateHousekeepingSuggestedReviews2.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UpdateHousekeepingSuggestedReviews21736206740420 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`-- sql
+            ALTER TABLE housekeeping_suggested_reviews DROP INDEX objectType;
+        `)
+    }
+
+    public async down(): Promise<void> {}
+
+}
