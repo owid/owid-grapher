a599e4f8bbaafffe15fe6e23119f19a7cd7d033f
Martin Račák <martin.racak@ourworldindata.org>
2025-01-08 10:39:39 +0100

    Stop adding Max as an extra author

--- site/clientFormatting.tsx
+++ site/clientFormatting.tsx
@@ -2,16 +2,11 @@ import { last } from "@ourworldindata/utils"
 
 export const formatAuthors = ({
     authors,
-    requireMax,
     forBibtex,
 }: {
     authors: string[]
-    requireMax?: boolean
     forBibtex?: boolean
 }) => {
-    if (requireMax && !authors.includes("Max Roser"))
-        authors = [...authors, "Max Roser"]
-
     let authorsText = authors.slice(0, -1).join(forBibtex ? " and " : ", ")
     if (authorsText.length === 0) authorsText = authors[0]
     else authorsText += ` and ${last(authors)}`
