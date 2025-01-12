a6f40169ef58a1e8079ddbfc82d79f9ab2e4ddaa
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-01-12 17:41:04 +0100

    remove interactions addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,7 @@ import { mergeConfig } from "vite"
 
 const config: StorybookConfig = {
     stories: ["../**/*.stories.@(js|jsx|ts|tsx|mdx)"],
-    addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+    addons: ["@storybook/addon-essentials"],
     framework: {
         name: "@storybook/react-vite",
         options: {},
