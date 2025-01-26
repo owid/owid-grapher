cbc57667f606834f53b51d031b95c790328927ec
Daniel Bachler <daniel@danielbachler.de>
2025-01-26 19:23:04 +0100

    🔨 Enable frameBounds width check again

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -1911,9 +1911,10 @@ export class GrapherState {
         //     isIFrame: this.isInIFrame,
         // })
         if (
-            //this.frameBounds.width > 940 &&
-            // don't use the panel if the grapher is embedded
-            (!this.isInIFrame && !this.isEmbeddedInAnOwidPage) ||
+            (this.frameBounds.width > 940 &&
+                // don't use the panel if the grapher is embedded
+                !this.isInIFrame &&
+                !this.isEmbeddedInAnOwidPage) ||
             // unless we're in full-screen mode
             this.isInFullScreenMode
         )
