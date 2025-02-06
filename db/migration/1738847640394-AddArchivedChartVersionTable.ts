5131231ea41de38f7daea395db0fcdc842397f62
Marcel Gerber <mgerber59@gmail.com>
2025-02-06 15:17:18 +0100

    feat(db): create table `archived_chart_versions`

---
+++ db/migration/1738847640394-AddArchivedChartVersionTable.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddArchivedChartVersionTable1738847640394
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`-- sql
+            CREATE TABLE archived_chart_versions (
+                id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                archivalTimestamp TIMESTAMP NOT NULL,
+                grapherId INT NOT NULL, -- theoretically this is a foreign key to charts(id), but charts may be deleted whereas archived_chart_versions should be kept forever
+                grapherSlug VARCHAR(255) NOT NULL,
+                hashOfInputs VARCHAR(255) NOT NULL,
+                manifest JSON,
+
+                UNIQUE INDEX grapherIdAndTimestampIdx (grapherId, archivalTimestamp), -- this kind of composite index is useful for querying the latest version of a chart
+                INDEX grapherSlugIdx (grapherSlug),
+                INDEX timestampIdx (archivalTimestamp)
+            )
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`-- sql
+            DROP TABLE archived_chart_versions
+        `)
+    }
+}
