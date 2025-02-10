1a031a787af9f4d282ccdd6225cae10f8985847d
Marcel Gerber <mgerber59@gmail.com>
2025-02-10 15:42:38 +0100

    feat: directly hash a stream

--- serverUtils/hash.ts
+++ serverUtils/hash.ts
@@ -1,16 +1,33 @@
-import crypto from "crypto"
+import crypto, { Hash } from "crypto"
+import { pipeline } from "node:stream/promises"
 
 const DEFAULT_HASH_LENGTH = 8
-export const hashBase36 = (
-    strOrBuffer: string | Buffer,
-    hashLength = DEFAULT_HASH_LENGTH
-) => {
-    const hashHex = crypto
-        .createHash("sha256")
-        .update(strOrBuffer)
-        .digest("hex")
+
+const _hashBase36 = (hash: Hash, hashLength = DEFAULT_HASH_LENGTH) => {
+    const hashHex = hash.digest("hex")
 
     // Convert hex to base36 to make it contain more information in fewer characters
     const hashBase36 = BigInt(`0x${hashHex}`).toString(36)
     return hashBase36.substring(0, hashLength)
+}
+
+export const hashBase36 = (
+    strOrBuffer: string | Buffer,
+    hashLength = DEFAULT_HASH_LENGTH
+) => {
+    const hash = crypto.createHash("sha256").update(strOrBuffer)
+
+    return _hashBase36(hash, hashLength)
+}
+
+export const hashBase36FromStream = async (
+    stream: NodeJS.ReadableStream,
+    hashLength = DEFAULT_HASH_LENGTH
+) => {
+    const hash = crypto.createHash("sha256")
+
+    await pipeline(stream, hash)
+    hash.end()
+
+    return _hashBase36(hash, hashLength)
 }
