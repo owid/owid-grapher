3889b242f47354b13e8c6d20fa77743b7b355905
Marcel Gerber <mgerber59@gmail.com>
2025-02-17 13:05:29 +0100

    chore(mysql): add combined indexes on `posts_gdocs`

--- db/migration/1739788202649-PostsGdocsIndexes.ts
+++ db/migration/1739788202649-PostsGdocsIndexes.ts
@@ -10,15 +10,27 @@ export class PostsGdocsIndexes1739788202649 implements MigrationInterface {
             ADD COLUMN type VARCHAR(255) GENERATED ALWAYS AS (JSON_UNQUOTE(JSON_EXTRACT(content, '$.type'))) STORED AFTER slug
         `)
 
+        // Add a combined index on `type` and `published`
+        // It can get used for just `type` queries, and often times we also filter by `published` in the same query
         await queryRunner.query(`-- sql
             ALTER TABLE posts_gdocs
-            ADD INDEX idx_posts_gdocs_published (published)
+            ADD INDEX idx_posts_gdocs_type_published (type, published)
+        `)
+
+        // Another common type of query is to filter by `published` and `publishedAt`
+        await queryRunner.query(`-- sql
+            ALTER TABLE posts_gdocs
+            ADD INDEX idx_posts_gdocs_published_publishedAt (published, publishedAt)
         `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`-- sql
-            DROP INDEX idx_posts_gdocs_published ON posts_gdocs
+            DROP INDEX idx_posts_gdocs_published_publishedAt ON posts_gdocs
+        `)
+
+        await queryRunner.query(`-- sql
+            DROP INDEX idx_posts_gdocs_type_published ON posts_gdocs
         `)
 
         await queryRunner.query(`-- sql
