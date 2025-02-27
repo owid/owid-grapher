f13c465e2954c2b42ffded293344ac38c010c2ff
Marcel Gerber <mgerber59@gmail.com>
2025-02-27 16:02:32 +0100

    cleanup: remove special handling of country profiles

--- site/dataPage.ts
+++ site/dataPage.ts
@@ -10,19 +10,7 @@ export function processRelatedResearch(
             const shared = intersection(research.tags, topicTags)
             return shared.length > 0
         })
-    } else {
-        relatedResearch = [...candidates]
-    }
-    for (const item of relatedResearch) {
-        // TODO: these are workarounds to not link to the (not really existing) template pages for energy or co2
-        // country profiles but instead to the topic page at the country selector.
-        if (item.url === "/co2-country-profile")
-            item.url =
-                "/co2-and-greenhouse-gas-emissions#co2-and-greenhouse-gas-emissions-country-profiles"
-        else if (item.url === "/energy-country-profile")
-            item.url = "/energy#country-profiles"
-        else if (item.url === "/coronavirus-country-profile")
-            item.url = "/coronavirus#coronavirus-country-profiles"
-    }
+    } else relatedResearch = [...candidates]
+
     return relatedResearch
 }
