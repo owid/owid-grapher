ac0fa7a9bacee61ae3aa0aa07a01e39657cbca72
Marcel Gerber <mgerber59@gmail.com>
2025-03-05 18:22:25 +0100

    enhance: don't init sentry when running vitest

--- serverUtils/errorLog.ts
+++ serverUtils/errorLog.ts
@@ -2,5 +2,6 @@ import * as Sentry from "@sentry/react"
 
 export async function logErrorAndMaybeCaptureInSentry(err: any) {
     console.error(err)
-    Sentry.captureException(err)
+
+    if (!process.env.VITEST) Sentry.captureException(err)
 }
