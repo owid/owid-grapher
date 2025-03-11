b2ad5f81ae32655851b8472cce6a57f2468324a6
Marcel Gerber <mgerber59@gmail.com>
2025-03-11 18:22:47 +0100

    refactor(env): allow loading two env files

--- settings/loadDotenv.ts
+++ settings/loadDotenv.ts
@@ -5,5 +5,15 @@ if (typeof __dirname !== "undefined") {
     // only run this code in node, not in the browser.
     // in the browser, process.env is already populated by vite.
     const baseDir = findBaseDir(__dirname)
-    if (baseDir) dotenv.config({ path: `${baseDir}/.env` })
+    if (!baseDir) throw new Error("could not locate base package.json")
+
+    // If a ENV_FILE is specified, load its variables first, and then load the default .env file
+    const additionalEnvFile = process.env.ENV_FILE || undefined
+    let dotenvPath: string[]
+    if (additionalEnvFile) {
+        dotenvPath = [`${baseDir}/${additionalEnvFile}`, `${baseDir}/.env`]
+    } else {
+        dotenvPath = [`${baseDir}/.env`]
+    }
+    dotenv.config({ path: dotenvPath })
 }
