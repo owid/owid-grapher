6530a74539006619c871dc3f18139d5acc349166
Marcel Gerber <mgerber59@gmail.com>
2025-03-12 13:55:45 +0100

    refactor: rename `ENV_FILE` -> `PRIMARY_ENV_FILE`

--- settings/loadDotenv.ts
+++ settings/loadDotenv.ts
@@ -7,11 +7,14 @@ if (typeof __dirname !== "undefined") {
     const baseDir = findBaseDir(__dirname)
     if (!baseDir) throw new Error("could not locate base package.json")
 
-    // If a ENV_FILE is specified, load its variables first, and then load the default .env file
-    const additionalEnvFile = process.env.ENV_FILE || undefined
+    // If a PRIMARY_ENV_FILE is specified, load its variables first, and then load the default .env file
+    const additionalEnvFile = process.env.PRIMARY_ENV_FILE || undefined
     let dotenvPath: string[]
     if (additionalEnvFile) {
         dotenvPath = [`${baseDir}/${additionalEnvFile}`, `${baseDir}/.env`]
+        console.log(
+            `Loading environment variables from ${dotenvPath.join(", ")}`
+        )
     } else {
         dotenvPath = [`${baseDir}/.env`]
     }
