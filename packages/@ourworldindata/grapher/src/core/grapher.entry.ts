fe0939ed3a79e2b1573ba56115084485d95bd7ff
Marcel Gerber <mgerber59@gmail.com>
2025-03-12 14:12:31 +0100

    chore(deps): remove mobx-formatters

--- packages/@ourworldindata/grapher/src/core/grapher.entry.ts
+++ packages/@ourworldindata/grapher/src/core/grapher.entry.ts
@@ -1,12 +1,5 @@
 import "./grapher.scss"
 
-// Enable mobx-formatters
-import * as Mobx from "mobx"
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const mobxFormatters = require("mobx-formatters").default
-mobxFormatters(Mobx)
-//Mobx.useStrict(true)
-
 import { Grapher } from "./Grapher"
 declare let window: any
 window.Grapher = Grapher
