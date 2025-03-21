e57d291ea0e26f8d3b4333f587bcdd684aa9cbea
Ike Saunders <ikesau@protonmail.com>
2025-03-21 15:35:52 -0400

    🎉 add mims table and types

--- packages/@ourworldindata/types/src/index.ts
+++ packages/@ourworldindata/types/src/index.ts
@@ -571,6 +571,12 @@ export {
     ImagesTableName,
 } from "./dbTypes/Images.js"
 export {
+    type DbInsertMim,
+    type DbPlainMim,
+    MimIncomeGroup,
+    MimsTableName,
+} from "./dbTypes/Mims.js"
+export {
     type DbEnrichedMultiDimDataPage,
     type DbInsertMultiDimDataPage,
     type DbPlainMultiDimDataPage,
