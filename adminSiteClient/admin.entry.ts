eaf6005693e83204bd09636c53a9cfb193947210
Marigold <mojmir.vinkler@gmail.com>
2025-03-25 18:58:03 +0100

    wip

--- adminSiteClient/admin.entry.ts
+++ adminSiteClient/admin.entry.ts
@@ -4,7 +4,7 @@ import "./instrument.js"
 
 import "./admin.scss"
 import "@ourworldindata/grapher/src/core/grapher.scss"
-import "../adminSiteClient/ExplorerCreatePage.scss"
+import "./ExplorerCreatePage.scss"
 import "handsontable/dist/handsontable.full.css"
 import updateLocale from "dayjs/plugin/updateLocale"
 import { dayjs } from "@ourworldindata/utils"
