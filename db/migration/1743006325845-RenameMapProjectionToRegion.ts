8b971ee7ce0e36011a6ff2c1a8ec37bc32edb02e
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-03-28 15:46:04 +0100

    🔨 update schema version of all configs

--- db/migration/1743006325845-RenameMapProjectionToRegion.ts
+++ db/migration/1743006325845-RenameMapProjectionToRegion.ts
@@ -7,6 +7,23 @@ const tables = [
     { table: "suggested_chart_revisions", column: "suggestedConfig" },
 ]
 
+async function updateGrapherSchemaField(
+    queryRunner: QueryRunner,
+    newVersion: `${number}${number}${number}`
+): Promise<void> {
+    const schema = `https://files.ourworldindata.org/schemas/grapher-schema.${newVersion}.json`
+
+    for (const { table, column } of tables) {
+        await queryRunner.query(
+            `-- sql
+                UPDATE ${table}
+                SET ${column} = JSON_SET(${column}, '$.$schema', ?)
+            `,
+            [schema]
+        )
+    }
+}
+
 export class RenameMapProjectionToRegion1743006325845
     implements MigrationInterface
 {
@@ -27,6 +44,8 @@ export class RenameMapProjectionToRegion1743006325845
                 `
             )
         }
+
+        await updateGrapherSchemaField(queryRunner, "007")
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -46,5 +65,7 @@ export class RenameMapProjectionToRegion1743006325845
                 `
             )
         }
+
+        await updateGrapherSchemaField(queryRunner, "006")
     }
 }
