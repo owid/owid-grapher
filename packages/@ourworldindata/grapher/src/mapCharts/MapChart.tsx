ee57332548f5183b94a98f0efd5509251a1987f4
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-03-28 17:41:20 +0100

    🐛 remove outline when tooltip is dismissed

--- packages/@ourworldindata/grapher/src/mapCharts/MapChart.tsx
+++ packages/@ourworldindata/grapher/src/mapCharts/MapChart.tsx
@@ -570,6 +570,10 @@ export class MapChart
                         colorScaleManager={this}
                         targetTime={this.targetTime}
                         sparklineWidth={sparklineWidth}
+                        dismissTooltip={() => {
+                            this.tooltipState.target = null
+                            this.focusEntity = undefined
+                        }}
                     />
                 )}
             </g>
