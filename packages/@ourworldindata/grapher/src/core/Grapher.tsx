a55f2e35823dd42a735f777f61975d2166586e8f
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-03-30 19:01:43 +0200

    🎉 add button to go back to the 2d world map

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -3309,15 +3309,6 @@ export class Grapher
         }
     }
 
-    @action.bound private switchToGlobeIfRegionSelectedByDefault(): void {
-        if (
-            this.map.region !== MapRegionName.World &&
-            this.shouldUseSimpleMapSearch
-        ) {
-            this.globeController.showGlobe()
-        }
-    }
-
     componentDidMount(): void {
         this.setBaseFontSize()
         this.setUpIntersectionObserver()
@@ -3345,8 +3336,6 @@ export class Grapher
         )
         if (this.props.bindUrlToWindow) this.bindToWindow()
         if (this.props.enableKeyboardShortcuts) this.bindKeyboardShortcuts()
-
-        this.switchToGlobeIfRegionSelectedByDefault()
     }
 
     private _shortcutsBound = false
