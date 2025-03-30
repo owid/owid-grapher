87eff8cc472ad47dac5e97b48c0882f2eaeb2c73
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-03-30 17:20:28 +0200

    ✨ pin tooltip to bottom for all touch devices

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -3269,7 +3269,7 @@ export class Grapher
     }
 
     @computed get shouldPinTooltipToBottom(): boolean {
-        return this.isNarrow && this.isTouchDevice
+        return this.isTouchDevice
     }
 
     // Binds chart properties to global window title and URL. This should only
