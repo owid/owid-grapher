4bab9fc6e027ae1000a881ef485a95c90ea7a1f9
Marcel Gerber <mgerber59@gmail.com>
2025-04-01 10:21:24 +0200

    cleanup: remove `gitCms`

--- gitCms/GitCmsServer.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, it } from "vitest"
-
-import { GitCmsServer } from "./GitCmsServer.js"
-
-// Just a sanity test, main tests are in the integration tests file.
-it("can init", () => {
-    expect(new GitCmsServer({ baseDir: __dirname })).toBeTruthy()
-})
