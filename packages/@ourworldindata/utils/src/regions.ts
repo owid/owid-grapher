2c99dd413b1303a78dd39c1ba3b4788a40387aa7
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-04-04 10:16:00 +0200

    🔨 add income_group to region types

--- packages/@ourworldindata/utils/src/regions.ts
+++ packages/@ourworldindata/utils/src/regions.ts
@@ -6,46 +6,56 @@ export enum RegionType {
     Other = "other",
     Aggregate = "aggregate",
     Continent = "continent",
+    IncomeGroup = "income_group",
 }
 
-export interface Country {
-    code: string
-    shortCode?: string
+export interface BaseRegion {
+    regionType: RegionType
     name: string
-    shortName?: string
+    code: string
     slug: string
-    regionType: "country" | "other"
+}
+
+export interface Country extends BaseRegion {
+    regionType: RegionType.Country | RegionType.Other
+    shortCode?: string
+    shortName?: string
     isMappable?: boolean
     isHistorical?: boolean
     isUnlisted?: boolean
     variantNames?: string[]
 }
 
-export interface Aggregate {
-    name: string
-    regionType: "aggregate"
-    code: string
+export interface Aggregate extends BaseRegion {
+    regionType: RegionType.Aggregate
     translationCodes?: string[]
     members: string[]
 }
 
-export interface Continent {
-    name:
-        | "Africa"
-        | "Asia"
-        | "Europe"
-        | "North America"
-        | "Oceania"
-        | "South America"
-    regionType: "continent"
-    code: string
+export interface Continent extends BaseRegion {
+    name: OwidContinentName
+    regionType: RegionType.Continent
     translationCodes?: string[]
     members: string[]
 }
 
-export type Region = Country | Aggregate | Continent
+interface IncomeGroup extends BaseRegion {
+    name: OwidIncomeGroupName
+    regionType: RegionType.IncomeGroup
+    members: string[]
+}
+
+export type Region = Country | Aggregate | Continent | IncomeGroup
 
 export const regions: Region[] = entities as Region[]
+
+type OwidContinentName =
+    | "Africa"
+    | "Asia"
+    | "Europe"
+    | "North America"
+    | "Oceania"
+    | "South America"
 
 export type OwidIncomeGroupName =
     | "OWID_LIC"
@@ -66,7 +76,7 @@ export function checkIsOwidIncomeGroupName(
 
 export const countries: Country[] = regions.filter(
     (entity) =>
-        entity.regionType === "country" &&
+        entity.regionType === RegionType.Country &&
         !entity.isUnlisted &&
         !entity.isHistorical
 ) as Country[]
@@ -77,20 +87,22 @@ export const mappableCountries: Country[] = countries.filter(
 
 export const getOthers = lazy(
     () =>
-        entities.filter((entity) => entity.regionType === "other") as Country[]
+        entities.filter(
+            (entity) => entity.regionType === RegionType.Other
+        ) as Country[]
 )
 
 export const getAggregates = lazy(
     () =>
         entities.filter(
-            (entity) => entity.regionType === "aggregate"
+            (entity) => entity.regionType === RegionType.Aggregate
         ) as Aggregate[]
 )
 
 export const getContinents = lazy(
     () =>
         entities.filter(
-            (entity) => entity.regionType === "continent"
+            (entity) => entity.regionType === RegionType.Continent
         ) as Continent[]
 )
 
@@ -121,7 +133,7 @@ const regionsByNameOrVariantNameLowercase = lazy(
 
 const currentAndHistoricalCountryNames = lazy(() =>
     regions
-        .filter(({ regionType }) => regionType === "country")
+        .filter(({ regionType }) => regionType === RegionType.Country)
         .map(({ name }) => name.toLowerCase())
 )
 
