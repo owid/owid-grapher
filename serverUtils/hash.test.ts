fe27f1848d06dc11e9783a9a2ad9f8c3c77e53c0
Marcel Gerber <mgerber59@gmail.com>
2025-04-10 14:35:13 +0200

    refactor: implement md5 using node built-ins

--- serverUtils/hash.test.ts
+++ serverUtils/hash.test.ts
@@ -1,7 +1,18 @@
 import { expect, it, describe } from "vitest"
 
 import { PassThrough } from "node:stream"
-import { hashBase36, hashBase36FromStream } from "./hash.js"
+import { hashBase36, hashBase36FromStream, hashHex, hashMd5 } from "./hash.js"
+
+describe(hashMd5, () => {
+    it("hashes a string", () => {
+        const hash = hashMd5("hello")
+        expect(hash).toBe("5d41402abc4b2a76b9719d911017c592")
+    })
+    it("hashes a string with a non-default hash length", () => {
+        const hash = hashMd5("hello", 12)
+        expect(hash).toBe("5d41402abc4b")
+    })
+})
 
 describe(hashBase36, () => {
     it("hashes a string", () => {
@@ -28,5 +39,22 @@ describe(hashBase36, () => {
 
         const hash = await hashPromise
         expect(hash).toBe("14bu24ea")
+    })
+})
+
+describe(hashHex, () => {
+    it("hashes a string", () => {
+        const hash = hashHex("hello")
+        expect(hash).toBe("2cf24dba")
+    })
+
+    it("hashes a string with a non-default hash length", () => {
+        const hash = hashHex("hello", 12)
+        expect(hash).toBe("2cf24dba5fb0")
+    })
+
+    it("hashes a buffer", () => {
+        const hash = hashHex(Buffer.from("hello"))
+        expect(hash).toBe("2cf24dba")
     })
 })
--- serverUtils/hash.ts
+++ serverUtils/hash.ts
@@ -1,14 +1,20 @@
+import { isNil } from "@ourworldindata/utils"
 import crypto, { Hash } from "crypto"
 import { pipeline } from "node:stream/promises"
 
+export const hashMd5 = (
+    strOrBuffer: string | Buffer,
+    hashLength?: number | null
+) => {
+    const hash = crypto.createHash("md5").update(strOrBuffer)
+    return _hashHex(hash, hashLength)
+}
+
 const DEFAULT_HASH_LENGTH = 8
 
-const _hashHex = (
-    hash: Hash,
-    hashLength: number | null = DEFAULT_HASH_LENGTH
-) => {
+const _hashHex = (hash: Hash, hashLength: number | null | undefined) => {
     const hashHex = hash.digest("hex")
-    if (hashLength === null) return hashHex
+    if (isNil(hashLength)) return hashHex
     else return hashHex.substring(0, hashLength)
 }
 
@@ -20,15 +26,12 @@ export const hashHex = (
     return _hashHex(hash, hashLength)
 }
 
-const _hashBase36 = (
-    hash: Hash,
-    hashLength: number | null = DEFAULT_HASH_LENGTH
-) => {
+const _hashBase36 = (hash: Hash, hashLength: number | null | undefined) => {
     const hashHex = hash.digest("hex")
 
     // Convert hex to base36 to make it contain more information in fewer characters
     const hashBase36 = BigInt(`0x${hashHex}`).toString(36)
-    if (hashLength === null) return hashBase36
+    if (isNil(hashLength)) return hashBase36
     else return hashBase36.substring(0, hashLength)
 }
 
