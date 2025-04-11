f361cc0be38b287fe30548cd9a0d931dc7a3ee92
Daniel Bachler <daniel@danielbachler.de>
2025-04-11 16:34:08 +0200

    🔨 fix prettier issues

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -2787,7 +2787,7 @@ export class GrapherState {
         return this.archivedChartInfo.archiveUrl + this.queryStr
     }
 
-    @computed private get hasUserChangedTimeHandles(): boolean {
+    @computed get hasUserChangedTimeHandles(): boolean {
         const authorsVersion = this.authorsVersion
         return (
             this.minTime !== authorsVersion.minTime ||
