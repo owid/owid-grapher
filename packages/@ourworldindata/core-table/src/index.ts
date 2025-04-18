827b8ec56d03fb786d3846dc61f8589e1d8f193d
Marcel Gerber <mgerber59@gmail.com>
2025-04-18 21:48:53 +0200

    cleanup(table): remove `reverseColumnStore`

--- packages/@ourworldindata/core-table/src/index.ts
+++ packages/@ourworldindata/core-table/src/index.ts
@@ -47,7 +47,6 @@ export {
     rowsToColumnStore,
     autodetectColumnDefs,
     replaceDef,
-    reverseColumnStore,
     renameColumnStore,
     getDropIndexes,
     replaceRandomCellsInColumnStore,
