3c50c796fec71b5ac95389be7225ba78fa3cd448
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-05-06 22:06:11 +0200

    🔨 refactor should show entity selection toggle

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -4131,23 +4131,6 @@ export class Grapher
         )
     }
 
-    @computed get showEntitySelectionToggle(): boolean {
-        if (this.hideEntityControls) return false
-
-        const shouldShowDrawer =
-            this.shouldShowEntitySelectorAs === GrapherWindowType.drawer
-        const shouldShowModal =
-            this.shouldShowEntitySelectorAs === GrapherWindowType.modal
-
-        if (this.isOnMapTab) return shouldShowDrawer
-
-        return (
-            this.isOnChartTab &&
-            this.canChangeAddOrHighlightEntities &&
-            (shouldShowModal || shouldShowDrawer)
-        )
-    }
-
     @computed get isEntitySelectorModalOpen(): boolean {
         return (
             this.isEntitySelectorModalOrDrawerOpen &&
