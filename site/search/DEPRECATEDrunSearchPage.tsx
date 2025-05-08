2491d6311bc30d12fcce1d45f472efdecdca4484
Matthieu <matthieu@ourworldindata.org>
2025-05-08 12:13:21 +0000

    refactor: merge search and data catalog files (no reader facing changes)

---
+++ site/search/DEPRECATEDrunSearchPage.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom"
+import { DEPRECATEDInstantSearchContainer } from "./DEPRECATEDSearchPanel.js"
+
+export function DEPRECATEDrunSearchPage() {
+    render(<DEPRECATEDInstantSearchContainer />, document.querySelector("main"))
+}
