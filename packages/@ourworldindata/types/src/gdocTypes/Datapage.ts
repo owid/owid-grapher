owid/owid-grapher

commit 60127dc81fff60b69bac84fd9b7573f2f16e928a

Remove duplicate of getDatapageDataV2

site/dataPage.ts
@@ -1,4 +1,14 @@
-import { DataPageRelatedResearch, intersection } from "@ourworldindata/utils"
+import {
+    DataPageDataV2,
+    DataPageRelatedResearch,
+    getAttributionFragmentsFromVariable,
+    getLastUpdatedFromVariable,
+    getNextUpdateFromVariable,
+    GrapherInterface,
+    intersection,
+    omitUndefinedValues,
+    OwidVariableWithSource,
+} from "@ourworldindata/utils"
 
 export function processRelatedResearch(
     candidates: DataPageRelatedResearch[],
@@ -13,4 +23,49 @@ export function processRelatedResearch(
     } else relatedResearch = [...candidates]
 
     return relatedResearch
+}
+export function getDatapageDataV2(
+    variableMetadata: OwidVariableWithSource,
+    partialGrapherConfig: GrapherInterface
+): DataPageDataV2 {
+    const lastUpdated = getLastUpdatedFromVariable(variableMetadata) ?? ""
+    const nextUpdate = getNextUpdateFromVariable(variableMetadata)
+    return {
+        status: "draft",
+        title: variableMetadata.presentation?.titlePublic
+            ? omitUndefinedValues({
+                  title: variableMetadata.presentation?.titlePublic,
+                  attributionShort:
+                      variableMetadata.presentation?.attributionShort,
+                  titleVariant: variableMetadata.presentation?.titleVariant,
+              })
+            : {
+                  title:
+                      partialGrapherConfig.title ??
+                      variableMetadata.display?.name ??
+                      variableMetadata.name ??
+                      "",
+              },
+        description: variableMetadata.description,
+        descriptionShort: variableMetadata.descriptionShort,
+        descriptionFromProducer: variableMetadata.descriptionFromProducer,
+        attributionShort: variableMetadata.presentation?.attributionShort,
+        titleVariant: variableMetadata.presentation?.titleVariant,
+        topicTagsLinks: variableMetadata.presentation?.topicTagsLinks ?? [],
+        attributions: getAttributionFragmentsFromVariable(variableMetadata),
+        faqs: [],
+        descriptionKey: variableMetadata.descriptionKey ?? [],
+        descriptionProcessing: variableMetadata.descriptionProcessing,
+        owidProcessingLevel: variableMetadata.processingLevel,
+        dateRange: variableMetadata.timespan ?? "",
+        lastUpdated: lastUpdated,
+        nextUpdate: nextUpdate,
+        allCharts: [],
+        relatedResearch: [],
+        source: variableMetadata.source,
+        origins: variableMetadata.origins ?? [],
+        chartConfig: partialGrapherConfig as Record<string, unknown>,
+        unit: variableMetadata.display?.unit ?? variableMetadata.unit,
+        unitConversionFactor: variableMetadata.display?.conversionFactor,
+    }
 }

site/multiDim/MultiDimDataPageContent.tsx
@@ -9,13 +9,7 @@ import {
 } from "@ourworldindata/grapher"
 import {
     DataPageDataV2,
-    GrapherInterface,
     joinTitleFragments,
-    OwidVariableWithSource,
-    getLastUpdatedFromVariable,
-    getNextUpdateFromVariable,
-    omitUndefinedValues,
-    getAttributionFragmentsFromVariable,
     compact,
     MultiDimDataPageConfig,
     extractMultiDimChoicesFromSearchParams,
@@ -41,7 +35,7 @@ import TopicTags from "../TopicTags.js"
 import MetadataSection from "../MetadataSection.js"
 import { useElementBounds, useMobxStateToReactState } from "../hooks.js"
 import { MultiDimSettingsPanel } from "./MultiDimDataPageSettingsPanel.js"
-import { processRelatedResearch } from "../dataPage.js"
+import { getDatapageDataV2, processRelatedResearch } from "../dataPage.js"
 import DataPageResearchAndWriting from "../DataPageResearchAndWriting.js"
 import { AttachmentsContext } from "../gdocs/AttachmentsContext.js"
 import {
@@ -58,56 +52,6 @@ const baseGrapherConfig: GrapherProgrammaticInterface = {
     adminBaseUrl: ADMIN_BASE_URL,
     dataApiUrl: DATA_API_URL,
     canHideExternalControlsInEmbed: true,
-}
-
-// From DataPageUtils
-const getDatapageDataV2 = async (
-    variableMetadata: OwidVariableWithSource,
-    partialGrapherConfig: GrapherInterface | undefined
-): Promise<DataPageDataV2> => {
-    {
-        const lastUpdated = getLastUpdatedFromVariable(variableMetadata) ?? ""
-        const nextUpdate = getNextUpdateFromVariable(variableMetadata)
-        const datapageJson: DataPageDataV2 = {
-            status: "draft",
-            title: variableMetadata.presentation?.titlePublic
-                ? omitUndefinedValues({
-                      title: variableMetadata.presentation?.titlePublic,
-                      attributionShort:
-                          variableMetadata.presentation?.attributionShort,
-                      titleVariant: variableMetadata.presentation?.titleVariant,
-                  })
-                : {
-                      title:
-                          partialGrapherConfig?.title ??
-                          variableMetadata.display?.name ??
-                          variableMetadata.name ??
-                          "",
-                  },
-            description: variableMetadata.description,
-            descriptionShort: variableMetadata.descriptionShort,
-            descriptionFromProducer: variableMetadata.descriptionFromProducer,
-            attributionShort: variableMetadata.presentation?.attributionShort,
-            titleVariant: variableMetadata.presentation?.titleVariant,
-            topicTagsLinks: variableMetadata.presentation?.topicTagsLinks ?? [],
-            attributions: getAttributionFragmentsFromVariable(variableMetadata),
-            faqs: variableMetadata.presentation?.faqs ?? [],
-            descriptionKey: variableMetadata.descriptionKey ?? [],
-            descriptionProcessing: variableMetadata.descriptionProcessing,
-            owidProcessingLevel: variableMetadata.processingLevel,
-            dateRange: variableMetadata.timespan ?? "",
-            lastUpdated: lastUpdated,
-            nextUpdate: nextUpdate,
-            allCharts: [],
-            relatedResearch: [],
-            source: variableMetadata.source,
-            origins: variableMetadata.origins ?? [],
-            chartConfig: partialGrapherConfig as Record<string, unknown>,
-            unit: variableMetadata.display?.unit ?? variableMetadata.unit,
-            unitConversionFactor: variableMetadata.display?.conversionFactor,
-        }
-        return datapageJson
-    }
 }
 
 const useTitleFragments = (config: MultiDimDataPageConfig) => {
@@ -189,7 +133,7 @@ export function DataPageContent({
             ).then((json) =>
                 getDatapageDataV2(
                     merge(json, config.config?.metadata, newView.metadata),
-                    newView.config
+                    newView.config ?? {}
                 )
             )
             const grapherConfigUuid = newView.fullConfigId

baker/DatapageHelpers.ts
@@ -2,13 +2,8 @@ import "dayjs"
 import { getLinkType, getUrlTarget } from "@ourworldindata/components"
 import {
     GdocsContentSource,
-    DataPageDataV2,
     OwidVariableWithSource,
     gdocIdRegex,
-    getAttributionFragmentsFromVariable,
-    getLastUpdatedFromVariable,
-    getNextUpdateFromVariable,
-    omitUndefinedValues,
     partition,
 } from "@ourworldindata/utils"
 import {
@@ -20,7 +15,6 @@ import { OwidGoogleAuth } from "../db/OwidGoogleAuth.js"
 import {
     EnrichedFaq,
     FaqDictionary,
-    GrapherInterface,
     OwidGdocBaseInterface,
 } from "@ourworldindata/types"
 import { KnexReadonlyTransaction } from "../db/db.js"
@@ -28,55 +22,6 @@ import { parseFaqs } from "../db/model/Gdoc/rawToEnriched.js"
 import { logErrorAndMaybeCaptureInSentry } from "../serverUtils/errorLog.js"
 import { getSlugForTopicTag } from "./GrapherBakingUtils.js"
 import { getShortPageCitation } from "../site/gdocs/utils.js"
-
-export const getDatapageDataV2 = async (
-    variableMetadata: OwidVariableWithSource,
-    partialGrapherConfig: GrapherInterface
-): Promise<DataPageDataV2> => {
-    {
-        const lastUpdated = getLastUpdatedFromVariable(variableMetadata) ?? ""
-        const nextUpdate = getNextUpdateFromVariable(variableMetadata)
-        const datapageJson: DataPageDataV2 = {
-            status: "draft",
-            title: variableMetadata.presentation?.titlePublic
-                ? omitUndefinedValues({
-                      title: variableMetadata.presentation?.titlePublic,
-                      attributionShort:
-                          variableMetadata.presentation?.attributionShort,
-                      titleVariant: variableMetadata.presentation?.titleVariant,
-                  })
-                : {
-                      title:
-                          partialGrapherConfig.title ??
-                          variableMetadata.display?.name ??
-                          variableMetadata.name ??
-                          "",
-                  },
-            description: variableMetadata.description,
-            descriptionShort: variableMetadata.descriptionShort,
-            descriptionFromProducer: variableMetadata.descriptionFromProducer,
-            attributionShort: variableMetadata.presentation?.attributionShort,
-            titleVariant: variableMetadata.presentation?.titleVariant,
-            topicTagsLinks: variableMetadata.presentation?.topicTagsLinks ?? [],
-            attributions: getAttributionFragmentsFromVariable(variableMetadata),
-            faqs: [],
-            descriptionKey: variableMetadata.descriptionKey ?? [],
-            descriptionProcessing: variableMetadata.descriptionProcessing,
-            owidProcessingLevel: variableMetadata.processingLevel,
-            dateRange: variableMetadata.timespan ?? "",
-            lastUpdated: lastUpdated,
-            nextUpdate: nextUpdate,
-            allCharts: [],
-            relatedResearch: [],
-            source: variableMetadata.source,
-            origins: variableMetadata.origins ?? [],
-            chartConfig: partialGrapherConfig as Record<string, unknown>,
-            unit: variableMetadata.display?.unit ?? variableMetadata.unit,
-            unitConversionFactor: variableMetadata.display?.conversionFactor,
-        }
-        return datapageJson
-    }
-}
 
 /**
  * Get the datapage companion gdoc, if any.

baker/GrapherBaker.tsx
@@ -45,10 +45,10 @@ import {
 } from "../db/model/Variable.js"
 import {
     fetchAndParseFaqs,
-    getDatapageDataV2,
     getPrimaryTopic,
     resolveFaqsForVariable,
 } from "./DatapageHelpers.js"
+import { getDatapageDataV2 } from "../site/dataPage.js"
 import { getAllImages } from "../db/model/Image.js"
 import { logErrorAndMaybeCaptureInSentry } from "../serverUtils/errorLog.js"
 
@@ -208,10 +208,7 @@ export async function renderDataPageV2(
         ]
         grapher.dimensions = dimensions
     }
-    const datapageData = await getDatapageDataV2(
-        variableMetadata,
-        grapher ?? {}
-    )
+    const datapageData = getDatapageDataV2(variableMetadata, grapher ?? {})
 
     datapageData.primaryTopic = await getPrimaryTopic(
         knex,

commit bcaf47165d3ea096c9de33e8d87a154f09a650cb

Fix missing FAQs in multi-dim data page
Regression introduced in 60127dc81. We construct the FAQs for mdims
differently than for the classic data page, which was one difference
between the duplicated `getDatapageDataV2` function, that I missed.

packages/@ourworldindata/types/src/gdocTypes/Datapage.ts
@@ -32,7 +32,6 @@ export interface DataPageDataV2 {
     description?: string
     descriptionShort?: string
     descriptionFromProducer?: string
-    faqs: FaqLink[] // Todo: resolve these at this level to the point where we can preview them
     descriptionKey: string[]
     descriptionProcessing?: string
     owidProcessingLevel?: OwidProcessingLevel

site/dataPage.ts
@@ -56,7 +56,6 @@ export function getDatapageDataV2(
         titleVariant: variableMetadata.presentation?.titleVariant,
         topicTagsLinks: variableMetadata.presentation?.topicTagsLinks ?? [],
         attributions: getAttributionFragmentsFromVariable(variableMetadata),
-        faqs: [],
         descriptionKey: variableMetadata.descriptionKey ?? [],
         descriptionProcessing: variableMetadata.descriptionProcessing,
         owidProcessingLevel: variableMetadata.processingLevel,

site/multiDim/MultiDimDataPageContent.tsx
@@ -24,6 +24,7 @@ import {
 import {
     DataPageRelatedResearch,
     FaqEntryKeyedByGdocIdAndFragmentId,
+    FaqLink,
     GrapherQueryParams,
     ImageMetadata,
     MultiDimDataPageConfigEnriched,
@@ -89,6 +90,10 @@ declare global {
     }
 }
 
+interface VariableDataPageData extends DataPageDataV2 {
+    faqs: FaqLink[]
+}
+
 export function DataPageContent({
     slug,
     canonicalUrl,
@@ -105,7 +110,7 @@ export function DataPageContent({
     const [searchParams, setSearchParams] = useSearchParams()
     const [manager, setManager] = useState({})
     const [varDatapageData, setVarDatapageData] =
-        useState<DataPageDataV2 | null>(null)
+        useState<VariableDataPageData | null>(null)
     const titleFragments = useTitleFragments(config)
 
     const settings = useMemo(() => {
@@ -130,12 +135,17 @@ export function DataPageContent({
 
             const datapageDataPromise = cachedGetVariableMetadata(
                 variableId
-            ).then((json) =>
-                getDatapageDataV2(
-                    merge(json, config.config?.metadata, newView.metadata),
-                    newView.config ?? {}
+            ).then((json) => {
+                const mergedMetadata = merge(
+                    json,
+                    config.config?.metadata,
+                    newView.metadata
                 )
-            )
+                return {
+                    ...getDatapageDataV2(mergedMetadata, newView.config ?? {}),
+                    faqs: mergedMetadata.presentation?.faqs ?? [],
+                }
+            })
             const grapherConfigUuid = newView.fullConfigId
             const grapherConfigPromise = cachedGetGrapherConfigByUuid(
                 grapherConfigUuid,