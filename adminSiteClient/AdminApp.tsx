53016a0e2d8a2fdeffd6160f623ef5e6da29a60f
Martin Račák <martin.racak@ourworldindata.org>
2025-05-14 15:28:36 +0200

    Fix chart editor state after navigating to "New chart"
    Previously clicking the "New chart" link in the top admin nav when on a
    chart edit page, e.g. /admin/charts/8642/edit would not reset the state.

--- adminSiteClient/AdminApp.tsx
+++ adminSiteClient/AdminApp.tsx
@@ -154,6 +154,7 @@ export class AdminApp extends React.Component<{
                                     path="/charts/:chartId/edit"
                                     render={({ match }) => (
                                         <ChartEditorPage
+                                            key={match.params.chartId}
                                             grapherId={parseInt(
                                                 match.params.chartId
                                             )}
