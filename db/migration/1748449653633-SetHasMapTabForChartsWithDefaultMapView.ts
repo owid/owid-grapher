fa7b162e4bc3510fe54366c1322e6b9e3014a41a
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-05-28 18:26:34 +0200

    🔨 set hasMapTab for charts with a default map view

---
+++ db/migration/1748449653633-SetHasMapTabForChartsWithDefaultMapView.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+const tables = [
+    { table: "chart_configs", column: "patch" },
+    { table: "chart_configs", column: "full" },
+    { table: "chart_revisions", column: "config" },
+    { table: "suggested_chart_revisions", column: "suggestedConfig" },
+]
+
+export class SetHasMapTabForChartsWithDefaultMapView1748449653633
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        for (const { table, column } of tables) {
+            // Find all charts that show a map by default (tab=map),
+            // but don't explicitly set hasMapTab to true
+            await queryRunner.query(
+                `-- sql
+                    UPDATE ${table}
+                    SET ${column} = JSON_SET(
+                        ${column},
+                        "$.hasMapTab",
+                        true
+                    )
+                    WHERE ${column} ->> "$.tab" = "map"
+                      AND (${column} ->> "$.hasMapTab" IS NULL OR ${column} ->> "$.hasMapTab" = "false")
+                `
+            )
+        }
+    }
+
+    public async down(): Promise<void> {
+        // intentionally empty
+    }
+}
