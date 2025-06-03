e2c68f1ac8a23e8939fb9ca9f97ea244c9c5fa6d
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-06-03 09:44:34 +0200

    ✨ add error message for single-time line charts

--- packages/@ourworldindata/grapher/src/lineCharts/LineChartState.ts
+++ packages/@ourworldindata/grapher/src/lineCharts/LineChartState.ts
@@ -306,6 +306,14 @@ export class LineChartState implements ChartState, ColorScaleManager {
         const message = getDefaultFailMessage(this.manager)
         if (message) return { reason: message }
         if (!this.series.length) return { reason: "No matching data" }
+        if (
+            this.manager.startTime !== undefined &&
+            this.manager.startTime === this.manager.endTime
+        )
+            return {
+                reason: "Two time points needed",
+                help: "Click the timeline to select a second time point",
+            }
         return { reason: "" }
     }
 }
