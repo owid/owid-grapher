83682153adbb458b7baaf68edd81bb61f2f777f6
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-06-06 16:24:24 +0200

    ✨ make second time slider more discoverable
    Fixes #4043

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -1368,7 +1368,7 @@ export class GrapherState {
         return this.checkOnlySingleTimeSelectionPossible(this.activeTab)
     }
 
-    @computed private get isSingleTimeSelectionActive(): boolean {
+    @computed get isSingleTimeSelectionActive(): boolean {
         return (
             this.onlySingleTimeSelectionPossible ||
             this.isSingleTimeScatterAnimationActive
