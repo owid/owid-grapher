eb83c9debee66bd8b2f60ba08e91d683d95b9791
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-06-08 11:20:38 +0200

    🔨 drop the entity name from time highlights in line charts
    It was ignored in code

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -42,7 +42,6 @@ import {
     setWindowQueryStr,
     getWindowUrl,
     Url,
-    EntityTimeHighlight,
     ColumnSlug,
     DimensionProperty,
     SortBy,
@@ -259,7 +258,7 @@ export interface GrapherProgrammaticInterface extends GrapherInterface {
     bakedGrapherURL?: string
     adminBaseUrl?: string
     env?: string
-    entityTimeHighlights?: EntityTimeHighlight[]
+    highlightedTimesInLineChart?: Time[]
     baseFontSize?: number
     staticBounds?: Bounds
     staticFormat?: GrapherStaticFormat
@@ -397,10 +396,10 @@ export class GrapherState {
     }
 
     /**
-     * Used to highlight an entity at a particular time in a line chart.
+     * Used to highlight particular times in a line chart.
      * The sparkline in map tooltips makes use of this.
      */
-    @observable.ref entityTimeHighlights?: EntityTimeHighlight[]
+    @observable.ref highlightedTimesInLineChart?: Time[]
 
     @observable.ref hideFacetControl = true
 
