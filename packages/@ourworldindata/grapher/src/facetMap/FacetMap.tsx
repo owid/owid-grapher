96e641fd494553b5abd7b411f994eb52a6c150d3
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-06-10 16:31:07 +0200

    ✨ (mobile) don't switch to globe when faceted on searching a country

--- packages/@ourworldindata/grapher/src/facetMap/FacetMap.tsx
+++ packages/@ourworldindata/grapher/src/facetMap/FacetMap.tsx
@@ -234,6 +234,7 @@ export class FacetMap
             tooltip,
             shouldPinTooltipToBottom,
             projectionColumnInfoBySlug,
+            isFaceted,
         } = manager
 
         // Use a custom viewport for the World map that zooms in a little bit to make best use of the space
@@ -269,6 +270,7 @@ export class FacetMap
                 projectionColumnInfoBySlug,
                 highlightedTimesInTooltip: targetTimes,
                 mapViewport,
+                isFaceted,
                 ...series.manager,
             }
 
