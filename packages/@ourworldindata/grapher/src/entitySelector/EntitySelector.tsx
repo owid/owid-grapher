2e2bc1d40065e0a1411e3e6a93b8b4f12f7eb6c6
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-06-10 10:21:55 +0200

    🐛 fix entities incorrectly showing 'No data' in the entity selector
    The tableForSelection doesn't necessarily contain all entities. Since interpolated columns are computed only once and then persisted, some entities incorrectly show 'No data', because they weren't contained in the table when the column was computed.

--- packages/@ourworldindata/grapher/src/entitySelector/EntitySelector.tsx
+++ packages/@ourworldindata/grapher/src/entitySelector/EntitySelector.tsx
@@ -99,6 +99,7 @@ export interface EntitySelectorState {
 
 export interface EntitySelectorManager {
     entitySelectorState: Partial<EntitySelectorState>
+    table: OwidTable
     tableForSelection: OwidTable
     selection: SelectionArray
     entityType?: string
@@ -411,7 +412,7 @@ export class EntitySelector extends React.Component<{
     }
 
     private interpolateSortColumn(slug: ColumnSlug): CoreColumn {
-        return this.table
+        return this.inputTable
             .interpolateColumnWithTolerance(
                 slug,
                 this.toleranceOverride.value,
@@ -633,6 +634,10 @@ export class EntitySelector extends React.Component<{
             this.manager.entitySelectorState.isLoadingExternalSortColumn ??
             false
         )
+    }
+
+    @computed private get inputTable(): OwidTable {
+        return this.manager.table
     }
 
     @computed private get table(): OwidTable {
@@ -1201,7 +1206,7 @@ export class EntitySelector extends React.Component<{
             const variable = await additionalDataLoaderFn(indicatorId)
             const variableTable = buildVariableTable(variable)
             const column = variableTable
-                .filterByEntityNames(this.availableEntityNames)
+                .filterByEntityNames(this.inputTable.availableEntityNames)
                 .interpolateColumnWithTolerance(slug, Infinity)
                 .get(slug)
             if (column) this.setInterpolatedSortColumn(column)
