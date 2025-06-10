b8aa362d8eb7afef71662621d485bcc7c7bb9536
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-06-10 10:27:44 +0200

    🐛 include all entities when splitting by type
    The tableForSelection might have more entities than the given table since the entity selector on the map tab includes _all_ countries, even if they have no data for any of the years

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -2683,7 +2683,7 @@ export class GrapherState {
         return this.tableForSelection.availableEntityNames
     }
     @computed get entityRegionTypeGroups(): EntityRegionTypeGroup[] {
-        return groupEntityNamesByRegionType(this.table.availableEntityNames)
+        return groupEntityNamesByRegionType(this.availableEntityNames)
     }
 
     @computed get entityNamesByRegionType(): EntityNamesByRegionType {
