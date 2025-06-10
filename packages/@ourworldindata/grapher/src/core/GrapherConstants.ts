bca77036a4362ef4a47b64c14676765932ab28ff
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-06-10 14:43:10 +0200

    ✨ move the map facet legend closer to the map

--- packages/@ourworldindata/grapher/src/core/GrapherConstants.ts
+++ packages/@ourworldindata/grapher/src/core/GrapherConstants.ts
@@ -55,6 +55,7 @@ export const GRAPHER_FONT_SCALE_12 = 12 / BASE_FONT_SIZE
 export const GRAPHER_FONT_SCALE_12_8 = 12.8 / BASE_FONT_SIZE
 export const GRAPHER_FONT_SCALE_13 = 13 / BASE_FONT_SIZE
 export const GRAPHER_FONT_SCALE_14 = 14 / BASE_FONT_SIZE
+export const GRAPHER_FONT_SCALE_15 = 15 / BASE_FONT_SIZE
 
 // keep in sync with $max-tooltip-width in Tooltip.scss
 export const GRAPHER_MAX_TOOLTIP_WIDTH = 400
