981489b85c83098b6b657c787e926555ab05750a
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-06-12 16:33:09 +0200

    🐛 prevent tab switching during timeline animation

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -812,7 +812,11 @@ export class GrapherState {
         if (
             (activeTab === GRAPHER_TAB_NAMES.LineChart ||
                 activeTab === GRAPHER_TAB_NAMES.SlopeChart) &&
-            this.shouldShowDiscreteBarWhenSingleTime
+            this.shouldShowDiscreteBarWhenSingleTime &&
+            // Don't switch to a bar chart while the timeline animation is playing.
+            // This is necessary because the time handles are at the same time
+            // at the beginning of the timeline animation
+            !this.isTimelineAnimationActive
         ) {
             return GRAPHER_TAB_NAMES.DiscreteBar
         }
