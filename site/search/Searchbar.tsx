838dd9f6f136d4e457d38d0fca8a58638e47c697
Matthieu <matthieu@ourworldindata.org>
2025-06-13 21:09:20 +0000

    refactor: use search context across search components

--- site/search/Searchbar.tsx
+++ site/search/Searchbar.tsx
@@ -5,43 +5,27 @@ import { SearchInput } from "./SearchInput.js"
 import { SearchActiveFilters } from "./SearchActiveFilters.js"
 import { SearchAutocomplete } from "./SearchAutocomplete.js"
 import { SearchCountrySelector } from "./SearchCountrySelector.js"
-import { Filter, FilterType } from "./searchTypes.js"
-import {
-    createFocusInputOnClickHandler,
-    getFilterNamesOfType,
-} from "./searchUtils.js"
+import { FilterType } from "./searchTypes.js"
+import { createFocusInputOnClickHandler } from "./searchUtils.js"
 import { SearchAutocompleteContextProvider } from "./SearchAutocompleteContextProvider.js"
 import { SearchResetButton } from "./SearchResetButton.js"
+import { useSearchContext } from "./SearchContext.js"
+import { useSelectedCountryNames } from "./searchHooks.js"
 
-export const Searchbar = ({
-    allTopics,
-    filters,
-    query,
-    setQuery,
-    addCountry,
-    removeCountry,
-    addTopic,
-    removeTopic,
-    requireAllCountries,
-    toggleRequireAllCountries,
-    reset,
-}: {
-    allTopics: string[]
-    filters: Filter[]
-    query: string
-    setQuery: (query: string) => void
-    addCountry: (country: string) => void
-    removeCountry: (country: string) => void
-    addTopic: (topic: string) => void
-    removeTopic: (topic: string) => void
-    requireAllCountries: boolean
-    toggleRequireAllCountries: () => void
-    reset: () => void
-}) => {
-    const selectedCountryNames = getFilterNamesOfType(
-        filters,
-        FilterType.COUNTRY
-    )
+export const Searchbar = ({ allTopics }: { allTopics: string[] }) => {
+    const {
+        state: { filters, query, requireAllCountries },
+        actions: {
+            setQuery,
+            addCountry,
+            removeCountry,
+            removeTopic,
+            toggleRequireAllCountries,
+            reset,
+        },
+    } = useSearchContext()
+
+    const selectedCountryNames = useSelectedCountryNames()
     // Storing this in local state so that query params don't update during typing
     const [localQuery, setLocalQuery] = useState(query)
     // sync local query with global query when browser navigation occurs
@@ -95,20 +79,13 @@ export const Searchbar = ({
                             />
                         }
                     >
-                        <SearchActiveFilters
-                            filters={filters}
-                            removeCountry={removeCountry}
-                            removeTopic={removeTopic}
-                        />
+                        <SearchActiveFilters />
                     </SearchInput>
                     <SearchAutocomplete
                         localQuery={localQuery}
                         allTopics={allTopics}
-                        filters={filters}
                         setLocalQuery={setLocalQuery}
                         setQuery={setQuery}
-                        addCountry={addCountry}
-                        addTopic={addTopic}
                     />
                 </SearchAutocompleteContextProvider>
                 <SearchCountrySelector
