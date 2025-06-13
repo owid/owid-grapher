5a4763321a74401e7aec8767336d87ffe1dd8edc
Matthieu <matthieu@ourworldindata.org>
2025-06-13 19:40:59 +0000

    refactor: add SearchContext for handling search state and actions

---
+++ site/search/SearchContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react"
+import { SearchState } from "./searchTypes.js"
+import { createActions } from "./searchState.js"
+
+type SearchActions = ReturnType<typeof createActions>
+
+interface SearchContextType {
+    state: SearchState
+    actions: SearchActions
+}
+
+export const SearchContext = createContext<SearchContextType | null>(null)
+
+export const useSearchContext = () => {
+    const context = useContext(SearchContext)
+    if (!context) {
+        throw new Error("useSearchContext must be used within a SearchContext.Provider")
+    }
+    return context
+}
