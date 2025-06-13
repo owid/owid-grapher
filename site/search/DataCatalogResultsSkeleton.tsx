6b727a741cb29566ac3ef67301b5e96c18fd6303
Matthieu <matthieu@ourworldindata.org>
2025-06-13 07:40:51 +0000

    refactor: move refinements list heading up

--- site/search/DataCatalogResultsSkeleton.tsx
+++ site/search/DataCatalogResultsSkeleton.tsx
@@ -4,7 +4,7 @@ import { ChartSkeleton } from "./ChartSkeleton.js"
 export const DataCatalogResultsSkeleton = () => {
     return (
         <div className="data-catalog-results-skeleton grid grid-cols-12-full-width span-cols-14">
-            <div className="data-catalog-refinement-list data-catalog-refinement-list--skeleton span-cols-12 col-start-2" />
+            <div className="search-topics-refinement-list search-topics-refinement-list--skeleton span-cols-12 col-start-2" />
             <div className="span-cols-12 col-start-2 data-catalog-search-hits">
                 <span className="data-catalog-search-list__results-count--skeleton body-3-medium" />
                 <ul className="data-catalog-search-list grid grid-cols-4 grid-sm-cols-1">
