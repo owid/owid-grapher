8632056b09eb2878b141cec2f95d65b9d419aee4
Matthieu <matthieu@ourworldindata.org>
2025-06-13 21:09:20 +0000

    refactor: use search context across search components

--- site/search/DataCatalogRibbonView.tsx
+++ site/search/DataCatalogRibbonView.tsx
@@ -1,36 +1,29 @@
 import { TagGraphRoot } from "@ourworldindata/types"
-import { Region } from "@ourworldindata/utils"
 import { SearchClient } from "algoliasearch"
 import { useQuery } from "@tanstack/react-query"
 import { DataCatalogRibbon } from "./DataCatalogRibbon.js"
 import { DataCatalogRibbonViewSkeleton } from "./DataCatalogRibbonViewSkeleton.js"
-import { DataCatalogRibbonResult, SearchState } from "./searchTypes.js"
+import { DataCatalogRibbonResult } from "./searchTypes.js"
 import { queryDataCatalogRibbons } from "./searchUtils.js"
 import { searchQueryKeys } from "./searchQueryKeys.js"
+import { useSearchContext } from "./SearchContext.js"
 
 export const DataCatalogRibbonView = ({
-    addTopic,
-    selectedCountries,
-    topics,
     tagGraph,
     searchClient,
-    searchState,
 }: {
-    addTopic: (x: string) => void
-    selectedCountries: Region[]
-    topics: Set<string>
     tagGraph: TagGraphRoot
     searchClient: SearchClient
-    searchState: SearchState
 }) => {
+    const { state } = useSearchContext()
+
     const ribbonsQuery = useQuery<DataCatalogRibbonResult[], Error>({
-        queryKey: searchQueryKeys.ribbons(searchState),
-        queryFn: () =>
-            queryDataCatalogRibbons(searchClient, searchState, tagGraph),
+        queryKey: searchQueryKeys.ribbons(state),
+        queryFn: () => queryDataCatalogRibbons(searchClient, state, tagGraph),
     })
 
     if (ribbonsQuery.isLoading) {
-        return <DataCatalogRibbonViewSkeleton topics={topics} />
+        return <DataCatalogRibbonViewSkeleton />
     }
 
     const resultsSortedByHitCount = ribbonsQuery.data?.sort(
@@ -41,12 +34,7 @@ export const DataCatalogRibbonView = ({
         <>
             <div className="span-cols-14 grid grid-cols-12-full-width data-catalog-ribbons">
                 {resultsSortedByHitCount?.map((result) => (
-                    <DataCatalogRibbon
-                        key={result.title}
-                        result={result}
-                        addTopic={addTopic}
-                        selectedCountries={selectedCountries}
-                    />
+                    <DataCatalogRibbon key={result.title} result={result} />
                 ))}
             </div>
         </>
