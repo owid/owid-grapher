ab4f747504df809fb25caf6f277681bc7657af64
Matthieu <matthieu@ourworldindata.org>
2025-06-14 10:49:32 +0000

    feat: add draft data insight to search

---
+++ site/search/SearchDataInsightsSection.tsx
@@ -0,0 +1,48 @@
+import { AsDraft } from "../AsDraft/AsDraft.js"
+import { DataInsightHit, DataInsightSearchResponse } from "./searchTypes.js"
+import { SearchClient } from "algoliasearch"
+import { useQuery } from "@tanstack/react-query"
+import { queryDataInsights, searchQueryKeys } from "./useQueries.js"
+import { useSearchContext } from "./SearchContext.js"
+import { SearchDataInsightHit } from "./SearchDataInsightHit.js"
+
+export function SearchDataInsights({
+    searchClient,
+}: {
+    searchClient: SearchClient
+}) {
+    const { state } = useSearchContext()
+    const query = useQuery<DataInsightSearchResponse, Error>({
+        queryKey: [searchQueryKeys.dataInsights(state)],
+        queryFn: () => queryDataInsights(searchClient, state),
+    })
+
+    const hits = query.data?.hits
+    if (!query.data || !hits || !hits.length) return null
+
+    const { nbHits } = query.data
+
+    return (
+        <AsDraft name="Data Insights" className="grid span-cols-12 col-start-2">
+            <section className="search-data-insights span-cols-12">
+                <header className="search-data-insights__header">
+                    <h3 className="search-data-insights__title">
+                        Data Insights
+                    </h3>
+                    <span className="search-data-insights__count">
+                        ({nbHits} results)
+                    </span>
+                </header>
+                <div className="search-data-insights__hits grid">
+                    {hits.map((hit: DataInsightHit) => (
+                        <SearchDataInsightHit
+                            className="span-cols-3"
+                            key={hit.objectID}
+                            hit={hit}
+                        />
+                    ))}
+                </div>
+            </section>
+        </AsDraft>
+    )
+}
