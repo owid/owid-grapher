3700644d9733fa110e8d8b3bfcb407f5e8418d5f
Matthieu <matthieu@ourworldindata.org>
2025-06-16 20:25:56 +0000

    feat: wrap DataCatalogRibbon in AsDraft

--- site/search/DataCatalogRibbon.tsx
+++ site/search/DataCatalogRibbon.tsx
@@ -2,11 +2,12 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { commafyNumber } from "@ourworldindata/utils"
 import * as React from "react"
-import { ChartHit } from "./ChartHit.js"
 import { DataCatalogRibbonResult } from "./searchTypes.js"
 import { SiteAnalytics } from "../SiteAnalytics.js"
 import { useSearchContext } from "./SearchContext.js"
 import { useSelectedCountries } from "./searchHooks.js"
+import { ChartHitMedium } from "./ChartHitMedium.js"
+import { AsDraft } from "../AsDraft/AsDraft.js"
 
 const analytics = new SiteAnalytics()
 
@@ -30,54 +31,62 @@ export const DataCatalogRibbon = ({
     }
 
     return (
-        <div className="data-catalog-ribbon col-start-2 span-cols-12 col-sm-start-2 span-sm-cols-13">
-            <button
-                className="data-catalog-ribbon__header-button"
-                aria-label={`Add topic ${result.title} to filters`}
-                onClick={handleAddTopicClick}
-            >
-                <div className="data-catalog-ribbon__header">
-                    <h2 className="body-1-regular">{titleLabel}</h2>
-                    <span className="data-catalog-ribbon__hit-count body-2-semibold">
-                        {commafyNumber(result.nbHits)}{" "}
-                        {result.nbHits === 1 ? "chart" : "charts"}
-                        <FontAwesomeIcon icon={faArrowRight} />
-                    </span>
+        <AsDraft
+            name="Ribbon"
+            className="col-start-2 span-cols-12 col-sm-start-2 span-sm-cols-13>"
+        >
+            <div className="data-catalog-ribbon ">
+                <button
+                    className="data-catalog-ribbon__header-button"
+                    aria-label={`Add topic ${result.title} to filters`}
+                    onClick={handleAddTopicClick}
+                >
+                    <div className="data-catalog-ribbon__header">
+                        <h2 className="body-1-regular">{titleLabel}</h2>
+                        <span className="data-catalog-ribbon__hit-count body-2-semibold">
+                            {commafyNumber(result.nbHits)}{" "}
+                            {result.nbHits === 1 ? "chart" : "charts"}
+                            <FontAwesomeIcon icon={faArrowRight} />
+                        </span>
+                    </div>
+                </button>
+                <div className="data-catalog-ribbon-hits">
+                    <ul className="data-catalog-ribbon-list">
+                        {result.hits.map((hit, i) => (
+                            <li
+                                className="data-catalog-ribbon-hit"
+                                key={hit.objectID}
+                            >
+                                <ChartHitMedium
+                                    hit={hit}
+                                    onClick={() => {
+                                        analytics.logDataCatalogResultClick(
+                                            hit,
+                                            i + 1,
+                                            "ribbon",
+                                            result.title
+                                        )
+                                    }}
+                                    searchQueryRegionsMatches={
+                                        selectedCountries
+                                    }
+                                    showThumbnails={i === 0}
+                                />
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-            </button>
-            <div className="data-catalog-ribbon-hits">
-                <ul className="data-catalog-ribbon-list grid grid-cols-4">
-                    {result.hits.map((hit, i) => (
-                        <li
-                            className="data-catalog-ribbon-hit"
-                            key={hit.objectID}
-                        >
-                            <ChartHit
-                                hit={hit}
-                                onClick={() => {
-                                    analytics.logDataCatalogResultClick(
-                                        hit,
-                                        i + 1,
-                                        "ribbon",
-                                        result.title
-                                    )
-                                }}
-                                searchQueryRegionsMatches={selectedCountries}
-                            />
-                        </li>
-                    ))}
-                </ul>
+                <button
+                    className="data-catalog-ribbon__see-all-button"
+                    aria-label={`Add ${result.title} to filters`}
+                    onClick={handleAddTopicClick}
+                >
+                    {result.nbHits === 1
+                        ? `See 1 chart`
+                        : `See ${commafyNumber(result.nbHits)} charts`}
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </button>
             </div>
-            <button
-                className="data-catalog-ribbon__see-all-button"
-                aria-label={`Add ${result.title} to filters`}
-                onClick={handleAddTopicClick}
-            >
-                {result.nbHits === 1
-                    ? `See 1 chart`
-                    : `See ${commafyNumber(result.nbHits)} charts`}
-                <FontAwesomeIcon icon={faArrowRight} />
-            </button>
-        </div>
+        </AsDraft>
     )
 }
