0b582a6a5accf53e390fbe97fd6056ebe9be1f4b
Marcel Gerber <mgerber59@gmail.com>
2025-06-17 14:29:45 +0200

    use pruneUnusedBins

--- packages/@ourworldindata/grapher/src/color/BinningStrategies2.ts
+++ packages/@ourworldindata/grapher/src/color/BinningStrategies2.ts
@@ -87,11 +87,17 @@ export const pruneUnusedBins = (
     { minValue, maxValue }: { minValue?: number; maxValue?: number }
 ): number[] => {
     if (minValue !== undefined) {
-        bins = R.dropWhile(bins, (v, i) => bins[i + 1] <= minValue)
+        bins = R.dropWhile(
+            bins,
+            (v, i) => bins[i + 1] !== undefined && bins[i + 1] <= minValue
+        )
     }
 
     if (maxValue !== undefined) {
-        bins = R.dropLastWhile(bins, (v, i) => bins[i - 1] >= maxValue)
+        bins = R.dropLastWhile(
+            bins,
+            (v, i) => bins[i - 1] !== undefined && bins[i - 1] >= maxValue
+        )
     }
 
     return bins
@@ -449,36 +455,9 @@ export const fakeLogBins = ({
     const magnitudeMin = numberMagnitude(minValue) - 1
     const magnitudeMax = numberMagnitude(maxValue) - 1
 
-    // These are normalised to numbers between 1 and 9.99999
-    const normalisedMin = minValue / Math.pow(10, magnitudeMin)
-    const normalisedMax = maxValue / Math.pow(10, magnitudeMax)
-
-    // Find the steps we should use at the beginning.
-    // If minValue is 0.3 (and thus, normalisedMin is 3) and logSteps are [1, 2, 5],
-    // we want to place a first step at 0.2, and arrive at logSteps [2, 5].
-    const firstSteps = R.dropWhile(
-        logSteps,
-        (step, i) =>
-            logSteps[i + 1] !== undefined && logSteps[i + 1] <= normalisedMin
-    )
-
-    const lastSteps = R.dropLastWhile(
-        logSteps,
-        (step, i) =>
-            logSteps[i - 1] !== undefined && logSteps[i - 1] >= normalisedMax
-    )
-
     const candidates = R.range(magnitudeMin, magnitudeMax + 1).flatMap(
         (magnitude) => {
             const factor = Math.pow(10, magnitude)
-
-            if (magnitude === magnitudeMin)
-                return firstSteps.map((step) => step * factor)
-
-            if (magnitude === magnitudeMax)
-                return lastSteps.map((step) => step * factor)
-
-            // For all other magnitudes, we use the full set of steps
             return logSteps.map((step) => step * factor)
         }
     )
@@ -487,7 +466,7 @@ export const fakeLogBins = ({
         candidates.push(1 * Math.pow(10, magnitudeMax + 1))
     }
 
-    return candidates
+    return pruneUnusedBins(candidates, { minValue, maxValue })
 }
 
 export const mirrorBinsAroundMidpoint = (
