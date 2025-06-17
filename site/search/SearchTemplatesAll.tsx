3fa48df00b1fe4106222ad0e5253adb61d3cf2e0
Matthieu <matthieu@ourworldindata.org>
2025-06-17 08:49:27 +0000

    feat: upgrade debug navigator bar

--- site/search/SearchTemplatesAll.tsx
+++ site/search/SearchTemplatesAll.tsx
@@ -1,6 +1,5 @@
 import { match } from "ts-pattern"
 import { SearchTopicType } from "./searchTypes.js"
-import { DebugFigmaLink } from "./DebugFigmaLink.js"
 import { DataCatalogRibbonView } from "./DataCatalogRibbonView.js"
 import { DataCatalogResults } from "./DataCatalogResults.js"
 import { useSearchContext } from "./SearchContext.js"
@@ -17,71 +16,33 @@ export const SearchTemplatesAll = () => {
             // All + Topic + Country + Query
             .with([SearchTopicType.Topic, true, true], () => (
                 <>
-                    <DebugFigmaLink figmaNodeId="302%3A21526" />
                     <DataCatalogResults />
                 </>
             ))
             // All + Topic + Country + No Query
-            .with([SearchTopicType.Topic, true, false], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="297%3A8229" />
-                </>
-            ))
+            .with([SearchTopicType.Topic, true, false], () => <></>)
             // All + Topic + No Country + Query
-            .with([SearchTopicType.Topic, false, true], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="298%3A11950" />
-                </>
-            ))
+            .with([SearchTopicType.Topic, false, true], () => <></>)
             // All + Topic + No Country + No Query
-            .with([SearchTopicType.Topic, false, false], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="302%3A24427" />
-                </>
-            ))
+            .with([SearchTopicType.Topic, false, false], () => <></>)
             // All + Area + Country + Query
-            .with([SearchTopicType.Area, true, true], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="302%3A19676" />
-                </>
-            ))
+            .with([SearchTopicType.Area, true, true], () => <></>)
             // All + Area + Country + No Query
-            .with([SearchTopicType.Area, true, false], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="302%3A27071" />
-                </>
-            ))
+            .with([SearchTopicType.Area, true, false], () => <></>)
             // All + Area + No Country + Query
-            .with([SearchTopicType.Area, false, true], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="302%3A22059" />
-                </>
-            ))
+            .with([SearchTopicType.Area, false, true], () => <></>)
             // All + Area + No Country + No Query
-            .with([SearchTopicType.Area, false, false], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="113%3A8747" />
-                </>
-            ))
+            .with([SearchTopicType.Area, false, false], () => <></>)
             // All + No Topic + Country + Query
-            .with([null, true, true], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="298%3A11224" />
-                </>
-            ))
+            .with([null, true, true], () => <></>)
             // All + No Topic + Country + No Query
             .with([null, true, false], () => (
                 <>
-                    <DebugFigmaLink figmaNodeId="302%3A25073" />
                     <DataCatalogRibbonView />
                 </>
             ))
             // All + No Topic + No Country + Query
-            .with([null, false, true], () => (
-                <>
-                    <DebugFigmaLink figmaNodeId="130%3A5909" />
-                </>
-            ))
+            .with([null, false, true], () => <></>)
             // All + No Topic + No Country + No Query
             .with([null, false, false], () => null)
             .exhaustive()
