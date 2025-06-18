a88efaa836087fac133801e74da6fc757df969bd
Matthieu <matthieu@ourworldindata.org>
2025-06-18 19:09:27 +0000

    feat: integrate children topics into "writing" queries

--- site/search/searchHooks.ts
+++ site/search/searchHooks.ts
@@ -1,5 +1,9 @@
 import { FilterType } from "./searchTypes.js"
-import { getCountryData, getFilterNamesOfType } from "./searchUtils.js"
+import {
+    getCountryData,
+    getFilterNamesOfType,
+    getSelectedTopic,
+} from "./searchUtils.js"
 import { useSearchContext } from "./SearchContext.js"
 import { Region } from "@ourworldindata/utils"
 
@@ -10,8 +14,7 @@ export const useSelectedCountries = (): Region[] => {
 
 export const useSelectedTopic = (): string | undefined => {
     const { state } = useSearchContext()
-    const selectedTopics = getFilterNamesOfType(state.filters, FilterType.TOPIC)
-    return selectedTopics.size > 0 ? [...selectedTopics][0] : undefined
+    return getSelectedTopic(state.filters)
 }
 
 export const useSelectedCountryNames = (): Set<string> => {
