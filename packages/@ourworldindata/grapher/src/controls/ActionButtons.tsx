9b274b768c5069de465e08f497a5d18a1b32e498
Marcel Gerber <mgerber59@gmail.com>
2025-06-19 18:46:38 +0200

    enhance: enable using `<a>` tag for ActionButton

--- packages/@ourworldindata/grapher/src/controls/ActionButtons.tsx
+++ packages/@ourworldindata/grapher/src/controls/ActionButtons.tsx
@@ -18,6 +18,7 @@ import {
     shouldShareUsingShareApi,
 } from "./ShareMenu.js"
 import { DEFAULT_BOUNDS, Bounds } from "@ourworldindata/utils"
+import classNames from "classnames"
 
 export interface ActionButtonsManager extends ShareMenuManager {
     isShareMenuActive?: boolean
@@ -304,22 +305,15 @@ export class ActionButtons extends React.Component<{
                     )}
                     {this.hasExploreTheDataButton && (
                         <li style={{ width: this.exploreTheDataButtonWidth }}>
-                            <div
-                                className="ActionButton ActionButton--exploreData"
-                                style={{ width: "100%" }}
-                            >
-                                <a
-                                    data-track-note="chart_click_exploredata"
-                                    href={manager.canonicalUrl}
-                                    target="_blank"
-                                    rel="noopener"
-                                >
-                                    <span className="label">
-                                        Explore the data
-                                    </span>
-                                    <FontAwesomeIcon icon={faArrowRight} />
-                                </a>
-                            </div>
+                            <ActionButton
+                                label="Explore the data"
+                                dataTrackNote="chart_click_exploredata"
+                                icon={faArrowRight}
+                                iconPlacement="right"
+                                href={manager.canonicalUrl}
+                                className="ActionButton--exploreData"
+                                showLabel={true}
+                            />
                         </li>
                     )}
                 </ul>
@@ -331,43 +325,69 @@ export class ActionButtons extends React.Component<{
 export function ActionButton(props: {
     label: string
     icon: IconDefinition
+    iconPlacement?: "left" | "right"
+    href?: string
     width?: number
     dataTrackNote?: string
-    onClick?: React.MouseEventHandler<HTMLButtonElement>
-    onMouseDown?: React.MouseEventHandler<HTMLButtonElement>
+    onClick?: React.MouseEventHandler<HTMLDivElement>
+    onMouseDown?: React.MouseEventHandler<HTMLDivElement>
     showLabel?: boolean
     isActive?: boolean
     style?: React.CSSProperties
+    className?: string
 }): React.ReactElement {
     const [showTooltip, setShowTooltip] = useState(false)
 
+    const buttonClassnames = classNames({
+        active: props.isActive,
+        "icon-only": !props.showLabel,
+    })
+
+    const iconPlacement = props.iconPlacement ?? "left"
+
+    const buttonContents = (
+        <>
+            {iconPlacement === "left" && <FontAwesomeIcon icon={props.icon} />}
+            {props.showLabel && <span className="label">{props.label}</span>}
+            {iconPlacement === "right" && <FontAwesomeIcon icon={props.icon} />}
+        </>
+    )
+
     return (
-        <div className="ActionButton" style={props.style}>
-            <button
-                className={
-                    (props.isActive ? "active" : "") +
-                    (props.showLabel ? "" : " icon-only")
-                }
-                data-track-note={props.dataTrackNote}
-                onClick={(e: React.MouseEvent<HTMLButtonElement>): void => {
-                    if (props.onClick) props.onClick(e)
-                    setShowTooltip(false)
-                }}
-                onMouseDown={props.onMouseDown}
-                onMouseEnter={(): void => {
-                    if (!props.showLabel) setShowTooltip(true)
-                }}
-                onMouseLeave={(): void => {
-                    setShowTooltip(false)
-                }}
-                aria-label={props.label}
-                type="button"
-            >
-                <FontAwesomeIcon icon={props.icon} />
-                {props.showLabel && (
-                    <span className="label">{props.label}</span>
-                )}
-            </button>
+        <div
+            className={classNames("ActionButton", props.className)}
+            style={props.style}
+            data-track-note={props.dataTrackNote}
+            onClick={(e: React.MouseEvent<HTMLDivElement>): void => {
+                if (props.onClick) props.onClick(e)
+                setShowTooltip(false)
+            }}
+            onMouseDown={props.onMouseDown}
+            onMouseEnter={(): void => {
+                if (!props.showLabel) setShowTooltip(true)
+            }}
+            onMouseLeave={(): void => {
+                setShowTooltip(false)
+            }}
+        >
+            {props.href ? (
+                <a
+                    href={props.href}
+                    target="_blank"
+                    className={buttonClassnames}
+                    rel="noopener"
+                >
+                    {buttonContents}
+                </a>
+            ) : (
+                <button
+                    className={buttonClassnames}
+                    aria-label={props.label}
+                    type="button"
+                >
+                    {buttonContents}
+                </button>
+            )}
             {showTooltip && <div className="hover-label">{props.label}</div>}
         </div>
     )
