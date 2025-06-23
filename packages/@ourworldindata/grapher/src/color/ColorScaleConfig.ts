df6310868186d01f513b1a0c11e6da8a6cf7c37d
Marcel Gerber <mgerber59@gmail.com>
2025-06-23 12:29:46 +0200

    enhance: address review comments

--- packages/@ourworldindata/grapher/src/color/ColorScaleConfig.ts
+++ packages/@ourworldindata/grapher/src/color/ColorScaleConfig.ts
@@ -125,9 +125,6 @@ export class ColorScaleConfig
                 })
         }
 
-        // TODO: once Grammar#parse() is called for all values, we can remove parseFloat() here
-        // See issue: https://www.notion.so/owid/ColumnGrammar-parse-function-does-not-get-applied-67b578b8af7642c5859a1db79c8d5712
-
         const customNumericColorsActive =
             customNumericColors.length > 0 ? true : undefined
 
