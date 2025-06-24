6201c7746fb6eaed3fa2f20bc1106db3e1d62ec9
Marcel Gerber <mgerber59@gmail.com>
2025-06-24 15:23:01 +0200

    enhance: deny more Google Analytics fields
    see https://support.google.com/tagmanager/answer/13695607

--- site/Head.tsx
+++ site/Head.tsx
@@ -17,7 +17,7 @@ export const GTMScriptTags = ({ gtmId }: { gtmId: string }) => {
                     __html: `/* Prepare Google Tag Manager */
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-gtag("consent","default",{"ad_storage":"denied","analytics_storage":"denied","wait_for_update":1000});
+gtag("consent","default",{"ad_storage":"denied","ad_user_data":"denied","ad_personalization":"denied","analytics_storage":"denied","wait_for_update":1000});
 `,
                 }}
             />
