92270508f49a8bef5b6e075dc92b60b9402b2a6e
Matthieu <matthieu@ourworldindata.org>
2025-06-24 13:47:05 +0000

    refactor: colocate and harmonize skeleton code

--- site/search/ChartSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-export const ChartSkeleton = () => (
-    <li className="data-catalog-search-hit--skeleton span-cols-1">
-        <div className="data-catalog-search-hit__thumbnail--skeleton" />
-        <div className="data-catalog-search-hit__thumbnail-title--skeleton" />
-        <div className="data-catalog-search-hit__thumbnail-title--skeleton" />
-    </li>
-)
