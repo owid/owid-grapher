3858d9b053079c270cd020ec764045348caaa26a
Ike Saunders <11844404+ikesau@users.noreply.github.com>
2025-07-04 14:36:33 -0400

    🎉 admin server API for static viz

--- adminSiteServer/apiRouter.ts
+++ adminSiteServer/apiRouter.ts
@@ -155,6 +155,13 @@ import {
     getDodsUsage,
     getParsedDods,
 } from "./apiRoutes/dods.js"
+import {
+    getStaticVizListHandler,
+    getStaticVizByIdHandler,
+    createStaticViz,
+    updateStaticViz,
+    deleteStaticViz,
+} from "./apiRoutes/staticViz.js"
 
 const apiRouter = new FunctionalRouter()
 
@@ -281,6 +288,29 @@ getRouteWithROTransaction(apiRouter, "/parsed-dods.json", getParsedDods)
 getRouteWithROTransaction(apiRouter, "/dods-usage.json", getDodsUsage)
 patchRouteWithRWTransaction(apiRouter, "/dods/:id", updateDod)
 postRouteWithRWTransaction(apiRouter, "/dods", createDod)
+
+// Static viz routes
+getRouteWithROTransaction(
+    apiRouter,
+    "/static-viz.json",
+    getStaticVizListHandler
+)
+getRouteWithROTransaction(
+    apiRouter,
+    "/static-viz/:staticVizId.json",
+    getStaticVizByIdHandler
+)
+postRouteWithRWTransaction(apiRouter, "/static-viz", createStaticViz)
+putRouteWithRWTransaction(
+    apiRouter,
+    "/static-viz/:staticVizId",
+    updateStaticViz
+)
+deleteRouteWithRWTransaction(
+    apiRouter,
+    "/static-viz/:staticVizId",
+    deleteStaticViz
+)
 
 // explorer routes
 postRouteWithRWTransaction(apiRouter, "/explorer/:slug/tags", addExplorerTags)
