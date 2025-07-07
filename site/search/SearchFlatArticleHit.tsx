799e8d06c400b0fe9cd432e44b314f4b5dd2b2a6
Martin Račák <martin.racak@ourworldindata.org>
2025-07-07 14:55:15 +0300

    Implement desktop design for browsing writing in search

--- site/search/SearchFlatArticleHit.tsx
+++ site/search/SearchFlatArticleHit.tsx
@@ -3,46 +3,33 @@ import { OwidGdocType } from "@ourworldindata/types"
 import { formatAuthors } from "@ourworldindata/utils"
 import { Snippet } from "react-instantsearch"
 import DataInsightDateline from "../gdocs/components/DataInsightDateline.js"
+import { FlatArticleHit } from "./searchTypes.js"
 import { SearchAsDraft } from "./SearchAsDraft.js"
-import { ArticleHit } from "./searchTypes.js"
-import cx from "classnames"
 
-export const SearchArticleHit = ({
-    hit,
-    variant = "flat",
-}: {
-    hit: ArticleHit
-    variant?: "flat" | "stacked"
-}) => {
+export function SearchFlatArticleHit({ hit }: { hit: FlatArticleHit }) {
     const isArticle = hit.type === OwidGdocType.Article
-
-    const href = getCanonicalPath(hit.slug, hit.type)
-
     return (
         <SearchAsDraft name="Article Hit">
             <a
-                href={href}
-                className={cx("search-article-hit", {
-                    "search-article-hit--stacked": variant === "stacked",
-                    "search-article-hit--flat": variant === "flat",
-                })}
+                className="search-flat-article-hit"
+                href={getCanonicalPath(hit.slug, hit.type)}
             >
-                <div className="search-article-hit__content">
+                <article className="search-flat-article-hit__content">
                     {hit.thumbnailUrl && (
-                        <div className="search-article-hit__image-container">
+                        <div className="search-flat-article-hit__image-container">
                             <img
                                 src={hit.thumbnailUrl}
                                 role="presentation"
-                                className="search-article-hit__image"
+                                className="search-flat-article-hit__image"
                                 alt=""
                             />
                         </div>
                     )}
-                    <div className="search-article-hit__text">
-                        <header className="search-article-hit__header">
+                    <div className="search-flat-article-hit__text">
+                        <header className="search-flat-article-hit__header">
                             {isArticle && hit.date && (
                                 <DataInsightDateline
-                                    className="search-article-hit__dateline"
+                                    className="search-flat-article-hit__dateline"
                                     publishedAt={new Date(hit.date)}
                                     formatOptions={{
                                         year: "numeric",
@@ -51,23 +38,23 @@ export const SearchArticleHit = ({
                                     }}
                                 />
                             )}
-                            <h3 className="search-article-hit__title">
+                            <h3 className="search-flat-article-hit__title">
                                 {hit.title}
                             </h3>
                             {isArticle && (
-                                <span className="search-article-hit__authors">
+                                <span className="search-flat-article-hit__authors">
                                     {formatAuthors(hit.authors)}
                                 </span>
                             )}
                         </header>
                         <Snippet
-                            className="search-article-hit__excerpt"
+                            className="search-flat-article-hit__excerpt"
                             attribute="content"
                             highlightedTagName="strong"
                             hit={hit}
                         />
                     </div>
-                </div>
+                </article>
             </a>
         </SearchAsDraft>
     )
