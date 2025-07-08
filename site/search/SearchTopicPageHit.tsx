b81ab491f8389efd359bd564c3db22d9e7e0b994
Martin Račák <martin.racak@ourworldindata.org>
2025-07-08 16:41:13 +0300

    Implement browser design for writing search results
    - Add solid-light-blue theme and disabled style to Button
    - Implement desktop design of these components
      - SearchShowMore
      - SearchResultHeader
      - SearchFlatArticleHit
      - SearchTopicPageHit
      - SearchWritingResults

--- site/search/SearchTopicPageHit.tsx
+++ site/search/SearchTopicPageHit.tsx
@@ -1,23 +1,26 @@
+import cx from "classnames"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons"
 import { getCanonicalPath } from "@ourworldindata/components"
-import { SearchAsDraft } from "./SearchAsDraft.js"
 import { TopicPageHit } from "./searchTypes.js"
 
-export const SearchTopicPageHit = ({ hit }: { hit: TopicPageHit }) => (
-    <SearchAsDraft name={"Topic Page Hit"}>
-        <a
-            href={getCanonicalPath(hit.slug, hit.type)}
-            className="search-topic-page-hit"
-        >
-            <div className="search-topic-page-hit__content">
-                <header className="search-topic-page-hit__header">
-                    <h3 className="search-topic-page-hit__title">
-                        {hit.title}
-                    </h3>
-                </header>
-                <div className="search-topic-page-hit__excerpt">
-                    {hit.excerpt}
-                </div>
-            </div>
-        </a>
-    </SearchAsDraft>
+export const SearchTopicPageHit = ({
+    className,
+    hit,
+}: {
+    className?: string
+    hit: TopicPageHit
+}) => (
+    <a
+        className={cx("search-topic-page-hit", className)}
+        href={getCanonicalPath(hit.slug, hit.type)}
+    >
+        <h3 className="search-topic-page-hit__title">
+            <span className="search-topic-page-hit__title-text">
+                {hit.title}
+            </span>{" "}
+            <FontAwesomeIcon icon={faArrowRightLong} />
+        </h3>
+        <div className="search-topic-page-hit__excerpt">{hit.excerpt}</div>
+    </a>
 )
