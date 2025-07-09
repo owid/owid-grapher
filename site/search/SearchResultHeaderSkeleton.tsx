51650db6891e75e754ea7d46c5710bd9f12f088e
Martin Račák <martin.racak@ourworldindata.org>
2025-07-09 16:35:33 +0300

    Improve new search loading states
    - Each result section manages its own loading state
    - Show skeletons only when no data is loaded yet
    - When additional data is loading, use loading state in "Show more"
      button
    - "Show more" button switches to the "Loading" label only when the
      request is slow

---
+++ site/search/SearchResultHeaderSkeleton.tsx
@@ -0,0 +1,8 @@
+export const SearchResultHeaderSkeleton = () => {
+    return (
+        <div className="search-result-header-skeleton">
+            <div className="search-result-header-skeleton__title" />
+            <div className="search-result-header-skeleton__count" />
+        </div>
+    )
+}
