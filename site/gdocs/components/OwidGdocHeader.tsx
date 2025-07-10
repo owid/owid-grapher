5bcdab2f27e8b4de45ebd0b3d1979949410f1434
Ike Saunders <ikesau@protonmail.com>
2025-07-10 11:24:26 -0400

    Revert "🐛 fix topic page heading size"
    This reverts commit c6976ff93ecb0d2971b4720066efddaa7ccef88c.
    
    The display-2-semibold class was the correct one, and shouldn't have been reverted in the first place.

--- site/gdocs/components/OwidGdocHeader.tsx
+++ site/gdocs/components/OwidGdocHeader.tsx
@@ -125,7 +125,7 @@ function OwidArticleHeader({
 function OwidTopicPageHeader({ content }: { content: OwidGdocPostContent }) {
     return (
         <header className="topic-page-header grid span-cols-14 grid-cols-12-full-width">
-            <h1 className="display-1-semibold col-start-2 span-cols-8 col-sm-start-2 span-sm-cols-12">
+            <h1 className="display-2-semibold col-start-2 span-cols-8 col-sm-start-2 span-sm-cols-12">
                 {content.title}
             </h1>
             <p className="topic-page-header__subtitle body-1-regular col-start-2 span-cols-8 col-sm-start-2 span-sm-cols-12">
@@ -157,7 +157,7 @@ function OwidLinearTopicPageHeader({
 }) {
     return (
         <header className="topic-page-header grid span-cols-14 grid-cols-12-full-width">
-            <h1 className="display-1-semibold col-start-5 span-cols-6 col-md-start-3 span-md-cols-10 span-sm-cols-12 col-sm-start-2">
+            <h1 className="display-2-semibold col-start-5 span-cols-6 col-md-start-3 span-md-cols-10 span-sm-cols-12 col-sm-start-2">
                 {content.title}
             </h1>
             <p className="topic-page-header__subtitle body-1-regular col-start-5 span-cols-6 col-md-start-3 span-md-cols-10 span-sm-cols-12 col-sm-start-2">
