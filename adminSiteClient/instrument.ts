985ddb21c95e5fc47e8da49053a86fe87f09e031
Marcel Gerber <mgerber59@gmail.com>
2025-07-15 23:31:52 +0200

    fix: add manual `@types/node` reference

--- adminSiteClient/instrument.ts
+++ adminSiteClient/instrument.ts
@@ -5,6 +5,10 @@ import {
     SENTRY_ADMIN_DSN,
 } from "../settings/clientSettings.js"
 
+// This is a weird no-op, but we need to import the node types here to get
+// the types for `process.env` to work correctly.
+import "node"
+
 if (!process.env.VITEST) {
     Sentry.init({
         dsn: SENTRY_ADMIN_DSN,
