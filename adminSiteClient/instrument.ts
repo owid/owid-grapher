84da08c21b1e18d00e5fd6d13f7d68ac710b57ea
Marcel Gerber <mgerber59@gmail.com>
2025-07-16 09:16:44 +0200

    fix: change node import

--- adminSiteClient/instrument.ts
+++ adminSiteClient/instrument.ts
@@ -1,13 +1,13 @@
+// This is a weird thing, but we need to import the node types here to get
+// the types for `process.env` to work correctly.
+/// <reference types="node" />
+
 import * as Sentry from "@sentry/react"
 import {
     COMMIT_SHA,
     ENV,
     SENTRY_ADMIN_DSN,
 } from "../settings/clientSettings.js"
-
-// This is a weird no-op, but we need to import the node types here to get
-// the types for `process.env` to work correctly.
-import "node"
 
 if (!process.env.VITEST) {
     Sentry.init({
