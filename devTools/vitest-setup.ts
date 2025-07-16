3f81e5ddefd054ed50daa8e93a1660df3f983c82
Marcel Gerber <mgerber59@gmail.com>
2025-07-16 08:16:27 +0200

    docs: add comments to testing-library vitest setup

--- devTools/vitest-setup.ts
+++ devTools/vitest-setup.ts
@@ -1,4 +1,7 @@
+// https://github.com/testing-library/jest-dom#with-vitest
 import "@testing-library/jest-dom/vitest"
+
+// https://testing-library.com/docs/react-testing-library/setup#auto-cleanup-in-vitest
 import { cleanup } from "@testing-library/react"
 import { afterEach } from "vitest"
 
