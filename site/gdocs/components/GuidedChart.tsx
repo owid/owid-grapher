98d777c08e4567431b1e123dc11fa794f890d6f9
Ike Saunders <11844404+ikesau@users.noreply.github.com>
2025-07-18 16:50:58 -0400

    🎉 control grapher state from GuidedChart

--- site/gdocs/components/GuidedChart.tsx
+++ site/gdocs/components/GuidedChart.tsx
@@ -1,51 +1,39 @@
-import {
-    EnrichedBlockChart,
-    EnrichedBlockGuidedChart,
-    OwidEnrichedGdocBlock,
-} from "@ourworldindata/types"
+import { EnrichedBlockGuidedChart } from "@ourworldindata/types"
 import { Container } from "./layout.js"
-import { useRef, useCallback, useContext, useMemo } from "react"
-import {
-    GrapherState,
-    Grapher,
-    loadVariableDataAndMetadata,
-} from "@ourworldindata/grapher"
-import { traverseEnrichedBlock, guidedChartRegex } from "@ourworldindata/utils"
-import ArticleBlock from "./ArticleBlock.js"
-import { DocumentContext } from "../DocumentContext.js"
-// import { useChartConfig } from "./ChartUtils.js"
-import { DATA_API_URL } from "../../../settings/clientSettings.js"
-import SpanElements from "./SpanElements.js"
+import { useRef, useCallback } from "react"
+import { Url } from "@ourworldindata/utils"
 import { ArticleBlocks } from "./ArticleBlocks.js"
-
-function getChartBlock(
-    content: OwidEnrichedGdocBlock[]
-): EnrichedBlockChart | undefined {
-    let chart: EnrichedBlockChart | undefined
-    for (const block of content) {
-        traverseEnrichedBlock(block, (node) => {
-            if (node.type === "chart") {
-                chart = node
-            }
-        })
-    }
-    return chart
-}
+import { GuidedChartContext, GrapherState } from "@ourworldindata/grapher"
 
 export default function GuidedChart({
     d,
-    className = "",
     containerType = "default",
 }: {
     d: EnrichedBlockGuidedChart
-    className?: string
     containerType?: Container
 }) {
-    const chartBlock = getChartBlock(d.content)
-    console.log("chartBlock", chartBlock)
+    const stateRef = useRef<GrapherState | null>(null)
+
+    const handleGuidedChartLinkClick = useCallback((href: string) => {
+        if (!stateRef.current) return
+
+        stateRef.current.clearQueryParams()
+        stateRef.current.populateFromQueryParams(Url.fromURL(href).queryParams)
+    }, [])
+
     return (
-        <div className={className}>
-            <ArticleBlocks blocks={d.content} containerType={containerType} />
-        </div>
+        <GuidedChartContext.Provider
+            value={{
+                grapherStateRef: stateRef as React.RefObject<GrapherState>,
+                onGuidedChartLinkClick: handleGuidedChartLinkClick,
+            }}
+        >
+            <div className="grid grid-cols-12-full-width span-cols-14">
+                <ArticleBlocks
+                    blocks={d.content}
+                    containerType={containerType}
+                />
+            </div>
+        </GuidedChartContext.Provider>
     )
 }
