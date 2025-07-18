25c48b5cc5bcec7e76ffe00e48306ad6ac067082
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-07-18 16:37:16 +0200

    🎉 small chart hit component

--- site/search/SearchChartHitMedium.tsx
+++ site/search/SearchChartHitMedium.tsx
@@ -64,7 +64,7 @@ export function SearchChartHitMedium({
         searchQueryRegionsMatches,
     ])
     const entityQueryStr = useMemo(
-        () => getEntityQueryStr(entities),
+        () => getEntityQueryStr({ entities }),
         [entities]
     )
 
