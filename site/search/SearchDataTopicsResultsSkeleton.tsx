5192280864f52386111970baf037c68ba2cc0750
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-07-21 16:47:21 +0200

    ✨ layout of search data results

--- site/search/SearchDataTopicsResultsSkeleton.tsx
+++ site/search/SearchDataTopicsResultsSkeleton.tsx
@@ -2,35 +2,31 @@ export const SearchDataTopicsResultsSkeleton = () => {
     return (
         <>
             <div className="search-data-topics-results span-cols-14 grid grid-cols-12-full-width">
-                <DataTopicSkeleton />
-                <DataTopicSkeleton />
-                <DataTopicSkeleton />
-                <DataTopicSkeleton />
-                <DataTopicSkeleton />
-                <DataTopicSkeleton />
-                <DataTopicSkeleton />
+                <SearchChartHitMediumSkeleton />
+                <SearchChartHitMediumSkeleton />
+                <SearchChartHitMediumSkeleton />
             </div>
         </>
     )
 }
 
-const SearchChartSkeleton = () => (
+export const SearchChartHitMediumSkeleton = () => (
+    <div className="search-data-topic--skeleton span-cols-12 col-start-2">
+        <div className="search-data-topic__header--skeleton animate-pulse"></div>
+        <ul className="search-data-topic__list--skeleton grid grid-cols-4">
+            <CaptionedThumbnailSkeleton />
+            <CaptionedThumbnailSkeleton />
+            <CaptionedThumbnailSkeleton />
+            <CaptionedThumbnailSkeleton />
+        </ul>
+        <div className="search-data-topic__see-all-button--skeleton" />
+    </div>
+)
+
+const CaptionedThumbnailSkeleton = () => (
     <li className="search-chart-skeleton animate-pulse span-cols-1">
         <div className="search-chart-skeleton__thumbnail" />
         <div className="search-chart-skeleton__title" />
         <div className="search-chart-skeleton__title" />
     </li>
-)
-
-const DataTopicSkeleton = () => (
-    <div className="search-data-topic--skeleton span-cols-12 col-start-2">
-        <div className="search-data-topic__header--skeleton animate-pulse"></div>
-        <ul className="search-data-topic__list--skeleton grid grid-cols-4">
-            <SearchChartSkeleton />
-            <SearchChartSkeleton />
-            <SearchChartSkeleton />
-            <SearchChartSkeleton />
-        </ul>
-        <div className="search-data-topic__see-all-button--skeleton" />
-    </div>
 )
