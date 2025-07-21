e335564d82bc39b73a66fbec21cefc77bcc10706
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-07-21 16:47:21 +0200

    ✨ layout of search data results

--- site/search/SearchDataResultsSkeleton.tsx
+++ site/search/SearchDataResultsSkeleton.tsx
@@ -1,37 +1,21 @@
-import { SearchAsDraft } from "./SearchAsDraft.js"
 import { SearchResultHeaderSkeleton } from "./SearchResultHeaderSkeleton.js"
+import { SearchChartHitMediumSkeleton } from "./SearchDataTopicsResultsSkeleton.js"
 
 export const SearchDataResultsSkeleton = () => {
     return (
-        <SearchAsDraft name="Data Results" className="span-cols-12 col-start-2">
-            <div className="search-data-results__hits animate-pulse">
-                <SearchResultHeaderSkeleton />
-                <ul className="search-data-results__list">
-                    <li className="search-data-results__hit">
-                        <SearchChartHitSkeleton />
-                    </li>
-                    <li className="search-data-results__hit">
-                        <SearchChartHitSkeleton />
-                    </li>
-                    <li className="search-data-results__hit">
-                        <SearchChartHitSkeleton />
-                    </li>
-                    <li className="search-data-results__hit">
-                        <SearchChartHitSkeleton />
-                    </li>
-                </ul>
-            </div>
-        </SearchAsDraft>
+        <div className="search-data-results__hits span-cols-12 col-start-2 animate-pulse">
+            <SearchResultHeaderSkeleton />
+            <ul className="search-data-results__list">
+                <li className="search-data-results__hit">
+                    <SearchChartHitMediumSkeleton />
+                </li>
+                <li className="search-data-results__hit">
+                    <SearchChartHitMediumSkeleton />
+                </li>
+                <li className="search-data-results__hit">
+                    <SearchChartHitMediumSkeleton />
+                </li>
+            </ul>
+        </div>
     )
 }
-
-const SearchChartHitSkeleton = () => (
-    <div className="search-chart-hit-skeleton">
-        <div className="search-chart-hit-skeleton__thumbnail" />
-        <div className="search-chart-hit-skeleton__content">
-            <div className="search-chart-hit-skeleton__title" />
-            <div className="search-chart-hit-skeleton__subtitle" />
-            <div className="search-chart-hit-skeleton__description" />
-        </div>
-    </div>
-)
