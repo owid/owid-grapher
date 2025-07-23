f8d7698d19e5d5672ab10f28688fd4ecf37ce57e
Marcel Gerber <mgerber59@gmail.com>
2025-07-23 11:41:13 +0200

    fix(tsup): solve the problem another way
    fixes "Grapher is not a constructor"

--- packages/@ourworldindata/grapher/tsup.config.ts
+++ packages/@ourworldindata/grapher/tsup.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     esbuildPlugins: [sassPlugin()],
     tsconfig: "./tsconfig.json",
 
-    // Needed for imports of `use-sync-external-store` (in our dependencies) to work correctly
-    noExternal: ["react"],
+    // Needed for imports of `react-aria-components` to work correctly
+    // This doesn't bundle any "import" calls, meaning the output files can't be used in a browser directly,
+    // but need to be bundled again in another step. This is what we do anyway.
+    skipNodeModulesBundle: true,
+    noExternal: [/@ourworldindata\/.*/],
 })
