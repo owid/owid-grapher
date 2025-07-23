1509f28fc092c2613d73e3dc2761a98e70b8f88f
Marcel Gerber <mgerber59@gmail.com>
2025-07-23 15:00:22 +0200

    fix(tsup): enable using `use-sync-external-store` inside grapher (#5171)

--- packages/@ourworldindata/explorer/tsup.config.ts
+++ packages/@ourworldindata/explorer/tsup.config.ts
@@ -9,4 +9,10 @@ export default defineConfig({
     format: "esm",
     esbuildPlugins: [sassPlugin()],
     tsconfig: "./tsconfig.json",
+
+    // Needed for imports of `react-aria-components` to work correctly
+    // This doesn't bundle any "import" calls, meaning the output files can't be used in a browser directly,
+    // but need to be bundled again in another step. This is what we do anyway.
+    skipNodeModulesBundle: true,
+    noExternal: [/@ourworldindata\/.*/],
 })
