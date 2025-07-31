bbe568bbed08d492e5f24bd0bbcf00c6faa8f7e8
Martin Račák <martin.racak@ourworldindata.org>
2025-07-31 15:19:01 +0200

    Fix SearchDataInsightsResultsSkeleton

--- site/search/SearchDataInsightsResultsSkeleton.tsx
+++ site/search/SearchDataInsightsResultsSkeleton.tsx
@@ -1,28 +1,27 @@
-import { SearchAsDraft } from "./SearchAsDraft.js"
 import { SearchResultHeaderSkeleton } from "./SearchResultHeaderSkeleton.js"
 
 export const SearchDataInsightsResultsSkeleton = () => {
     return (
-        <SearchAsDraft
-            name="Data Insights"
-            className="grid span-cols-12 col-start-2"
-        >
+        <div className="grid span-cols-12 col-start-2">
             <section className="search-data-insights-results animate-pulse span-cols-12">
                 <SearchResultHeaderSkeleton />
-                <div className="search-data-insights-results__hits grid">
+                <div className="search-data-insights-results__hits">
                     <SearchDataInsightHitSkeleton />
                     <SearchDataInsightHitSkeleton />
                     <SearchDataInsightHitSkeleton />
                     <SearchDataInsightHitSkeleton />
                 </div>
             </section>
-        </SearchAsDraft>
+        </div>
     )
 }
 
 const SearchDataInsightHitSkeleton = () => (
-    <div className="search-data-insight-hit-skeleton span-cols-3">
-        <div className="search-data-insight-hit-skeleton__image" />
+    <div className="search-data-insight-hit-skeleton">
+        <div className="search-data-insight-hit__image-container">
+            {/* Needs non-empty children to maintain aspect ratio. */}
+            <div />
+        </div>
         <div className="search-data-insight-hit-skeleton__date" />
         <div className="search-data-insight-hit-skeleton__title" />
         <div className="search-data-insight-hit-skeleton__title" />
