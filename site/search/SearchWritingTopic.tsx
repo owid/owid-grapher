f122bd69ad2b78cae77183460a07c25c873c8ada
Martin Račák <martin.racak@ourworldindata.org>
2025-08-03 12:02:19 +0200

    Implement mobile design for new search writing landing page

--- site/search/SearchWritingTopic.tsx
+++ site/search/SearchWritingTopic.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { commafyNumber } from "@ourworldindata/utils"
 import { SearchWritingTopicsResponse } from "./searchTypes.js"
 import { useSearchContext } from "./SearchContext.js"
-import { getCanonicalPath } from "@ourworldindata/components"
+import { Button, getCanonicalPath } from "@ourworldindata/components"
 import { SearchStackedArticleHit } from "./SearchStackedArticleHit.js"
 
 export const SearchWritingTopic = ({
@@ -23,59 +23,79 @@ export const SearchWritingTopic = ({
     }
 
     return (
-        <section className="search-writing-topic span-cols-12 col-start-2">
-            <button
-                className="search-writing-topic__header"
-                type="button"
-                aria-label={`Filter by topic ${result.title}`}
-                onClick={handleAddTopicClick}
-            >
-                <h2 className="search-writing-topic__title">{result.title}</h2>
-                <div className="search-writing-topic__count">
-                    {commafyNumber(result.totalCount)} articles and topic pages
-                    <div className="search-writing-topic__arrow">
-                        <FontAwesomeIcon icon={faArrowRight} />
+        <section className="search-writing-topic-container span-cols-12 col-start-2">
+            <div className="search-writing-topic">
+                <button
+                    className="search-writing-topic__header"
+                    type="button"
+                    aria-label={`Filter by topic ${result.title}`}
+                    onClick={handleAddTopicClick}
+                >
+                    <h2 className="search-writing-topic__title">
+                        {result.title}
+                    </h2>
+                    <div className="search-writing-topic__count">
+                        {commafyNumber(result.totalCount)} articles and topic
+                        pages
+                        <div className="search-writing-topic__arrow">
+                            <FontAwesomeIcon icon={faArrowRight} />
+                        </div>
                     </div>
-                </div>
-            </button>
+                </button>
 
-            <div className="search-writing-topic__content">
-                <div className="search-writing-topic__featured-articles">
-                    <h3 className="search-writing-topic__sub-title">
-                        Featured articles
-                    </h3>
-                    <div className="search-writing-featured-articles__list">
-                        {result.articles.hits.map((hit) => (
-                            <SearchStackedArticleHit
-                                key={hit.objectID}
-                                hit={hit}
-                            />
-                        ))}
+                <div className="search-writing-topic__content">
+                    <div className="search-writing-topic__featured-articles">
+                        <h3 className="search-writing-topic__sub-title">
+                            Featured articles
+                        </h3>
+                        <div className="search-writing-featured-articles__list">
+                            {result.articles.hits.map((hit) => (
+                                <SearchStackedArticleHit
+                                    key={hit.objectID}
+                                    hit={hit}
+                                />
+                            ))}
+                        </div>
                     </div>
-                </div>
 
-                <div className="search-writing-topic__featured-topic-pages">
-                    <h3 className="search-writing-topic__sub-title">
-                        Featured topic pages
-                    </h3>
-                    <ul className="search-writing-featured-topics__list">
-                        {result.topicPages.hits.map((hit) => (
-                            <li key={hit.objectID}>
-                                <a
-                                    className="search-writing-topic-link"
-                                    href={getCanonicalPath(hit.slug, hit.type)}
+                    <hr className="search-writing-topic__divider" />
+
+                    <div className="search-writing-topic__featured-topic-pages">
+                        <h3 className="search-writing-topic__sub-title">
+                            Featured topic pages
+                        </h3>
+                        <ul className="search-writing-featured-topics__list">
+                            {result.topicPages.hits.map((hit) => (
+                                <li
+                                    key={hit.objectID}
+                                    className="search-writing-featured-topics__list-item"
                                 >
-                                    {hit.title}
-                                    <FontAwesomeIcon
-                                        className="search-writing-topic-link__icon"
-                                        icon={faArrowRight}
-                                    />
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
+                                    <a
+                                        className="search-writing-topic-link"
+                                        href={getCanonicalPath(
+                                            hit.slug,
+                                            hit.type
+                                        )}
+                                    >
+                                        {hit.title}
+                                        <FontAwesomeIcon
+                                            className="search-writing-topic-link__icon"
+                                            icon={faArrowRight}
+                                        />
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
+
+            <Button
+                className="search-writing-topic__see-all"
+                theme="solid-light-blue"
+                text={`See all ${commafyNumber(result.totalCount)} articles and topic pages`}
+                onClick={handleAddTopicClick}
+            />
         </section>
     )
 }
