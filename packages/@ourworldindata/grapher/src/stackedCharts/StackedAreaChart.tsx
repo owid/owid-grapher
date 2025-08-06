8571bd3a5dcf95678057435b9918b3cd6ab98805
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-06 14:09:49 +0200

    🎉 stacked area chart thumbnail

--- packages/@ourworldindata/grapher/src/stackedCharts/StackedAreaChart.tsx
+++ packages/@ourworldindata/grapher/src/stackedCharts/StackedAreaChart.tsx
@@ -45,6 +45,7 @@ import { HorizontalColorLegendManager } from "../horizontalColorLegend/Horizonta
 import { CategoricalBin } from "../color/ColorScaleBin"
 import { ChartComponentProps } from "../chart/ChartTypeMap.js"
 import { InteractionState } from "../interaction/InteractionState"
+import { resolveCollision } from "./StackedUtils"
 
 const STACKED_AREA_CHART_CLASS_NAME = "StackedArea"
 
@@ -123,64 +124,23 @@ export class StackedAreaChart
     }
 
     @computed private get horizontalAxisPart(): HorizontalAxis {
-        const axis = this.xAxisConfig.toHorizontalAxis()
-        axis.updateDomainPreservingUserSettings(
-            this.chartState.transformedTable.timeDomainFor(
-                this.chartState.yColumnSlugs
-            )
-        )
-        axis.formatColumn = this.chartState.inputTable.timeColumn
-        axis.hideFractionalTicks = true
-        return axis
+        return this.chartState.toHorizontalAxis(this.xAxisConfig)
     }
 
     @computed private get yAxisConfig(): AxisConfig {
-        return new AxisConfig(
-            {
-                nice: true,
-                ...this.manager.yAxisConfig,
-            },
-            this
-        )
+        const { yAxisConfig } = this.manager
+        const custom = { nice: true }
+        return new AxisConfig({ ...custom, ...yAxisConfig }, this)
     }
 
     @computed private get verticalAxisPart(): VerticalAxis {
-        const axis = this.yAxisConfig.toVerticalAxis()
-        // Use user settings for axis, unless relative mode
-        if (this.manager.isRelativeMode) axis.domain = [0, 100]
-        else axis.updateDomainPreservingUserSettings(this.yAxisDomain)
-        axis.formatColumn = this.chartState.yColumns[0]
-        return axis
-    }
-
-    @computed private get yAxisDomain(): [number, number] {
-        const yValues = this.chartState.allStackedPoints.map(
-            (point) => point.value + point.valueOffset
-        )
-        return [0, _.max(yValues) ?? 0]
+        return this.chartState.toVerticalAxis(this.yAxisConfig)
     }
 
     @computed private get xAxisConfig(): AxisConfig {
-        return new AxisConfig(
-            {
-                hideGridlines: true,
-                ...this.manager.xAxisConfig,
-            },
-            this
-        )
-    }
-
-    @computed private get midpoints(): number[] {
-        let prevY = 0
-        return this.stackedSeries.map((series) => {
-            const lastValue = R.last(series.points)
-            if (!lastValue) return 0
-
-            const y = lastValue.value + lastValue.valueOffset
-            const middleY = prevY + (y - prevY) / 2
-            prevY = y
-            return middleY
-        })
+        const { xAxisConfig } = this.manager
+        const custom = { hideGridlines: true }
+        return new AxisConfig({ ...custom, ...xAxisConfig }, this)
     }
 
     private hoverStateForSeries(
@@ -193,13 +153,12 @@ export class StackedAreaChart
     }
 
     @computed private get lineLegendSeries(): LineLabelSeries[] {
-        const { midpoints } = this
         return this.stackedSeries
             .map((series, index) => ({
                 color: series.color,
                 seriesName: series.seriesName,
                 label: series.seriesName,
-                yValue: midpoints[index],
+                yValue: this.chartState.midpoints[index],
                 isAllZeros: series.isAllZeros,
                 hover: this.hoverStateForSeries(series),
             }))
@@ -281,27 +240,9 @@ export class StackedAreaChart
                     if (!s1) return PREFER_S2
                     if (!s2) return PREFER_S1
 
-                    // early return if one series is all zeroes
-                    if (s1.isAllZeros && !s2.isAllZeros) return PREFER_S2
-                    if (s2.isAllZeros && !s1.isAllZeros) return PREFER_S1
-
-                    // prefer series with a higher maximum value
-                    const yMax1 = _.maxBy(s1.points, (p) => p.value)?.value ?? 0
-                    const yMax2 = _.maxBy(s2.points, (p) => p.value)?.value ?? 0
-                    if (yMax1 > yMax2) return PREFER_S1
-                    if (yMax2 > yMax1) return PREFER_S2
-
-                    // prefer series with a higher last value
-                    const yLast1 = R.last(s1.points)?.value ?? 0
-                    const yLast2 = R.last(s2.points)?.value ?? 0
-                    if (yLast1 > yLast2) return PREFER_S1
-                    if (yLast2 > yLast1) return PREFER_S2
-
-                    // prefer series with a higher total area
-                    const area1 = _.sumBy(s1.points, (p) => p.value)
-                    const area2 = _.sumBy(s2.points, (p) => p.value)
-                    if (area1 > area2) return PREFER_S1
-                    if (area2 > area1) return PREFER_S2
+                    const picked = resolveCollision(s1, s2)
+                    if (picked === s1) return PREFER_S1
+                    if (picked === s2) return PREFER_S2
 
                     return 0
                 }
