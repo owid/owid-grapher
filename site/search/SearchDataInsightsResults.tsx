bfe8a25be63c7ff437083f3d0753a449d7a19dfc
Martin Račák <martin.racak@ourworldindata.org>
2025-08-06 13:02:34 +0200

    Implement mobile design for new search writing results

--- site/search/SearchDataInsightsResults.tsx
+++ site/search/SearchDataInsightsResults.tsx
@@ -1,3 +1,6 @@
+import { useRef } from "react"
+import { useIntersectionObserver } from "usehooks-ts"
+
 import { DataInsightHit, SearchDataInsightResponse } from "./searchTypes.js"
 import { queryDataInsights, searchQueryKeys } from "./queries.js"
 import { SearchDataInsightHit } from "./SearchDataInsightHit.js"
@@ -5,11 +8,25 @@ import { SearchResultHeader } from "./SearchResultHeader.js"
 import { SearchShowMore } from "./SearchShowMore.js"
 import { useInfiniteSearch } from "./searchHooks.js"
 import { SearchDataInsightsResultsSkeleton } from "./SearchDataInsightsResultsSkeleton.js"
+import { SearchHorizontalDivider } from "./SearchHorizontalDivider.js"
 
 export function SearchDataInsightsResults() {
     const query = useInfiniteSearch<SearchDataInsightResponse, DataInsightHit>({
         queryKey: (state) => searchQueryKeys.dataInsights(state),
         queryFn: queryDataInsights,
+    })
+
+    const container = useRef<HTMLDivElement>(null)
+    const { ref: triggerRef } = useIntersectionObserver({
+        root: container.current,
+        // Observe only the x-axis.
+        // https://stackoverflow.com/a/68714239/9846837
+        rootMargin: "100% 600px 100% 0%",
+        onChange: (isIntersecting) => {
+            if (isIntersecting && !query.isFetchingNextPage) {
+                void query.fetchNextPage()
+            }
+        },
     })
 
     const { hits, totalResults, isInitialLoading } = query
@@ -18,21 +35,34 @@ export function SearchDataInsightsResults() {
     if (totalResults === 0) return null
 
     return (
-        <section className="col-start-2 span-cols-12">
-            <SearchResultHeader count={totalResults}>
-                Data Insights
-            </SearchResultHeader>
-            <div className="search-data-insights-results__hits">
-                {hits.map((hit: DataInsightHit) => (
-                    <SearchDataInsightHit key={hit.objectID} hit={hit} />
-                ))}
-            </div>
-            {query.hasNextPage && (
-                <SearchShowMore
-                    isLoading={query.isFetchingNextPage}
-                    onClick={query.fetchNextPage}
-                />
-            )}
-        </section>
+        <>
+            <section>
+                <SearchResultHeader count={totalResults}>
+                    Data Insights
+                </SearchResultHeader>
+                <div
+                    ref={container}
+                    className="search-data-insights-results__hits"
+                >
+                    {hits.map((hit: DataInsightHit) => (
+                        <SearchDataInsightHit key={hit.objectID} hit={hit} />
+                    ))}
+                    {query.hasNextPage && (
+                        <div
+                            ref={triggerRef}
+                            className="search-data-insights-results__trigger"
+                        />
+                    )}
+                </div>
+                {query.hasNextPage && (
+                    <SearchShowMore
+                        className="search-data-insights-results__show-more"
+                        isLoading={query.isFetchingNextPage}
+                        onClick={query.fetchNextPage}
+                    />
+                )}
+            </section>
+            <SearchHorizontalDivider />
+        </>
     )
 }
