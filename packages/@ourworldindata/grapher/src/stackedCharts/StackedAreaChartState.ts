78e473874a6222b7b538a0503abdbdf4c490ab6b
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-06 14:09:49 +0200

    🎉 stacked area chart thumbnail

--- packages/@ourworldindata/grapher/src/stackedCharts/StackedAreaChartState.ts
+++ packages/@ourworldindata/grapher/src/stackedCharts/StackedAreaChartState.ts
@@ -1,3 +1,4 @@
+import * as R from "remeda"
 import { computed, makeObservable } from "mobx"
 import { AbstractStackedChartState } from "./AbstractStackedChartState.js"
 import { ChartState } from "../chart/ChartInterface.js"
@@ -16,11 +17,24 @@ export class StackedAreaChartState
 
     shouldRunLinearInterpolation = true
 
+    @computed get useValueBasedColorScheme(): boolean {
+        return false
+    }
+
     @computed get series(): readonly StackedSeries<number>[] {
         return stackSeries(withMissingValuesAsZeroes(this.unstackedSeries))
     }
 
-    @computed get useValueBasedColorScheme(): boolean {
-        return false
+    @computed get midpoints(): number[] {
+        let prevY = 0
+        return this.series.map((series) => {
+            const lastValue = R.last(series.points)
+            if (!lastValue) return 0
+
+            const y = lastValue.value + lastValue.valueOffset
+            const middleY = prevY + (y - prevY) / 2
+            prevY = y
+            return middleY
+        })
     }
 }
