a082fdb9f4d673e2d525f0eec2067c5c78b88550
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-07 16:24:17 +0200

    ✨ render map legend

--- packages/@ourworldindata/grapher/src/mapCharts/MapChartThumbnail.tsx
+++ packages/@ourworldindata/grapher/src/mapCharts/MapChartThumbnail.tsx
@@ -3,21 +3,12 @@ import { computed, makeObservable } from "mobx"
 import { observer } from "mobx-react"
 import { ChartInterface } from "../chart/ChartInterface"
 import { MapChartState } from "./MapChartState"
-import { MapChartProps } from "./MapChart"
-import {
-    ChoroplethMapManager,
-    ChoroplethSeriesByName,
-} from "./MapChartConstants"
-import { Bounds } from "@ourworldindata/utils"
-import { DEFAULT_GRAPHER_BOUNDS } from "../core/GrapherConstants"
-import { MapConfig } from "./MapConfig"
-import { CoreColumn } from "@ourworldindata/core-table"
-import { ChoroplethMap } from "./ChoroplethMap.js"
+import { MapChart, MapChartProps } from "./MapChart"
 
 @observer
 export class MapChartThumbnail
     extends React.Component<MapChartProps>
-    implements ChartInterface, ChoroplethMapManager
+    implements ChartInterface
 {
     constructor(props: MapChartProps) {
         super(props)
@@ -28,28 +19,7 @@ export class MapChartThumbnail
         return this.props.chartState
     }
 
-    @computed private get bounds(): Bounds {
-        return this.props.bounds ?? DEFAULT_GRAPHER_BOUNDS
-    }
-
-    @computed get choroplethMapBounds(): Bounds {
-        return this.bounds
-    }
-
-    @computed get choroplethData(): ChoroplethSeriesByName {
-        return this.chartState.seriesMap
-    }
-
-    @computed get mapConfig(): MapConfig {
-        return this.chartState.mapConfig
-    }
-
-    @computed get mapColumn(): CoreColumn {
-        return this.chartState.mapColumn
-    }
-
     override render(): React.ReactElement {
-        // TODO: Render MapChart with legend
-        return <ChoroplethMap manager={this} />
+        return <MapChart {...this.props} />
     }
 }
