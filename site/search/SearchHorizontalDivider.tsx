1e0918d7b44e449438bfa6f73a26c2785c92328d
Martin Račák <martin.racak@ourworldindata.org>
2025-08-12 17:39:18 +0200

    Implement feedback from new search QA

--- site/search/SearchHorizontalDivider.tsx
+++ site/search/SearchHorizontalDivider.tsx
@@ -1,9 +1,49 @@
 import cx from "classnames"
+import { Button } from "@ourworldindata/components"
+import { useState } from "react"
+import { useTimeout } from "usehooks-ts"
 
 export function SearchHorizontalDivider({
-    className = "",
+    className,
+    hasButton,
+    isLoading,
+    onClick,
 }: {
     className?: string
+    hasButton?: boolean
+    isLoading?: boolean
+    onClick?: () => void
 }) {
-    return <hr className={cx("search-horizontal-divider", className)} />
+    // Don't flash the loading text when the loading is fast.
+    const [showLoadingText, setShowLoadingText] = useState(false)
+    useTimeout(() => setShowLoadingText(true), isLoading ? 200 : null)
+
+    // Reset immediately when not loading.
+    if (!isLoading && showLoadingText) {
+        setShowLoadingText(false)
+    }
+
+    return (
+        <div
+            className={cx(
+                "search-horizontal-divider",
+                {
+                    "search-horizontal-divider--has-button": hasButton,
+                },
+                className
+            )}
+        >
+            {hasButton && (
+                <Button
+                    className="search-horizontal-divider__button"
+                    theme="solid-light-blue"
+                    icon={null}
+                    text={showLoadingText ? "Loading..." : "Show more"}
+                    onClick={onClick}
+                    disabled={isLoading}
+                    ariaLabel="Load more results"
+                />
+            )}
+        </div>
+    )
 }
