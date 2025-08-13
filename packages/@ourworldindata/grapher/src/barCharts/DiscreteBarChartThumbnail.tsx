f614f02a694b304412fa2499d76105d1fb2e62a4
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-13 09:20:39 +0200

    🎉 add minimal/thumbnail rendering

--- packages/@ourworldindata/grapher/src/barCharts/DiscreteBarChartThumbnail.tsx
+++ packages/@ourworldindata/grapher/src/barCharts/DiscreteBarChartThumbnail.tsx
@@ -3,8 +3,13 @@ import { computed, makeObservable } from "mobx"
 import { observer } from "mobx-react"
 import { ChartInterface } from "../chart/ChartInterface"
 import { DiscreteBarChartState } from "./DiscreteBarChartState"
-import { type DiscreteBarChartProps } from "./DiscreteBarChart.js"
+import {
+    DiscreteBarChart,
+    type DiscreteBarChartProps,
+} from "./DiscreteBarChart.js"
 import { DiscreteBars } from "./DiscreteBars"
+import { DiscreteBarChartManager } from "./DiscreteBarChartConstants.js"
+import { GrapherVariant } from "@ourworldindata/types"
 
 @observer
 export class DiscreteBarChartThumbnail
@@ -20,7 +25,19 @@ export class DiscreteBarChartThumbnail
         return this.props.chartState
     }
 
+    @computed get manager(): DiscreteBarChartManager {
+        return this.props.chartState.manager
+    }
+
+    @computed private get isMinimal(): boolean {
+        return this.manager.variant === GrapherVariant.MinimalThumbnail
+    }
+
     override render(): React.ReactElement {
-        return <DiscreteBars {...this.props} series={this.chartState.series} />
+        return this.isMinimal ? (
+            <DiscreteBars {...this.props} series={this.chartState.series} />
+        ) : (
+            <DiscreteBarChart {...this.props} />
+        )
     }
 }
