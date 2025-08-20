bf68dcfd8eb94fc8531217f9492d4c48570d3516
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-20 09:25:39 +0200

    🎉 map thumbnail

--- packages/@ourworldindata/grapher/src/mapCharts/MapChart.tsx
+++ packages/@ourworldindata/grapher/src/mapCharts/MapChart.tsx
@@ -22,7 +22,6 @@ import {
     GeoFeature,
     MapBracket,
     MapChartManager,
-    ChoroplethSeries,
     DEFAULT_STROKE_COLOR,
     ChoroplethSeriesByName,
     ChoroplethMapManager,
@@ -49,7 +48,7 @@ import {
     isProjectedDataBin,
     NumericBin,
 } from "../color/ColorScaleBin"
-import { ColumnSlug, MapRegionName, SeriesName } from "@ourworldindata/types"
+import { ColumnSlug, MapRegionName } from "@ourworldindata/types"
 import { ClipPath, makeClipPath } from "../chart/ChartUtils"
 import { NoDataModal } from "../noDataModal/NoDataModal"
 import { Component, createRef } from "react"
@@ -131,7 +130,7 @@ export class MapChart
     }
 
     @computed get choroplethData(): ChoroplethSeriesByName {
-        return this.seriesMap
+        return this.chartState.seriesMap
     }
 
     base = createRef<SVGGElement>()
@@ -206,18 +205,6 @@ export class MapChart
 
     @action.bound onRegionChange(value: MapRegionName): void {
         this.mapConfig.region = value
-    }
-
-    @computed private get series(): ChoroplethSeries[] {
-        return this.chartState.series
-    }
-
-    @computed private get seriesMap(): ChoroplethSeriesByName {
-        const map = new Map<SeriesName, ChoroplethSeries>()
-        this.series.forEach((series) => {
-            map.set(series.seriesName, series)
-        })
-        return map
     }
 
     @computed private get colorScale(): ColorScale {
@@ -539,6 +526,10 @@ export class MapChart
             renderUid: this.renderUid,
             box: this.choroplethMapBounds,
         })
+    }
+
+    @computed get numericBinSize(): number {
+        return 0.625 * this.fontSize
     }
 
     renderMapLegend(): React.ReactElement {
