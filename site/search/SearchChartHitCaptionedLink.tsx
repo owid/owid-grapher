95d8114671e95f622cf84346bd546af79d7cc4fb
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-20 14:35:14 +0200

    🔨 refactor medium chart component

---
+++ site/search/SearchChartHitCaptionedLink.tsx
@@ -0,0 +1,28 @@
+import cx from "classnames"
+
+export function CaptionedLink({
+    url,
+    className,
+    onClick,
+    children,
+    caption,
+}: {
+    url: string
+    className?: string
+    onClick?: () => void
+    children: React.ReactNode
+    caption: string
+}): React.ReactElement {
+    return (
+        <a
+            href={url}
+            className={cx("search-chart-hit-captioned-link", className)}
+            onClick={onClick}
+        >
+            {children}
+            <div className="search-chart-hit-captioned-link__label">
+                {caption}
+            </div>
+        </a>
+    )
+}
