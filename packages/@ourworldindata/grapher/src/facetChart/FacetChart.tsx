acee8132012083c381fe436e3bfcb4e58c9e567a
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-22 12:09:38 +0200

    ✨ center-align numeric legends

--- packages/@ourworldindata/grapher/src/facetChart/FacetChart.tsx
+++ packages/@ourworldindata/grapher/src/facetChart/FacetChart.tsx
@@ -705,7 +705,9 @@ export class FacetChart
     }
 
     @computed get legendAlign(): HorizontalAlign {
-        return HorizontalAlign.left
+        return this.isNumericLegend
+            ? HorizontalAlign.center
+            : HorizontalAlign.left
     }
 
     @computed get legendTitle(): string | undefined {
