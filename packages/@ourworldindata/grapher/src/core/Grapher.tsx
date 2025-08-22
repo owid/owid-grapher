9e95ed54210f0f32cdb632dd8ee523cd24bca179
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-22 08:52:25 +0200

    🎉 line chart thumbnail

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -139,6 +139,7 @@ import {
     DEFAULT_GRAPHER_BOUNDS_SQUARE,
     GrapherModal,
     CHART_TYPES_THAT_SWITCH_TO_DISCRETE_BAR_WHEN_SINGLE_TIME,
+    GRAPHER_THUMBNAIL_PADDING,
 } from "../core/GrapherConstants"
 import Cookies from "js-cookie"
 import { ChartDimension } from "../chart/ChartDimension"
@@ -2736,7 +2737,7 @@ export class GrapherState {
     /** Bounds of the chart area if no CaptionedChart is rendered */
     @computed get chartAreaBounds(): Bounds {
         // 2px accounts for the border
-        return this.activeBounds.pad(2)
+        return this.activeBounds.pad(GRAPHER_THUMBNAIL_PADDING + 2)
     }
 
     /** Bounds of the entity selector if rendered into the side panel */
@@ -2994,6 +2995,7 @@ export class GrapherState {
     @computed get isModalOpen(): boolean {
         return this.isEntitySelectorModalOpen || this.activeModal !== undefined
     }
+
     // Whether a server-side download is available for the download modal
     @computed get isServerSideDownloadAvailable(): boolean {
         return (
@@ -3010,16 +3012,20 @@ export class GrapherState {
     }
     _baseFontSize = BASE_FONT_SIZE
     @computed get baseFontSize(): number {
+        if (this.isStatic && this.initialOptions.baseFontSize)
+            return this.initialOptions.baseFontSize
         if (this.isStaticAndSmall) {
             return this.computeBaseFontSizeFromHeight(this.staticBounds)
         }
         if (this.isStatic) return 18
         return this._baseFontSize
     }
+
     // the header and footer don't rely on the base font size unless explicitly specified
     @computed get useBaseFontSize(): boolean {
         return this.initialOptions.baseFontSize !== undefined
     }
+
     private computeBaseFontSizeFromHeight(bounds: Bounds): number {
         const squareBounds = DEFAULT_GRAPHER_BOUNDS_SQUARE
         const factor = squareBounds.height / 21
@@ -3060,6 +3066,7 @@ export class GrapherState {
         if (!this.isStatic) return false
         return this.areStaticBoundsSmall
     }
+
     @computed get areStaticBoundsSmall(): boolean {
         const { defaultBounds, staticBounds } = this
         const idealPixelCount = defaultBounds.width * defaultBounds.height
@@ -4172,6 +4179,7 @@ export class Grapher extends React.Component<GrapherProps> {
             this.hasBeenVisible = true
         }
     }
+
     @action.bound private setBaseFontSize(): void {
         this.grapherState.baseFontSize =
             this.grapherState.computeBaseFontSizeFromWidth(
