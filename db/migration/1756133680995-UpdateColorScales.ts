6041f39e0599599e16325d8c9fe2f21d76956b17
Marcel Gerber <mgerber59@gmail.com>
2025-08-26 15:12:59 +0200

    add db migration

---
+++ db/migration/1756133680995-UpdateColorScales.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+const tables = [
+    { table: "chart_configs", column: "patch" },
+    { table: "chart_configs", column: "full" },
+    { table: "chart_revisions", column: "config" },
+]
+
+const fields = ["$.map.colorScale", "$.colorScale"]
+
+export class UpdateColorScales1756133680995 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        for (const { table, column } of tables) {
+            for (const field of fields) {
+                await queryRunner.query(
+                    `-- sql
+                    UPDATE ${table}
+                    SET ${column} = JSON_REMOVE(
+                        ${column},
+                        '${field}.binningStrategyBinCount'
+                    )
+                    WHERE
+                        ${column}->>'${field}.binningStrategyBinCount' IS NOT NULL
+                `
+                )
+                await queryRunner.query(
+                    `-- sql
+                    UPDATE ${table}
+                    SET ${column} = JSON_REMOVE(
+                        ${column},
+                        '${field}.binningStrategy'
+                    )
+                    WHERE
+                        ${column}->>'${field}.binningStrategy' != 'manual'
+                `
+                )
+            }
+        }
+    }
+
+    public async down(_queryRunner: QueryRunner): Promise<void> {
+        // No down migration possible without losing information
+    }
+}
