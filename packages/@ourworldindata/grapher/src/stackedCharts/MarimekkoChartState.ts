c9568907d7cf1ef043512964382cd63dbfa01161
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-26 09:33:22 +0200

    🎉 marimekko chart thumbnail

--- packages/@ourworldindata/grapher/src/stackedCharts/MarimekkoChartState.ts
+++ packages/@ourworldindata/grapher/src/stackedCharts/MarimekkoChartState.ts
@@ -1,23 +1,43 @@
+import * as _ from "lodash-es"
+import * as R from "remeda"
 import { computed, makeObservable } from "mobx"
 import { ChartState } from "../chart/ChartInterface"
 import { ColorScale, ColorScaleManager } from "../color/ColorScale"
 import {
+    Bar,
+    BarShape,
     EntityColorData,
+    Item,
     MarimekkoChartManager,
+    SimpleChartSeries,
+    SimplePoint,
 } from "./MarimekkoChartConstants"
 import { CoreColumn, OwidTable } from "@ourworldindata/core-table"
-import { autoDetectYColumnSlugs } from "../chart/ChartUtils"
+import {
+    autoDetectYColumnSlugs,
+    getShortNameForEntity,
+    makeSelectionArray,
+} from "../chart/ChartUtils"
 import { ColorScaleConfig } from "../color/ColorScaleConfig"
 import {
     ChartErrorInfo,
     ColorSchemeName,
     EntityName,
     ColorScaleConfigInterface,
+    OwidVariableRow,
+    SortConfig,
+    SortBy,
+    SortOrder,
 } from "@ourworldindata/types"
 import { OWID_NO_DATA_GRAY } from "../color/ColorConstants"
-import { StackedSeries } from "./StackedConstants"
+import { StackedPoint, StackedSeries } from "./StackedConstants"
 import { ColorScheme } from "../color/ColorScheme"
 import { ColorSchemes } from "../color/ColorSchemes"
+import { excludeUndefined } from "@ourworldindata/utils"
+import { SelectionArray } from "../selection/SelectionArray"
+import { FocusArray } from "../focus/FocusArray"
+import { AxisConfig } from "../axis/AxisConfig.js"
+import { HorizontalAxis, VerticalAxis } from "../axis/Axis.js"
 
 export class MarimekkoChartState implements ChartState, ColorScaleManager {
     manager: MarimekkoChartManager
@@ -79,6 +99,18 @@ export class MarimekkoChartState implements ChartState, ColorScaleManager {
         }
 
         return table
+    }
+
+    @computed get selectionArray(): SelectionArray {
+        return makeSelectionArray(this.manager.selection)
+    }
+
+    @computed get focusArray(): FocusArray {
+        return this.manager.focusArray ?? new FocusArray()
+    }
+
+    @computed get isFocusModeActive(): boolean {
+        return this.focusArray.hasFocusedSeries
     }
 
     @computed get yColumnSlugs(): string[] {
@@ -146,32 +178,8 @@ export class MarimekkoChartState implements ChartState, ColorScaleManager {
         )
     }
 
-    @computed get domainColorForEntityMap(): Map<string, EntityColorData> {
-        const { colorColumn, colorScale, uniqueEntityNames } = this
-        const hasColorColumn = !colorColumn.isMissing
-        const colorRowsByEntity = hasColorColumn
-            ? colorColumn.owidRowsByEntityName
-            : undefined
-        const domainColorMap = new Map<string, EntityColorData>()
-        if (uniqueEntityNames !== undefined) {
-            for (const name of uniqueEntityNames) {
-                const colorDomainValue = colorRowsByEntity?.get(name)?.[0]
-
-                if (colorDomainValue) {
-                    const color = colorScale.getColor(colorDomainValue.value)
-                    if (color)
-                        domainColorMap.set(name, {
-                            color,
-                            colorDomainValue: colorDomainValue.value,
-                        })
-                }
-            }
-        }
-        return domainColorMap
-    }
-
-    @computed get uniqueEntityNames(): EntityName[] | undefined {
-        return this.xColumn?.uniqEntityNames ?? this.yColumns[0].uniqEntityNames
+    @computed private get sortConfig(): SortConfig {
+        return this.manager.sortConfig ?? {}
     }
 
     @computed private get unstackedSeries(): StackedSeries<EntityName>[] {
@@ -209,6 +217,235 @@ export class MarimekkoChartState implements ChartState, ColorScaleManager {
                 return newPoint
             }),
         }))
+    }
+
+    @computed private get allPoints(): StackedPoint<EntityName>[] {
+        return this.series.flatMap((series) => series.points)
+    }
+
+    @computed get x0(): number {
+        return 0
+    }
+
+    @computed get y0(): number {
+        return 0
+    }
+
+    @computed get xSeries(): SimpleChartSeries | undefined {
+        const createStackedXPoints = (
+            rows: OwidVariableRow<any>[]
+        ): SimplePoint[] => {
+            const points: SimplePoint[] = []
+            for (const row of rows) {
+                points.push({
+                    time: row.originalTime,
+                    value: row.value,
+                    entity: row.entityName,
+                })
+            }
+            return points
+        }
+        if (this.xColumn === undefined) return undefined
+        const column = this.xColumn
+        return {
+            seriesName: column.displayName,
+            points: createStackedXPoints(column.owidRows),
+        }
+    }
+
+    @computed get domainColorForEntityMap(): Map<string, EntityColorData> {
+        const { colorColumn, colorScale, uniqueEntityNames } = this
+        const hasColorColumn = !colorColumn.isMissing
+        const colorRowsByEntity = hasColorColumn
+            ? colorColumn.owidRowsByEntityName
+            : undefined
+        const domainColorMap = new Map<string, EntityColorData>()
+        if (uniqueEntityNames !== undefined) {
+            for (const name of uniqueEntityNames) {
+                const colorDomainValue = colorRowsByEntity?.get(name)?.[0]
+
+                if (colorDomainValue) {
+                    const color = colorScale.getColor(colorDomainValue.value)
+                    if (color)
+                        domainColorMap.set(name, {
+                            color,
+                            colorDomainValue: colorDomainValue.value,
+                        })
+                }
+            }
+        }
+        return domainColorMap
+    }
+
+    @computed private get uniqueEntityNames(): EntityName[] | undefined {
+        return this.xColumn?.uniqEntityNames ?? this.yColumns[0].uniqEntityNames
+    }
+
+    @computed get items(): Item[] {
+        const { xSeries, series, domainColorForEntityMap, uniqueEntityNames } =
+            this
+
+        if (uniqueEntityNames === undefined) return []
+
+        const items: Item[] = uniqueEntityNames
+            .map((entityName) => {
+                const xPoint = xSeries
+                    ? xSeries.points.find(
+                          (point) => point.entity === entityName
+                      )
+                    : undefined
+                if (xSeries && !xPoint) return undefined
+
+                const color = domainColorForEntityMap.get(entityName)
+
+                return {
+                    entityName,
+                    shortEntityName: getShortNameForEntity(entityName),
+                    xPoint: xPoint,
+                    entityColor: color,
+                    bars: excludeUndefined(
+                        series.map((series): Bar | undefined => {
+                            const point = series.points.find(
+                                (point) => point.position === entityName
+                            )
+                            if (!point) return undefined
+                            return {
+                                kind: BarShape.Bar,
+                                yPoint: point,
+                                color: series.color,
+                                seriesName: series.seriesName,
+                                columnSlug: series.columnSlug,
+                            }
+                        })
+                    ),
+                    focus: this.focusArray.state(entityName),
+                }
+            })
+            .filter((item) => item !== undefined) satisfies Item[]
+
+        return items
+    }
+
+    @computed get sortedItems(): Item[] {
+        const { items, sortConfig } = this
+
+        let sortByFuncs: ((item: Item) => number | string | undefined)[]
+        switch (sortConfig.sortBy) {
+            case SortBy.custom:
+                sortByFuncs = [(): undefined => undefined]
+                break
+            case SortBy.entityName:
+                sortByFuncs = [(item: Item): string => item.entityName]
+                break
+            case SortBy.column: {
+                const sortColumnSlug = sortConfig.sortColumnSlug
+                sortByFuncs = [
+                    (item: Item): number =>
+                        item.bars.find((b) => b.seriesName === sortColumnSlug)
+                            ?.yPoint.value ?? 0,
+                    (item: Item): string => item.entityName,
+                ]
+                break
+            }
+            default:
+            case SortBy.total:
+                sortByFuncs = [
+                    (item: Item): number => {
+                        const lastPoint = R.last(item.bars)?.yPoint
+                        if (!lastPoint) return 0
+                        return lastPoint.valueOffset + lastPoint.value
+                    },
+                    (item: Item): string => item.entityName,
+                ]
+        }
+        const sortedItems = _.sortBy(items, sortByFuncs)
+        const sortOrder = sortConfig.sortOrder ?? SortOrder.desc
+        if (sortOrder === SortOrder.desc) sortedItems.reverse()
+
+        const [itemsWithValues, itemsWithoutValues] = _.partition(
+            sortedItems,
+            (item) => item.bars.length !== 0
+        )
+
+        return [...itemsWithValues, ...itemsWithoutValues]
+    }
+
+    @computed get selectedItems(): Item[] {
+        const selectedSet = this.selectionArray.selectedSet
+        if (selectedSet.size === 0) return []
+        return this.sortedItems.filter((item) =>
+            selectedSet.has(item.entityName)
+        )
+    }
+
+    @computed get yDomainDefault(): [number, number] {
+        const maxValues = this.allPoints.map(
+            (point) => point.value + point.valueOffset
+        )
+        return [
+            Math.min(this.y0, _.min(maxValues) as number),
+            Math.max(this.y0, _.max(maxValues) as number),
+        ]
+    }
+
+    @computed get xDomainDefault(): [number, number] {
+        if (this.xSeries !== undefined) {
+            const sum = _.sumBy(this.xSeries.points, (point) => point.value)
+
+            return [this.x0, sum]
+        } else return [this.x0, this.items.length]
+    }
+
+    @computed private get xAxisLabelBase(): string {
+        const xDimName = this.defaultXAxisLabel
+        if (this.manager.xOverrideTime !== undefined)
+            return `${xDimName} in ${this.manager.xOverrideTime}`
+        return xDimName ?? "" // This sets the axis label to emtpy if we don't have an x column - not entirely sure this is what we want
+    }
+
+    @computed private get defaultXAxisLabel(): string | undefined {
+        return this.xColumn?.displayName
+    }
+
+    @computed get horizontalAxisLabel(): string {
+        const { xAxisLabelBase } = this
+        const config = this.manager.xAxisConfig
+        return config?.label || xAxisLabelBase
+    }
+
+    toHorizontalAxis(config: AxisConfig): HorizontalAxis {
+        let axis = config.toHorizontalAxis()
+        if (this.manager.isRelativeMode && this.xColumn) {
+            // MobX and classes  interact in an annoying way here so we have to construct a new object via
+            // an object copy of the AxisConfig class instance to be able to set a property without
+            // making MobX unhappy about a mutation originating from a computed property
+            axis = new HorizontalAxis(
+                new AxisConfig(
+                    { ...config.toObject(), maxTicks: 10 },
+                    config.axisManager
+                ),
+                config.axisManager
+            )
+            axis.domain = [0, 100]
+        } else {
+            axis.updateDomainPreservingUserSettings(this.xDomainDefault)
+        }
+
+        axis.formatColumn = this.xColumn
+
+        axis.label = this.horizontalAxisLabel
+
+        return axis
+    }
+
+    toVerticalAxis(config: AxisConfig): VerticalAxis {
+        const axis = config.toVerticalAxis()
+        axis.updateDomainPreservingUserSettings(this.yDomainDefault)
+
+        axis.formatColumn = this.yColumns[0]
+        axis.label = ""
+
+        return axis
     }
 
     @computed get errorInfo(): ChartErrorInfo {
