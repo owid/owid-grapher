39cede7a934fffc3c35bf14770e3ae99565f0ae2
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-08-26 17:36:25 +0200

    🔨 refactor minimal variant

--- packages/@ourworldindata/grapher/src/mapCharts/MapChartThumbnail.tsx
+++ packages/@ourworldindata/grapher/src/mapCharts/MapChartThumbnail.tsx
@@ -4,22 +4,11 @@ import { observer } from "mobx-react"
 import { ChartInterface } from "../chart/ChartInterface"
 import { MapChartState } from "./MapChartState"
 import { MapChart, MapChartProps } from "./MapChart"
-import {
-    ChoroplethMapManager,
-    ChoroplethSeriesByName,
-    MapChartManager,
-} from "./MapChartConstants"
-import { Bounds, GrapherVariant } from "@ourworldindata/utils"
-import { DEFAULT_GRAPHER_BOUNDS } from "../core/GrapherConstants"
-import { MapConfig } from "./MapConfig"
-import { CoreColumn } from "@ourworldindata/core-table"
-import { ChoroplethMap } from "./ChoroplethMap.js"
-import { ChoroplethGlobe } from "./ChoroplethGlobe"
 
 @observer
 export class MapChartThumbnail
     extends React.Component<MapChartProps>
-    implements ChartInterface, ChoroplethMapManager
+    implements ChartInterface
 {
     constructor(props: MapChartProps) {
         super(props)
@@ -30,48 +19,7 @@ export class MapChartThumbnail
         return this.props.chartState
     }
 
-    @computed private get manager(): MapChartManager {
-        return this.chartState.manager
-    }
-
-    @computed private get isMinimal(): boolean {
-        return this.manager.variant === GrapherVariant.MinimalThumbnail
-    }
-
-    @computed private get bounds(): Bounds {
-        return this.props.bounds ?? DEFAULT_GRAPHER_BOUNDS
-    }
-
-    @computed get choroplethMapBounds(): Bounds {
-        return this.bounds
-    }
-
-    @computed get choroplethData(): ChoroplethSeriesByName {
-        return this.chartState.seriesMap
-    }
-
-    @computed get mapConfig(): MapConfig {
-        return this.chartState.mapConfig
-    }
-
-    @computed get mapColumn(): CoreColumn {
-        return this.chartState.mapColumn
-    }
-
-    private renderMapOrGlobe(): React.ReactElement {
-        return this.mapConfig.globe.isActive ? (
-            <ChoroplethGlobe manager={this} />
-        ) : (
-            <ChoroplethMap manager={this} />
-        )
-    }
-
     override render(): React.ReactElement {
-        // The minimal version only renders the map, without legend
-        return this.isMinimal ? (
-            this.renderMapOrGlobe()
-        ) : (
-            <MapChart {...this.props} />
-        )
+        return <MapChart {...this.props} />
     }
 }
