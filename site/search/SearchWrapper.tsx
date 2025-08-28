29b984dba83a50e5faac42b59afacd83c891754e
Matthieu <matthieu@ourworldindata.org>
2025-08-28 08:25:08 +0000

    feat(search): add sentry error logging for queries

--- site/search/SearchWrapper.tsx
+++ site/search/SearchWrapper.tsx
@@ -6,7 +6,12 @@ import {
 import { Search } from "./Search.js"
 import { getInitialSearchState } from "./searchState.js"
 import algoliasearch from "algoliasearch"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import {
+    QueryClient,
+    QueryClientProvider,
+    QueryCache,
+} from "@tanstack/react-query"
+import * as Sentry from "@sentry/react"
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -17,6 +22,25 @@ const queryClient = new QueryClient({
             staleTime: 60 * 1000, // 1 minute
         },
     },
+    // Manually sending errors to Sentry as React Query silently captures
+    // errors. Some things to consider before setting up an error handling
+    // strategy:
+    // - throwOnError (v5) or useErrorBoundary (v4) defined at the query client
+    //   level means that errors in queries are thrown and can be caught by an
+    //   error boundary.
+    // - however, a global error boundary around the search component means that
+    //   a single fetch error (e.g. in SearchChartHitSmall) crashes the whole
+    //   search results page. This calls for a more fine-grained approach.
+    //
+    // Logging is not a substitute for actual error handling, but rather a way
+    // to gauge the prevalence of fetch errors in a React Query world, where
+    // queries are now automatically retried (3 times by default) before
+    // actually failing.
+    queryCache: new QueryCache({
+        onError: (error) => {
+            Sentry.captureException(error)
+        },
+    }),
 })
 
 export const SearchWrapper = ({
