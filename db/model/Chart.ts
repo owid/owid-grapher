028ea1bb2c5f225ebbbf59aab8f71918df4e2753
Marigold <mojmir.vinkler@gmail.com>
2025-08-29 11:02:38 +0200

    :sparkles: Update lastEditedAt if tags are updated

--- db/model/Chart.ts
+++ db/model/Chart.ts
@@ -389,8 +389,9 @@ export async function setChartTags(
     const isIndexable = tags.some((t) => t.name === "Unlisted")
         ? false
         : parentIds.some((t) => PUBLIC_TAG_PARENT_IDS.includes(t.parentId))
-    await db.knexRaw(knex, "update charts set isIndexable = ? where id = ?", [
+    await db.knexRaw(knex, "update charts set isIndexable = ?, lastEditedAt = ? where id = ?", [
         isIndexable,
+        new Date(),
         chartId,
     ])
 }
