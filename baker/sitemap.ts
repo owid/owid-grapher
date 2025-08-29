e5fddd7ac777343918bed5a2b6401bf9f6a13701
Matthieu Bergel <matthieu.bergel@gmail.com>
2025-08-29 12:29:43 +0200

    Remove /data from sitemap.ts

--- baker/sitemap.ts
+++ baker/sitemap.ts
@@ -104,7 +104,7 @@ export const makeSitemap = async (
         `
     )
 
-    const STATIC_PAGES = ["/explorers", "/data", SEARCH_BASE_PATH, "/donate"]
+    const STATIC_PAGES = ["/explorers", SEARCH_BASE_PATH, "/donate"]
 
     const explorers = await explorerAdminServer.getAllPublishedExplorers(knex)
 
