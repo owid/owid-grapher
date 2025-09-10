7d1db3a1f5d9b7d3feb8645e3813eeb0806508d8
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-09-10 13:32:53 +0200

    ✨ move owid logo to the left

--- site/search/SearchChartHitHeader.tsx
+++ site/search/SearchChartHitHeader.tsx
@@ -6,6 +6,7 @@ interface SearchChartHitHeaderProps {
     hit: SearchChartHit
     url: string
     source?: string
+    showLogo?: boolean
     onClick?: () => void
 }
 
@@ -13,6 +14,7 @@ export function SearchChartHitHeader({
     hit,
     url,
     source,
+    showLogo = false,
     onClick,
 }: SearchChartHitHeaderProps) {
     return (
@@ -26,28 +28,41 @@ export function SearchChartHitHeader({
             data-algolia-object-id={hit.objectID}
             data-algolia-position={hit.__position}
         >
-            <header className="search-chart-hit-header__content">
-                <div className="search-chart-hit-header__title-container">
-                    <h3 className="search-chart-hit-header__title">
-                        {hit.title}
-                    </h3>
+            <header className="search-chart-hit-header__content-wrapper">
+                {showLogo && (
+                    <img
+                        src="owid-logo.svg"
+                        alt="Our World in Data logo"
+                        className="search-chart-hit-header__logo"
+                        width={104}
+                        height={57}
+                    />
+                )}
+                <div className="search-chart-hit-header__content">
+                    <div className="search-chart-hit-header__title-container">
+                        <h3 className="search-chart-hit-header__title">
+                            {hit.title}
+                        </h3>
+                        {source && (
+                            <span className="search-chart-hit-header__source search-chart-hit-header__source--desktop">
+                                Source: {source}
+                            </span>
+                        )}
+                    </div>
+                    <Highlight
+                        hit={hit}
+                        attribute="subtitle"
+                        highlightedTagName="strong"
+                        classNames={{
+                            root: "search-chart-hit-header__subtitle",
+                        }}
+                    />
                     {source && (
-                        <span className="search-chart-hit-header__source search-chart-hit-header__source--desktop">
+                        <span className="search-chart-hit-header__source search-chart-hit-header__source--mobile">
                             Source: {source}
                         </span>
                     )}
                 </div>
-                <Highlight
-                    hit={hit}
-                    attribute="subtitle"
-                    highlightedTagName="strong"
-                    classNames={{ root: "search-chart-hit-header__subtitle" }}
-                />
-                {source && (
-                    <span className="search-chart-hit-header__source search-chart-hit-header__source--mobile">
-                        Source: {source}
-                    </span>
-                )}
             </header>
         </a>
     )
