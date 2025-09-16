f15d512b6edb629f150edb0a83139fc1992ecb5c
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-09-16 12:53:31 +0200

    ✨ enable rich data display for csv based explorers

--- site/search/SearchChartHitComponent.tsx
+++ site/search/SearchChartHitComponent.tsx
@@ -2,14 +2,10 @@ import { match } from "ts-pattern"
 import { useMediaQuery } from "usehooks-ts"
 import { SearchChartHitSmall } from "./SearchChartHitSmall.js"
 import {
-    ChartRecordType,
-    ExplorerType,
-    SearchChartHit,
     SearchChartHitComponentProps,
     SearchChartHitComponentVariant,
 } from "./searchTypes.js"
 import { SearchChartHitRichData } from "./SearchChartHitRichData.js"
-import { SearchChartHitRichDataFallback } from "./SearchChartHitRichDataFallback.js"
 import { MEDIUM_BREAKPOINT_MEDIA_QUERY } from "../SiteConstants.js"
 
 export const SearchChartHitComponent = (
@@ -28,10 +24,6 @@ export const SearchChartHitComponent = (
 const SearchChartHitMedium = (
     props: SearchChartHitComponentProps
 ): React.ReactElement => {
-    // If the hit doesn't support rich data display, render the fallback component
-    if (!hasRichDataDisplay(props.hit))
-        return <SearchChartHitRichDataFallback {...props} />
-
     return <SearchChartHitRichData variant="medium" {...props} />
 }
 
@@ -40,21 +32,9 @@ const SearchChartHitLarge = (
 ): React.ReactElement => {
     const isMediumScreen = useMediaQuery(MEDIUM_BREAKPOINT_MEDIA_QUERY)
 
-    // If the hit doesn't support rich data display, render the fallback component
-    if (!hasRichDataDisplay(props.hit))
-        return <SearchChartHitRichDataFallback {...props} />
-
     // On smaller screens, render the medium variant which implements the
     // mobile layout for smaller viewports
     if (isMediumScreen) return <SearchChartHitMedium {...props} />
 
     return <SearchChartHitRichData variant="large" {...props} />
-}
-
-function hasRichDataDisplay(hit: SearchChartHit): boolean {
-    // CSV-based explorers don't support rich data display
-    return !(
-        hit.type === ChartRecordType.ExplorerView &&
-        hit.explorerType === ExplorerType.Csv
-    )
 }
