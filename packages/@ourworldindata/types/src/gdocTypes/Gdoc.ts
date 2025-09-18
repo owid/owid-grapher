0d3741c303be4d909fb1486a20931ab63642ff8c
Ike Saunders <11844404+ikesau@users.noreply.github.com>
2025-09-18 16:15:08 -0400

    🎉 new homepage

--- packages/@ourworldindata/types/src/gdocTypes/Gdoc.ts
+++ packages/@ourworldindata/types/src/gdocTypes/Gdoc.ts
@@ -137,6 +137,7 @@ export interface OwidGdocMinimalPostInterface {
     excerpt: string // used in prominent links
     type: OwidGdocType // used in useLinkedDocument to prepend /data-insights/ to the slug
     "featured-image"?: string // used in prominent links and research & writing block
+    kicker?: string // used in homepage announcements
 }
 
 export type OwidGdocIndexItem = Pick<
@@ -240,6 +241,7 @@ export interface OwidGdocHomepageMetadata {
     chartCount?: number
     topicCount?: number
     tagGraph?: TagGraphRoot
+    announcements?: OwidGdocMinimalPostInterface[]
 }
 
 export interface OwidGdocHomepageInterface extends OwidGdocBaseInterface {
