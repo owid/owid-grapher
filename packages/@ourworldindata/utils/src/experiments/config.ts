f456b1856e42ca7bf0913075ee35d586b595fba7
bnjmacdonald <bnjmacdonald@gmail.com>
2025-09-23 01:12:46 -0300

    :scroll: revise experiment description

--- packages/@ourworldindata/utils/src/experiments/config.ts
+++ packages/@ourworldindata/utils/src/experiments/config.ts
@@ -11,8 +11,8 @@ export const experiments: Experiment[] = [
          *
          * Goal of experiment is to better understand user demand for "insights"
          * when viewing a data page. Each arm shows the user one or more buttons
-         * beneath the grapher, varying the number of buttons, the type of text in
-         * these buttons, and whether or not a thumbnail is shown.
+         * beneath the grapher, varying the number of buttons and the type of text in
+         * these buttons.
          *
          * Conditions:
          * - (a) "placebo" vs "generic" vs "specific" text
@@ -20,11 +20,6 @@ export const experiments: Experiment[] = [
          *     - generic text is button text that refers to a generic insights page (e.g. "View insights about this data")
          *     - specific text is button text that describes a concrete insight (e.g. "Women live longer than men, but how much longer varies widely around the world")
          * - (b) 1 vs 3 links shown
-         * - (c) no thumbnail vs thumbnail
-         *      - for "placebo" text, we exclude the thumbnail condition
-         *      - for "generic" text, thumbnail is of the latest data insight on the data page's primary {topic}
-         *      - for "specific" text, thumbnail is of the data insight that matches the button text
-         *
          * Control arm: no changes
          * Control arm control1: "what you should know about this data" block is moved down the page (also the case in all treatment arms)
          * Treatment arm treat00: placebo - 1 link
