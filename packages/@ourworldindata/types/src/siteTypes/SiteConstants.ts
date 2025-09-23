a9b6207d026ee2a40f8bc423662e154810ef5f40
Ike Saunders <11844404+ikesau@users.noreply.github.com>
2025-09-23 15:34:01 -0400

    🎉 new subscribe page

--- packages/@ourworldindata/types/src/siteTypes/SiteConstants.ts
+++ packages/@ourworldindata/types/src/siteTypes/SiteConstants.ts
@@ -10,3 +10,5 @@ export const GRAPHER_PREVIEW_CLASS = "grapherPreview"
 export const SENTRY_DEFAULT_REPLAYS_SESSION_SAMPLE_RATE = 0.08
 export const SENTRY_SESSION_STORAGE_KEY = "sentryReplaySession"
 export const SENTRY_SAMPLED_RATE_KEY = "sentrySampledRate"
+
+export const SUBSCRIBE_PAGE_FORM_CONTAINER_ID = "subscribe-page-form-container"
