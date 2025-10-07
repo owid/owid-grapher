37dd726c1bffcb88fe428360d3727423167120dc
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-10-07 10:58:12 +0200

    Revert "✨ make source line clickable"
    This reverts commit f7bb39942a80ab645e42a67576472c1461dad503.

--- site/search/SearchChartHitHeader.tsx
+++ site/search/SearchChartHitHeader.tsx
@@ -5,7 +5,7 @@ import { Highlight } from "react-instantsearch"
 interface SearchChartHitHeaderProps {
     hit: SearchChartHit
     url: string
-    source?: { text: string; url: string }
+    source?: string
     showLogo?: boolean
     onClick?: () => void
 }
@@ -18,58 +18,52 @@ export function SearchChartHitHeader({
     onClick,
 }: SearchChartHitHeaderProps) {
     return (
-        <header className="search-chart-hit-header">
-            {showLogo && (
-                <img
-                    src="owid-logo.svg"
-                    alt="Our World in Data logo"
-                    className="search-chart-hit-header__logo"
-                    width={104}
-                    height={57}
-                />
+        <a
+            className="search-chart-hit-header"
+            href={url}
+            onClick={onClick}
+            data-algolia-index={getIndexName(
+                SearchIndexName.ExplorerViewsMdimViewsAndCharts
             )}
-            <div className="search-chart-hit-header__content">
-                <div className="search-chart-hit-header__title-container">
-                    <a
-                        href={url}
-                        onClick={onClick}
-                        data-algolia-index={getIndexName(
-                            SearchIndexName.ExplorerViewsMdimViewsAndCharts
-                        )}
-                        data-algolia-object-id={hit.objectID}
-                        data-algolia-position={hit.__position}
-                    >
+            data-algolia-object-id={hit.objectID}
+            data-algolia-position={hit.__position}
+        >
+            <header className="search-chart-hit-header__content-wrapper">
+                {showLogo && (
+                    <img
+                        src="owid-logo.svg"
+                        alt="Our World in Data logo"
+                        className="search-chart-hit-header__logo"
+                        width={104}
+                        height={57}
+                    />
+                )}
+                <div className="search-chart-hit-header__content">
+                    <div className="search-chart-hit-header__title-container">
                         <h3 className="search-chart-hit-header__title">
                             {hit.title}
                         </h3>
-                    </a>
-
-                    {source?.text && (
-                        <a
-                            className="search-chart-hit-header__source search-chart-hit-header__source--desktop"
-                            href={source.url}
-                        >
-                            Source: {source.text}
-                        </a>
+                        {source && (
+                            <span className="search-chart-hit-header__source search-chart-hit-header__source--desktop">
+                                Source: {source}
+                            </span>
+                        )}
+                    </div>
+                    <Highlight
+                        hit={hit}
+                        attribute="subtitle"
+                        highlightedTagName="strong"
+                        classNames={{
+                            root: "search-chart-hit-header__subtitle",
+                        }}
+                    />
+                    {source && (
+                        <span className="search-chart-hit-header__source search-chart-hit-header__source--mobile">
+                            Source: {source}
+                        </span>
                     )}
                 </div>
-                <Highlight
-                    hit={hit}
-                    attribute="subtitle"
-                    highlightedTagName="strong"
-                    classNames={{
-                        root: "search-chart-hit-header__subtitle",
-                    }}
-                />
-                {source?.text && (
-                    <a
-                        className="search-chart-hit-header__source search-chart-hit-header__source--mobile"
-                        href={source.url}
-                    >
-                        Source: {source.text}
-                    </a>
-                )}
-            </div>
-        </header>
+            </header>
+        </a>
     )
 }
