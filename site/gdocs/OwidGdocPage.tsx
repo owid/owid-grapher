2d623fe94138615f0db11865798d0f879895de02
Ike Saunders <11844404+ikesau@users.noreply.github.com>
2025-10-23 14:00:07 -0400

    ✨ implement profile rendering pipeline

--- site/gdocs/OwidGdocPage.tsx
+++ site/gdocs/OwidGdocPage.tsx
@@ -31,6 +31,7 @@ import {
     EnrichedBlockText,
     OwidGdocPostInterface,
     OwidGdocAuthorInterface,
+    OwidGdocProfileInterface,
 } from "@ourworldindata/types"
 import { DATA_INSIGHT_ATOM_FEED_PROPS } from "../SiteConstants.js"
 import { Html } from "../Html.js"
@@ -82,8 +83,8 @@ function getPageDesc(gdoc: OwidGdocUnionType): string | undefined {
                   )
                 : undefined
         })
-        .with({ content: { type: OwidGdocType.Profile } }, (match) => {
-            return match.content.excerpt
+        .with({ content: { type: OwidGdocType.Profile } }, (gdoc) => {
+            return gdoc.content.excerpt
         })
         .with(
             {
@@ -100,7 +101,7 @@ type JsonLdAuthor = Person | Organization
 
 function makeJsonLdAuthors(
     baseUrl: string,
-    gdoc: OwidGdocPostInterface
+    gdoc: OwidGdocPostInterface | OwidGdocProfileInterface
 ): JsonLdAuthor[] {
     return gdoc.content.authors.map((gdocAuthor) => {
         if (gdocAuthor.toLowerCase().includes("our world in data")) {
@@ -134,7 +135,7 @@ function JsonLdArticle({
     baseUrl,
     imageUrl,
 }: {
-    gdoc: OwidGdocPostInterface
+    gdoc: OwidGdocPostInterface | OwidGdocProfileInterface
     baseUrl: string
     imageUrl?: string
 }) {
@@ -226,6 +227,18 @@ function isPostPredicate(
     )
 }
 
+function isProfilePredicate(
+    gdoc: OwidGdocUnionType
+): gdoc is OwidGdocProfileInterface {
+    return gdoc.content.type === OwidGdocType.Profile
+}
+
+function isArticleLikePredicate(
+    gdoc: OwidGdocUnionType
+): gdoc is OwidGdocPostInterface | OwidGdocProfileInterface {
+    return isPostPredicate(gdoc) || isProfilePredicate(gdoc)
+}
+
 export default function OwidGdocPage({
     baseUrl,
     gdoc,
@@ -250,6 +263,8 @@ export default function OwidGdocPage({
     const isDataInsight = gdoc.content.type === OwidGdocType.DataInsight
     const isAuthor = checkIsAuthor(gdoc)
     const isPost = isPostPredicate(gdoc)
+    const isProfile = isProfilePredicate(gdoc)
+    const isArticleLike = isArticleLikePredicate(gdoc)
 
     let imageUrl
     if (
@@ -297,7 +312,7 @@ export default function OwidGdocPage({
                         canonicalUrl={canonicalUrl}
                     />
                 )}
-                {isPost && (
+                {isArticleLike && (
                     <JsonLdArticle
                         gdoc={gdoc}
                         baseUrl={baseUrl}
