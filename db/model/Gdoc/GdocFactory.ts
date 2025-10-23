9f2138bc569118680e802e4307008a6c1bb81d60
Ike Saunders <11844404+ikesau@users.noreply.github.com>
2025-10-22 23:35:05 -0400

    ✨ create GdocProfile class

--- db/model/Gdoc/GdocFactory.ts
+++ db/model/Gdoc/GdocFactory.ts
@@ -52,6 +52,7 @@ import { GdocAuthor } from "./GdocAuthor.js"
 import { extractFilenamesFromBlock } from "./gdocUtils.js"
 import { getGdocComponentsWithoutChildren } from "./extractGdocComponentInfo.js"
 import { GdocAnnouncement } from "./GdocAnnouncement.js"
+import { GdocProfile } from "./GdocProfile.js"
 
 export function gdocFromJSON(
     json: Record<string, any>
@@ -61,7 +62,8 @@ export function gdocFromJSON(
     | GdocHomepage
     | GdocAbout
     | GdocAuthor
-    | GdocAnnouncement {
+    | GdocAnnouncement
+    | GdocProfile {
     if (typeof json.content === "string") {
         json.content = JSON.parse(json.content)
     }
@@ -113,9 +115,9 @@ export function gdocFromJSON(
             // TODO: better validation here?
             () => GdocAnnouncement.create({ ...(json as any) })
         )
-        .with(OwidGdocType.Profile, () => {
-            throw new Error("Profiles are not yet supported in GdocFactory")
-        })
+        .with(OwidGdocType.Profile, () =>
+            GdocProfile.create({ ...(json as any) })
+        )
         .exhaustive()
 }
 
@@ -157,6 +159,7 @@ export async function updateGdocContentOnly(
         | GdocAbout
         | GdocAuthor
         | GdocAnnouncement
+        | GdocProfile
 ): Promise<void> {
     gdoc.updateMarkdown()
     await knex
@@ -351,6 +354,7 @@ export async function getAndLoadGdocBySlug(
     | GdocAbout
     | GdocAuthor
     | GdocAnnouncement
+    | GdocProfile
 > {
     const base = await getPublishedGdocBaseObjectBySlug(knex, slug, true)
     if (!base) {
@@ -373,6 +377,7 @@ export async function getAndLoadGdocById(
     | GdocAbout
     | GdocAuthor
     | GdocAnnouncement
+    | GdocProfile
 > {
     const base = await getGdocBaseObjectById(knex, id, true)
     if (!base)
@@ -408,6 +413,7 @@ export async function loadGdocFromGdocBase(
     | GdocAbout
     | GdocAuthor
     | GdocAnnouncement
+    | GdocProfile
 > {
     const shouldLoadState = options?.loadState ?? true
 
@@ -437,9 +443,7 @@ export async function loadGdocFromGdocBase(
         .with(OwidGdocType.Homepage, () => GdocHomepage.create(base))
         .with(OwidGdocType.Author, () => GdocAuthor.create(base))
         .with(OwidGdocType.Announcement, () => GdocAnnouncement.create(base))
-        .with(OwidGdocType.Profile, () => {
-            throw new Error("Profiles are not yet supported in GdocFactory")
-        })
+        .with(OwidGdocType.Profile, () => GdocProfile.create(base))
         .exhaustive()
 
     if (contentSource === GdocsContentSource.Gdocs) {
@@ -735,6 +739,7 @@ export async function upsertGdoc(
     | GdocAbout
     | GdocAuthor
     | GdocAnnouncement
+    | GdocProfile
 > {
     let sql = undefined
     try {
@@ -803,6 +808,7 @@ export async function setImagesInContentGraph(
         | GdocAbout
         | GdocAuthor
         | GdocAnnouncement
+        | GdocProfile
 ): Promise<void> {
     const id = gdoc.id
     // Deleting and recreating these is simpler than tracking orphans over the next code block
