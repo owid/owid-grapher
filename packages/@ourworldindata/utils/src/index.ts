7c02014d8d4c9d4f63a939242aaafbd6b58b979a
Ike Saunders <11844404+ikesau@users.noreply.github.com>
2025-10-30 18:16:12 -0400

    ✨ country profiles baking

--- packages/@ourworldindata/utils/src/index.ts
+++ packages/@ourworldindata/utils/src/index.ts
@@ -227,7 +227,11 @@ export {
     getSiblingRegions,
     articulateEntity,
 } from "./regions.js"
-export { instantiateProfile, type ProfileEntity } from "./profiles.js"
+export {
+    instantiateProfile,
+    type ProfileEntity,
+    getEntitiesForProfile,
+} from "./profiles.js"
 
 export { type GridBounds, FontFamily, Bounds } from "./Bounds.js"
 
