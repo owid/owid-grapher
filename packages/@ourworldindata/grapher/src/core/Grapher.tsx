00464d329963f6de02c4e204a3b564f0589fe348
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-11-06 10:11:27 +0100

    fix

--- packages/@ourworldindata/grapher/src/core/Grapher.tsx
+++ packages/@ourworldindata/grapher/src/core/Grapher.tsx
@@ -33,6 +33,7 @@ import {
     AdditionalGrapherDataFetchFn,
     GrapherVariant,
     Time,
+    GrapherTooltipAnchor,
 } from "@ourworldindata/types"
 import { OwidTable } from "@ourworldindata/core-table"
 import {
@@ -566,7 +567,7 @@ export class Grapher extends React.Component<GrapherProps> {
                     <BodyPortal>
                         <TooltipContainer
                             tooltipManager={this.grapherState}
-                            anchor={GrapherTooltipAnchor.bottom}
+                            anchor={GrapherTooltipAnchor.Bottom}
                         />
                     </BodyPortal>
                 ) : (
@@ -574,7 +575,7 @@ export class Grapher extends React.Component<GrapherProps> {
                         tooltipManager={this.grapherState}
                         containerBounds={this.grapherState.captionedChartBounds}
                     />
-                </BodyPortal>
+                )}
             </>
         )
     }
