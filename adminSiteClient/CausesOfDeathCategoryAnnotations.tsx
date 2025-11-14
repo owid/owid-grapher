94b0efc89611541eefa858fcd4e85e5e0623ce62
Sophia Mersmann <sophia.mersmann1@gmail.com>
2025-11-14 11:01:21 +0100

    refactor annotations to be more robust

--- adminSiteClient/CausesOfDeathCategoryAnnotations.tsx
+++ adminSiteClient/CausesOfDeathCategoryAnnotations.tsx
@@ -10,14 +10,139 @@ import Arrow from "./Arrow"
 import * as d3 from "d3"
 import * as R from "remeda"
 import { MarkdownTextWrap } from "@ourworldindata/components"
-import { getDropIndexes } from "@ourworldindata/core-table"
-import { match } from "ts-pattern"
 
 interface AnnotationCandidate {
     name: string
     total: number
     edge: "top" | "bottom"
     bounds: { y: number; x0: number; x1: number }
+}
+
+export interface Category {
+    name: string
+    total: number
+}
+
+interface TextFragment {
+    text: string
+    style: React.SVGProps<SVGTextElement>
+}
+
+export interface PlacedCategory extends Category {
+    textFragments: TextFragment[]
+    bounds: Bounds
+    placement: "top" | "bottom"
+    arrowAnchor: "left" | "right"
+    textAnchor: "start" | "end"
+    fontSize: number
+}
+
+export function NewCategoryAnnotations({
+    placedAnnotations,
+}: {
+    placedAnnotations: PlacedCategory[]
+}): React.ReactElement | null {
+    if (placedAnnotations.length === 0) return null
+    return (
+        <g className="new-annotations">
+            {placedAnnotations.map((placedAnnotation) => {
+                return (
+                    <NewCategoryAnnotation
+                        key={placedAnnotation.name}
+                        bounds={placedAnnotation.bounds}
+                        textFragments={placedAnnotation.textFragments}
+                        position={placedAnnotation.placement}
+                        anchor={placedAnnotation.textAnchor}
+                        arrowWidth={50} // todo
+                        fontSize={placedAnnotation.fontSize}
+                    />
+                )
+            })}
+        </g>
+    )
+}
+
+function NewCategoryAnnotation({
+    bounds,
+    textFragments,
+    fontSize,
+    position = "top",
+    anchor,
+    color = "#5b5b5b",
+    arrowWidth = 50,
+}: {
+    bounds: Bounds
+    textFragments: TextFragment[]
+    fontSize: number
+    position?: "top" | "bottom"
+    anchor: "start" | "end" // anchor the text at the start of the end of the bounds
+    color?: string
+    arrowWidth?: number
+}) {
+    const isEndAnchored = anchor === "end"
+
+    const direction = position === "top" ? 1 : -1
+
+    const x = isEndAnchored
+        ? bounds.right - arrowWidth
+        : bounds.left + arrowWidth
+    const y = position === "top" ? bounds.bottom - 12 : bounds.top + 12 // space between annotation and treemap
+
+    const textStyle: React.CSSProperties = {
+        fontSize,
+        fill: "#5b5b5b",
+        textAnchor: anchor,
+        dominantBaseline: position === "bottom" ? "hanging" : undefined,
+    }
+
+    const arrowStart: [number, number] = [
+        x + (isEndAnchored ? 3 : -3),
+        y - direction * (fontSize / 2),
+    ]
+    const arrowEnd: [number, number] = [
+        isEndAnchored ? bounds.right - 11 : bounds.left + 11,
+        position === "top" ? bounds.bottom - 3 : bounds.top + 3,
+    ]
+
+    const arrowStartHandleOffset: [number, number] = isEndAnchored
+        ? position === "top"
+            ? [20, -5]
+            : [20, 5]
+        : [-20, -5]
+    const arrowEndHandleOffset: [number, number] = isEndAnchored
+        ? position === "top"
+            ? [-5, -20]
+            : [-5, 20]
+        : [5, -20]
+
+    return (
+        <g>
+            <text x={x} y={y} style={textStyle}>
+                {textFragments.map(({ text, style }) => (
+                    <tspan key={text} {...style}>
+                        {text}
+                    </tspan>
+                ))}
+            </text>
+            {/* <rect
+                {...bounds.toProps()}
+                fill="none"
+                stroke="blue"
+                strokeWidth={1}
+            /> */}
+            <Arrow
+                start={arrowStart}
+                end={arrowEnd}
+                startHandleOffset={arrowStartHandleOffset}
+                endHandleOffset={arrowEndHandleOffset}
+                color={color}
+                width={1}
+                opacity={0.7}
+                headLength={6}
+                headAngle={45}
+            />
+        </g>
+    )
 }
 
 export function CausesOfDeathCategoryAnnotations({
@@ -69,8 +194,6 @@ export function CausesOfDeathCategoryAnnotations({
 
     const candidates = findCandidatesForAnnotations(enrichedCategories)
 
-    console.log(candidates)
-
     const topCandidates = candidates.filter(
         (candidate) => candidate.edge === "top"
     )
@@ -79,7 +202,7 @@ export function CausesOfDeathCategoryAnnotations({
     )
 
     return (
-        <g>
+        <g opacity={0.3}>
             <CausesOfDeathCategoryAnnotationsTop
                 candidates={topCandidates}
                 data={data}
@@ -94,7 +217,6 @@ export function CausesOfDeathCategoryAnnotations({
                 treeNodes={treeNodes}
                 width={width}
                 annotationHeight={annotationHeight}
-                debug={debug}
             />
         </g>
     )
@@ -194,14 +316,12 @@ function CausesOfDeathCategoryAnnotationsBottom({
     treeNodes,
     width,
     annotationHeight,
-    debug,
 }: {
     candidates: AnnotationCandidate[]
     data: DataRow[]
     treeNodes: TreeNode[]
     width: number
     annotationHeight: number
-    debug: boolean
 }) {
     console.log("Bottom candidates:", candidates)
     if (candidates.length === 0) return null
@@ -269,6 +389,9 @@ function CausesOfDeathCategoryAnnotationsTop({
 }) {
     // Don't render anything if there are no candidates
     if (candidates.length === 0) return null
+
+    // TODO: easy case
+    if (candidates.length === 1) return null
 
     const numAllDeaths = d3.sum(data, (d) => d.value) || 0
     const formattedPercentages = candidates.map((c) =>
