3432f31d9be7d1318d91dfdd460c63541130749c
Marcel Gerber <mgerber59@gmail.com>
2025-11-19 13:26:32 +0100

    wip: add jotai

--- bespoke/projects/income-plots/src/utils/incomePlotUtils.ts
+++ bespoke/projects/income-plots/src/utils/incomePlotUtils.ts
@@ -2,8 +2,8 @@ import { roundSigFig } from "@ourworldindata/utils"
 import * as fastKde from "fast-kde"
 
 export function formatCurrency(num: number) {
-    if (num > 10) return "$" + roundSigFig(num, 2)
-    if (num > 1) return "$" + (Math.round(num * 10) / 10).toFixed(2)
+    if (num >= 10) return "$" + roundSigFig(num, 2)
+    if (num >= 1) return "$" + (Math.round(num * 10) / 10).toFixed(2)
     else return "$" + num.toFixed(2)
 }
 
