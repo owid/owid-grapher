94a525370552106827d196b8bae33571ce327a56
Marcel Gerber <mgerber59@gmail.com>
2025-11-19 13:36:29 +0100

    wip: use react

---
+++ bespoke/projects/income-plots/src/components/App.tsx
@@ -0,0 +1,10 @@
+import { IncomePlot } from "./IncomePlot.tsx"
+
+export const App = () => {
+    return (
+        <div>
+            <h1>Income Distribution Visualization</h1>
+            <IncomePlot />
+        </div>
+    )
+}
