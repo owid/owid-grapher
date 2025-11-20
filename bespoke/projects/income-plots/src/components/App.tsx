cc56c54e16ca52c479940e8badb3478312b82d55
Marcel Gerber <mgerber59@gmail.com>
2025-11-20 19:10:57 +0100

    wip: add sizing

--- bespoke/projects/income-plots/src/components/App.tsx
+++ bespoke/projects/income-plots/src/components/App.tsx
@@ -2,14 +2,24 @@ import { Suspense } from "react"
 import { IncomePlot } from "./IncomePlot.tsx"
 import { IncomePlotControlsRow } from "./IncomePlotControlsRow.tsx"
 import { IncomePlotLegend } from "./IncomePlotLegend.tsx"
+import { PLOT_HEIGHT, PLOT_WIDTH } from "../utils/incomePlotConstants.ts"
 
 export const App = () => {
     return (
         <div>
             <IncomePlotLegend />
-            <Suspense fallback={<div>Loading plot...</div>}>
-                <IncomePlot />
-            </Suspense>
+            <div
+                style={{
+                    width: PLOT_WIDTH,
+                    aspectRatio: PLOT_WIDTH / PLOT_HEIGHT,
+                    maxWidth: "100%",
+                    margin: "auto",
+                }}
+            >
+                <Suspense fallback={<>Loading...</>}>
+                    <IncomePlot />
+                </Suspense>
+            </div>
             <IncomePlotControlsRow />
         </div>
     )
