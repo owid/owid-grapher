a202f66c88c80981d5d7ffdc694a9b3efffa3ff3
Marcel Gerber <mgerber59@gmail.com>
2025-11-20 19:06:18 +0100

    wip: support decorators

--- bespoke/projects/income-plots/vite.config.ts
+++ bespoke/projects/income-plots/vite.config.ts
@@ -3,5 +3,16 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
     root: "./src",
-    plugins: [react()],
+    esbuild: {
+        target: "es2024", // needed so decorators are compiled by esbuild
+    },
+    plugins: [
+        react({
+            babel: {
+                parserOpts: {
+                    plugins: ["decorators"], // needed so mobx decorators work correctly
+                },
+            },
+        }),
+    ],
 })
