0c0042e0c2a19512212b8b8893594209f9dccfcb
Marcel Gerber <mgerber59@gmail.com>
2025-11-20 11:53:32 +0100

    wip: basic controls

--- bespoke/projects/income-plots/src/components/App.tsx
+++ bespoke/projects/income-plots/src/components/App.tsx
@@ -1,10 +1,12 @@
 import { IncomePlot } from "./IncomePlot.tsx"
+import { IncomePlotControlsRow } from "./IncomePlotControlsRow.tsx"
 
 export const App = () => {
     return (
         <div>
             <h1>Income Distribution Visualization</h1>
             <IncomePlot />
+            <IncomePlotControlsRow />
         </div>
     )
 }
