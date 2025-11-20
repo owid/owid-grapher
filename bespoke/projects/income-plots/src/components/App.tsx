29bcb0f3d1cf7b1b0f584716f5d2678c0a797fc8
Marcel Gerber <mgerber59@gmail.com>
2025-11-20 13:50:55 +0100

    wip: first legend

--- bespoke/projects/income-plots/src/components/App.tsx
+++ bespoke/projects/income-plots/src/components/App.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react"
 import { IncomePlot } from "./IncomePlot.tsx"
 import { IncomePlotControlsRow } from "./IncomePlotControlsRow.tsx"
+import { IncomePlotLegend } from "./IncomePlotLegend.tsx"
 
 export const App = () => {
     return (
         <div>
+            <IncomePlotLegend />
             <Suspense fallback={<div>Loading plot...</div>}>
                 <IncomePlot />
             </Suspense>
