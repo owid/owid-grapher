43822ec9651134cec3c067f28f04f9989536b78b
Martin Račák <martin.racak@ourworldindata.org>
2025-11-24 12:29:11 +0100

    Lower SENTRY_DEFAULT_REPLAYS_SESSION_SAMPLE_RATE

--- packages/@ourworldindata/types/src/siteTypes/SiteConstants.ts
+++ packages/@ourworldindata/types/src/siteTypes/SiteConstants.ts
@@ -7,7 +7,7 @@ export const HIDE_IF_JS_DISABLED_CLASSNAME = "js--hide-if-js-disabled"
 export const GRAPHER_PREVIEW_CLASS = "grapherPreview"
 
 // Sentry constants
-export const SENTRY_DEFAULT_REPLAYS_SESSION_SAMPLE_RATE = 0.08
+export const SENTRY_DEFAULT_REPLAYS_SESSION_SAMPLE_RATE = 0.05
 export const SENTRY_SESSION_STORAGE_KEY = "sentryReplaySession"
 export const SENTRY_SAMPLED_RATE_KEY = "sentrySampledRate"
 
