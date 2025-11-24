224ed542549e19f292522d8dda8a1b81e424c7e2
Marigold <mojmir.vinkler@gmail.com>
2025-11-24 08:55:34 +0100

    docs: add comment explaining types.d.ts

--- functions/types.d.ts
+++ functions/types.d.ts
@@ -1,3 +1,7 @@
+// This file allows TypeScript to handle imports of non-code files.
+// We need this because the Cloudflare Workers environment imports .wasm modules
+// and .bin files (for fonts) directly, which TypeScript doesn't understand by default.
+
 declare module "*.wasm" {
     const content: any
     export default content
