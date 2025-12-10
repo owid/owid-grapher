a53e8d0d4c23554bba50d320806d6d36cdb4ef50
Marcel Gerber <mgerber59@gmail.com>
2025-12-10 11:13:53 +0100

    refactor: turn table of contents into <details> element with CSS animations (#5802)

--- site/gdocs/components/TableOfContents.tsx
+++ site/gdocs/components/TableOfContents.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react"
 import { TocHeadingWithTitleSupertitle } from "@ourworldindata/utils"
 import { faArrowDown, faPlus, faMinus } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import cx from "classnames"
-import AnimateHeight from "react-animate-height"
-
-// See ARIA roles: https://w3c.github.io/aria-practices/examples/menu-button/menu-button-links.html
 
 export default function TableOfContents({
     toc,
@@ -16,81 +12,61 @@ export default function TableOfContents({
     className?: string
     title: string
 }) {
-    const [height, setHeight] = useState<"auto" | 0>(0)
-    const [isOpen, setIsOpen] = useState(false)
-
-    const toggleIsOpen = () => {
-        setHeight(height === 0 ? "auto" : 0)
-    }
-
     return (
-        <nav
-            className={cx(className, "toc", { open: isOpen })}
-            role="button"
-            onClick={toggleIsOpen}
-            aria-haspopup="true"
-            aria-controls="toc-menu"
-            data-track-note="toc_toggle"
-        >
-            <button
-                aria-label="Toggle table of contents"
-                id="toc-menu-button"
-                className="toc-toggle span-cols-6 span-md-cols-8 span-sm-cols-10"
-                onClick={toggleIsOpen}
-                aria-haspopup="true"
-                aria-controls="toc-menu"
-                data-track-note="toc_toggle"
-            >
-                <span>{title}</span>
-                <span>
-                    <FontAwesomeIcon icon={isOpen ? faMinus : faPlus} />
-                </span>
-            </button>
-            <AnimateHeight
-                className="toc-content span-cols-6 span-md-cols-8 span-sm-cols-10"
-                height={height}
-                onHeightAnimationStart={(newHeight) => {
-                    if (newHeight !== 0) setIsOpen(true)
-                }}
-                onHeightAnimationEnd={(newHeight) => {
-                    if (newHeight === 0) setIsOpen(false)
-                }}
-                animateOpacity
-            >
-                <ul id="toc-menu" role="menu" aria-labelledby="toc-menu-button">
-                    {toc.map(
-                        (
-                            { title, supertitle, isSubheading, slug },
-                            i: number
-                        ) => (
-                            <li
-                                key={i}
-                                className={
-                                    isSubheading ? "subsection" : "section"
-                                }
-                                role="none"
-                            >
-                                <a
-                                    href={`#${slug}`}
-                                    data-track-note="toc_link"
-                                    role="menuitem"
-                                    onClick={(e) => e.stopPropagation()}
+        <div className={cx(className, "toc")}>
+            <details className="toc-details-wrapper span-cols-6 span-md-cols-8 span-sm-cols-10">
+                <summary
+                    className="toc-toggle"
+                    aria-label="Toggle table of contents"
+                    data-track-note="toc_toggle"
+                >
+                    <span>{title}</span>
+                    <span className="toc-toggle-icon">
+                        <FontAwesomeIcon
+                            className="icon-closed"
+                            icon={faPlus}
+                        />
+                        <FontAwesomeIcon className="icon-open" icon={faMinus} />
+                    </span>
+                </summary>
+                <div className="toc-content">
+                    <ul
+                        id="toc-menu"
+                        role="menu"
+                        aria-labelledby="toc-menu-button"
+                    >
+                        {toc.map(
+                            (
+                                { title, supertitle, isSubheading, slug },
+                                i: number
+                            ) => (
+                                <li
+                                    key={i}
+                                    className={
+                                        isSubheading ? "subsection" : "section"
+                                    }
                                 >
-                                    {supertitle ? (
-                                        <span className="supertitle">
-                                            {supertitle}
-                                        </span>
-                                    ) : null}
-                                    {title}
-                                </a>
-                                {!isSubheading && (
-                                    <FontAwesomeIcon icon={faArrowDown} />
-                                )}
-                            </li>
-                        )
-                    )}
-                </ul>
-            </AnimateHeight>
-        </nav>
+                                    <a
+                                        href={`#${slug}`}
+                                        data-track-note="toc_link"
+                                        role="menuitem"
+                                    >
+                                        {supertitle ? (
+                                            <span className="supertitle">
+                                                {supertitle}
+                                            </span>
+                                        ) : null}
+                                        {title}
+                                    </a>
+                                    {!isSubheading && (
+                                        <FontAwesomeIcon icon={faArrowDown} />
+                                    )}
+                                </li>
+                            )
+                        )}
+                    </ul>
+                </div>
+            </details>
+        </div>
     )
 }
