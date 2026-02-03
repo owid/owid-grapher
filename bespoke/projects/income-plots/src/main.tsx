449a1840c5964e90ffd368673dba9655ed3af812
Marcel Gerber <mgerber59@gmail.com>
2026-02-03 13:09:53 +0100

    add variants for demo purposes

--- bespoke/projects/income-plots/src/main.tsx
+++ bespoke/projects/income-plots/src/main.tsx
@@ -2,18 +2,31 @@ import { createRoot } from "react-dom/client"
 import { App } from "./components/App.tsx"
 import { ShadowRootContext } from "./ShadowRootContext.tsx"
 
-export const mount = (hostElement: HTMLElement) => {
+export const mount = (
+    hostElement: HTMLElement,
+    opts?: { variant: string; config: Record<string, unknown> }
+) => {
     // Create a container for React
     const container = document.createElement("div")
     hostElement.appendChild(container)
 
     const root = createRoot(container)
 
-    root.render(
-        <ShadowRootContext.Provider value={hostElement}>
-            <App />
-        </ShadowRootContext.Provider>
-    )
+    if (opts?.variant === "distribution") {
+        root.render(
+            <ShadowRootContext.Provider value={hostElement}>
+                <App />
+            </ShadowRootContext.Provider>
+        )
+    } else if (opts?.variant === "upside-down") {
+        root.render(
+            <div style={{ transform: "scaleY(-1)" }}>
+                <ShadowRootContext.Provider value={hostElement}>
+                    <App />
+                </ShadowRootContext.Provider>
+            </div>
+        )
+    }
 
     return () => {
         root.unmount()
