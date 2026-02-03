225fd5d252057049683f0e1a8f3b29ae5220b828
Marcel Gerber <mgerber59@gmail.com>
2026-02-03 14:00:08 +0100

    vite lib mode

--- bespoke/projects/income-plots/vite.config.ts
+++ bespoke/projects/income-plots/vite.config.ts
@@ -5,12 +5,18 @@ export default defineConfig({
     root: "./src",
     base: "/admin",
     build: {
+        lib: {
+            entry: ["main.tsx", "main.scss"],
+            fileName: "income-plots",
+            formats: ["es"],
+        },
         emptyOutDir: true,
         outDir: "../../../../dist/income-plots",
     },
     esbuild: {
         target: "es2024", // needed so decorators are compiled by esbuild
     },
+    // TODO: exports: named or preserveMethodNames or sth
     plugins: [
         react({
             babel: {
