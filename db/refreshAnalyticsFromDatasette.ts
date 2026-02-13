b2242419eef4848d4aee70135b6eaf50f32c8e53
Matthieu <matthieu@ourworldindata.org>
2026-02-13 21:05:27 +0000

    refactor: type refresh analytics script

--- db/refreshAnalyticsFromDatasette.ts
+++ db/refreshAnalyticsFromDatasette.ts
@@ -1,6 +1,14 @@
 import Papa from "papaparse"
 import * as db from "./db.js"
 import { omitUndefinedValues } from "@ourworldindata/utils"
+import {
+    AnalyticsGrapherViewsTableName,
+    AnalyticsPageviewsTableName,
+} from "@ourworldindata/types"
+import type {
+    DbPlainAnalyticsGrapherView,
+    DbPlainAnalyticsPageview,
+} from "@ourworldindata/types"
 
 const emojiRegex =
     /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/gu
@@ -11,16 +19,37 @@ interface TableConfig {
     keyColumn: string
 }
 
+const analyticsPageviewsColumns: Array<keyof DbPlainAnalyticsPageview> = [
+    "day",
+    "url",
+    "views_7d",
+    "views_14d",
+    "views_365d",
+]
+
+const analyticsPageviewsKeyColumn: keyof DbPlainAnalyticsPageview = "url"
+
+const analyticsGrapherViewsColumns: Array<keyof DbPlainAnalyticsGrapherView> = [
+    "day",
+    "grapher_slug",
+    "views_7d",
+    "views_14d",
+    "views_365d",
+]
+
+const analyticsGrapherViewsKeyColumn: keyof DbPlainAnalyticsGrapherView =
+    "grapher_slug"
+
 const tables: TableConfig[] = [
     {
-        tableName: "analytics_pageviews",
-        columns: ["day", "url", "views_7d", "views_14d", "views_365d"],
-        keyColumn: "url",
+        tableName: AnalyticsPageviewsTableName,
+        columns: analyticsPageviewsColumns,
+        keyColumn: analyticsPageviewsKeyColumn,
     },
     {
-        tableName: "analytics_grapher_views",
-        columns: ["day", "grapher_slug", "views_7d", "views_14d", "views_365d"],
-        keyColumn: "grapher_slug",
+        tableName: AnalyticsGrapherViewsTableName,
+        columns: analyticsGrapherViewsColumns,
+        keyColumn: analyticsGrapherViewsKeyColumn,
     },
 ]
 
