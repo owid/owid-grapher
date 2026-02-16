4fba68c42c2f0891a7f181f610ffecce869f268b
Marcel Gerber <mgerber59@gmail.com>
2026-02-16 15:00:41 +0100

    portrait chart

--- bespoke/projects/income-plots/src/components/App.tsx
+++ bespoke/projects/income-plots/src/components/App.tsx
@@ -4,38 +4,46 @@ import {
     IncomePlotControlsRowBottom,
     IncomePlotControlsRowTop,
 } from "./IncomePlotControlsRow.tsx"
-import { PLOT_HEIGHT, PLOT_WIDTH } from "../utils/incomePlotConstants.ts"
 import { useAtomValue, useSetAtom } from "jotai"
 import { atomCurrentYear, atomIsMobile } from "../store.ts"
-import { useResizeObserver } from "usehooks-ts"
+import { useResizeObserver, useWindowSize } from "usehooks-ts"
 
 export const App = () => {
     const currentYear = useAtomValue(atomCurrentYear)
     const containerRef = useRef<HTMLDivElement>(null)
-    const { width = PLOT_WIDTH } = useResizeObserver({
+    const { width = 0 } = useResizeObserver({
         ref: containerRef as React.RefObject<HTMLDivElement>,
     })
 
     const setIsMobile = useSetAtom(atomIsMobile)
 
-    const isMobile = useMemo(() => width < 720, [width])
+    const isMobile = useMemo(() => !!(width && width < 720), [width])
     useEffect(() => {
         setIsMobile(isMobile)
     }, [isMobile, setIsMobile])
+
+    const { height: windowHeight, width: windowWidth } = useWindowSize()
+    const isPortrait = windowHeight >= windowWidth
+
+    const aspectRatio = isMobile && isPortrait ? 1 : 5 / 3
 
     return (
         <div ref={containerRef}>
             <IncomePlotControlsRowTop />
             <div
                 style={{
-                    width: PLOT_WIDTH,
-                    aspectRatio: PLOT_WIDTH / PLOT_HEIGHT,
-                    maxWidth: "100%",
+                    width: "max-content",
+                    maxWidth: width,
                     margin: "auto",
+                    aspectRatio: aspectRatio,
                 }}
             >
                 <Suspense fallback={<>Loading...</>}>
-                    <IncomePlot key={currentYear} isMobile={isMobile} />
+                    <IncomePlot
+                        key={currentYear}
+                        aspectRatio={aspectRatio}
+                        isMobile={isMobile}
+                    />
                 </Suspense>
             </div>
             <IncomePlotControlsRowBottom isMobile={isMobile} />
