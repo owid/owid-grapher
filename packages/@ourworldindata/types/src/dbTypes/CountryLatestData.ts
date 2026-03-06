88eb1157bfc19ad689ac5ae4bfde3ae5bd1c77e4
Ike Saunders <11844404+ikesau@users.noreply.github.com>
2026-03-06 11:14:53 +0000

    🔨 drop unused country_latest_data table

--- packages/@ourworldindata/types/src/dbTypes/CountryLatestData.ts
+++
@@ -1,8 +0,0 @@
-export const CountryLatestDataTableName = "country_latest_data"
-export interface DbInsertCountryLatestData {
-    country_code?: string | null
-    value?: string | null
-    variable_id?: number | null
-    year?: number | null
-}
-export type DbPlainCountryLatestData = Required<DbInsertCountryLatestData>
