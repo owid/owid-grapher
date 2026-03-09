2c04ec166a9a38a25489b6af2b44ffc304b2e85d
Sophia Mersmann <sophia.mersmann1@gmail.com>
2026-03-09 16:34:52 +0100

    drop migration

--- db/migration/1773056346341-AddDumbbellToLineCharts.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
-
-export class AddDumbbellToLineCharts1773056346341 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {}
-
-    public async down(queryRunner: QueryRunner): Promise<void> {}
-}
