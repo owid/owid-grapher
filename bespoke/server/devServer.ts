18af51892426730603816db8c7fc5449bfd74ad4
Marcel Gerber <mgerber59@gmail.com>
2026-03-13 13:14:05 +0100

    add note for shadowDom / noShadowDom mode

--- bespoke/server/devServer.ts
+++ bespoke/server/devServer.ts
@@ -222,6 +222,7 @@ function serveDemoPage(
         .replaceAll("{{SHARED_DIR}}", SHARED_DIR)
         .replaceAll("{{ENTRYPOINT_JS}}", entrypoints?.js ?? "src/index.ts")
         .replaceAll("{{ENTRYPOINT_CSS}}", entrypoints?.css ?? "src/index.css")
+        .replaceAll("{{DEMO_CSS}}", path.join(dirname, "component-demo.css"))
     res.writeHead(200, { "Content-Type": "text/html" })
     res.end(html)
 }
