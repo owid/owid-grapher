f43be59dc8c3cfa69458f83dc3dd2af9f7bb0009
Sophia Mersmann <sophia.mersmann1@gmail.com>
2026-03-17 15:02:58 +0100

    🎉 add shared bespoke hooks

---
+++ bespoke/components/Frame/Frame.tsx
@@ -0,0 +1,11 @@
+import cx from "classnames"
+
+export function Frame({
+    children,
+    className,
+}: {
+    children?: React.ReactNode
+    className?: string
+}): React.ReactElement {
+    return <div className={cx("frame", className)}>{children}</div>
+}
