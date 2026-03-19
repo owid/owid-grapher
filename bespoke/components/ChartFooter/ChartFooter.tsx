a2619997721d3bc57293d6cb785dae1b111c5ad3
Sophia Mersmann <sophia.mersmann1@gmail.com>
2026-03-19 09:40:12 +0100

    🐛 fix cc tooltip in the footer

--- bespoke/components/ChartFooter/ChartFooter.tsx
+++ bespoke/components/ChartFooter/ChartFooter.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react"
 import cx from "classnames"
 import { Link, Tooltip, TooltipTrigger } from "react-aria-components"
 
@@ -10,8 +11,10 @@ export function ChartFooter({
     note?: string
     className?: string
 }) {
+    const footerRef = useRef<HTMLElement>(null)
+
     return (
-        <footer className={cx("chart-footer", className)}>
+        <footer ref={footerRef} className={cx("chart-footer", className)}>
             <div className="chart-footer__source-and-cc">
                 <div>
                     <strong>Data source:</strong> {source}
@@ -25,7 +28,12 @@ export function ChartFooter({
                     >
                         CC BY
                     </Link>
-                    <Tooltip className="chart-footer__tooltip">
+                    <Tooltip
+                        className="chart-footer__tooltip"
+                        UNSTABLE_portalContainer={
+                            footerRef.current ?? undefined
+                        }
+                    >
                         Our World in Data charts are licensed under Creative
                         Commons; you are free to use, share, and adapt this
                         material. Click through to the CC BY page for more
