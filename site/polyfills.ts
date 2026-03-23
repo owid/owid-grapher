a943c9e9656901fb1ca470fa287f7dccc667f841
Marcel Gerber <mgerber59@gmail.com>
2026-03-23 15:47:50 +0100

    chore: use es polyfills

--- site/polyfills.ts
+++ site/polyfills.ts
@@ -3,15 +3,15 @@
 // See docs/browser-support.md for details on our supported browsers.
 
 // ES2022
-import "core-js/actual/array/at"
-import "core-js/actual/string/at"
-import "core-js/actual/object/has-own"
+import "core-js/es/array/at"
+import "core-js/es/string/at"
+import "core-js/es/object/has-own"
 
 // ES2023
-import "core-js/actual/array/find-last"
-import "core-js/actual/array/find-last-index"
+import "core-js/es/array/find-last"
+import "core-js/es/array/find-last-index"
 
-import "core-js/actual/array/to-reversed"
-import "core-js/actual/array/to-sorted"
-import "core-js/actual/array/to-spliced"
-import "core-js/actual/array/with"
+import "core-js/es/array/to-reversed"
+import "core-js/es/array/to-sorted"
+import "core-js/es/array/to-spliced"
+import "core-js/es/array/with"
