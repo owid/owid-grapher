2d8c2a559c2ce224145907136e2686f2dd4ff2d3
Marcel Gerber <mgerber59@gmail.com>
2026-03-25 08:19:35 +0100

    chore(polyfill): use `proposals` for polyfills, thereby not polyfilling `Set` etc.

--- site/polyfills.ts
+++ site/polyfills.ts
@@ -3,16 +3,18 @@
 // See docs/browser-support.md for details on our supported browsers.
 // There are a few polyfills in here for features that are supported in all our supported browsers, but we include them here anyway so that there's at least a chance that older browser versions continue to work, even though they're unsupported.
 
-// ES2022
-import "core-js/es/array/at" // Chrome 92+, Safari 15.4+, Firefox 90+
-import "core-js/es/string/at" // Chrome 92+, Safari 15.4+, Firefox 90+
-import "core-js/es/object/has-own" // Chrome 93+, Safari 15.4+, Firefox 92+
+// Relative indexing method: Array.prototype.at, String.prototype.at
+// https://core-js.io/docs/features/proposals/relative-indexing-method
+import "core-js/proposals/relative-indexing-method" // Chrome 92+, Safari 15.4+, Firefox 90+
 
-// ES2023
-import "core-js/es/array/find-last" // Chrome 97+, Safari 15.4+, Firefox 104+
-import "core-js/es/array/find-last-index" // Chrome 97+, Safari 15.4+, Firefox 104+
+// Object.hasOwn
+// https://core-js.io/docs/features/proposals/accessible-object-prototype-hasownproperty
+import "core-js/proposals/accessible-object-hasownproperty" // Chrome 93+, Safari 15.4+, Firefox 92+
 
-import "core-js/es/array/to-reversed" // Chrome 110+, Safari 16.0+, Firefox 115+
-import "core-js/es/array/to-sorted" // Chrome 110+, Safari 16.0+, Firefox 115+
-import "core-js/es/array/to-spliced" // Chrome 110+, Safari 16.0+, Firefox 115+
-import "core-js/es/array/with" // Chrome 110+, Safari 16.0+, Firefox 115+
+// Array find from last: findLast, findLastIndex
+// https://core-js.io/docs/features/proposals/array-find-from-last
+import "core-js/proposals/array-find-from-last" // Chrome 97+, Safari 15.4+, Firefox 104+
+
+// Array change by copy: toReversed, toSorted, toSpliced, with
+// https://core-js.io/docs/features/proposals/change-array-by-copy
+import "core-js/proposals/change-array-by-copy-stage-4" // Chrome 110+, Safari 16.0+, Firefox 115+
