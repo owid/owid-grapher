399ca9a66d868d547ab88e28d7f1d94e2453aee4
Marcel Gerber <mgerber59@gmail.com>
2026-04-08 14:11:04 +0200

    fix: properly render react-aria portals inside shadow dom

--- bespoke/projects/income-plots/src/components/App.tsx
+++ bespoke/projects/income-plots/src/components/App.tsx
@@ -19,8 +19,11 @@ import {
     atomisNarrow,
 } from "../store.ts"
 import { useResizeObserver, useWindowSize } from "usehooks-ts"
+import { UNSAFE_PortalProvider } from "@react-aria/overlays"
+import { useShadowRoot } from "../ShadowRootContext.tsx"
 
 export const App = () => {
+    const shadowRoot = useShadowRoot()
     useAtom(atomEffectIncludeLocalCountryInSelection) // include local country in selection once the local country is detected
 
     const currentYear = useAtomValue(atomCurrentYear)
@@ -45,31 +48,38 @@ export const App = () => {
     const closeDrawer = useCallback(() => setDrawerOpen(false), [])
 
     return (
-        <div ref={containerRef}>
-            <IncomePlotControlsRowTop
-                isNarrow={isNarrow}
-                onOpenSettings={() => setDrawerOpen(true)}
-            />
-            <div
-                style={{
-                    width: "max-content",
-                    maxWidth: width,
-                    aspectRatio: aspectRatio,
-                }}
-            >
-                <Suspense fallback={<>Loading...</>}>
-                    <IncomePlot
-                        key={currentYear}
-                        width={width}
-                        aspectRatio={aspectRatio}
-                        isNarrow={isNarrow}
+        <UNSAFE_PortalProvider
+            getContainer={shadowRoot ? () => shadowRoot : undefined}
+        >
+            <div ref={containerRef}>
+                <IncomePlotControlsRowTop
+                    isNarrow={isNarrow}
+                    onOpenSettings={() => setDrawerOpen(true)}
+                />
+                <div
+                    style={{
+                        width: "max-content",
+                        maxWidth: width,
+                        aspectRatio: aspectRatio,
+                    }}
+                >
+                    <Suspense fallback={<>Loading...</>}>
+                        <IncomePlot
+                            key={currentYear}
+                            width={width}
+                            aspectRatio={aspectRatio}
+                            isNarrow={isNarrow}
+                        />
+                    </Suspense>
+                    {!isNarrow && <IncomePlotControlsRowBottom />}
+                </div>
+                {isNarrow && (
+                    <IncomePlotDrawer
+                        isOpen={drawerOpen}
+                        onClose={closeDrawer}
                     />
-                </Suspense>
-                {!isNarrow && <IncomePlotControlsRowBottom />}
+                )}
             </div>
-            {isNarrow && (
-                <IncomePlotDrawer isOpen={drawerOpen} onClose={closeDrawer} />
-            )}
-        </div>
+        </UNSAFE_PortalProvider>
     )
 }
