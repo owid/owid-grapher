0cf55aab7c667095d4bec6996f80d3f1f0dd6cc0
Martin Račák <rakyi@users.noreply.github.com>
2026-04-19 15:23:02 +0200

    Remove unused files (#6376)

--- db/model/GdocXImage.ts
+++
@@ -1,8 +0,0 @@
-// import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
-
-// @Entity("posts_gdocs_x_images")
-// export class GdocXImage extends BaseEntity {
-//     @PrimaryGeneratedColumn() id!: number
-//     @Column() gdocId!: string
-//     @Column() imageId!: number
-// }
