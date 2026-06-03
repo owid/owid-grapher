77a6e544c98dce64b055f1e9f1d1634117e53a4a
Marcel Gerber <mgerber59@gmail.com>
2026-06-03 09:58:57 +0200

    use usePrompt (doesn't work)

--- adminSiteClient/Prompt.tsx
+++ adminSiteClient/Prompt.tsx
@@ -1,28 +1,10 @@
-import * as React from "react"
-import { useBlocker } from "react-router"
+import { unstable_usePrompt } from "react-router"
 
 export function Prompt({ when, message }: { when: boolean; message: string }) {
-    const blocker = useBlocker(
-        React.useCallback(
-            ({ currentLocation, nextLocation }) => {
-                return (
-                    when && currentLocation.pathname !== nextLocation.pathname
-                )
-            },
-            [when]
-        )
-    )
-
-    React.useEffect(() => {
-        if (blocker.state === "blocked") {
-            const proceed = window.confirm(message)
-            if (proceed) {
-                blocker.proceed()
-            } else {
-                blocker.reset()
-            }
-        }
-    }, [blocker, message])
+    unstable_usePrompt({
+        when,
+        message,
+    })
 
     return null
 }
