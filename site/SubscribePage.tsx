5d431f476b364586093732cdcce1babfb595312b
Martin Račák <martin.racak@ourworldindata.org>
2026-07-08 15:40:33 +0200

    WIP: Email notifications

--- site/SubscribePage.tsx
+++ site/SubscribePage.tsx
@@ -4,18 +4,23 @@ import { SiteHeader } from "./SiteHeader.js"
 import { SiteFooter } from "./SiteFooter.js"
 import {
     SiteFooterContext,
-    SUBSCRIBE_PAGE_FORM_CONTAINER_ID,
+    SUBSCRIBE_PAGE_NOTIFICATIONS_FORM_CONTAINER_ID,
+    TagGraphRoot,
 } from "@ourworldindata/types"
-import { NewsletterSubscriptionForm } from "./NewsletterSubscription.js"
 import { NewsletterSubscriptionContext } from "./newsletter.js"
+import { EmailNotificationsSubscribeForm } from "./EmailNotificationsSubscribeForm.js"
 import { OwidSocials } from "./OwidSocials.js"
 import { BAKED_BASE_URL } from "../settings/clientSettings.js"
 
 export interface SubscribePageProps {
     baseUrl: string
+    topicTagGraph: TagGraphRoot
 }
 
-export const SubscribePage = ({ baseUrl }: SubscribePageProps) => {
+export const SubscribePage = ({
+    baseUrl,
+    topicTagGraph,
+}: SubscribePageProps) => {
     return (
         <Html>
             <Head
@@ -24,7 +29,13 @@ export const SubscribePage = ({ baseUrl }: SubscribePageProps) => {
                 pageDesc="Stay up to date with our latest research and data insights by subscribing to our newsletter."
                 baseUrl={baseUrl}
                 imageUrl={`${BAKED_BASE_URL}/images/biweekly-newsletter.webp`}
-            />
+            >
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `window._OWID_TOPIC_TAG_GRAPH = ${JSON.stringify(topicTagGraph)}`,
+                    }}
+                ></script>
+            </Head>
             <body>
                 <SiteHeader />
                 <main className="subscribe-page grid grid-cols-12-full-width">
@@ -32,11 +43,11 @@ export const SubscribePage = ({ baseUrl }: SubscribePageProps) => {
                         Subscribe to our newsletters
                     </h1>
                     <div className="span-cols-6 col-start-5 span-md-cols-10 col-md-start-3 span-sm-cols-12 col-sm-start-2">
-                        <div id={SUBSCRIBE_PAGE_FORM_CONTAINER_ID}>
-                            <NewsletterSubscriptionForm
-                                context={
-                                    NewsletterSubscriptionContext.SubscribePage
-                                }
+                        <div
+                            id={SUBSCRIBE_PAGE_NOTIFICATIONS_FORM_CONTAINER_ID}
+                        >
+                            <EmailNotificationsSubscribeForm
+                                topicTagGraph={topicTagGraph}
                             />
                         </div>
                         <OwidSocials
