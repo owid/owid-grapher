451d800d958b444993cac477fd8ceac8b35fefba
Marcel Gerber <mgerber59@gmail.com>
2026-08-21 16:04:12 +0200

    chore(deps): replace js-cookie with es-cookie

--- packages/@ourworldindata/grapher/src/core/GrapherState.tsx
+++ packages/@ourworldindata/grapher/src/core/GrapherState.tsx
@@ -102,7 +102,7 @@ import {
     sortNumeric,
     isMobile,
 } from "@ourworldindata/utils"
-import Cookies from "js-cookie"
+import { get as getCookie } from "es-cookie"
 import * as _ from "lodash-es"
 import {
     computed,
@@ -1689,7 +1689,7 @@ export class GrapherState
         try {
             // Cookie access can be restricted by iframe sandboxing, in which case the below code will throw an error
             // see https://github.com/owid/owid-grapher/pull/2452
-            return !!Cookies.get(CookieKey.isAdmin)
+            return !!getCookie(CookieKey.isAdmin)
         } catch {
             return false
         }
